f86ac1d5c2421c444ed3e762e617813d5fd250d5
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2021-03-15 12:09:32 +0000

    wcn36xx: Add set_rekey_data callback
    Add a callback for Group Temporal Key tracking as provided by the standard
    WiFi ops structure.
    
    We track the key to integrate GTK offloading into the WoWLAN suspend path
    later on. Code comes from the Intel iwlwifi driver with minimal name
    changes.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- drivers/net/wireless/ath/wcn36xx/main.c
+++ drivers/net/wireless/ath/wcn36xx/main.c
@@ -1155,6 +1155,24 @@ out:
 	return 0;
 }
 
+void wcn36xx_set_rekey_data(struct ieee80211_hw *hw,
+			    struct ieee80211_vif *vif,
+			    struct cfg80211_gtk_rekey_data *data)
+{
+	struct wcn36xx *wcn = hw->priv;
+	struct wcn36xx_vif *vif_priv = wcn36xx_vif_to_priv(vif);
+
+	mutex_lock(&wcn->conf_mutex);
+
+	memcpy(vif_priv->rekey_data.kek, data->kek, NL80211_KEK_LEN);
+	memcpy(vif_priv->rekey_data.kck, data->kck, NL80211_KCK_LEN);
+	vif_priv->rekey_data.replay_ctr =
+		cpu_to_le64(be64_to_cpup((__be64 *)data->replay_ctr));
+	vif_priv->rekey_data.valid = true;
+
+	mutex_unlock(&wcn->conf_mutex);
+}
+
 #endif
 
 static int wcn36xx_ampdu_action(struct ieee80211_hw *hw,
@@ -1257,6 +1275,7 @@ static const struct ieee80211_ops wcn36xx_ops = {
 #ifdef CONFIG_PM
 	.suspend		= wcn36xx_suspend,
 	.resume			= wcn36xx_resume,
+	.set_rekey_data		= wcn36xx_set_rekey_data,
 #endif
 	.config			= wcn36xx_config,
 	.prepare_multicast	= wcn36xx_prepare_multicast,
