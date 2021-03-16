0cf3e245eea46deda49597c361af1fb0be9a300e
Mihai Carabas <mihai.carabas@oracle.com>
2021-03-16 14:20:27 +0200

    misc/pvpanic: split-up generic and platform dependent code
    Split-up generic and platform dependent code in order to be able to re-use
    generic event handling code in pvpanic PCI device driver in the next patches.
    
    The code from pvpanic.c was split in two new files:
    - pvpanic.c: generic code that handles pvpanic events
    - pvpanic-mmio.c: platform/bus dependent code
    
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>

---
+++ drivers/misc/pvpanic/pvpanic.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ *  Pvpanic Device Support
+ *
+ *  Copyright (C) 2021 Oracle.
+ */
+
+#ifndef PVPANIC_H_
+#define PVPANIC_H_
+
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+void pvpanic_probe(void __iomem *base, unsigned int dev_cap);
+void pvpanic_remove(void);
+void pvpanic_set_events(unsigned int dev_events);
+
+#endif /* PVPANIC_H_ */
