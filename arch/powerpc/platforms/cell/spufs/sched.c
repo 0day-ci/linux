243f594d0eca45afd501b5573d0023cf7624372a
Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
2021-03-17 15:33:39 +0800

    sched: replace if (cond) BUG() with WARN_ON()
    Fix the following coccicheck warnings:
    
    ./arch/powerpc/platforms/cell/spufs/sched.c:908:2-5: WARNING: Use BUG_ON
    instead of if condition followed by BUG.
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Signed-off-by: Jiapeng Chong <jiapeng.chong@linux.alibaba.com>

--- arch/powerpc/platforms/cell/spufs/sched.c
+++ arch/powerpc/platforms/cell/spufs/sched.c
@@ -904,8 +904,7 @@ static noinline void spusched_tick(struct spu_context *ctx)
 	struct spu_context *new = NULL;
 	struct spu *spu = NULL;
 
-	if (spu_acquire(ctx))
-		BUG();	/* a kernel thread never has signals pending */
+	WARN_ON(spu_acquire(ctx));	/* a kernel thread never has signals pending */
 
 	if (ctx->state != SPU_STATE_RUNNABLE)
 		goto out;
