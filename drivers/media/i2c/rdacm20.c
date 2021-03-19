f54d2ddfdb629dbe5b7997295f4a190bba8d031c
Jacopo Mondi <jacopo+renesas@jmondi.org>
2021-03-19 17:41:43 +0100

    media: i2c: rdacm20: Enable noise immunity
    Enable the noise immunity threshold at the end of the rdacm20
    initialization routine.
    
    The rdacm20 camera module has been so far tested with a startup
    delay that allowed the embedded MCU to program the serializer. If
    the initialization routine is run before the MCU programs the
    serializer and the image sensor and their addresses gets changed
    by the rdacm20 driver it is required to manually enable the noise
    immunity threshold to make the communication on the control channel
    more reliable.
    
    Reviewed-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Signed-off-by: Jacopo Mondi <jacopo+renesas@jmondi.org>

--- drivers/media/i2c/rdacm20.c
+++ drivers/media/i2c/rdacm20.c
@@ -518,7 +518,19 @@ again:
 
 	dev_info(dev->dev, "Identified MAX9271 + OV10635 device\n");
 
-	return 0;
+	/*
+	 * Set reverse channel high threshold to increase noise immunity.
+	 *
+	 * This should be compensated by increasing the reverse channel
+	 * amplitude on the remote deserializer side.
+	 *
+	 * TODO Inspect the embedded MCU programming sequence to make sure
+	 * there are no conflicts with the configuration applied here.
+	 *
+	 * TODO Clarify the embedded MCU startup delay to avoid write
+	 * collisions on the I2C bus.
+	 */
+	return max9271_set_high_threshold(&dev->serializer, true);
 }
 
 static const struct v4l2_subdev_core_ops rdacm20_core_ops = {
