70b65645fc2e90ffe0cb80a1122daeb2ed55695a
Kaike Wan <kaike.wan@intel.com>
2021-03-19 08:56:29 -0400

    RDMA/rv: Add the rv module
    Add the rv module, the Makefile, and Kconfig file.
    
    Also add the functions to manage IB devices.
    
    Signed-off-by: Todd Rimmer <todd.rimmer@intel.com>
    Signed-off-by: Kaike Wan <kaike.wan@intel.com>

---
+++ drivers/infiniband/ulp/rv/trace.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
+/*
+ * Copyright(c) 2020 - 2021 Intel Corporation.
+ */
+#define CREATE_TRACE_POINTS
+#include <rdma/rv_user_ioctls.h>
+#include "trace.h"
