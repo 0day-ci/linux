be017689840de3722c6adde2777e9e155a9e2b7e
Miroslav Benes <mbenes@suse.cz>
2021-03-26 15:30:21 +0100

    livepatch: Replace the fake signal sending with TIF_NOTIFY_SIGNAL infrastructure
    Livepatch sends a fake signal to all remaining blocking tasks of a
    running transition after a set period of time. It uses TIF_SIGPENDING
    flag for the purpose. Commit 12db8b690010 ("entry: Add support for
    TIF_NOTIFY_SIGNAL") added a generic infrastructure to achieve the same.
    Replace our bespoke solution with the generic one.
    
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>

--- kernel/signal.c
+++ kernel/signal.c
@@ -181,8 +181,7 @@ void recalc_sigpending_and_wake(struct task_struct *t)
 
 void recalc_sigpending(void)
 {
-	if (!recalc_sigpending_tsk(current) && !freezing(current) &&
-	    !klp_patch_pending(current))
+	if (!recalc_sigpending_tsk(current) && !freezing(current))
 		clear_thread_flag(TIF_SIGPENDING);
 
 }
