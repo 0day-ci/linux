15bdb2728346da8506f5b99d3c7e27476f90bf14
Jakub Kicinski <kuba@kernel.org>
2021-03-25 19:07:26 -0700

    ethtool: fec: sanitize ethtool_fecparam->fec
    Reject NONE on set, this mode means device does not support
    FEC so it's a little out of place in the set interface.
    
    This should be safe to do - user space ethtool does not allow
    the use of NONE on set. A few drivers treat it the same as OFF,
    but none use it instead of OFF.
    
    Similarly reject an empty FEC mask. The common user space tool
    will not send such requests and most drivers correctly reject
    it already.
    
    v2: - use mask not bit pos
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ethtool/ioctl.c
+++ net/ethtool/ioctl.c
@@ -2574,6 +2574,9 @@ static int ethtool_set_fecparam(struct net_device *dev, void __user *useraddr)
 	if (copy_from_user(&fecparam, useraddr, sizeof(fecparam)))
 		return -EFAULT;
 
+	if (!fecparam.fec || fecparam.fec & ETHTOOL_FEC_NONE)
+		return -EINVAL;
+
 	fecparam.active_fec = 0;
 	fecparam.reserved = 0;
 
