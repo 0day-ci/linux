d73a64c2c40058ca5425d1142b0c4a861f7a58e2
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-03-26 19:24:00 +0200

    mux: gpio: Make it OF independent
    Module doesn't use OF APIs anyhow, make it OF independent by replacing
    headers and dropping useless of_match_ptr() call.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/mux/gpio.c
+++ drivers/mux/gpio.c
@@ -10,8 +10,8 @@
 #include <linux/err.h>
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
+#include <linux/mod_devicetable.h>
 #include <linux/mux/driver.h>
-#include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/property.h>
 
@@ -97,7 +97,7 @@ static int mux_gpio_probe(struct platform_device *pdev)
 static struct platform_driver mux_gpio_driver = {
 	.driver = {
 		.name = "gpio-mux",
-		.of_match_table	= of_match_ptr(mux_gpio_dt_ids),
+		.of_match_table	= mux_gpio_dt_ids,
 	},
 	.probe = mux_gpio_probe,
 };
