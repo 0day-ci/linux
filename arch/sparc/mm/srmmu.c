e160e645863d7ebb006543a2ff04921764aec90a
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2021-03-26 07:09:41 +0530

    sparc/mm/srmmu.c: A typo fix
    s/guarentees/guarantees/
    
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- arch/sparc/mm/srmmu.c
+++ arch/sparc/mm/srmmu.c
@@ -1480,7 +1480,7 @@ static struct sparc32_cachetlb_ops viking_ops __ro_after_init = {
  * flush resulting in corruption.
  *
  * Our workaround is to take a global spinlock around the TLB flushes,
- * which guarentees we won't ever have too many pending.  It's a big
+ * which guarantees we won't ever have too many pending.  It's a big
  * hammer, but a semaphore like system to make sure we only have N TLB
  * flushes going at once will require SMP locking anyways so there's
  * no real value in trying any harder than this.
