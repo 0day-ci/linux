83651cab2e01e77ebf15798aa454bac655699abf
Jeff Layton <jlayton@kernel.org>
2021-03-26 13:32:16 -0400

    ceph: add routine to create fscrypt context prior to RPC
    After pre-creating a new inode, do an fscrypt prepare on it, fetch a
    new encryption context and then marshal that into the security context
    to be sent along with the RPC. Call the new function from
    ceph_new_inode.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/ceph/crypto.h
+++ fs/ceph/crypto.h
@@ -18,6 +18,9 @@ static inline void ceph_fscrypt_free_dummy_policy(struct ceph_fs_client *fsc)
 	fscrypt_free_dummy_policy(&fsc->dummy_enc_policy);
 }
 
+int ceph_fscrypt_prepare_context(struct inode *dir, struct inode *inode,
+				 struct ceph_acl_sec_ctx *as);
+
 #else /* CONFIG_FS_ENCRYPTION */
 
 static inline void ceph_fscrypt_set_ops(struct super_block *sb)
@@ -27,6 +30,15 @@ static inline void ceph_fscrypt_set_ops(struct super_block *sb)
 static inline void ceph_fscrypt_free_dummy_policy(struct ceph_fs_client *fsc)
 {
 }
+
+static inline int ceph_fscrypt_prepare_context(struct inode *dir, struct inode *inode,
+						struct ceph_acl_sec_ctx *as)
+{
+	if (IS_ENCRYPTED(dir))
+		return -EOPNOTSUPP;
+	return 0;
+}
+
 #endif /* CONFIG_FS_ENCRYPTION */
 
 #endif
