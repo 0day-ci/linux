c83af30a7b389424eefe1368c2669011cd52d2d1
Zhen Lei <thunder.leizhen@huawei.com>
2021-03-27 09:20:28 +0800

    watchdog: dw_wdt: Remove duplicated header file inclusion
    The header file <linux/kernel.h> is already included above and can be
    removed here.
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>

--- drivers/watchdog/dw_wdt.c
+++ drivers/watchdog/dw_wdt.c
@@ -19,7 +19,6 @@
 #include <linux/delay.h>
 #include <linux/err.h>
 #include <linux/io.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/interrupt.h>
