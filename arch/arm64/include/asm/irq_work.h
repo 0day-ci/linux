cbf4ed533df6083e730f5592df4fbdc02a049bf1
Chen Lifu <chenlifu@huawei.com>
2021-03-27 15:06:51 +0800

    arm64: smp: Add missing prototype for some smp.c functions
    In commit eb631bb5bf5b
    ("arm64: Support arch_irq_work_raise() via self IPIs") a new
    function "arch_irq_work_raise" was added without a prototype
    in header irq_work.h
    
    In commit d914d4d49745
    ("arm64: Implement panic_smp_self_stop()") a new
    function "panic_smp_self_stop" was added without a prototype
    in header irq_work.h
    
    We get the following warnings on W=1:
    arch/arm64/kernel/smp.c:842:6: warning: no previous prototype
    for ‘arch_irq_work_raise’ [-Wmissing-prototypes]
    arch/arm64/kernel/smp.c:862:6: warning: no previous prototype
    for ‘panic_smp_self_stop’ [-Wmissing-prototypes]
    
    Fix the same by adding the missing prototype in header irq_work.h
    
    Signed-off-by: Chen Lifu <chenlifu@huawei.com>

--- arch/arm64/include/asm/irq_work.h
+++ arch/arm64/include/asm/irq_work.h
@@ -2,6 +2,9 @@
 #ifndef __ASM_IRQ_WORK_H
 #define __ASM_IRQ_WORK_H
 
+extern void arch_irq_work_raise(void);
+extern void panic_smp_self_stop(void);
+
 static inline bool arch_irq_work_has_interrupt(void)
 {
 	return true;
