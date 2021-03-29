c61808381b24764047a7bf35b09b8f135c227023
Yang Shi <shy828301@gmail.com>
2021-03-29 11:33:12 -0700

    mm: migrate: remove redundant page count check for THP
    Don't have to keep the redundant page count check for THP anymore after
    switching to use generic migration code.
    
    Signed-off-by: Yang Shi <shy828301@gmail.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -2097,18 +2097,6 @@ static int numamigrate_isolate_page(pg_data_t *pgdat, struct page *page)
 	if (isolate_lru_page(page))
 		return 0;
 
-	/*
-	 * migrate_misplaced_transhuge_page() skips page migration's usual
-	 * check on page_count(), so we must do it here, now that the page
-	 * has been isolated: a GUP pin, or any other pin, prevents migration.
-	 * The expected page count is 3: 1 for page's mapcount and 1 for the
-	 * caller's pin and 1 for the reference taken by isolate_lru_page().
-	 */
-	if (PageTransHuge(page) && page_count(page) != 3) {
-		putback_lru_page(page);
-		return 0;
-	}
-
 	page_lru = page_is_file_lru(page);
 	mod_node_page_state(page_pgdat(page), NR_ISOLATED_ANON + page_lru,
 				thp_nr_pages(page));
