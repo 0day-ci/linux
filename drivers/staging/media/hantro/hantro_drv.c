e8ce7632e03b7e005b05579c75c3ceb418b727e3
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2021-03-29 08:57:42 +0200

    media: hantro: IMX8M: add variant for G2/HEVC codec
    Add variant to IMX8M to enable G2/HEVC codec.
    Define the capabilities for the hardware up to 3840x2160.
    G2 doesn't have postprocessor, use the same clocks and got it
    own interruption.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>
    Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/staging/media/hantro/hantro_drv.c
+++ drivers/staging/media/hantro/hantro_drv.c
@@ -574,6 +574,7 @@ static const struct of_device_id of_hantro_match[] = {
 #endif
 #ifdef CONFIG_VIDEO_HANTRO_IMX8M
 	{ .compatible = "nxp,imx8mq-vpu", .data = &imx8mq_vpu_variant, },
+	{ .compatible = "nxp,imx8mq-vpu-g2", .data = &imx8mq_vpu_g2_variant },
 #endif
 	{ /* sentinel */ }
 };
