39a6912a3b5504d3290b6b137136842ad28b1745
Jin Yao <yao.jin@linux.intel.com>
2021-03-29 15:00:24 +0800

    perf pmu: Save detected hybrid pmus to a global pmu list
    We identify the cpu_core pmu and cpu_atom pmu by explicitly
    checking following files:
    
    For cpu_core, checks:
    "/sys/bus/event_source/devices/cpu_core/cpus"
    
    For cpu_atom, checks:
    "/sys/bus/event_source/devices/cpu_atom/cpus"
    
    If the 'cpus' file exists, the pmu exists.
    
    But in order not to hardcode the "cpu_core" and "cpu_atom",
    and make the code in a generic way. So if the path
    "/sys/bus/event_source/devices/cpu_xxx/cpus" exists, the hybrid
    pmu exists. All the detected hybrid pmus are linked to a
    global list 'perf_pmu__hybrid_pmus' and then next we just need
    to iterate the list to get all hybrid pmu by using
    perf_pmu__for_each_hybrid_pmu.
    
    Signed-off-by: Jin Yao <yao.jin@linux.intel.com>

---
+++ tools/perf/util/pmu-hybrid.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/list.h>
+#include <linux/compiler.h>
+#include <linux/string.h>
+#include <linux/zalloc.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <locale.h>
+#include <api/fs/fs.h>
+#include "fncache.h"
+#include "pmu-hybrid.h"
+
+LIST_HEAD(perf_pmu__hybrid_pmus);
+
+bool perf_pmu__hybrid_mounted(const char *name)
+{
+	char path[PATH_MAX];
+	const char *sysfs;
+
+	if (strncmp(name, "cpu_", 4))
+		return false;
+
+	sysfs = sysfs__mountpoint();
+	if (!sysfs)
+		return false;
+
+	snprintf(path, PATH_MAX, CPUS_TEMPLATE_CPU, sysfs, name);
+	return file_available(path);
+}
