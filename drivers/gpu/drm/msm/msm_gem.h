af8059726f5095af9772f5b6013573b300b7e69a
Rob Clark <robdclark@chromium.org>
2021-03-31 15:16:28 -0700

    drm/msm: Fix debugfs deadlock
    In normal cases the gem obj lock is acquired first before mm_lock.  The
    exception is iterating the various object lists.  In the shrinker path,
    deadlock is avoided by using msm_gem_trylock() and skipping over objects
    that cannot be locked.  But for debugfs the straightforward thing is to
    split things out into a separate list of all objects protected by it's
    own lock.
    
    Fixes: d984457b31c4 ("drm/msm: Add priv->mm_lock to protect active/inactive lists")
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_gem.h
+++ drivers/gpu/drm/msm/msm_gem.h
@@ -60,13 +60,20 @@ struct msm_gem_object {
 	 */
 	uint8_t vmap_count;
 
-	/* And object is either:
-	 *  inactive - on priv->inactive_list
+	/**
+	 * Node in list of all objects (mainly for debugfs, protected by
+	 * struct_mutex
+	 */
+	struct list_head node;
+
+	/**
+	 * An object is either:
+	 *  inactive - on priv->inactive_dontneed or priv->inactive_willneed
+	 *     (depending on purgability status)
 	 *  active   - on one one of the gpu's active_list..  well, at
 	 *     least for now we don't have (I don't think) hw sync between
 	 *     2d and 3d one devices which have both, meaning we need to
 	 *     block on submit if a bo is already on other ring
-	 *
 	 */
 	struct list_head mm_list;
 
