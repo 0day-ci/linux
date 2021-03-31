fe7efe24a001d02337359b327b3db92b3c20aaa7
wangfangpeng <wangfangpeng1@huawei.com>
2021-03-31 14:27:23 +0800

    ubifs: fix read fail but return ok
    do_readpage() may return err, but ubifs_readpage() always return ok.
    The vfs will ignore the err happen in ubifs.
    
    Fixes: 1e51764a3c2ac05a2 ("UBIFS: add new flash file system")
    Cc: <stable@vger.kernel.org> #v2.6.27
    Signed-off-by: wangfangpeng <wangfangpeng1@huawei.com>

--- fs/ubifs/file.c
+++ fs/ubifs/file.c
@@ -892,11 +892,13 @@ out_unlock:
 
 static int ubifs_readpage(struct file *file, struct page *page)
 {
+	int ret;
+
 	if (ubifs_bulk_read(page))
 		return 0;
-	do_readpage(page);
+	ret = do_readpage(page);
 	unlock_page(page);
-	return 0;
+	return ret;
 }
 
 static int do_writepage(struct page *page, int len)
