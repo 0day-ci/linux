1d73453653c6d4fdbd9b158a31421d960c5eab6a
Hassan Shahbazi <h.shahbazi.git@gmail.com>
2021-03-31 19:34:29 +0300

    fix NULL pointer deference crash
    The patch has fixed a NULL pointer deference crash in hiding the cursor. It
    is verified by syzbot patch tester.
    
    Reported by: syzbot
    https://syzkaller.appspot.com/bug?id=defb47bf56e1c14d5687280c7bb91ce7b608b94b
    
    Signed-off-by: Hassan Shahbazi <h.shahbazi.git@gmail.com>

--- drivers/video/fbdev/core/fbcon.c
+++ drivers/video/fbdev/core/fbcon.c
@@ -1333,8 +1333,9 @@ static void fbcon_cursor(struct vc_data *vc, int mode)
 
 	ops->cursor_flash = (mode == CM_ERASE) ? 0 : 1;
 
-	ops->cursor(vc, info, mode, get_color(vc, info, c, 1),
-		    get_color(vc, info, c, 0));
+	if (ops && ops->cursor)
+		ops->cursor(vc, info, mode, get_color(vc, info, c, 1),
+				get_color(vc, info, c, 0));
 }
 
 static int scrollback_phys_max = 0;
