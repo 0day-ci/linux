28d23544cd54a59ee3b92c16b9a6fe9e93712b09
Adrien Grassein <adrien.grassein@gmail.com>
2021-04-02 18:45:05 +0200

    dt-bindings: power: Add documentation for imx8m power domain driver
    Add documentation for the imx8m(m|n|p) power domain driver.
    
    Signed-off-by: Adrien Grassein <adrien.grassein@gmail.com>

---
+++ include/dt-bindings/power/imx8mn-power.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+/*
+ *  Copyright (C) 2021 Adrien Grassein <adrien.grassein@gmail.com>
+ */
+
+#ifndef __DT_BINDINGS_IMX8MN_POWER_H__
+#define __DT_BINDINGS_IMX8MN_POWER_H__
+
+#define IMX8MN_POWER_DOMAIN_HSIOMIX	0
+#define IMX8MN_POWER_DOMAIN_USB_OTG1	2
+#define IMX8MN_POWER_DOMAIN_GPU		4
+#define IMX8MN_POWER_DOMAIN_DISPLAY	9
+#define IMX8MN_POWER_DOMAIN_MIPI	10
+
+#endif
