c4ecfe9b994754b705d4f0eccdd5dad743190a4b
Pavle Rohalj <pavle.rohalj@gmail.com>
2021-04-06 23:43:31 -0700

    staging: sm750fb: Rename function parameter of sii164_enable_hot_plug_detection
    Fix "Avoid CamelCase" checkpatch.pl check for the function parameter
    enable_hot_plug in the prototype of the function
    sii164_enable_hot_plug_detection.
    
    Signed-off-by: Pavle Rohalj <pavle.rohalj@gmail.com>

--- drivers/staging/sm750fb/ddk750_sii164.h
+++ drivers/staging/sm750fb/ddk750_sii164.h
@@ -34,7 +34,7 @@ unsigned short sii164_get_device_id(void);
 void sii164_reset_chip(void);
 char *sii164_get_chip_string(void);
 void sii164_set_power(unsigned char powerUp);
-void sii164_enable_hot_plug_detection(unsigned char enableHotPlug);
+void sii164_enable_hot_plug_detection(unsigned char enable_hot_plug);
 unsigned char sii164_is_connected(void);
 unsigned char sii164_check_interrupt(void);
 void sii164_clear_interrupt(void);
