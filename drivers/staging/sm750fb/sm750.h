a0bee80e8bb8512dccd48f9d473832e7690abb3f
Pavle Rohalj <pavle.rohalj@gmail.com>
2021-04-06 23:46:04 -0700

    staging: sm750fb: Rename functions *_deWait to snake case
    Fix "Avoid CamelCase" checkpatch.pl checks for the functions
    hw_sm750le_deWait and hw_sm750_deWait.
    
    Signed-off-by: Pavle Rohalj <pavle.rohalj@gmail.com>

--- drivers/staging/sm750fb/sm750.h
+++ drivers/staging/sm750fb/sm750.h
@@ -194,8 +194,8 @@ static inline unsigned long ps_to_hz(unsigned int psvalue)
 int hw_sm750_map(struct sm750_dev *sm750_dev, struct pci_dev *pdev);
 int hw_sm750_inithw(struct sm750_dev *sm750_dev, struct pci_dev *pdev);
 void hw_sm750_init_accel(struct sm750_dev *sm750_dev);
-int hw_sm750_deWait(void);
-int hw_sm750le_deWait(void);
+int hw_sm750_de_wait(void);
+int hw_sm750le_de_wait(void);
 
 int hw_sm750_output_set_mode(struct lynxfb_output *output,
 			    struct fb_var_screeninfo *var,
