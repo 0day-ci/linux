5b9d5ff7bee0f2726bae3a73d32130c7ed2685b8
Colin Ian King <colin.king@canonical.com>
2021-04-07 15:10:04 +0100

    media: venus: hfi,pm,firmware: Fix dereference before null check on hdev
    The pointer hdev is being dereferenced twice on the assignment of
    pointers cpu_cs_base and wrapper_base before hdev is being null
    checked.  Fix the potential null pointer dereference issues by
    performing the null check of hdev before dereferencing it when
    assigning cpu_cs_base and wrapper_base.
    
    Addresses-Coverity: ("Dereference before null check")
    Fixes: ff2a7013b3e6 ("media: venus: hfi,pm,firmware: Convert to block relative addressing")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- drivers/media/platform/qcom/venus/hfi_venus.c
+++ drivers/media/platform/qcom/venus/hfi_venus.c
@@ -1094,11 +1094,13 @@ static irqreturn_t venus_isr(struct venus_core *core)
 {
 	struct venus_hfi_device *hdev = to_hfi_priv(core);
 	u32 status;
-	void __iomem *cpu_cs_base = hdev->core->cpu_cs_base;
-	void __iomem *wrapper_base = hdev->core->wrapper_base;
+	void __iomem *cpu_cs_base, *wrapper_base;
 
 	if (!hdev)
 		return IRQ_NONE;
+
+	cpu_cs_base = hdev->core->cpu_cs_base;
+	wrapper_base = hdev->core->wrapper_base;
 
 	status = readl(wrapper_base + WRAPPER_INTR_STATUS);
 	if (IS_V6(core)) {
