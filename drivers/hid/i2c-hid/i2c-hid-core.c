3e842ed548be9be50d905facc4332cbc30920f40
Johnny Chuang <johnny.chuang.emc@gmail.com>
2021-04-07 14:49:17 +0800

    HID: i2c-hid: Skip ELAN power-on command after reset
    Previous commit 43b7029f475e ("HID: i2c-hid:
    Send power-on command after reset"), it fixed issue for SIS touchscreen.
    
    For ELAN touchscreen, we found our boot code of IC was not flexible enough
    to receive and handle this command.
    Once the FW main code of our controller is crashed for some reason,
    the controller could not be enumerated successfully to be recognized
    by the system host. therefore, it lost touch functionality.
    
    Add quirk for skip send power-on command after reset.
    It will impact to ELAN touchscreen and touchpad on HID over I2C projects.
    
    Signed-off-by: Johnny Chuang <johnny.chuang.emc@gmail.com>

--- drivers/hid/i2c-hid/i2c-hid-core.c
+++ drivers/hid/i2c-hid/i2c-hid-core.c
@@ -45,6 +45,7 @@
 #define I2C_HID_QUIRK_BOGUS_IRQ			BIT(4)
 #define I2C_HID_QUIRK_RESET_ON_RESUME		BIT(5)
 #define I2C_HID_QUIRK_BAD_INPUT_SIZE		BIT(6)
+#define I2C_HID_QUIRK_NO_WAKEUP_AFTER_RESET	BIT(7)
 
 
 /* flags */
@@ -157,6 +158,10 @@ struct i2c_hid {
 	struct i2chid_ops	*ops;
 };
 
+/*
+ * Sending the wakeup after reset actually break ELAN touchscreen controller
+ * So add I2C_HID_QUIRK_NO_WAKEUP_AFTER_RESET to skip wakeup after reset
+ */
 static const struct i2c_hid_quirks {
 	__u16 idVendor;
 	__u16 idProduct;
@@ -178,6 +183,8 @@ static const struct i2c_hid_quirks {
 		 I2C_HID_QUIRK_RESET_ON_RESUME },
 	{ USB_VENDOR_ID_ITE, I2C_DEVICE_ID_ITE_LENOVO_LEGION_Y720,
 		I2C_HID_QUIRK_BAD_INPUT_SIZE },
+	{ USB_VENDOR_ID_ELAN, HID_ANY_ID,
+		 I2C_HID_QUIRK_NO_WAKEUP_AFTER_RESET },
 	{ 0, 0 }
 };
 
@@ -461,7 +468,8 @@ static int i2c_hid_hwreset(struct i2c_client *client)
 	}
 
 	/* At least some SIS devices need this after reset */
-	ret = i2c_hid_set_power(client, I2C_HID_PWR_ON);
+	if (!(ihid->quirks & I2C_HID_QUIRK_NO_WAKEUP_AFTER_RESET))
+		ret = i2c_hid_set_power(client, I2C_HID_PWR_ON);
 
 out_unlock:
 	mutex_unlock(&ihid->reset_lock);
