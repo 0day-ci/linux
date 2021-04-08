7cf24d53400842a5f0237600ea251238fc814969
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-04-08 14:51:29 +0200

    tty: audit: move some local functions out of tty.h
    The functions tty_audit_add_data() and tty_audit_tiocsti() are local to
    the tty core code, and do not need to be in a "kernel-wide" header file
    so move them to drivers/tty/tty.h
    
    Cc: Jiri Slaby <jirislaby@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty.h
+++ drivers/tty/tty.h
@@ -6,4 +6,18 @@
 #ifndef _TTY_INTERNAL_H
 #define _TTY_INTERNAL_H
 
+/* tty_audit.c */
+#ifdef CONFIG_AUDIT
+void tty_audit_add_data(struct tty_struct *tty, const void *data, size_t size);
+void tty_audit_tiocsti(struct tty_struct *tty, char ch);
+#else
+static inline void tty_audit_add_data(struct tty_struct *tty, const void *data,
+				      size_t size)
+{
+}
+static inline void tty_audit_tiocsti(struct tty_struct *tty, char ch)
+{
+}
+#endif
+
 #endif
