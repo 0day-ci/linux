dc947045c8e12bdfea819561919d245457080283
Lucas De Marchi <lucas.demarchi@intel.com>
2021-04-07 21:52:23 -0700

    drm/i915: finish removal of gen from intel_device_info
    Now that it's not being used anymore, finish its removal. Like for
    gen_mask, we replace INTEL_GEN() and IS_GEN()  macros to use the new
    field.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1240,11 +1240,15 @@ static inline struct drm_i915_private *pdev_to_i915(struct pci_dev *pdev)
  * Deprecated: this will be replaced by individual IP checks:
  * GRAPHICS_VER(), MEDIA_VER and DISPLAY_VER()
  */
-#define INTEL_GEN(dev_priv)		(INTEL_INFO(dev_priv)->gen)
+#define INTEL_GEN(dev_priv)		GRAPHICS_VER(dev_priv)
 /*
  * Deprecated: use IS_GRAPHICS_RANGE()
  */
 #define IS_GEN_RANGE(dev_priv, s, e)	IS_GRAPHICS_RANGE(dev_priv, (s), (e))
+/*
+ * Deprecated: use IS_GRAPHICS_VER()
+ */
+#define IS_GEN(dev_priv, n)		IS_GRAPHICS_VER(dev_priv, (n))
 
 #define GRAPHICS_VER(i915)		(INTEL_INFO(i915)->graphics_ver)
 #define IS_GRAPHICS_RANGE(i915, from, until) \
@@ -1263,10 +1267,6 @@ static inline struct drm_i915_private *pdev_to_i915(struct pci_dev *pdev)
 
 #define REVID_FOREVER		0xff
 #define INTEL_REVID(dev_priv)	(to_pci_dev((dev_priv)->drm.dev)->revision)
-
-#define IS_GEN(dev_priv, n) \
-	(BUILD_BUG_ON_ZERO(!__builtin_constant_p(n)) + \
-	 INTEL_INFO(dev_priv)->gen == (n))
 
 #define HAS_DSB(dev_priv)	(INTEL_INFO(dev_priv)->display.has_dsb)
 
