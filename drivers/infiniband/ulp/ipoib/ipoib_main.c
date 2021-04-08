bb6afa78dad9262ce1d3fd862a3a39eef1800675
Jack Wang <jinpu.wang@ionos.com>
2021-04-08 10:34:35 +0200

    RDMA/ipoib: print a message if only child interface is UP
    When "enhanced IPoIB" enabled for CX-5 devices, it requires
    the parent device to be UP, otherwise the child devices won't
    work.[1]
    
    This add a debug message to give admin a hint, if only child interface
    is UP, but parent interface is not.
    
    [1]https://lore.kernel.org/linux-rdma/CAMGffE=3YYxv9i7_qQr3-Uv-NGr-7VsnMk8DTjR0YbX1vJBzXQ@mail.gmail.com/T/#u
    Signed-off-by: Jack Wang <jinpu.wang@ionos.com>

--- drivers/infiniband/ulp/ipoib/ipoib_main.c
+++ drivers/infiniband/ulp/ipoib/ipoib_main.c
@@ -164,8 +164,12 @@ int ipoib_open(struct net_device *dev)
 			dev_change_flags(cpriv->dev, flags | IFF_UP, NULL);
 		}
 		up_read(&priv->vlan_rwsem);
-	}
+	} else if (priv->parent) {
+		struct ipoib_dev_priv *ppriv = ipoib_priv(priv->parent);
 
+		if (!test_bit(IPOIB_FLAG_ADMIN_UP, &ppriv->flags))
+			ipoib_dbg(priv, "parent deivce %s is not up, so child may be not functioning.\n", ((struct ipoib_dev_priv *) ppriv)->dev->name);
+	}
 	netif_start_queue(dev);
 
 	return 0;
