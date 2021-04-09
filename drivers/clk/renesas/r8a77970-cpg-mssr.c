52f70acb1d4811aa741864ba38d047775107c1e7
Ye Bin <yebin10@huawei.com>
2021-04-09 17:51:50 +0800

    clk: renesas: r8a77970: Use DEFINE_SPINLOCK() for spinlock
    spinlock can be initialized automatically with DEFINE_SPINLOCK()
    rather than explicitly calling spin_lock_init().
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Ye Bin <yebin10@huawei.com>

--- drivers/clk/renesas/r8a77970-cpg-mssr.c
+++ drivers/clk/renesas/r8a77970-cpg-mssr.c
@@ -47,7 +47,7 @@ enum clk_ids {
 	MOD_CLK_BASE
 };
 
-static spinlock_t cpg_lock;
+static DEFINE_SPINLOCK(cpg_lock);
 
 static const struct clk_div_table cpg_sd0h_div_table[] = {
 	{  0,  2 }, {  1,  3 }, {  2,  4 }, {  3,  6 },
@@ -211,8 +211,6 @@ static int __init r8a77970_cpg_mssr_init(struct device *dev)
 	error = rcar_rst_read_mode_pins(&cpg_mode);
 	if (error)
 		return error;
-
-	spin_lock_init(&cpg_lock);
 
 	cpg_pll_config = &cpg_pll_configs[CPG_PLL_CONFIG_INDEX(cpg_mode)];
 
