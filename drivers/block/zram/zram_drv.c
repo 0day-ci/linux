3ae021f483bc6b3598018b8cb3036674e67fbd52
Matthew Wilcox (Oracle) <willy@infradead.org>
2021-04-09 03:51:26 +0100

    zram: Add zram_table_lock
    Allow lockdep to track the zram bit spin locks.
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -59,20 +59,22 @@ static void zram_free_page(struct zram *zram, size_t index);
 static int zram_bvec_read(struct zram *zram, struct bio_vec *bvec,
 				u32 index, int offset, struct bio *bio);
 
+static DEFINE_SPLIT_LOCK(zram_table_lock);
 
 static int zram_slot_trylock(struct zram *zram, u32 index)
 {
-	return bit_spin_trylock(ZRAM_LOCK, &zram->table[index].flags);
+	return bit_spin_trylock(ZRAM_LOCK, &zram->table[index].flags,
+			&zram_table_lock);
 }
 
 static void zram_slot_lock(struct zram *zram, u32 index)
 {
-	bit_spin_lock(ZRAM_LOCK, &zram->table[index].flags);
+	bit_spin_lock(ZRAM_LOCK, &zram->table[index].flags, &zram_table_lock);
 }
 
 static void zram_slot_unlock(struct zram *zram, u32 index)
 {
-	bit_spin_unlock(ZRAM_LOCK, &zram->table[index].flags);
+	bit_spin_unlock(ZRAM_LOCK, &zram->table[index].flags, &zram_table_lock);
 }
 
 static inline bool init_done(struct zram *zram)
