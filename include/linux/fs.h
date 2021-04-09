aec499039e7b21224ef29e5a2daba328aec14442
Yuqi Jin <jinyuqi@huawei.com>
2021-04-09 11:47:37 +0800

    fs: Optimized file struct to improve performance
    In the syscall process, @f_count and @f_mod are frequently used, if
    we put them together with each other and they will share the same
    cacheline. It is useful for the performance.
    
    syscall of unixbench is tested on Intel 8180.
    before this patch
    80 CPUs in system; running 80 parallel copies of tests
    
    System Call Overhead                    3789860.2 lps   (10.0 s, 1 samples)
    
    System Benchmarks Partial Index              BASELINE       RESULT    INDEX
    System Call Overhead                          15000.0    3789860.2   2526.6
                                                                       ========
    System Benchmarks Index Score (Partial Only)                         2526.6
    
    after this patch
    80 CPUs in system; running 80 parallel copies of tests
    
    System Call Overhead                    3951328.1 lps   (10.0 s, 1 samples)
    
    System Benchmarks Partial Index              BASELINE       RESULT    INDEX
    System Call Overhead                          15000.0    3951328.1   2634.2
                                                                       ========
    System Benchmarks Index Score (Partial Only)                         2634.2
    
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Yuqi Jin <jinyuqi@huawei.com>
    Signed-off-by: Shaokun Zhang <zhangshaokun@hisilicon.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -919,7 +919,6 @@ struct file {
 		struct llist_node	fu_llist;
 		struct rcu_head 	fu_rcuhead;
 	} f_u;
-	struct path		f_path;
 	struct inode		*f_inode;	/* cached value */
 	const struct file_operations	*f_op;
 
@@ -928,13 +927,14 @@ struct file {
 	 * Must not be taken from IRQ context.
 	 */
 	spinlock_t		f_lock;
-	enum rw_hint		f_write_hint;
 	atomic_long_t		f_count;
 	unsigned int 		f_flags;
 	fmode_t			f_mode;
 	struct mutex		f_pos_lock;
 	loff_t			f_pos;
 	struct fown_struct	f_owner;
+	enum rw_hint		f_write_hint;
+	struct path		f_path;
 	const struct cred	*f_cred;
 	struct file_ra_state	f_ra;
 
