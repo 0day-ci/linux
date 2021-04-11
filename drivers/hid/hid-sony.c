94a12bf133323601d778fae08dc7c03c9af9164d
Henry Castro <hcvcastro@gmail.com>
2021-04-11 11:06:23 -0400

    drivers/hid: avoid invalid denominator
    Avoid a potential panic in case wrong denominator
    is given.
    
    Signed-off-by: Henry Castro <hcvcastro@gmail.com>

--- drivers/hid/hid-sony.c
+++ drivers/hid/hid-sony.c
@@ -1134,11 +1134,16 @@ static void dualshock4_parse_report(struct sony_sc *sc, u8 *rd, int size)
 		 * Note: we swap numerator 'x' and 'numer' in mult_frac for
 		 *       precision reasons so we don't need 64-bit.
 		 */
-		int calib_data = mult_frac(calib->sens_numer,
-					   raw_data - calib->bias,
-					   calib->sens_denom);
+		if (calib->sens_denom != 0) {
+			int calib_data = mult_frac(calib->sens_numer,
+						   raw_data - calib->bias,
+						   calib->sens_denom);
 
-		input_report_abs(sc->sensor_dev, calib->abs_code, calib_data);
+			input_report_abs(sc->sensor_dev, calib->abs_code, calib_data);
+		} else {
+			hid_warn(sc->hdev, "DualShock 4 parse report, avoid invalid denominator");
+		}
+
 		offset += 2;
 	}
 	input_sync(sc->sensor_dev);
