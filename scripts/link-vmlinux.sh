1bb2dfd16367c423f608c45829d5d3f523e3b39e
Ilya Leoshkevich <iii@linux.ibm.com>
2021-04-12 23:56:29 +0200

    bpf: Generate BTF_KIND_FLOAT when linking vmlinux
    pahole v1.21 supports the --btf_gen_floats flag, which makes it
    generate the information about the floating-point types [1].
    
    Adjust link-vmlinux.sh to pass this flag to pahole in case it's
    supported, which is determined using a simple version check.
    
    [1] https://lore.kernel.org/dwarves/YHRiXNX1JUF2Az0A@kernel.org/
    
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -227,8 +227,13 @@ gen_btf()
 
 	vmlinux_link ${1}
 
+	local extra_paholeopt=
+	if [ "${pahole_ver}" -ge "121" ]; then
+		extra_paholeopt="${extra_paholeopt} --btf_gen_floats"
+	fi
+
 	info "BTF" ${2}
-	LLVM_OBJCOPY=${OBJCOPY} ${PAHOLE} -J ${1}
+	LLVM_OBJCOPY=${OBJCOPY} ${PAHOLE} -J${extra_paholeopt} ${1}
 
 	# Create ${2} which contains just .BTF section but no symbols. Add
 	# SHF_ALLOC because .BTF will be part of the vmlinux image. --strip-all
