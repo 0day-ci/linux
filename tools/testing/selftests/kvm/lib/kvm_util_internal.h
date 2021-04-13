6ddd1b7b4350dae49eba926607dc08e525a33855
Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
2021-04-13 16:08:27 +0200

    KVM: selftests: Keep track of memslots more efficiently
    The KVM selftest framework was using a simple list for keeping track of
    the memslots currently in use.
    This resulted in lookups and adding a single memslot being O(n), the
    later due to linear scanning of the existing memslot set to check for
    the presence of any conflicting entries.
    
    Before this change, benchmarking high count of memslots was more or less
    impossible as pretty much all the benchmark time was spent in the
    selftest framework code.
    
    We can simply use a rbtree for keeping track of both of gfn and hva.
    We don't need an interval tree for hva here as we can't have overlapping
    memslots because we allocate a completely new memory chunk for each new
    memslot.
    
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
    Reviewed-by: Andrew Jones <drjones@redhat.com>

--- tools/testing/selftests/kvm/lib/kvm_util_internal.h
+++ tools/testing/selftests/kvm/lib/kvm_util_internal.h
@@ -8,6 +8,9 @@
 #ifndef SELFTEST_KVM_UTIL_INTERNAL_H
 #define SELFTEST_KVM_UTIL_INTERNAL_H
 
+#include "linux/hashtable.h"
+#include "linux/rbtree.h"
+
 #include "sparsebit.h"
 
 #define KVM_DEV_PATH		"/dev/kvm"
@@ -20,7 +23,9 @@ struct userspace_mem_region {
 	void *host_mem;
 	void *mmap_start;
 	size_t mmap_size;
-	struct list_head list;
+	struct rb_node gpa_node;
+	struct rb_node hva_node;
+	struct hlist_node slot_node;
 };
 
 struct vcpu {
@@ -31,6 +36,12 @@ struct vcpu {
 	struct kvm_dirty_gfn *dirty_gfns;
 	uint32_t fetch_index;
 	uint32_t dirty_gfns_count;
+};
+
+struct userspace_mem_regions {
+	struct rb_root gpa_tree;
+	struct rb_root hva_tree;
+	DECLARE_HASHTABLE(slot_hash, 9);
 };
 
 struct kvm_vm {
@@ -45,7 +56,7 @@ struct kvm_vm {
 	unsigned int va_bits;
 	uint64_t max_gfn;
 	struct list_head vcpus;
-	struct list_head userspace_mem_regions;
+	struct userspace_mem_regions regions;
 	struct sparsebit *vpages_valid;
 	struct sparsebit *vpages_mapped;
 	bool has_irqchip;
