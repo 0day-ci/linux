d5ab3639a360c2fa90f3d5c3bae641a04d889bfa
Vitaly Chikunov <vt@altlinux.org>
2021-04-14 19:29:42 +0300

    perf beauty: Fix fsconfig generator
    After gnulib update sed stopped matching `[[:space:]]*+' as before,
    causing the following compilation error:
    
      In file included from builtin-trace.c:719:
      trace/beauty/generated/fsconfig_arrays.c:2:3: error: expected expression before ']' token
          2 |  [] = "",
    	|   ^
      trace/beauty/generated/fsconfig_arrays.c:2:3: error: array index in initializer not of integer type
      trace/beauty/generated/fsconfig_arrays.c:2:3: note: (near initialization for 'fsconfig_cmds')
    
    Fix this by correcting the regular expression used in the generator.
    Also, clean up the script by removing redundant egrep, xargs, and printf
    invocations.
    
    Fixes: d35293004a5e4 ("perf beauty: Add generator for fsconfig's 'cmd' arg values")
    Co-authored-by: Dmitry V. Levin <ldv@altlinux.org>
    Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

--- tools/perf/trace/beauty/fsconfig.sh
+++ tools/perf/trace/beauty/fsconfig.sh
@@ -10,8 +10,6 @@ fi
 linux_mount=${linux_header_dir}/mount.h
 
 printf "static const char *fsconfig_cmds[] = {\n"
-regex='^[[:space:]]*+FSCONFIG_([[:alnum:]_]+)[[:space:]]*=[[:space:]]*([[:digit:]]+)[[:space:]]*,[[:space:]]*.*'
-egrep $regex ${linux_mount} | \
-	sed -r "s/$regex/\2 \1/g"	| \
-	xargs printf "\t[%s] = \"%s\",\n"
+regex='^[[:space:]]*FSCONFIG_([[:alnum:]_]+)[[:space:]]*=[[:space:]]*([[:digit:]]+)[[:space:]]*,.*'
+sed -nr "s/$regex/\t[\2] = \"\1\",/p" ${linux_mount}
 printf "};\n"
