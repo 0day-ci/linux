4b86743244e9b15edeb1242f26a95ce38242cf90
Daniel Latypov <dlatypov@google.com>
2021-04-16 15:09:55 -0700

    kunit: add unit test for filtering suites by names
    This adds unit tests for kunit_filter_subsuite() and
    kunit_filter_suites().
    
    Note: what the executor means by "subsuite" is the array of suites
    corresponding to each test file.
    
    This patch lightly refactors executor.c to avoid the use of global
    variables to make it testable.
    It also includes a clever `kfree_at_end()` helper that makes this test
    easier to write than it otherwise would have been.
    
    Tested by running just the new tests using itself
    $ ./tools/testing/kunit/kunit.py run '*exec*'
    
    Signed-off-by: Daniel Latypov <dlatypov@google.com>
    Reviewed-by: David Gow <davidgow@google.com>

--- lib/kunit/executor.c
+++ lib/kunit/executor.c
@@ -19,7 +19,7 @@ MODULE_PARM_DESC(filter_glob,
 		"Filter which KUnit test suites run at boot-time, e.g. list*");
 
 static struct kunit_suite * const *
-kunit_filter_subsuite(struct kunit_suite * const * const subsuite)
+kunit_filter_subsuite(struct kunit_suite * const * const subsuite, const char *filter_glob)
 {
 	int i, n = 0;
 	struct kunit_suite **filtered;
@@ -52,19 +52,14 @@ struct suite_set {
 	struct kunit_suite * const * const *end;
 };
 
-static struct suite_set kunit_filter_suites(void)
+static struct suite_set kunit_filter_suites(const struct suite_set *suite_set,
+					    const char *filter_glob)
 {
 	int i;
 	struct kunit_suite * const **copy, * const *filtered_subsuite;
 	struct suite_set filtered;
 
-	const size_t max = __kunit_suites_end - __kunit_suites_start;
-
-	if (!filter_glob) {
-		filtered.start = __kunit_suites_start;
-		filtered.end = __kunit_suites_end;
-		return filtered;
-	}
+	const size_t max = suite_set->end - suite_set->start;
 
 	copy = kmalloc_array(max, sizeof(*filtered.start), GFP_KERNEL);
 	filtered.start = copy;
@@ -74,7 +69,7 @@ static struct suite_set kunit_filter_suites(void)
 	}
 
 	for (i = 0; i < max; ++i) {
-		filtered_subsuite = kunit_filter_subsuite(__kunit_suites_start[i]);
+		filtered_subsuite = kunit_filter_subsuite(suite_set->start[i], filter_glob);
 		if (filtered_subsuite)
 			*copy++ = filtered_subsuite;
 	}
@@ -98,8 +93,13 @@ static void kunit_print_tap_header(struct suite_set *suite_set)
 int kunit_run_all_tests(void)
 {
 	struct kunit_suite * const * const *suites;
+	struct suite_set suite_set = {
+		.start = __kunit_suites_start,
+		.end = __kunit_suites_end,
+	};
 
-	struct suite_set suite_set = kunit_filter_suites();
+	if (filter_glob)
+		suite_set = kunit_filter_suites(&suite_set, filter_glob);
 
 	kunit_print_tap_header(&suite_set);
 
@@ -114,5 +114,9 @@ int kunit_run_all_tests(void)
 
 	return 0;
 }
+
+#if IS_BUILTIN(CONFIG_KUNIT_TEST)
+#include "executor_test.c"
+#endif
 
 #endif /* IS_BUILTIN(CONFIG_KUNIT) */
