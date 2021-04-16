94985c2b09eb03159b60f807739999b6188ee06b
Bhaumik Bhatt <bbhatt@codeaurora.org>
2021-04-16 11:47:02 -0700

    bus: mhi: Add MMIO region length to controller structure
    Make controller driver specify the MMIO register region length
    for range checking of BHI or BHIe space. This can help validate
    that offsets are in acceptable memory region or not and avoid any
    boot-up issues due to BHI or BHIe memory accesses.
    
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- include/linux/mhi.h
+++ include/linux/mhi.h
@@ -293,6 +293,7 @@ struct mhi_controller_config {
  * @mhi_dev: MHI device instance for the controller
  * @debugfs_dentry: MHI controller debugfs directory
  * @regs: Base address of MHI MMIO register space (required)
+ * @reg_len: Length of the MHI MMIO region (required)
  * @bhi: Points to base of MHI BHI register space
  * @bhie: Points to base of MHI BHIe register space
  * @wake_db: MHI WAKE doorbell register address
@@ -375,6 +376,7 @@ struct mhi_controller {
 	struct mhi_device *mhi_dev;
 	struct dentry *debugfs_dentry;
 	void __iomem *regs;
+	size_t reg_len;
 	void __iomem *bhi;
 	void __iomem *bhie;
 	void __iomem *wake_db;
