66602df6ff911254fb4b2f7bfa97107ecb98b6f6
Lukas Middendorf <kernel@tuxforce.de>
2021-04-18 02:12:03 +0200

    media dvb_frontend: add suspend and resume callbacks to dvb_frontend_ops
    While dvb_tuner_ops already has dedicated suspend and resume callbacks,
    dvb_frontend_ops currently does not have them. Add those callbacks and
    use them for suspend and resume. If they are not set, the old behavior
    of calling sleep or init is used.
    
    This allows dvb_frontend drivers to handle resume differently from init,
    and suspend differently from sleep. No change is required for drivers
    not needing this functionality.
    
    Signed-off-by: Lukas Middendorf <kernel@tuxforce.de>

--- drivers/media/dvb-core/dvb_frontend.c
+++ drivers/media/dvb-core/dvb_frontend.c
@@ -2923,7 +2923,9 @@ int dvb_frontend_suspend(struct dvb_frontend *fe)
 	else if (fe->ops.tuner_ops.sleep)
 		ret = fe->ops.tuner_ops.sleep(fe);
 
-	if (fe->ops.sleep)
+	if (fe->ops.suspend)
+		ret = fe->ops.suspend(fe);
+	else if (fe->ops.sleep)
 		ret = fe->ops.sleep(fe);
 
 	return ret;
@@ -2939,7 +2941,9 @@ int dvb_frontend_resume(struct dvb_frontend *fe)
 		fe->id);
 
 	fe->exit = DVB_FE_DEVICE_RESUME;
-	if (fe->ops.init)
+	if (fe->ops.resume)
+		ret = fe->ops.resume(fe);
+	else if (fe->ops.init)
 		ret = fe->ops.init(fe);
 
 	if (fe->ops.tuner_ops.resume)
