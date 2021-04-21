0dc9ee88d95eac34c490edbd9692f107120a0ee6
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-04-21 15:00:50 +0200

    Revert "ipv6/route: Add a missing check on proc_dointvec"
    This reverts commit f0fb9b288d0a7e9cc324ae362e2dfd2cc2217ded.
    
    Commits from @umn.edu addresses have been found to be submitted in "bad
    faith" to try to test the kernel community's ability to review "known
    malicious" changes.  The result of these submissions can be found in a
    paper published at the 42nd IEEE Symposium on Security and Privacy
    entitled, "Open Source Insecurity: Stealthily Introducing
    Vulnerabilities via Hypocrite Commits" written by Qiushi Wu (University
    of Minnesota) and Kangjie Lu (University of Minnesota).
    
    Because of this, all submissions from this group must be reverted from
    the kernel tree and will need to be re-reviewed again to determine if
    they actually are a valid fix.  Until that work is complete, remove this
    change to ensure that no problems are being introduced into the
    codebase.
    
    Cc: Aditya Pakki <pakki001@umn.edu>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -6169,16 +6169,12 @@ static int ipv6_sysctl_rtcache_flush(struct ctl_table *ctl, int write,
 {
 	struct net *net;
 	int delay;
-	int ret;
 	if (!write)
 		return -EINVAL;
 
 	net = (struct net *)ctl->extra1;
 	delay = net->ipv6.sysctl.flush_delay;
-	ret = proc_dointvec(ctl, write, buffer, lenp, ppos);
-	if (ret)
-		return ret;
-
+	proc_dointvec(ctl, write, buffer, lenp, ppos);
 	fib6_run_gc(delay <= 0 ? 0 : (unsigned long)delay, net, delay > 0);
 	return 0;
 }
