742d82ba4cf2a33b78f1570f67c7331f254654e9
James Smart <jsmart2021@gmail.com>
2021-04-23 16:34:26 -0700

    elx: libefc_sli: SLI Descriptors and Queue entries
    This patch continues the libefc_sli SLI-4 library population.
    
    This patch add SLI-4 Data structures and defines for:
    - Buffer Descriptors (BDEs)
    - Scatter/Gather List elements (SGEs)
    - Queues and their Entry Descriptions for:
       Event Queues (EQs), Completion Queues (CQs),
       Receive Queues (RQs), and the Mailbox Queue (MQ).
    
    Co-developed-by: Ram Vegesna <ram.vegesna@broadcom.com>
    Signed-off-by: Ram Vegesna <ram.vegesna@broadcom.com>
    Signed-off-by: James Smart <jsmart2021@gmail.com>
    Reviewed-by: Daniel Wagner <dwagner@suse.de>

---
+++ drivers/scsi/elx/include/efc_common.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2021 Broadcom. All Rights Reserved. The term
+ * “Broadcom” refers to Broadcom Inc. and/or its subsidiaries.
+ */
+
+#ifndef __EFC_COMMON_H__
+#define __EFC_COMMON_H__
+
+#include <linux/pci.h>
+
+#define EFC_SUCCESS	0
+#define EFC_FAIL	1
+
+struct efc_dma {
+	void		*virt;
+	void            *alloc;
+	dma_addr_t	phys;
+
+	size_t		size;
+	size_t          len;
+	struct pci_dev	*pdev;
+};
+
+#endif /* __EFC_COMMON_H__ */
