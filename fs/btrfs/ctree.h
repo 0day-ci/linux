ba21abebbe3e8300bb46d64031bec861fdb9238f
Omar Sandoval <osandov@fb.com>
2021-04-26 12:06:11 -0700

    btrfs: implement RWF_ENCODED reads
    There are 4 main cases:
    
    1. Inline extents: we copy the data straight out of the extent buffer.
    2. Hole/preallocated extents: we fill in zeroes.
    3. Regular, uncompressed extents: we read the sectors we need directly
       from disk.
    4. Regular, compressed extents: we read the entire compressed extent
       from disk and indicate what subset of the decompressed extent is in
       the file.
    
    This initial implementation simplifies a few things that can be improved
    in the future:
    
    - We hold the inode lock during the operation.
    - Cases 1, 3, and 4 allocate temporary memory to read into before
      copying out to userspace.
    - We don't do read repair, because it turns out that read repair is
      currently broken for compressed data.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Omar Sandoval <osandov@fb.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -3190,6 +3190,8 @@ int btrfs_run_delalloc_range(struct btrfs_inode *inode, struct page *locked_page
 int btrfs_writepage_cow_fixup(struct page *page, u64 start, u64 end);
 void btrfs_writepage_endio_finish_ordered(struct page *page, u64 start,
 					  u64 end, int uptodate);
+ssize_t btrfs_encoded_read(struct kiocb *iocb, struct iov_iter *iter);
+
 extern const struct dentry_operations btrfs_dentry_operations;
 extern const struct iomap_ops btrfs_dio_iomap_ops;
 extern const struct iomap_dio_ops btrfs_dio_ops;
