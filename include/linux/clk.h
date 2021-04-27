9b6a2e8097803aa0fc059214a68f6176cf8b7dfc
Randy Dunlap <rdunlap@infradead.org>
2021-04-26 20:13:42 -0700

    clk: <linux/clk.h>: correct clk_get_parent() documentation
    Make the kernel-doc return value agree with both the stub implementation
    in <linux/clk.h> and the non-stub function in drivers/clk/clk.c.
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: linux-clk@vger.kernel.org

--- include/linux/clk.h
+++ include/linux/clk.h
@@ -744,8 +744,8 @@ int clk_set_parent(struct clk *clk, struct clk *parent);
  * clk_get_parent - get the parent clock source for this clock
  * @clk: clock source
  *
- * Returns struct clk corresponding to parent clock source, or
- * valid IS_ERR() condition containing errno.
+ * Return: &struct clk corresponding to parent clock source, or
+ * %NULL if clk is %NULL.
  */
 struct clk *clk_get_parent(struct clk *clk);
 
