7627f4238d07707e4df397c9535f8d05d4843c57
Daejun Park <daejun7.park@samsung.com>
2021-04-28 16:58:10 +0900

    scsi: ufs: Add HPB 2.0 support
    This patch supports the HPB 2.0.
    
    The HPB 2.0 supports read of varying sizes from 4KB to 512KB.
    In the case of Read (<= 32KB) is supported as single HPB read.
    In the case of Read (36KB ~ 512KB) is supported by as a combination of
    write buffer command and HPB read command to deliver more PPN.
    The write buffer commands may not be issued immediately due to busy tags.
    To use HPB read more aggressively, the driver can requeue the write buffer
    command. The requeue threshold is implemented as timeout and can be
    modified with requeue_timeout_ms entry in sysfs.
    
    Reviewed-by: Can Guo <cang@codeaurora.org>
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Tested-by: Can Guo <cang@codeaurora.org>
    Signed-off-by: Daejun Park <daejun7.park@samsung.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -654,6 +654,8 @@ struct ufs_hba_variant_params {
  * @srgn_size: device reported HPB sub-region size
  * @slave_conf_cnt: counter to check all lu finished initialization
  * @hpb_disabled: flag to check if HPB is disabled
+ * @max_hpb_single_cmd: maximum size of single HPB command
+ * @is_legacy: flag to check HPB 1.0
  */
 struct ufshpb_dev_info {
 	int num_lu;
@@ -661,6 +663,8 @@ struct ufshpb_dev_info {
 	int srgn_size;
 	atomic_t slave_conf_cnt;
 	bool hpb_disabled;
+	int max_hpb_single_cmd;
+	bool is_legacy;
 };
 #endif
 
@@ -1096,6 +1100,9 @@ int ufshcd_read_desc_param(struct ufs_hba *hba,
 			   u8 param_offset,
 			   u8 *param_read_buf,
 			   u8 param_size);
+int ufshcd_query_attr_retry(struct ufs_hba *hba, enum query_opcode opcode,
+			    enum attr_idn idn, u8 index, u8 selector,
+			    u32 *attr_val);
 int ufshcd_query_attr(struct ufs_hba *hba, enum query_opcode opcode,
 		      enum attr_idn idn, u8 index, u8 selector, u32 *attr_val);
 int ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode,
