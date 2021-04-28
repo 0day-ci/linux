a5d48e4e3d93b4c12ac3f188a8449a9b85ecb988
Thomas Bogendoerfer <tsbogend@alpha.franken.de>
2021-04-28 18:33:34 +0200

    ata: pata_rb532: Make driver COMPILE_TESTable
    By removing the not needed #include <asm/mach-rc32434/rb.h> driver
    could be COMPILE_TESTed.
    
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- drivers/ata/pata_rb532_cf.c
+++ drivers/ata/pata_rb532_cf.c
@@ -28,8 +28,6 @@
 #include <linux/libata.h>
 #include <scsi/scsi_host.h>
 
-#include <asm/mach-rc32434/rb.h>
-
 #define DRV_NAME	"pata-rb532-cf"
 #define DRV_VERSION	"0.1.0"
 #define DRV_DESC	"PATA driver for RouterBOARD 532 Compact Flash"
