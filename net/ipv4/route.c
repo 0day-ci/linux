bd06909710250356551ee46ae35a665c067d00ca
Pavel Balaev <balaevpa@infotecs.ru>
2021-04-28 15:31:33 +0300

    net/ipv4: multipath routing: configurable seed
    Ability for a user to assign seed value to multipath route hashes.
    Now kernel uses random seed value to prevent hash-flooding DoS attacks;
    however, it disables some use cases, f.e:
    
    +-------+        +------+        +--------+
    |       |-eth0---| FW0  |---eth0-|        |
    |       |        +------+        |        |
    |  GW0  |ECMP                ECMP|  GW1   |
    |       |        +------+        |        |
    |       |-eth1---| FW1  |---eth1-|        |
    +-------+        +------+        +--------+
    
    In this use case, two ECMP routers balance traffic between two firewalls.
    If some flow transmits a response over a different channel than request,
    such flow will be dropped, because keep-state rules are created on
    the other firewall.
    
    This patch adds sysctl variable: net.ipv4.fib_multipath_hash_seed.
    User can set the same seed value on GW0 and GW1 for traffic to be
    mirror-balanced. By default, random value is used.
    
    Signed-off-by: Pavel Balaev <balaevpa@infotecs.ru>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -1912,6 +1912,7 @@ int fib_multipath_hash(const struct net *net, const struct flowi4 *fl4,
 {
 	u32 multipath_hash = fl4 ? fl4->flowi4_multipath_hash : 0;
 	struct flow_keys hash_keys;
+	siphash_key_t *seed_ctx;
 	u32 mhash;
 
 	switch (net->ipv4.sysctl_fib_multipath_hash_policy) {
@@ -1989,7 +1990,14 @@ int fib_multipath_hash(const struct net *net, const struct flowi4 *fl4,
 		}
 		break;
 	}
-	mhash = flow_hash_from_keys(&hash_keys);
+
+	rcu_read_lock();
+	seed_ctx = rcu_dereference(net->ipv4.fib_multipath_hash_seed_ctx);
+	if (seed_ctx)
+		mhash = flow_multipath_hash_from_keys(&hash_keys, seed_ctx);
+	else
+		mhash = flow_hash_from_keys(&hash_keys);
+	rcu_read_unlock();
 
 	if (multipath_hash)
 		mhash = jhash_2words(mhash, multipath_hash, 0);
