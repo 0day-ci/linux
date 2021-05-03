c92311a45d268696111c34460e1c84365753dcf8
Chen Li <chenli@uniontech.com>
2021-05-03 20:44:51 +0800

    nommu: remove __GFP_HIGHMEM in vmalloc/vzalloc
    From mm/nommu.c:
    void *__vmalloc(unsigned long size, gfp_t gfp_mask)
    {
    	/*
    	 *  You can't specify __GFP_HIGHMEM with kmalloc() since kmalloc()
    	 * returns only a logical address.
    	 */
    	return kmalloc(size, (gfp_mask | __GFP_COMP) & ~__GFP_HIGHMEM);
    }
    
    nommu's __vmalloc just uses kmalloc internally and elimitates __GFP_HIGHMEM,
    so it makes no sense to add __GFP_HIGHMEM for nommu's vmalloc/vzalloc.
    
    Signed-off-by: Chen Li <chenli@uniontech.com>

--- mm/nommu.c
+++ mm/nommu.c
@@ -237,7 +237,7 @@ long vread(char *buf, char *addr, unsigned long count)
  */
 void *vmalloc(unsigned long size)
 {
-       return __vmalloc(size, GFP_KERNEL | __GFP_HIGHMEM);
+       return __vmalloc(size, GFP_KERNEL);
 }
 EXPORT_SYMBOL(vmalloc);
 
@@ -255,7 +255,7 @@ EXPORT_SYMBOL(vmalloc);
  */
 void *vzalloc(unsigned long size)
 {
-	return __vmalloc(size, GFP_KERNEL | __GFP_HIGHMEM | __GFP_ZERO);
+	return __vmalloc(size, GFP_KERNEL | __GFP_ZERO);
 }
 EXPORT_SYMBOL(vzalloc);
 
