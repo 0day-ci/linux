cd8a8dd187e6e7578434025f88daaf40fe0e1ef8
Jens Axboe <axboe@kernel.dk>
2021-05-03 08:57:45 -0600

    eventfd: convert to using ->write_iter()
    Had a report on writing to eventfd with io_uring is slower than it
    should be, and it's the usual case of if a file type doesn't support
    ->write_iter(), then io_uring cannot rely on IOCB_NOWAIT being honored
    alongside O_NONBLOCK for whether or not this is a non-blocking write
    attempt. That means io_uring will punt the operation to an io thread,
    which will slow us down unnecessarily.
    
    Convert eventfd to using fops->write_iter() instead of fops->write().
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/eventfd.c
+++ fs/eventfd.c
@@ -264,17 +264,18 @@ static ssize_t eventfd_read(struct kiocb *iocb, struct iov_iter *to)
 	return sizeof(ucnt);
 }
 
-static ssize_t eventfd_write(struct file *file, const char __user *buf, size_t count,
-			     loff_t *ppos)
+static ssize_t eventfd_write(struct kiocb *kiocb, struct iov_iter *from)
 {
+	struct file *file = kiocb->ki_filp;
 	struct eventfd_ctx *ctx = file->private_data;
+	size_t count = iov_iter_count(from);
 	ssize_t res;
 	__u64 ucnt;
 	DECLARE_WAITQUEUE(wait, current);
 
 	if (count < sizeof(ucnt))
 		return -EINVAL;
-	if (copy_from_user(&ucnt, buf, sizeof(ucnt)))
+	if (copy_from_iter(&ucnt, count, from) != count)
 		return -EFAULT;
 	if (ucnt == ULLONG_MAX)
 		return -EINVAL;
@@ -282,7 +283,8 @@ static ssize_t eventfd_write(struct file *file, const char __user *buf, size_t c
 	res = -EAGAIN;
 	if (ULLONG_MAX - ctx->count > ucnt)
 		res = sizeof(ucnt);
-	else if (!(file->f_flags & O_NONBLOCK)) {
+	else if (!(file->f_flags & O_NONBLOCK) &&
+		 !(kiocb->ki_flags & IOCB_NOWAIT)) {
 		__add_wait_queue(&ctx->wqh, &wait);
 		for (res = 0;;) {
 			set_current_state(TASK_INTERRUPTIBLE);
@@ -331,7 +333,7 @@ static const struct file_operations eventfd_fops = {
 	.release	= eventfd_release,
 	.poll		= eventfd_poll,
 	.read_iter	= eventfd_read,
-	.write		= eventfd_write,
+	.write_iter	= eventfd_write,
 	.llseek		= noop_llseek,
 };
 
