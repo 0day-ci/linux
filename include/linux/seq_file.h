1795880710006d6df6f9f9322ca941657d35226f
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-05-04 13:26:48 +0300

    seq_file: Drop unused *_escape_mem_ascii()
    There are no more users of the seq_escape_mem_ascii()
    followed by string_escape_mem_ascii().
    
    Remove them for good.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- include/linux/seq_file.h
+++ include/linux/seq_file.h
@@ -146,8 +146,6 @@ static inline void seq_escape(struct seq_file *m, const char *s, const char *esc
 	seq_escape_mem(m, s, strlen(s), ESCAPE_OCTAL, esc);
 }
 
-void seq_escape_mem_ascii(struct seq_file *m, const char *src, size_t isz);
-
 void seq_hex_dump(struct seq_file *m, const char *prefix_str, int prefix_type,
 		  int rowsize, int groupsize, const void *buf, size_t len,
 		  bool ascii);
