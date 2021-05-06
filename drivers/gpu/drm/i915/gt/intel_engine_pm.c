95c8235deea7e29beefd735e59208158bc97bbb3
Matthew Brost <matthew.brost@intel.com>
2021-05-06 12:14:48 -0700

    drm/i915/guc: Don't call switch_to_kernel_context with GuC submission
    Calling switch_to_kernel_context isn't needed if the engine PM reference
    is taken while all contexts are pinned. By not calling
    switch_to_kernel_context we save on issuing a request to the engine.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/i915/gt/intel_engine_pm.c
+++ drivers/gpu/drm/i915/gt/intel_engine_pm.c
@@ -162,6 +162,10 @@ static bool switch_to_kernel_context(struct intel_engine_cs *engine)
 	unsigned long flags;
 	bool result = true;
 
+	/* No need to switch_to_kernel_context if GuC submission */
+	if (intel_engine_uses_guc(engine))
+		return true;
+
 	/* GPU is pointing to the void, as good as in the kernel context. */
 	if (intel_gt_is_wedged(engine->gt))
 		return true;
