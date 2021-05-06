b3f7112b91a17c5dd1c8f4555306dd4f18f83212
Dave Chinner <dchinner@redhat.com>
2021-05-06 17:20:38 +1000

    xfs: convert xfs_iwalk to use perag references
    Rather than manually walking the ags and passing agnunbers around,
    pass the perag for the AG we are currently working on around in the
    iwalk structure.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/libxfs/xfs_ag.h
+++ fs/xfs/libxfs/xfs_ag.h
@@ -117,12 +117,16 @@ void	xfs_perag_put(struct xfs_perag *pag);
 /*
  * Perag iteration APIs
  */
-#define for_each_perag(mp, next_agno, pag) \
-	for ((next_agno) = 0, (pag) = xfs_perag_get((mp), 0); \
+#define for_each_perag_from(mp, next_agno, pag) \
+	for ((pag) = xfs_perag_get((mp), (next_agno)); \
 		(pag) != NULL; \
 		(next_agno) = (pag)->pag_agno + 1, \
 		xfs_perag_put(pag), \
 		(pag) = xfs_perag_get((mp), (next_agno)))
+
+#define for_each_perag(mp, next_agno, pag) \
+	(next_agno) = 0; \
+	for_each_perag_from((mp), (next_agno), (pag))
 
 #define for_each_perag_tag(mp, next_agno, pag, tag) \
 	for ((next_agno) = 0, (pag) = xfs_perag_get_tag((mp), 0, (tag)); \
