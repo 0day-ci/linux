d2338885fb74aa9dce57bc841f84a9e9756af00a
Brendan Higgins <brendanhiggins@google.com>
2021-05-07 14:31:09 -0700

    kunit: tool: add support for QEMU
    Add basic support to run QEMU via kunit_tool. Add support for i386,
    x86_64, arm, arm64, and a bunch more.
    
    Signed-off-by: Brendan Higgins <brendanhiggins@google.com>
    Tested-by: David Gow <davidgow@google.com>

---
+++ tools/testing/kunit/qemu_config.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0
+#
+# Collection of configs for building non-UML kernels and running them on QEMU.
+#
+# Copyright (C) 2021, Google LLC.
+# Author: Brendan Higgins <brendanhiggins@google.com>
+
+from collections import namedtuple
+
+
+QemuArchParams = namedtuple('QemuArchParams', ['linux_arch',
+					       'qemuconfig',
+					       'qemu_arch',
+					       'kernel_path',
+					       'kernel_command_line',
+					       'extra_qemu_params'])
+
