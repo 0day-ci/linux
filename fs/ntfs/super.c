d2d38d974b8581f04447793c44cee26475882c23
Zhen Lei <thunder.leizhen@huawei.com>
2021-05-08 17:43:56 +0800

    ntfs: Remove several unneeded semicolons
    The semicolon immediately following '}' is unneeded.
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>

--- fs/ntfs/super.c
+++ fs/ntfs/super.c
@@ -1612,7 +1612,7 @@ read_partial_attrdef_page:
 		memcpy((u8*)vol->attrdef + (index++ << PAGE_SHIFT),
 				page_address(page), size);
 		ntfs_unmap_page(page);
-	};
+	}
 	if (size == PAGE_SIZE) {
 		size = i_size & ~PAGE_MASK;
 		if (size)
@@ -1681,7 +1681,7 @@ read_partial_upcase_page:
 		memcpy((char*)vol->upcase + (index++ << PAGE_SHIFT),
 				page_address(page), size);
 		ntfs_unmap_page(page);
-	};
+	}
 	if (size == PAGE_SIZE) {
 		size = i_size & ~PAGE_MASK;
 		if (size)
