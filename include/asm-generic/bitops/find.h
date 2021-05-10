843a54481be51456235f7c2cd87e1b20f60ba6a2
Yury Norov <yury.norov@gmail.com>
2021-05-10 16:34:21 -0700

    all: remove GENERIC_FIND_FIRST_BIT
    In the 5.12 cycle we enabled the GENERIC_FIND_FIRST_BIT config option
    for ARM64 and MIPS. It increased performance and shrunk .text size; and
    so far I didn't receive any negative feedback on the change.
    
    https://lore.kernel.org/linux-arch/20210225135700.1381396-1-yury.norov@gmail.com/
    
    I think it's time to make all architectures use find_{first,last}_bit()
    unconditionally and remove the corresponding config option.
    
    This patch doesn't introduce functional changes for arc, arm64, mips,
    s390 and x86 because they already enable GENERIC_FIND_FIRST_BIT. There
    will be no changes for arm because it implements find_{first,last}_bit
    in arch code. For other architectures I expect improvement both in
    performance and .text size.
    
    It would be great if people with an access to real hardware would share
    the output of bloat-o-meter and lib/find_bit_benchmark.
    
    Signed-off-by: Yury Norov <yury.norov@gmail.com>

--- include/asm-generic/bitops/find.h
+++ include/asm-generic/bitops/find.h
@@ -95,8 +95,6 @@ unsigned long find_next_zero_bit(const unsigned long *addr, unsigned long size,
 }
 #endif
 
-#ifdef CONFIG_GENERIC_FIND_FIRST_BIT
-
 /**
  * find_first_bit - find the first set bit in a memory region
  * @addr: The address to start the search at
@@ -136,16 +134,6 @@ unsigned long find_first_zero_bit(const unsigned long *addr, unsigned long size)
 
 	return _find_first_zero_bit(addr, size);
 }
-#else /* CONFIG_GENERIC_FIND_FIRST_BIT */
-
-#ifndef find_first_bit
-#define find_first_bit(addr, size) find_next_bit((addr), (size), 0)
-#endif
-#ifndef find_first_zero_bit
-#define find_first_zero_bit(addr, size) find_next_zero_bit((addr), (size), 0)
-#endif
-
-#endif /* CONFIG_GENERIC_FIND_FIRST_BIT */
 
 #ifndef find_last_bit
 /**
