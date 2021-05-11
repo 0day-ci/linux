a17c2d425b770b8ad54ce34939b70a262ba54905
Davide Caratti <dcaratti@redhat.com>
2021-05-11 19:46:16 +0200

    mptcp: validate 'id' when stopping the ADD_ADDR retransmit timer
    when Linux receives an echo-ed ADD_ADDR, it checks the IP address against
    the list of "announced" addresses. In case of a positive match, the timer
    that handles retransmissions is stopped regardless of the 'Address Id' in
    the received packet: this behaviour does not comply with RFC8684 §3.4.1.
    
    Fix it by validating the 'Address Id' in received echo-ed ADD_ADDRs.
    Tested using packetdrill, with the following captured output:
    
     unpatched kernel:
    
     Out <...> Flags [.], ack 1, win 256, options [mptcp add-addr v1 id 1 198.51.100.2 hmac 0xfd2e62517888fe29,mptcp dss ack 3007449509], length 0
     In  <...> Flags [.], ack 1, win 257, options [mptcp add-addr v1-echo id 1 1.2.3.4,mptcp dss ack 3013740213], length 0
     Out <...> Flags [.], ack 1, win 256, options [mptcp add-addr v1 id 1 198.51.100.2 hmac 0xfd2e62517888fe29,mptcp dss ack 3007449509], length 0
     In  <...> Flags [.], ack 1, win 257, options [mptcp add-addr v1-echo id 90 198.51.100.2,mptcp dss ack 3013740213], length 0
            ^^^ retransmission is stopped here, but 'Address Id' is 90
    
     patched kernel:
    
     Out <...> Flags [.], ack 1, win 256, options [mptcp add-addr v1 id 1 198.51.100.2 hmac 0x1cf372d59e05f4b8,mptcp dss ack 3007449509], length 0
     In  <...> Flags [.], ack 1, win 257, options [mptcp add-addr v1-echo id 1 1.2.3.4,mptcp dss ack 1672384568], length 0
     Out <...> Flags [.], ack 1, win 256, options [mptcp add-addr v1 id 1 198.51.100.2 hmac 0x1cf372d59e05f4b8,mptcp dss ack 3007449509], length 0
     In  <...> Flags [.], ack 1, win 257, options [mptcp add-addr v1-echo id 90 198.51.100.2,mptcp dss ack 1672384568], length 0
     Out <...> Flags [.], ack 1, win 256, options [mptcp add-addr v1 id 1 198.51.100.2 hmac 0x1cf372d59e05f4b8,mptcp dss ack 3007449509], length 0
     In  <...> Flags [.], ack 1, win 257, options [mptcp add-addr v1-echo id 1 198.51.100.2,mptcp dss ack 1672384568], length 0
            ^^^ retransmission is stopped here, only when both 'Address Id' and 'IP Address' match
    
    Fixes: 00cfd77b9063 ("mptcp: retransmit ADD_ADDR when timeout")
    Signed-off-by: Davide Caratti <dcaratti@redhat.com>

--- net/mptcp/pm_netlink.c
+++ net/mptcp/pm_netlink.c
@@ -353,11 +353,11 @@ mptcp_pm_del_add_timer(struct mptcp_sock *msk,
 
 	spin_lock_bh(&msk->pm.lock);
 	entry = mptcp_lookup_anno_list_by_saddr(msk, addr);
-	if (entry)
+	if (entry && entry->addr.id == addr->id)
 		entry->retrans_times = ADD_ADDR_RETRANS_MAX;
 	spin_unlock_bh(&msk->pm.lock);
 
-	if (entry)
+	if (entry && entry->addr.id == addr->id)
 		sk_stop_timer_sync(sk, &entry->add_timer);
 
 	return entry;
