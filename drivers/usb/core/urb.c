ac4b315dfad0016f22dbf438aaa10bd16f6f1bc6
Alan Stern <stern@rowland.harvard.edu>
2021-05-20 16:20:56 -0400

    USB: core: WARN if pipe direction != setup packet direction
    When a control URB is submitted, the direction indicated by URB's pipe
    member is supposed to match the direction indicated by the setup
    packet's bRequestType member.  A mismatch could lead to trouble,
    depending on which field the host controller drivers use for
    determining the actual direction.
    
    This shouldn't ever happen; it would represent a careless bug in a
    kernel driver somewhere.  This patch adds a dev_WARN to let people
    know about the potential problem.
    
    Suggested-by: "Geoffrey D. Bennett" <g@b4.vu>
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>

--- drivers/usb/core/urb.c
+++ drivers/usb/core/urb.c
@@ -407,6 +407,9 @@ int usb_submit_urb(struct urb *urb, gfp_t mem_flags)
 			return -ENOEXEC;
 		is_out = !(setup->bRequestType & USB_DIR_IN) ||
 				!setup->wLength;
+		if (usb_pipeout(urb->pipe) != is_out)
+			dev_WARN(&dev->dev, "BOGUS control dir, pipe %x doesn't match bRequestType %x\n",
+					urb->pipe, setup->bRequestType);
 	} else {
 		is_out = usb_endpoint_dir_out(&ep->desc);
 	}
