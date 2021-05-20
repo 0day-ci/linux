fd67295bdf2da038ac5a782a0729c2f0f169ea21
Aniket Bhattacharyea <aniketmail669@gmail.com>
2021-05-20 21:20:13 +0530

    media: atomisp: Replace whitespace at the beginning of line
    This patch replaces the whitespaces at the beginning of line with tabs.
    Identified by checkpatch: WARNING: please, no spaces at the start of a line.
    
    Signed-off-by: Aniket Bhattacharyea <aniketmail669@gmail.com>

--- drivers/staging/media/atomisp/pci/atomisp_gmin_platform.c
+++ drivers/staging/media/atomisp/pci/atomisp_gmin_platform.c
@@ -1041,9 +1041,9 @@ static struct camera_sensor_platform_data acpi_gmin_plat = {
 };
 
 struct camera_sensor_platform_data *gmin_camera_platform_data(
-    struct v4l2_subdev *subdev,
-    enum atomisp_input_format csi_format,
-    enum atomisp_bayer_order csi_bayer)
+	struct v4l2_subdev *subdev,
+	enum atomisp_input_format csi_format,
+	enum atomisp_bayer_order csi_bayer)
 {
 	u8 pmic_i2c_addr = gmin_detect_pmic(subdev);
 	struct gmin_subdev *gs;
