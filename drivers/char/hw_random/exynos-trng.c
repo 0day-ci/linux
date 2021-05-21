29f6d496c3104c0503110f737ed99e62dadbdacf
Tian Tao <tiantao6@hisilicon.com>
2021-05-21 11:58:09 +0800

    hwrng: exynos: Use pm_runtime_resume_and_get() to replace open coding
    use pm_runtime_resume_and_get() to replace pm_runtime_get_sync and
    pm_runtime_put_noidle. this change is just to simplify the code, no
    actual functional changes.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>

--- drivers/char/hw_random/exynos-trng.c
+++ drivers/char/hw_random/exynos-trng.c
@@ -196,10 +196,9 @@ static int __maybe_unused exynos_trng_resume(struct device *dev)
 {
 	int ret;
 
-	ret = pm_runtime_get_sync(dev);
-	if (ret < 0) {
+	ret = pm_runtime_resume_and_get(dev);
+	if (ret) {
 		dev_err(dev, "Could not get runtime PM.\n");
-		pm_runtime_put_noidle(dev);
 		return ret;
 	}
 
