139419de88c1733f12d88776194abcd84c955e42
Kelvin Cheung <keguang.zhang@gmail.com>
2021-05-21 08:04:05 +0800

    MIPS: Loongson1B: Add Loongson1 NAND device
    This patch adds NAND device and its platform data for Loongson1B.
    
    Signed-off-by: Kelvin Cheung <keguang.zhang@gmail.com>

--- arch/mips/loongson32/common/platform.c
+++ arch/mips/loongson32/common/platform.c
@@ -287,6 +287,33 @@ struct platform_device ls1x_gpio1_pdev = {
 	.resource	= ls1x_gpio1_resources,
 };
 
+/* NAND Flash */
+static struct resource ls1x_nand_resources[] = {
+	[0] = {
+		.start	= LS1X_NAND_BASE,
+		.end	= LS1X_NAND_BASE + SZ_32 - 1,
+		.flags	= IORESOURCE_MEM,
+	},
+	[1] = {
+		.name	= "dmachan0",
+		.start	= LS1X_DMA_CHANNEL0,
+		.end	= LS1X_DMA_CHANNEL0,
+		.flags	= IORESOURCE_DMA,
+	},
+};
+
+struct platform_device ls1x_nand_pdev = {
+	.name		= "ls1x-nand",
+	.id		= -1,
+	.num_resources	= ARRAY_SIZE(ls1x_nand_resources),
+	.resource	= ls1x_nand_resources,
+};
+
+void __init ls1x_nand_set_platdata(struct plat_ls1x_nand *pdata)
+{
+	ls1x_nand_pdev.dev.platform_data = pdata;
+}
+
 /* USB EHCI */
 static u64 ls1x_ehci_dmamask = DMA_BIT_MASK(32);
 
