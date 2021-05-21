fa91e2e901579c4eb2252b4bc1b23cd76c4b9820
Mina Almasry <almasrymina@google.com>
2021-05-21 00:44:33 -0700

    mm, hugetlb: fix resv_huge_pages underflow on UFFDIO_COPY
    The userfaultfd hugetlb tests detect a resv_huge_pages underflow. This
    happens when hugetlb_mcopy_atomic_pte() is called with !is_continue on
    an index for which we already have a page in the cache. When this
    happens, we allocate a second page, double consuming the reservation,
    and then fail to insert the page into the cache and return -EEXIST.
    
    To fix this, we first if there exists a page in the cache which already
    consumed the reservation, and return -EEXIST immediately if so.
    
    Secondly, if we fail to copy the page contents while holding the
    hugetlb_fault_mutex, we will drop the mutex and return to the caller
    after allocating a page that consumed a reservation. In this case there
    may be a fault that double consumes the reservation. To handle this, we
    free the allocated page, fix the reservations, and allocate a temporary
    hugetlb page and return that to the caller. When the caller does the
    copy outside of the lock, we again check the cache, and allocate a page
    consuming the reservation, and copy over the contents.
    
    Test:
    Hacked the code locally such that resv_huge_pages underflows produce
    a warning and the copy_huge_page_from_user() always fails, then:
    
    ./tools/testing/selftests/vm/userfaultfd hugetlb_shared 10
    	2 /tmp/kokonut_test/huge/userfaultfd_test && echo test success
    ./tools/testing/selftests/vm/userfaultfd hugetlb 10
    	2 /tmp/kokonut_test/huge/userfaultfd_test && echo test success
    
    Both tests succeed and produce no warnings. After the test runs
    number of free/resv hugepages is correct.
    
    Signed-off-by: Mina Almasry <almasrymina@google.com>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: linux-mm@kvack.org
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: linux-mm@kvack.org
    Cc: linux-kernel@vger.kernel.org

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -212,6 +212,8 @@ unsigned long hugetlb_change_protection(struct vm_area_struct *vma,
 bool is_hugetlb_entry_migration(pte_t pte);
 void hugetlb_unshare_all_pmds(struct vm_area_struct *vma);
 
+void hugetlb_copy_page(struct page *dst, struct page *src);
+
 #else /* !CONFIG_HUGETLB_PAGE */
 
 static inline void reset_vma_resv_huge_pages(struct vm_area_struct *vma)
@@ -401,6 +403,8 @@ static inline vm_fault_t hugetlb_fault(struct mm_struct *mm,
 }
 
 static inline void hugetlb_unshare_all_pmds(struct vm_area_struct *vma) { }
+
+static inline void hugetlb_copy_page(struct page *dst, struct page *src);
 
 #endif /* !CONFIG_HUGETLB_PAGE */
 /*
