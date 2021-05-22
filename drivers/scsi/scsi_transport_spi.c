0f5eb3a3d5538a4c813d78f24f6915bdaf1de050
Hui Tang <tanghui20@huawei.com>
2021-05-22 16:37:16 +0800

    scsi: scsi_transport_spi: remove leading spaces before tabs
    There are a few leading spaces before tabs and remove it by running
    the following commard:
    
        $ find . -name '*.[ch]' | xargs sed -r -i 's/^[ ]+\t/\t/'
    
    Signed-off-by: Hui Tang <tanghui20@huawei.com>

--- drivers/scsi/scsi_transport_spi.c
+++ drivers/scsi/scsi_transport_spi.c
@@ -1230,8 +1230,8 @@ int spi_populate_tag_msg(unsigned char *msg, struct scsi_cmnd *cmd)
 {
         if (cmd->flags & SCMD_TAGGED) {
 		*msg++ = SIMPLE_QUEUE_TAG;
-        	*msg++ = cmd->request->tag;
-        	return 2;
+		*msg++ = cmd->request->tag;
+		return 2;
 	}
 
 	return 0;
