4d39fd5f33eddef7362238bb0942e4e6e00308dc
Karen Sornek <karen.sornek@intel.com>
2021-05-24 12:04:14 -0700

    i40e: Add restoration of VF MSI-X state during PCI reset
    During a PCI FLR the MSI-X Enable flag in the VF PCI MSI-X capability
    register will be cleared. This can lead to issues when a VF is
    assigned to a VM because in these cases the VF driver receives no
    indication of the PF PCI error/reset and additionally it is incapable
    of restoring the cleared flag in the hypervisor configuration space
    without fully reinitializing the driver interrupt functionality.
    
    Since the VF driver is unable to easily resolve this condition on its own,
    restore the VF MSI-X flag during the PF PCI reset handling.
    
    Signed-off-by: Slawomir Laba <slawomirx.laba@intel.com>
    Signed-off-by: Nick Nunley <nicholas.d.nunley@intel.com>
    Signed-off-by: Karen Sornek <karen.sornek@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -16098,6 +16098,8 @@ static void i40e_pci_error_reset_done(struct pci_dev *pdev)
 	struct i40e_pf *pf = pci_get_drvdata(pdev);
 
 	i40e_reset_and_rebuild(pf, false, false);
+
+	i40e_restore_all_vfs_msi_state(pdev);
 }
 
 /**
