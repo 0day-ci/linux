02f26270df82f0e1a7df9fab0bf2f30a666573ed
Denys Zagorui <dzagorui@cisco.com>
2021-05-24 04:15:13 -0700

    perf tests: avoid storing an absolute path in perf binary
    python binding test uses PYTHONPATH definition to find python/perf.so
    library. This definition is an absolute path that makes perf binary
    unreproducible. This path can be found during runtime execution.
    
    Signed-off-by: Denys Zagorui <dzagorui@cisco.com>

--- tools/perf/tests/python-use.c
+++ tools/perf/tests/python-use.c
@@ -8,18 +8,37 @@
 #include <linux/compiler.h>
 #include "tests.h"
 #include "util/debug.h"
+#include "util/util.h"
+#include <sys/stat.h>
+#include <limits.h>
+#include <libgen.h>
 
 int test__python_use(struct test *test __maybe_unused, int subtest __maybe_unused)
 {
 	char *cmd;
-	int ret;
+	int ret = -1;
+	char *exec_path;
+	char buf[PATH_MAX];
+	char *pythonpath;
+	struct stat sb;
+
+	perf_exe(buf, PATH_MAX);
+	exec_path = dirname(buf);
+
+	if (asprintf(&pythonpath, "%s/python", exec_path) < 0)
+		return ret;
+
+	if (stat(pythonpath, &sb) || !S_ISDIR(sb.st_mode))
+		pythonpath[0] = 0;
 
 	if (asprintf(&cmd, "echo \"import sys ; sys.path.append('%s'); import perf\" | %s %s",
-		     PYTHONPATH, PYTHON, verbose > 0 ? "" : "2> /dev/null") < 0)
-		return -1;
+		     pythonpath, PYTHON, verbose > 0 ? "" : "2> /dev/null") < 0)
+		goto out;
 
 	pr_debug("python usage test: \"%s\"\n", cmd);
 	ret = system(cmd) ? -1 : 0;
 	free(cmd);
+out:
+	free(pythonpath);
 	return ret;
 }
