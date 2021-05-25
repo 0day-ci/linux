75544853d7fd6e3edeeff090df5795ff9c2bfaa7
Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
2021-05-24 22:47:51 -0700

    drm/i915/pxp: allocate a vcs context for pxp usage
    The context is required to send the session termination commands to the
    VCS, which will be implemented in a follow-up patch. We can also use the
    presence of the context as a check of pxp initialization completion.
    
    v2: use perma-pinned context (Chris)
    v3: rename pinned_context functions (Chris)
    v4: split export of pinned_context functions to a separate patch (Rodrigo)
    
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>

---
+++ drivers/gpu/drm/i915/pxp/intel_pxp_types.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright(c) 2020, Intel Corporation. All rights reserved.
+ */
+
+#ifndef __INTEL_PXP_TYPES_H__
+#define __INTEL_PXP_TYPES_H__
+
+struct intel_context;
+
+struct intel_pxp {
+	struct intel_context *ce;
+};
+
+#endif /* __INTEL_PXP_TYPES_H__ */
