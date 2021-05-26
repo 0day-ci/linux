9db78890da8cae5d043e1ad94fc074645a8bb7c6
Yang Shi <shy828301@gmail.com>
2021-05-26 13:12:39 -0700

    mm: rmap: make try_to_unmap() void function
    Currently try_to_unmap() return bool value by checking page_mapcount(),
    however this may return false positive since page_mapcount() doesn't
    check all subpages of compound page.  The total_mapcount() could be used
    instead, but its cost is higher since it traverses all subpages.
    
    Actually the most callers of try_to_unmap() don't care about the
    return value at all.  So just need check if page is still mapped by
    page_mapped() when necessary.  And page_mapped() does bail out early
    when it finds mapped subpage.
    
    Suggested-by: Hugh Dickins <hughd@google.com>
    Acked-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Yang Shi <shy828301@gmail.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -1749,9 +1749,10 @@ static int page_not_mapped(struct page *page)
  * Tries to remove all the page table entries which are mapping this
  * page, used in the pageout path.  Caller must hold the page lock.
  *
- * If unmap is successful, return true. Otherwise, false.
+ * It is the callers' responsibility to check if the page is still
+ * mapped when needed.
  */
-bool try_to_unmap(struct page *page, enum ttu_flags flags)
+void try_to_unmap(struct page *page, enum ttu_flags flags)
 {
 	struct rmap_walk_control rwc = {
 		.rmap_one = try_to_unmap_one,
@@ -1776,8 +1777,6 @@ bool try_to_unmap(struct page *page, enum ttu_flags flags)
 		rmap_walk_locked(page, &rwc);
 	else
 		rmap_walk(page, &rwc);
-
-	return !page_mapcount(page) ? true : false;
 }
 
 /**
