38ae7027bc26c3975d0c876572e4e3d1159cc135
Zhen Lei <thunder.leizhen@huawei.com>
2021-05-28 11:26:37 +0800

    EDAC, mpc85xx: Fix error return code in two functions
    Fix to return -EFAULT from the error handling case instead of 0, as done
    elsewhere in its function.
    
    Fixes: a9a753d53204 ("drivers-edac: add freescale mpc85xx driver")
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>

--- drivers/edac/mpc85xx_edac.c
+++ drivers/edac/mpc85xx_edac.c
@@ -248,6 +248,7 @@ static int mpc85xx_pci_err_probe(struct platform_device *op)
 
 	if (edac_pci_add_device(pci, pdata->edac_idx) > 0) {
 		edac_dbg(3, "failed edac_pci_add_device()\n");
+		res = -EFAULT;
 		goto err;
 	}
 
@@ -552,6 +553,7 @@ static int mpc85xx_l2_err_probe(struct platform_device *op)
 
 	if (edac_device_add_device(edac_dev) > 0) {
 		edac_dbg(3, "failed edac_device_add_device()\n");
+		res = -EFAULT;
 		goto err;
 	}
 
