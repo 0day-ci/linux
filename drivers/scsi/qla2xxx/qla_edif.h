c55df9e2c3920c4c62e505203b730b977b2d9116
Quinn Tran <qutran@marvell.com>
2021-05-31 00:05:38 -0700

    qla2xxx: Add send, receive and accept for auth_els
    Latest FC adapter from Marvell has the ability to encrypt
    data in flight (EDIF) feature. This feature require an
    application (ex: ipsec, etc) to act as an authenticator.
    
    This patch add ability for authentication application to
    send and retrieve message(s) as part of the authentication
    process via existing FC_BSG_HST_ELS_NOLOGIN BSG interface
    
    To send a message, application is expected to format the
    data in the AUTH ELS format. Refer to FC-SP2 for details.
    
    If a message was received, application is required to reply with
    either a LS_ACC or LS_RJT complete the exchange using the same
    interface. Otherwise, remote device will treat it as a
    timeout.
    
    Signed-off-by: Larry Wisneski <Larry.Wisneski@marvell.com>
    Signed-off-by: Duane Grigsby <duane.grigsby@marvell.com>
    Signed-off-by: Rick Hicksted Jr <rhicksted@marvell.com>
    Signed-off-by: Quinn Tran <qutran@marvell.com>
    Signed-off-by: Nilesh Javali <njavali@marvell.com>

--- drivers/scsi/qla2xxx/qla_edif.h
+++ drivers/scsi/qla2xxx/qla_edif.h
@@ -29,4 +29,42 @@ struct edif_dbell {
 	struct	completion	dbell;		/* doorbell ring */
 };
 
+#define        MAX_PAYLOAD     1024
+#define        PUR_GET         1
+
+#define	LSTATE_OFF	1	// node not on list
+#define	LSTATE_ON	2	// node on list
+#define	LSTATE_DEST	3	// node destoyed
+
+struct dinfo {
+	int		nodecnt;	// create seq count
+	int		lstate;		// node's list state
+};
+
+struct pur_ninfo {
+	unsigned int	pur_pend:1;
+	port_id_t       pur_sid;
+	port_id_t	pur_did;
+	uint8_t		vp_idx;
+	short           pur_bytes_rcvd;
+	unsigned short  pur_nphdl;
+	unsigned int    pur_rx_xchg_address;
+};
+
+struct purexevent {
+	struct  pur_ninfo	pur_info;
+	unsigned char		*msgp;
+	u32			msgp_len;
+};
+
+#define	N_UNDEF		0	// node not used/defined
+#define	N_PUREX		1	// purex info
+struct enode {
+	struct list_head	list;
+	struct dinfo		dinfo;
+	uint32_t		ntype;
+	union {
+		struct purexevent	purexinfo;
+	} u;
+};
 #endif	/* __QLA_EDIF_H */
