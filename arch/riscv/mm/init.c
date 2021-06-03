32c0a3f0657bbaf51b7cce83aba35f7e78ed1dc2
Jisheng Zhang <jszhang3@mail.ustc.edu.cn>
2021-06-03 20:52:37 +0800

    riscv: mm: Remove va_kernel_xip_pa_offset symbol from !XIP case
    The va_kernel_xip_pa_offset is only accessed for XIP case, so make it
    only available for XIP.
    
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>

--- arch/riscv/mm/init.c
+++ arch/riscv/mm/init.c
@@ -194,10 +194,8 @@ EXPORT_SYMBOL(va_kernel_pa_offset);
 #endif
 #ifdef CONFIG_XIP_KERNEL
 #define va_kernel_pa_offset    (*((unsigned long *)XIP_FIXUP(&va_kernel_pa_offset)))
-#endif
 unsigned long va_kernel_xip_pa_offset __ro_after_init;
 EXPORT_SYMBOL(va_kernel_xip_pa_offset);
-#ifdef CONFIG_XIP_KERNEL
 #define va_kernel_xip_pa_offset        (*((unsigned long *)XIP_FIXUP(&va_kernel_xip_pa_offset)))
 #endif
 unsigned long pfn_base __ro_after_init;
