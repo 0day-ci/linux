5fa9c2b545af067e0351498cdd52290e9552ad93
Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
2021-06-03 17:21:23 -0400

    drm/panfrost: Add AFBC_FEATURES parameter
    The value of the AFBC_FEATURES register is required by userspace to
    determine AFBC support on Bifrost. A user on our IRC channel (#panfrost)
    reported a workload that raised a fault on one system's Mali G31 but
    worked flawlessly with another system's Mali G31. We determined the
    cause to be missing AFBC support on one vendor's Mali implementation --
    it turns out AFBC is optional on Bifrost!
    
    Whether AFBC is supported or not is exposed in the AFBC_FEATURES
    register on Bifrost, which reads back as 0 on Midgard. A zero value
    indicates AFBC is fully supported, provided the architecture itself
    supports AFBC, allowing backwards-compatibility with Midgard. Bits 0 and
    15 indicate that AFBC support is absent for texturing and rendering
    respectively.
    
    The user experiencing the fault reports that AFBC_FEATURES reads back
    0x10001 on their system, confirming the architectural lack of AFBC.
    Userspace needs this this parameter to know to disable AFBC on that
    chip, and perhaps others.
    
    v2: Fix typo from copy-paste fail.
    
    v3: Bump the UABI version. This commit was cherry-picked from another
    series so chalking this up to a rebase fail.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Cc: Steven Price <steven.price@arm.com>

--- drivers/gpu/drm/panfrost/panfrost_drv.c
+++ drivers/gpu/drm/panfrost/panfrost_drv.c
@@ -63,6 +63,7 @@ static int panfrost_ioctl_get_param(struct drm_device *ddev, void *data, struct 
 		PANFROST_FEATURE(THREAD_MAX_BARRIER_SZ,
 				thread_max_barrier_sz);
 		PANFROST_FEATURE(COHERENCY_FEATURES, coherency_features);
+		PANFROST_FEATURE(AFBC_FEATURES, afbc_features);
 		PANFROST_FEATURE_ARRAY(TEXTURE_FEATURES, texture_features, 3);
 		PANFROST_FEATURE_ARRAY(JS_FEATURES, js_features, 15);
 		PANFROST_FEATURE(NR_CORE_GROUPS, nr_core_groups);
@@ -559,7 +560,7 @@ static const struct drm_driver panfrost_drm_driver = {
 	.desc			= "panfrost DRM",
 	.date			= "20180908",
 	.major			= 1,
-	.minor			= 1,
+	.minor			= 2,
 
 	.gem_create_object	= panfrost_gem_create_object,
 	.prime_handle_to_fd	= drm_gem_prime_handle_to_fd,
