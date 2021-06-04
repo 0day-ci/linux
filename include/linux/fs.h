064629d2a1c52927bb07babc8d519ccf47cf9dd0
Shiyang Ruan <ruansy.fnst@fujitsu.com>
2021-06-04 09:18:37 +0800

    fs: Introduce ->corrupted_range() for superblock
    Memory failure occurs in fsdax mode will finally be handled in
    filesystem.  We introduce this interface to find out files or metadata
    affected by the corrupted range, and try to recover the corrupted data
    if possiable.
    
    Signed-off-by: Shiyang Ruan <ruansy.fnst@fujitsu.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2176,6 +2176,8 @@ struct super_operations {
 				  struct shrink_control *);
 	long (*free_cached_objects)(struct super_block *,
 				    struct shrink_control *);
+	int (*corrupted_range)(struct super_block *sb, struct block_device *bdev,
+			       loff_t offset, size_t len, void *data);
 };
 
 /*
