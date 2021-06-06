edf0424ccb5c95176bd337d83d37d29ea7a73e14
Muchun Song <songmuchun@bytedance.com>
2021-06-06 12:17:50 +0800

    mm: slub: replace local_irq_save with local_irq_disable
    The caller of slub_cpu_dead cannot be irq disabled (because slab_mutex is
    holding during the processing), there is no need to use irq_save. Just use
    irq_disable directly.
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>

--- mm/slub.c
+++ mm/slub.c
@@ -2546,13 +2546,12 @@ static void flush_all(struct kmem_cache *s)
 static int slub_cpu_dead(unsigned int cpu)
 {
 	struct kmem_cache *s;
-	unsigned long flags;
 
 	mutex_lock(&slab_mutex);
 	list_for_each_entry(s, &slab_caches, list) {
-		local_irq_save(flags);
+		local_irq_disable();
 		__flush_cpu_slab(s, cpu);
-		local_irq_restore(flags);
+		local_irq_enable();
 	}
 	mutex_unlock(&slab_mutex);
 	return 0;
