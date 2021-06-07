8b12202f2b493ddf2ef348f9a825b754f2abe36f
Zheng Yongjun <zhengyongjun3@huawei.com>
2021-06-07 17:10:58 +0800

    ping: Check return value of function 'ping_queue_rcv_skb'
    Function 'ping_queue_rcv_skb' not always return success, which will
    also return fail. If not check the wrong return value of it, lead to function
    `ping_rcv` return success.
    
    Signed-off-by: Zheng Yongjun <zhengyongjun3@huawei.com>

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -968,10 +968,11 @@ bool ping_rcv(struct sk_buff *skb)
 		struct sk_buff *skb2 = skb_clone(skb, GFP_ATOMIC);
 
 		pr_debug("rcv on socket %p\n", sk);
-		if (skb2)
-			ping_queue_rcv_skb(sk, skb2);
+		if (skb2 && !ping_queue_rcv_skb(sk, skb2)) {
+			sock_put(sk);
+			return true;
+		}
 		sock_put(sk);
-		return true;
 	}
 	pr_debug("no socket, dropping\n");
 
