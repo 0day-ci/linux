22a939d2319ba1dabe0ca0d0693be008187cadde
Zheng Yongjun <zhengyongjun3@huawei.com>
2021-06-07 17:11:21 +0800

    xfrm: use BUG_ON instead of if condition followed by BUG
    Use BUG_ON instead of if condition followed by BUG.
    
    This issue was detected with the help of Coccinelle.
    
    Signed-off-by: Zheng Yongjun <zhengyongjun3@huawei.com>

--- net/xfrm/xfrm_policy.c
+++ net/xfrm/xfrm_policy.c
@@ -421,8 +421,7 @@ void xfrm_policy_destroy(struct xfrm_policy *policy)
 {
 	BUG_ON(!policy->walk.dead);
 
-	if (del_timer(&policy->timer) || del_timer(&policy->polq.hold_timer))
-		BUG();
+	BUG_ON(del_timer(&policy->timer) || del_timer(&policy->polq.hold_timer));
 
 	call_rcu(&policy->rcu, xfrm_policy_destroy_rcu);
 }
