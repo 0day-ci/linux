2686dfd92a8f93e56ceff0eae38742213c62a18c
Anshuman Khandual <anshuman.khandual@arm.com>
2021-06-08 11:02:07 +0530

    mm/thp: Define default pmd_pgtable()
    Currently most platforms define pmd_pgtable() as pmd_page() duplicating the
    same code all over. Instead just define a default value i.e pmd_page() for
    pmd_pgtable() and let platforms override when required via <asm/pgtable.h>.
    All the existing platform that override pmd_pgtable() have been moved into
    their respective <asm/pgtable.h> header in order to precede before the new
    generic definition. This makes it much cleaner with reduced code.
    
    Cc: Nick Hu <nickhu@andestech.com>
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: Vineet Gupta <vgupta@synopsys.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Guo Ren <guoren@kernel.org>
    Cc: Brian Cain <bcain@codeaurora.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Michal Simek <monstr@monstr.eu>
    Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Cc: Ley Foon Tan <ley.foon.tan@intel.com>
    Cc: Jonas Bonn <jonas@southpole.se>
    Cc: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
    Cc: Stafford Horne <shorne@gmail.com>
    Cc: "James E.J. Bottomley" <James.Bottomley@HansenPartnership.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Paul Walmsley <paul.walmsley@sifive.com>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Jeff Dike <jdike@addtoit.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Chris Zankel <chris@zankel.net>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: linux-arch@vger.kernel.org
    Cc: linux-mm@kvack.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>

--- arch/m68k/include/asm/pgtable.h
+++ arch/m68k/include/asm/pgtable.h
@@ -4,3 +4,12 @@
 #else
 #include <asm/pgtable_mm.h>
 #endif
+
+
+#if defined(CONFIG_COLDFIRE)
+#define pmd_pgtable(pmd) pfn_to_virt(pmd_val(pmd) >> PAGE_SHIFT)
+#elif defined(CONFIG_SUN3)
+#define pmd_pgtable(pmd) pmd_page(pmd)
+#else
+#define pmd_pgtable(pmd) ((pgtable_t)pmd_page_vaddr(pmd))
+#endif
