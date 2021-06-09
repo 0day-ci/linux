ba459f95dbdaf2e7bc08c9e2592e714fe6c1f538
William Breathitt Gray <vilhelm.gray@gmail.com>
2021-06-09 10:31:24 +0900

    counter: Rename counter_count_function to counter_function
    The phrase "Counter Count function" is verbose and unintentionally
    implies that function is a Count extension. This patch adjusts the
    Counter subsystem code to use the more direct "Counter function" phrase
    to make the intent of this code clearer.
    
    Cc: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Cc: Patrick Havelange <patrick.havelange@essensium.com>
    Cc: Oleksij Rempel <o.rempel@pengutronix.de>
    Cc: Kamel Bouhara <kamel.bouhara@bootlin.com>
    Cc: Maxime Coquelin <mcoquelin.stm32@gmail.com>
    Cc: Alexandre Torgue <alexandre.torgue@st.com>
    Cc: David Lechner <david@lechnology.com>
    Acked-by: Syed Nayyar Waris <syednwaris@gmail.com>
    Reviewed-by: Fabrice Gasnier <fabrice.gasnier@foss.st.com>
    Signed-off-by: William Breathitt Gray <vilhelm.gray@gmail.com>

--- drivers/counter/microchip-tcb-capture.c
+++ drivers/counter/microchip-tcb-capture.c
@@ -38,8 +38,8 @@ enum mchp_tc_count_function {
 };
 
 static const enum counter_count_function mchp_tc_count_functions[] = {
-	[MCHP_TC_FUNCTION_INCREASE] = COUNTER_COUNT_FUNCTION_INCREASE,
-	[MCHP_TC_FUNCTION_QUADRATURE] = COUNTER_COUNT_FUNCTION_QUADRATURE_X4,
+	[MCHP_TC_FUNCTION_INCREASE] = COUNTER_FUNCTION_INCREASE,
+	[MCHP_TC_FUNCTION_QUADRATURE] = COUNTER_FUNCTION_QUADRATURE_X4,
 };
 
 enum mchp_tc_synapse_action {
