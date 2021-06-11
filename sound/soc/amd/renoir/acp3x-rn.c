5c5f20685a51e12c8892fe419bcc78469bb45822
Mario Limonciello <mario.limonciello@amd.com>
2021-06-11 13:51:55 -0500

    ASoC: amd: Don't show messages about deferred probing by default
    Nearly every boot with a Lenovo P14s is showing
    acp_pdm_mach acp_pdm_mach.0: snd_soc_register_card(acp) failed: -517
    
    This isn't useful to a user, especially as probing will run again.
    Downgrade the message to debug, and only show errors.
    
    CC: markpearson@lenovo.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- sound/soc/amd/renoir/acp3x-rn.c
+++ sound/soc/amd/renoir/acp3x-rn.c
@@ -54,9 +54,14 @@ static int acp_probe(struct platform_device *pdev)
 	snd_soc_card_set_drvdata(card, machine);
 	ret = devm_snd_soc_register_card(&pdev->dev, card);
 	if (ret) {
-		dev_err(&pdev->dev,
-			"snd_soc_register_card(%s) failed: %d\n",
-			acp_card.name, ret);
+		if (ret != -EPROBE_DEFER)
+			dev_err(&pdev->dev,
+				"snd_soc_register_card(%s) failed: %d\n",
+				card->name, ret);
+		else
+			dev_dbg(&pdev->dev,
+				"snd_soc_register_card(%s) probe deferred: %d\n",
+				card->name, ret);
 		return ret;
 	}
 	return 0;
