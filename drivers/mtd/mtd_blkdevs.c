50f05700e2ffcfb774ee8b1bde1e50c0b8d349c6
Zhihao Cheng <chengzhihao1@huawei.com>
2021-06-13 19:30:30 +0800

    Revert "mtd: blkdevs: fix potential deadlock + lockdep warnings"
    This reverts commit f3c63795e90f0c6238306883b6c72f14d5355721.

--- drivers/mtd/mtd_blkdevs.c
+++ drivers/mtd/mtd_blkdevs.c
@@ -209,8 +209,8 @@ static int blktrans_open(struct block_device *bdev, fmode_t mode)
 	if (!dev)
 		return -ERESTARTSYS; /* FIXME: busy loop! -arnd*/
 
-	mutex_lock(&mtd_table_mutex);
 	mutex_lock(&dev->lock);
+	mutex_lock(&mtd_table_mutex);
 
 	if (dev->open)
 		goto unlock;
@@ -234,8 +234,8 @@ static int blktrans_open(struct block_device *bdev, fmode_t mode)
 
 unlock:
 	dev->open++;
-	mutex_unlock(&dev->lock);
 	mutex_unlock(&mtd_table_mutex);
+	mutex_unlock(&dev->lock);
 	blktrans_dev_put(dev);
 	return ret;
 
@@ -245,8 +245,8 @@ error_release:
 error_put:
 	module_put(dev->tr->owner);
 	kref_put(&dev->ref, blktrans_dev_release);
-	mutex_unlock(&dev->lock);
 	mutex_unlock(&mtd_table_mutex);
+	mutex_unlock(&dev->lock);
 	blktrans_dev_put(dev);
 	return ret;
 }
@@ -258,8 +258,8 @@ static void blktrans_release(struct gendisk *disk, fmode_t mode)
 	if (!dev)
 		return;
 
-	mutex_lock(&mtd_table_mutex);
 	mutex_lock(&dev->lock);
+	mutex_lock(&mtd_table_mutex);
 
 	if (--dev->open)
 		goto unlock;
@@ -273,8 +273,8 @@ static void blktrans_release(struct gendisk *disk, fmode_t mode)
 		__put_mtd_device(dev->mtd);
 	}
 unlock:
-	mutex_unlock(&dev->lock);
 	mutex_unlock(&mtd_table_mutex);
+	mutex_unlock(&dev->lock);
 	blktrans_dev_put(dev);
 }
 
