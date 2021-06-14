8e2a81e3104220daf84acab6c3df9e449fdfaeb9
Paul Mackerras <paulus@ozlabs.org>
2021-06-15 09:05:27 +1000

    powerpc/microwatt: Disable interrupts in boot wrapper main program
    This ensures that we don't get a decrementer interrupt arriving before
    we have set up a handler for it.
    
    Signed-off-by: Paul Mackerras <paulus@ozlabs.org>

--- arch/powerpc/boot/microwatt.c
+++ arch/powerpc/boot/microwatt.c
@@ -12,6 +12,7 @@ void platform_init(unsigned long r3, unsigned long r4, unsigned long r5)
 {
 	unsigned long heapsize = 16*1024*1024 - (unsigned long)_end;
 
+	__asm__ volatile("mtmsrd %0,1" : : "r" (0));
 	simple_alloc_init(_end, heapsize, 32, 64);
 	fdt_init(_dtb_start);
 	serial_console_init();
