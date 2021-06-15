d5ed31ec21236a7e586eaf4723dc81d29d5e048f
Matt Roper <matthew.d.roper@intel.com>
2021-06-14 20:34:33 -0700

    drm/i915: Add support for explicit L3BANK steering
    Because Render Power Gating restricts us to just a single subslice as a
    valid steering target for reads of multicast registers in a SUBSLICE
    range, the default steering we setup at init may not lead to a suitable
    target for L3BANK multicast register.  In cases where it does not, use
    explicit runtime steering whenever an L3BANK multicast register is read.
    
    While we're at it, let's simplify the function a little bit and drop its
    support for gen10/CNL since no such platforms ever materialized for real
    use.  Multicast register steering is already an area that causes enough
    confusion; no need to complicate it with what's effectively dead code.
    
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/gt/intel_gt.c
+++ drivers/gpu/drm/i915/gt/intel_gt.c
@@ -83,12 +83,24 @@ void intel_gt_init_hw_early(struct intel_gt *gt, struct i915_ggtt *ggtt)
 	gt->ggtt = ggtt;
 }
 
+static const struct intel_mmio_range icl_l3bank_steering_table[] = {
+       { 0x00B100, 0x00B3FF },
+       { 0xFFFFFF, 0xFFFFFF }, /* terminating entry */
+};
+
 int intel_gt_init_mmio(struct intel_gt *gt)
 {
 	intel_gt_init_clock_frequency(gt);
 
 	intel_uc_init_mmio(&gt->uc);
 	intel_sseu_info_init(gt);
+
+	if (GRAPHICS_VER(gt->i915) >= 11) {
+		gt->steering_table[L3BANK] = icl_l3bank_steering_table;
+		gt->info.l3bank_mask =
+			intel_uncore_read(&gt->i915->uncore, GEN10_MIRROR_FUSE3) &
+			GEN10_L3BANK_MASK;
+	}
 
 	return intel_engines_init_mmio(gt);
 }
@@ -744,6 +756,12 @@ static void intel_gt_get_valid_steering(struct intel_gt *gt,
 					u8 *sliceid, u8 *subsliceid)
 {
 	switch (type) {
+	case L3BANK:
+		GEM_DEBUG_WARN_ON(!gt->info.l3bank_mask); /* should be impossible! */
+
+		*sliceid = __ffs(gt->info.l3bank_mask);
+		*subsliceid = 0;        /* unused */
+		break;
 	default:
 		MISSING_CASE(type);
 		*sliceid = 0;
