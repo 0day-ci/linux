5e99224ae02dadb1e71dccf6e4638d2a2a838efd
Peng Li <lipeng321@huawei.com>
2021-06-16 17:33:57 +0800

    net: hdlc_ppp: add required space
    Add space required after that ','.
    
    Signed-off-by: Peng Li <lipeng321@huawei.com>
    Signed-off-by: Guangbin Huang <huangguangbin2@huawei.com>

--- drivers/net/wan/hdlc_ppp.c
+++ drivers/net/wan/hdlc_ppp.c
@@ -676,7 +676,8 @@ static int ppp_ioctl(struct net_device *dev, struct ifreq *ifr)
 
 		/* no settable parameters */
 
-		result = hdlc->attach(dev, ENCODING_NRZ,PARITY_CRC16_PR1_CCITT);
+		result = hdlc->attach(dev, ENCODING_NRZ,
+				      PARITY_CRC16_PR1_CCITT);
 		if (result)
 			return result;
 
