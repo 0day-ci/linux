ed4dd70aa16c4fba8b5d813c89cd281a28601de1
Zhen Lei <thunder.leizhen@huawei.com>
2021-06-16 14:36:59 +0800

    snic: remove unnecessary oom message
    Fixes scripts/checkpatch.pl warning:
    WARNING: Possible unnecessary 'out of memory' message
    
    Remove it can help us save a bit of memory.
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>

--- drivers/scsi/snic/vnic_wq.c
+++ drivers/scsi/snic/vnic_wq.c
@@ -48,11 +48,8 @@ static int vnic_wq_alloc_bufs(struct vnic_wq *wq)
 
 	for (i = 0; i < blks; i++) {
 		wq->bufs[i] = kzalloc(VNIC_WQ_BUF_BLK_SZ, GFP_ATOMIC);
-		if (!wq->bufs[i]) {
-			pr_err("Failed to alloc wq_bufs\n");
-
+		if (!wq->bufs[i])
 			return -ENOMEM;
-		}
 	}
 
 	for (i = 0; i < blks; i++) {
