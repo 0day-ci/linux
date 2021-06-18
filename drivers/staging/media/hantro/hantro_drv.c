aaf86b0560560ece9c7652e0bdcd437e8c9f7a75
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2021-06-18 15:15:21 +0200

    media: hantro: hevc: Allow 10-bits encoded streams
    Allow to use 10-bits encoded streams but force the output
    to remain in 8-bits.
    Add a function to get chroma offset for the output since
    it may now not match with internal reference buffers chroma
    offset.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>

--- drivers/staging/media/hantro/hantro_drv.c
+++ drivers/staging/media/hantro/hantro_drv.c
@@ -263,8 +263,9 @@ static int hantro_try_ctrl(struct v4l2_ctrl *ctrl)
 		if (sps->bit_depth_luma_minus8 != sps->bit_depth_chroma_minus8)
 			/* Luma and chroma bit depth mismatch */
 			return -EINVAL;
-		if (sps->bit_depth_luma_minus8 != 0)
-			/* Only 8-bit is supported */
+		if (sps->bit_depth_luma_minus8 != 0 &&
+		    sps->bit_depth_luma_minus8 != 2)
+			/* Only 8-bit or 10-bit is supported */
 			return -EINVAL;
 		if (sps->flags & V4L2_HEVC_SPS_FLAG_SCALING_LIST_ENABLED)
 			/* No scaling support */
