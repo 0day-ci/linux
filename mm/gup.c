915e0062bc1613b0a68e699fbf78bbe258c9f6af
Yang Shi <yang.shi@linux.alibaba.com>
2021-06-18 14:15:31 +0800

    mm/migrate: make migrate_pages() return nr_succeeded
    The migrate_pages() returns the number of pages that were not migrated,
    or an error code.  When returning an error code, there is no way to know
    how many pages were migrated or not migrated.
    
    In the following patch, migrate_pages() is used to demote pages to PMEM
    node, we need account how many pages are reclaimed (demoted) since page
    reclaim behavior depends on this.  Add *nr_succeeded parameter to make
    migrate_pages() return how many pages are demoted successfully for all
    cases.
    
    Signed-off-by: Yang Shi <yang.shi@linux.alibaba.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: Yang Shi <shy828301@gmail.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: osalvador <osalvador@suse.de>
    
    --
    
    Note: Yang Shi originally wrote the patch, thus the SoB.  There was
    also a Reviewed-by provided since there were some modifications made
    to this after the original work.
    
    Changes since 20210302:
     * Fix definition of CONFIG_MIGRATION=n stub migrate_pages().  Its
       parameters were wrong, but oddly enough did not generate any
       compile errors.
    
    Changes since 20200122:
     * Fix migrate_pages() to manipulate nr_succeeded *value*
       rather than the pointer.

--- mm/gup.c
+++ mm/gup.c
@@ -1615,6 +1615,7 @@ static long check_and_migrate_movable_pages(unsigned long nr_pages,
 	unsigned long i;
 	unsigned long isolation_error_count = 0;
 	bool drain_allow = true;
+	unsigned int nr_succeeded = 0;
 	LIST_HEAD(movable_page_list);
 	long ret = 0;
 	struct page *prev_head = NULL;
@@ -1672,7 +1673,7 @@ static long check_and_migrate_movable_pages(unsigned long nr_pages,
 	if (!list_empty(&movable_page_list)) {
 		ret = migrate_pages(&movable_page_list, alloc_migration_target,
 				    NULL, (unsigned long)&mtc, MIGRATE_SYNC,
-				    MR_LONGTERM_PIN);
+				    MR_LONGTERM_PIN, &nr_succeeded);
 		if (ret && !list_empty(&movable_page_list))
 			putback_movable_pages(&movable_page_list);
 	}
