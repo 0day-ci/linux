e8a2fe2fe842475dfaac88054c903a445cb415b8
Boris Brezillon <boris.brezillon@collabora.com>
2021-06-21 15:39:03 +0200

    drm/panfrost: Do the exception -> string translation using a table
    Do the exception -> string translation using a table so we can add extra
    fields if we need to. While at it add an error field to ease the
    exception -> error conversion which we'll need if we want to set the
    fence error to something that reflects the exception code.
    
    TODO: fix the error codes.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/gpu/drm/panfrost/panfrost_device.h
+++ drivers/gpu/drm/panfrost/panfrost_device.h
@@ -173,6 +173,7 @@ int panfrost_device_resume(struct device *dev);
 int panfrost_device_suspend(struct device *dev);
 
 const char *panfrost_exception_name(u32 exception_code);
+int panfrost_exception_to_error(u32 exception_code);
 
 static inline void
 panfrost_device_schedule_reset(struct panfrost_device *pfdev)
