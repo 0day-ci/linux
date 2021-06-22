f42dca4a2eda287ab255e148f568d6c290bc92d1
Iskren Chernev <iskren.chernev@gmail.com>
2021-06-22 23:05:07 +0300

    dt-bindings: clk: qcom: gcc-sm6115: Document SM6115 GCC
    Add device tree bindings for global clock controller on SM6115 and
    SM4250 SoCs (pin and software compatible).
    
    Signed-off-by: Iskren Chernev <iskren.chernev@gmail.com>

---
+++ include/dt-bindings/clock/qcom,gcc-sm4250.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_GCC_SM4250_H
+#define _DT_BINDINGS_CLK_QCOM_GCC_SM4250_H
+
+#include <dt-bindings/clock/qcom,gcc-sm6115.h>
+
+#endif
