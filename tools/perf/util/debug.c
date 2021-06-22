5d29b6ad86e769e13c19ee208e944dd828867c2f
Ian Rogers <irogers@google.com>
2021-06-21 21:38:11 -0700

    libtraceevent: Increase libtraceevent logging when verbose
    libtraceevent has added more levels of debug printout and with changes
    like:
    https://lore.kernel.org/linux-trace-devel/20210507095022.1079364-3-tz.stoyanov@gmail.com
    previously generated output like "registering plugin" is no longer
    displayed. This change makes it so that if perf's verbose debug output
    is enabled then the debug and info libtraceevent messages can be
    displayed.
    The code is conditionally enabled based on the libtraceevent version as
    discussed in the RFC:
    https://lore.kernel.org/lkml/20210610060643.595673-1-irogers@google.com/
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/util/debug.c
+++ tools/perf/util/debug.c
@@ -24,6 +24,9 @@
 #include "util/parse-sublevel-options.h"
 
 #include <linux/ctype.h>
+#include <traceevent/event-parse.h>
+
+#define MAKE_LIBTRACEEVENT_VERSION(a, b, c) ((a)*255*255+(b)*255+(c))
 
 int verbose;
 int debug_peo_args;
@@ -227,6 +230,15 @@ int perf_debug_option(const char *str)
 
 	/* Allow only verbose value in range (0, 10), otherwise set 0. */
 	verbose = (verbose < 0) || (verbose > 10) ? 0 : verbose;
+
+#if MAKE_LIBTRACEEVENT_VERSION(1, 3, 0) <= LIBTRACEEVENT_VERSION
+	if (verbose == 1)
+		tep_set_loglevel(TEP_LOG_INFO);
+	else if (verbose == 2)
+		tep_set_loglevel(TEP_LOG_DEBUG);
+	else if (verbose >= 3)
+		tep_set_loglevel(TEP_LOG_ALL);
+#endif
 
 	return 0;
 }
