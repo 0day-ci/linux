04016219ce8b02fe24a10323e87e5c259647e83c
Bhaumik Bhatt <bbhatt@codeaurora.org>
2021-06-21 19:34:52 -0700

    bus: mhi: core: Disable pre-emption for events tasklet processing
    With spin_lock_bh(), it is possible that a tasklet processing data
    events gets pre-empted by RT tasks or a storm of threaded interrupt
    handlers ready to run on that core. While the tasklet gets
    scheduled out, it can hold the event ring spinlock and block
    another tasklet, for example, one processing an M0 state change
    event, from acquiring it. This can starve the core which blocks in
    an attempt to acquire the spinlock until it gets released. Fix the
    issue by disabling pre-emption on the core processing data events
    and allow the tasklet to run to completion such that other tasklets
    do not block on it for long periods.
    
    Fixes: 1d3173a3bae7 ("bus: mhi: core: Add support for processing events from client device")
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>

--- drivers/bus/mhi/core/main.c
+++ drivers/bus/mhi/core/main.c
@@ -1026,11 +1026,12 @@ void mhi_ev_task(unsigned long data)
 {
 	struct mhi_event *mhi_event = (struct mhi_event *)data;
 	struct mhi_controller *mhi_cntrl = mhi_event->mhi_cntrl;
+	unsigned long flags;
 
 	/* process all pending events */
-	spin_lock_bh(&mhi_event->lock);
+	spin_lock_irqsave(&mhi_event->lock, flags);
 	mhi_event->process_event(mhi_cntrl, mhi_event, U32_MAX);
-	spin_unlock_bh(&mhi_event->lock);
+	spin_unlock_irqrestore(&mhi_event->lock, flags);
 }
 
 void mhi_ctrl_ev_task(unsigned long data)
