1e8d472de420e02cb5ef9a7d2f58309c9d6c37b0
Sreekanth Reddy <sreekanth.reddy@broadcom.com>
2021-06-23 12:51:53 +0530

    mpi3mr: Fix W=1 compilation warnings
    Fix for below W=1 compilation warning,
    'strncpy' output may be truncated copying 16 bytes
     from a string of length 64
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Sreekanth Reddy <sreekanth.reddy@broadcom.com>

--- drivers/scsi/mpi3mr/mpi3mr_fw.c
+++ drivers/scsi/mpi3mr/mpi3mr_fw.c
@@ -2608,10 +2608,8 @@ static int mpi3mr_issue_iocinit(struct mpi3mr_ioc *mrioc)
 	}
 	drv_info->information_length = cpu_to_le32(data_len);
 	strncpy(drv_info->driver_signature, "Broadcom", sizeof(drv_info->driver_signature));
-	strncpy(drv_info->os_name, utsname()->sysname, sizeof(drv_info->os_name));
-	drv_info->os_name[sizeof(drv_info->os_name) - 1] = 0;
-	strncpy(drv_info->os_version, utsname()->release, sizeof(drv_info->os_version));
-	drv_info->os_version[sizeof(drv_info->os_version) - 1] = 0;
+	memcpy(drv_info->os_name, utsname()->sysname, sizeof(drv_info->os_name) - 1);
+	memcpy(drv_info->os_version, utsname()->release, sizeof(drv_info->os_version) - 1);
 	strncpy(drv_info->driver_name, MPI3MR_DRIVER_NAME, sizeof(drv_info->driver_name));
 	strncpy(drv_info->driver_version, MPI3MR_DRIVER_VERSION, sizeof(drv_info->driver_version));
 	strncpy(drv_info->driver_release_date, MPI3MR_DRIVER_RELDATE, sizeof(drv_info->driver_release_date));
