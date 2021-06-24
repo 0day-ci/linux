7c688ebefd7826e5576259155d32b99f645aafca
Rajan Vaja <rajan.vaja@xilinx.com>
2021-06-24 05:16:30 -0700

    clk: zynqmp: Use firmware specific common clock flags
    Currently firmware passes CCF specific flags to ZynqMP clock driver.
    So firmware needs to be updated if CCF flags are changed. The firmware
    should have its own 'flag number space' that is distinct from the
    common clk framework's 'flag number space'. So define and use ZynqMP
    specific common clock flags instead of using CCF flags.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>

--- drivers/clk/zynqmp/clk-zynqmp.h
+++ drivers/clk/zynqmp/clk-zynqmp.h
@@ -10,6 +10,28 @@
 
 #include <linux/firmware/xlnx-zynqmp.h>
 
+/* Common Flags */
+/* must be gated across rate change */
+#define ZYNQMP_CLK_SET_RATE_GATE	BIT(0)
+/* must be gated across re-parent */
+#define ZYNQMP_CLK_SET_PARENT_GATE	BIT(1)
+/* propagate rate change up one level */
+#define ZYNQMP_CLK_SET_RATE_PARENT	BIT(2)
+/* do not gate even if unused */
+#define ZYNQMP_CLK_IGNORE_UNUSED	BIT(3)
+/* do not use the cached clk rate */
+#define ZYNQMP_CLK_GET_RATE_NOCACHE	BIT(6)
+/* don't re-parent on rate change */
+#define ZYNQMP_CLK_SET_RATE_NO_REPARENT	BIT(7)
+/* do not use the cached clk accuracy */
+#define ZYNQMP_CLK_GET_ACCURACY_NOCACHE	BIT(8)
+/* recalc rates after notifications */
+#define ZYNQMP_CLK_RECALC_NEW_RATES	BIT(9)
+/* clock needs to run to set rate */
+#define ZYNQMP_CLK_SET_RATE_UNGATE	BIT(10)
+/* do not gate, ever */
+#define ZYNQMP_CLK_IS_CRITICAL		BIT(11)
+
 enum topology_type {
 	TYPE_INVALID,
 	TYPE_MUX,
@@ -32,6 +54,8 @@ struct clock_topology {
 	u32 type_flag;
 	u8 custom_type_flag;
 };
+
+unsigned long zynqmp_clk_map_common_ccf_flags(const u32 zynqmp_flag);
 
 struct clk_hw *zynqmp_clk_register_pll(const char *name, u32 clk_id,
 				       const char * const *parents,
