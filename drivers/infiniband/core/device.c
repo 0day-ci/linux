0f7130c9ab4c4b2e6e1cc1cc67848dcb3d291370
Anand Khoje <anand.a.khoje@oracle.com>
2021-06-27 12:17:53 +0530

    IB/core: Read subnet_prefix in ib_query_port via cache.
    ib_query_port() calls device->ops.query_port() to get the port
    attributes. The method of querying is device driver specific.
    The same function calls device->ops.query_gid() to get the GID and
    extract the subnet_prefix (gid_prefix).
    
    The GID and subnet_prefix are stored in a cache. But they do not get
    read from the cache if the device is an Infiniband device. The
    following change takes advantage of the cached subnet_prefix.
    Testing with RDBMS has shown a significant improvement in performance
    with this change.
    
    Fixes: fad61ad ("IB/core: Add subnet prefix to port info")
    Signed-off-by: Anand Khoje <anand.a.khoje@oracle.com>
    Signed-off-by: Haakon Bugge <haakon.bugge@oracle.com>

--- drivers/infiniband/core/device.c
+++ drivers/infiniband/core/device.c
@@ -2050,7 +2050,6 @@ static int __ib_query_port(struct ib_device *device,
 			   u32 port_num,
 			   struct ib_port_attr *port_attr)
 {
-	union ib_gid gid = {};
 	int err;
 
 	memset(port_attr, 0, sizeof(*port_attr));
@@ -2063,11 +2062,9 @@ static int __ib_query_port(struct ib_device *device,
 	    IB_LINK_LAYER_INFINIBAND)
 		return 0;
 
-	err = device->ops.query_gid(device, port_num, 0, &gid);
-	if (err)
-		return err;
+	ib_get_cached_subnet_prefix(device, port_num,
+				    &port_attr->subnet_prefix);
 
-	port_attr->subnet_prefix = be64_to_cpu(gid.global.subnet_prefix);
 	return 0;
 }
 
