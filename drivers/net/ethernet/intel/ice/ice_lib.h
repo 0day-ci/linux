254108bf1dbc0a71e9add2baac9b0f2da62fe6c9
Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
2021-06-28 08:27:30 -0700

    ice: manage VSI antispoof and destination override
    Implement functions to make setting VSI security config easier.
    Main function ice_update_security fills security section field and
    checks against error in updating VSI. Rest functions are responsible
    for correct filling config according to user esceptations.
    
    This helper is needed because destination override is located in
    this section. Driver has to set this bit to allow sttering Tx packet
    on VSI based on value in Tx descriptors.
    
    Signed-off-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>

--- drivers/net/ethernet/intel/ice/ice_lib.h
+++ drivers/net/ethernet/intel/ice/ice_lib.h
@@ -116,4 +116,15 @@ bool ice_is_vsi_dflt_vsi(struct ice_sw *sw, struct ice_vsi *vsi);
 int ice_set_dflt_vsi(struct ice_sw *sw, struct ice_vsi *vsi);
 
 int ice_clear_dflt_vsi(struct ice_sw *sw);
+
+int ice_vsi_update_security(struct ice_vsi *vsi,
+			    void (*fill)(struct ice_vsi_ctx *));
+
+void ice_vsi_ctx_set_antispoof(struct ice_vsi_ctx *ctx);
+
+void ice_vsi_ctx_clear_antispoof(struct ice_vsi_ctx *ctx);
+
+void ice_vsi_ctx_set_allow_override(struct ice_vsi_ctx *ctx);
+
+void ice_vsi_ctx_clear_allow_override(struct ice_vsi_ctx *ctx);
 #endif /* !_ICE_LIB_H_ */
