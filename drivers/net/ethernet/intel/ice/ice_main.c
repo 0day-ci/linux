92c664980827c4e02ffefb381208ff8453127c3f
Grzegorz Nitka <grzegorz.nitka@intel.com>
2021-06-28 08:34:52 -0700

    ice: enable/disable switchdev when managing VFs
    Only way to enable switchdev is to create VFs when the eswitch
    mode is set to switchdev. Check if correct mode is set and
    enable switchdev in function which creating VFs.
    
    Disable switchdev when user change number of VFs to 0. Changing
    eswitch mode back to legacy when VFs are created in switchdev
    mode isn't allowed.
    
    As switchdev takes care of managing filter rules, adding new
    rules on VF is blocked.
    
    In case of resetting VF driver has to update pointer in ice_repr
    struct, because after reset VSI related things can change.
    
    Co-developed-by: Wojciech Drewek <wojciech.drewek@intel.com>
    Signed-off-by: Wojciech Drewek <wojciech.drewek@intel.com>
    Signed-off-by: Grzegorz Nitka <grzegorz.nitka@intel.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -6079,7 +6079,8 @@ int ice_vsi_setup_tx_rings(struct ice_vsi *vsi)
 		if (!ring)
 			return -EINVAL;
 
-		ring->netdev = vsi->netdev;
+		if (vsi->netdev)
+			ring->netdev = vsi->netdev;
 		err = ice_setup_tx_ring(ring);
 		if (err)
 			break;
@@ -6110,7 +6111,8 @@ int ice_vsi_setup_rx_rings(struct ice_vsi *vsi)
 		if (!ring)
 			return -EINVAL;
 
-		ring->netdev = vsi->netdev;
+		if (vsi->netdev)
+			ring->netdev = vsi->netdev;
 		err = ice_setup_rx_ring(ring);
 		if (err)
 			break;
