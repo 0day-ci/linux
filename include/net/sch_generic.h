d5a983b9bb5ac90ab786450ea34632e417521aac
Vadym Kochan <vkochan@marvell.com>
2021-07-02 21:29:14 +0300

    net: sched: introduce __tc_classid_to_hwtc() helper
    There might be a case when the ingress HW queues are globally shared in
    ASIC and are not per port (netdev). So add a __tc_classid_to_hwtc()
    version which accepts number of tc instead of netdev.
    
    Signed-off-by: Vadym Kochan <vkochan@marvell.com>

--- include/net/sch_generic.h
+++ include/net/sch_generic.h
@@ -668,11 +668,16 @@ qdisc_class_find(const struct Qdisc_class_hash *hash, u32 id)
 	return NULL;
 }
 
-static inline int tc_classid_to_hwtc(struct net_device *dev, u32 classid)
+static inline int __tc_classid_to_hwtc(u32 tc_num, u32 classid)
 {
 	u32 hwtc = TC_H_MIN(classid) - TC_H_MIN_PRIORITY;
 
-	return (hwtc < netdev_get_num_tc(dev)) ? hwtc : -EINVAL;
+	return (hwtc < tc_num) ? hwtc : -EINVAL;
+}
+
+static inline int tc_classid_to_hwtc(struct net_device *dev, u32 classid)
+{
+	return __tc_classid_to_hwtc(netdev_get_num_tc(dev), classid);
 }
 
 int qdisc_class_hash_init(struct Qdisc_class_hash *);
