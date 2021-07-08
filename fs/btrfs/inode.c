58e87c2f92b6dc840a5a0b421a56943bc8a851a1
Naohiro Aota <naohiro.aota@wdc.com>
2021-07-08 22:10:57 +0900

    btrfs: drop unnecessary ASSERT from btrfs_submit_direct()
    When on SINGLE block group, btrfs_get_io_geometry() will return "the
    size of the block group - the offset of the logical address within the
    block group" as geom.len. Since we allow up to 8 GB zone size on zoned
    btrfs, we can have up to 8 GB block group, so can have up to 8 GB
    geom.len. With this setup, we easily hit the "ASSERT(geom.len <=
    INT_MAX);".
    
    The ASSERT looks like to guard btrfs_bio_clone_partial() and bio_trim()
    which both take "int" (now "unsigned int" with the previous patch). So to
    be precise the ASSERT should check if clone_len <= UINT_MAX. But
    actually, clone_len is already capped by bio.bi_iter.bi_size which is
    unsigned int. So the ASSERT is not necessary.
    
    Drop the ASSERT and properly compare submit_len and geom.len in u64. Then,
    let the implicit casting to convert it to unsigned int.
    
    Signed-off-by: Naohiro Aota <naohiro.aota@wdc.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -8117,8 +8117,8 @@ static blk_qc_t btrfs_submit_direct(struct inode *inode, struct iomap *iomap,
 	u64 start_sector;
 	int async_submit = 0;
 	u64 submit_len;
-	int clone_offset = 0;
-	int clone_len;
+	unsigned int clone_offset = 0;
+	unsigned int clone_len;
 	u64 logical;
 	int ret;
 	blk_status_t status;
@@ -8166,9 +8166,13 @@ static blk_qc_t btrfs_submit_direct(struct inode *inode, struct iomap *iomap,
 			status = errno_to_blk_status(ret);
 			goto out_err_em;
 		}
-		ASSERT(geom.len <= INT_MAX);
 
-		clone_len = min_t(int, submit_len, geom.len);
+		/*
+		 * min()'s result is always capped by bio.bi_iter.bi_size
+		 * which is unsigned int. So the implicit casting it to
+		 * unsigned int is safe.
+		 */
+		clone_len = min(submit_len, geom.len);
 
 		/*
 		 * This will never fail as it's passing GPF_NOFS and
