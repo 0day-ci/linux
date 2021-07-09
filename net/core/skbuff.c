733afe6a87bd8c0cca1f15728c014efd5b019b2d
Ilias Apalodimas <ilias.apalodimas@linaro.org>
2021-07-09 09:29:39 +0300

    skbuff: Fix a potential race while recycling page_pool packets
    As Alexander points out, when we are trying to recycle a cloned/expanded
    SKB we might trigger a race.  The recycling code relies on the
    pp_recycle bit to trigger,  which we carry over to cloned SKBs.
    If that cloned SKB gets expanded or if we get references to the frags,
    call skbb_release_data() and overwrite skb->head, we are creating separate
    instances accessing the same page frags.  Since the skb_release_data()
    will first try to recycle the frags,  there's a potential race between
    the original and cloned SKB, since both will have the pp_recycle bit set.
    
    Fix this by explicitly those SKBs not recyclable.
    The atomic_sub_return effectively limits us to a single release case,
    and when we are calling skb_release_data we are also releasing the
    option to perform the recycling, or releasing the pages from the page pool.
    
    Fixes: 6a5bcd84e886 ("page_pool: Allow drivers to hint on SKB recycling")
    Reported-by: Alexander Duyck <alexanderduyck@fb.com>
    Suggested-by: Alexander Duyck <alexanderduyck@fb.com>
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -663,7 +663,7 @@ static void skb_release_data(struct sk_buff *skb)
 	if (skb->cloned &&
 	    atomic_sub_return(skb->nohdr ? (1 << SKB_DATAREF_SHIFT) + 1 : 1,
 			      &shinfo->dataref))
-		return;
+		goto exit;
 
 	skb_zcopy_clear(skb, true);
 
@@ -674,6 +674,8 @@ static void skb_release_data(struct sk_buff *skb)
 		kfree_skb_list(shinfo->frag_list);
 
 	skb_free_head(skb);
+exit:
+	skb->pp_recycle = 0;
 }
 
 /*
