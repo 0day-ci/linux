4c0875bd442c32864f364d828f2fcced32993e22
Tong Zhang <ztong0001@gmail.com>
2021-07-09 11:09:43 -0700

    fix vmwgfx compilation error due to a missing include
    This patch fixes vmwgfx driver compilation error due to a missing include
    
    drivers/gpu/drm/vmwgfx/vmwgfx_drv.c: In function ‘vmw_vram_manager_init’:
    drivers/gpu/drm/vmwgfx/vmwgfx_drv.c:678:8: error: implicit declaration of function ‘ttm_range_man_init’ [-Werror=implicit-function-declaration]
      678 |  ret = ttm_range_man_init(&dev_priv->bdev, TTM_PL_VRAM, false,
          |        ^~~~~~~~~~~~~~~~~~
    drivers/gpu/drm/vmwgfx/vmwgfx_drv.c: In function ‘vmw_vram_manager_fini’:
    drivers/gpu/drm/vmwgfx/vmwgfx_drv.c:690:2: error: implicit declaration of function ‘ttm_range_man_fini’ [-Werror=implicit-function-declaration]
      690 |  ttm_range_man_fini(&dev_priv->bdev, TTM_PL_VRAM);
          |  ^~~~~~~~~~~~~~~~~~
    cc1: some warnings being treated as errors
    
    Signed-off-by: Tong Zhang <ztong0001@gmail.com>

--- drivers/gpu/drm/vmwgfx/vmwgfx_drv.c
+++ drivers/gpu/drm/vmwgfx/vmwgfx_drv.c
@@ -37,6 +37,7 @@
 #include <drm/drm_sysfs.h>
 #include <drm/ttm/ttm_bo_driver.h>
 #include <drm/ttm/ttm_placement.h>
+#include <drm/ttm/ttm_range_manager.h>
 #include <generated/utsrelease.h>
 
 #include "ttm_object.h"
