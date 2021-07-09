b970ab1ea0fb847865275ab0c9128b9e606e4191
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2021-07-09 16:26:00 +0200

    rcar-vin: Add r8a779a0 support
    Add support for the R-Car V3U (r8a779a0) to the driver. The V3U has the
    VIN modules connected to a ISP instead of directly to the R-Car CSI-2
    receivers. The reason being that the ISP performs channel selection
    based on CSI-2 VC/DT pairs and routes the video data to different VIN
    modules. In other SoC versions this filtering is done by the VIN modules
    themself.
    
    While the media graph is very different from other SoCs the only
    difference in operating the VIN modules is that the VC/DT filtering
    should be skipped as that is performed by the ISP.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Jacopo Mondi <jacopo+renesas@jmondi.org>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -29,7 +29,7 @@
 #define HW_BUFFER_MASK 0x7f
 
 /* Max number on VIN instances that can be in a system */
-#define RCAR_VIN_NUM 8
+#define RCAR_VIN_NUM 32
 
 struct rvin_group;
 
@@ -48,7 +48,17 @@ enum rvin_csi_id {
 	RVIN_CSI_MAX,
 };
 
-#define RVIN_REMOTES_MAX RVIN_CSI_MAX
+enum rvin_isp_id {
+	RVIN_ISP0,
+	RVIN_ISP1,
+	RVIN_ISP2,
+	RVIN_ISP4,
+	RVIN_ISP_MAX,
+};
+
+#define RVIN_REMOTES_MAX \
+	(((unsigned int)RVIN_CSI_MAX) > ((unsigned int)RVIN_ISP_MAX) ? \
+	 RVIN_CSI_MAX : RVIN_ISP_MAX)
 
 /**
  * enum rvin_dma_state - DMA states
@@ -158,6 +168,7 @@ struct rvin_group_route {
 struct rvin_info {
 	enum model_id model;
 	bool use_mc;
+	bool use_isp;
 	bool nv12;
 
 	unsigned int max_width;
