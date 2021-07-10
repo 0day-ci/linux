9687f19e94ba7dcbb4213a164d89c5aaa51b5b33
Vinay Belgaumkar <vinay.belgaumkar@intel.com>
2021-07-09 18:20:25 -0700

    drm/i915/guc/slpc: slpc selftest
    Tests that exercise the slpc get/set frequency interfaces.
    
    Clamp_max will set max frequency to multiple levels and check
    that slpc requests frequency lower than or equal to it.
    
    Clamp_min will set min frequency to different levels and check
    if slpc requests are higher or equal to those levels.
    
    Signed-off-by: Vinay Belgaumkar <vinay.belgaumkar@intel.com>

---
+++ drivers/gpu/drm/i915/gt/selftest_slpc.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2020 Intel Corporation
+ */
+
+#ifndef SELFTEST_SLPC_H
+#define SELFTEST_SLPC_H
+
+int live_slpc_clamp_max(void *arg);
+int live_slpc_clamp_min(void *arg);
+
+#endif /* SELFTEST_SLPC_H */
