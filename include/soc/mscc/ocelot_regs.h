b7993a353d5898959c14f6b2861b31b80db881db
Colin Foster <colin.foster@in-advantage.com>
2021-07-10 12:25:59 -0700

    net: mscc: ocelot: split register definitions to a separate file
    Moving these to a separate file will allow them to be shared to other
    drivers.
    
    Signed-off-by: Colin Foster <colin.foster@in-advantage.com>

---
+++ include/soc/mscc/ocelot_regs.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+/*
+ * Microsemi Ocelot Switch driver
+ *
+ * Copyright (c) 2021 Innovative Advantage Inc.
+ */
+
+#ifndef OCELOT_REGS_H
+#define OCELOT_REGS_H
+
+extern const u32 ocelot_ana_regmap[];
+extern const u32 ocelot_qs_regmap[];
+extern const u32 ocelot_qsys_regmap[];
+extern const u32 ocelot_rew_regmap[];
+extern const u32 ocelot_sys_regmap[];
+extern const u32 ocelot_vcap_regmap[];
+extern const u32 ocelot_ptp_regmap[];
+extern const u32 ocelot_dev_gmii_regmap[];
+
+#endif
+
