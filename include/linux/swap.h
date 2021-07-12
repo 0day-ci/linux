055dbf4cbf84a539f309086f9eefee3db6415d21
Matthew Wilcox (Oracle) <willy@infradead.org>
2021-07-12 20:45:51 +0100

    mm/workingset: Convert workingset_activation to take a folio
    This function already assumed it was being passed a head page.  No real
    change here, except that thp_nr_pages() compiles away on kernels with
    THP compiled out while folio_nr_pages() is always present.  Also convert
    page_memcg_rcu() to folio_memcg_rcu().
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -324,7 +324,7 @@ struct vma_swap_readahead {
 void workingset_age_nonresident(struct lruvec *lruvec, unsigned long nr_pages);
 void *workingset_eviction(struct page *page, struct mem_cgroup *target_memcg);
 void workingset_refault(struct page *page, void *shadow);
-void workingset_activation(struct page *page);
+void workingset_activation(struct folio *folio);
 
 /* Only track the nodes of mappings with shadow entries */
 void workingset_update_node(struct xa_node *node);
