aff37d278d202e48d90b419f514e1dc873a6c942
Matthew Wilcox (Oracle) <willy@infradead.org>
2021-07-12 04:05:27 +0100

    mm/memcg: Convert mem_cgroup_migrate() to take folios
    Convert all callers of mem_cgroup_migrate() to call page_folio() first.
    They all look like they're using head pages already, but this proves it.
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- mm/migrate.c
+++ mm/migrate.c
@@ -589,6 +589,8 @@ void copy_huge_page(struct page *dst, struct page *src)
  */
 void migrate_page_states(struct page *newpage, struct page *page)
 {
+	struct folio *folio = page_folio(page);
+	struct folio *newfolio = page_folio(newpage);
 	int cpupid;
 
 	if (PageError(page))
@@ -656,7 +658,7 @@ void migrate_page_states(struct page *newpage, struct page *page)
 	copy_page_owner(page, newpage);
 
 	if (!PageHuge(page))
-		mem_cgroup_migrate(page, newpage);
+		mem_cgroup_migrate(folio, newfolio);
 }
 EXPORT_SYMBOL(migrate_page_states);
 
