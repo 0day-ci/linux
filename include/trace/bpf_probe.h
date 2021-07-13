3e30eccfdb335cc1e2811d8f6f4c1fa6021bf4dd
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-07-13 11:45:39 +0300

    kernel.h: Don't pollute header with single user macros
    The COUNT_ARGS() and CONCATENATE() macros are used by a single user.
    Let move them to it.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- include/trace/bpf_probe.h
+++ include/trace/bpf_probe.h
@@ -27,6 +27,13 @@
 #undef __perf_task
 #define __perf_task(t)	(t)
 
+/* This counts to 12. Any more, it will return 13th argument. */
+#define __COUNT_ARGS(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _n, X...) _n
+#define COUNT_ARGS(X...) __COUNT_ARGS(, ##X, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+
+#define __CONCAT(a, b) a ## b
+#define CONCATENATE(a, b) __CONCAT(a, b)
+
 /* cast any integer, pointer, or small struct to u64 */
 #define UINTTYPE(size) \
 	__typeof__(__builtin_choose_expr(size == 1,  (u8)1, \
