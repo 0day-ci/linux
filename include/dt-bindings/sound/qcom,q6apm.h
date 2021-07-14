a4b46a0248591b711350ecb3b2c89cda8d3fd362
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2021-07-14 16:30:27 +0100

    ASoC: qcom: dt-bindings: add bindings Audio Processing manager
    This patch adds bindings support for Qualcomm Audio Processing Manager
    service in Audio DSP.
    
    Audio Process Manager is one of the static service in DSP which is
    responsible for Command/response handling, graph Management
    and Control/Event management between modules.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

---
+++ include/dt-bindings/sound/qcom,q6apm.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_BINDINGS_Q6_APM_H__
+#define __DT_BINDINGS_Q6_APM_H__
+
+/* Audio Process Manager (APM) virtual ports IDs */
+#include <dt-bindings/sound/qcom,q6afe.h>
+
+#endif /* __DT_BINDINGS_Q6_APM_H__ */
