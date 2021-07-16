cad57acbc61bd9e1fc12087c932ce6aa22097365
Oliver Upton <oupton@google.com>
2021-07-16 21:26:27 +0000

    KVM: arm64: Provide userspace access to the physical counter offset
    Presently, KVM provides no facilities for correctly migrating a guest
    that depends on the physical counter-timer. While most guests (barring
    NV, of course) should not depend on the physical counter-timer, an
    operator may still wish to provide a consistent view of the physical
    counter-timer across migrations.
    
    Provide userspace with a new vCPU attribute to modify the guest physical
    counter-timer offset. Since the base architecture doesn't provide a
    physical counter-timer offset register, emulate the correct behavior by
    trapping accesses to the physical counter-timer whenever the offset
    value is non-zero.
    
    Uphold the same behavior as CNTVOFF_EL2 and broadcast the physical
    offset to all vCPUs whenever written. This guarantees that the
    counter-timer we provide the guest remains architectural, wherein all
    views of the counter-timer are consistent across vCPUs. Reconfigure
    timer traps for VHE on every guest entry, as different VMs will now have
    different traps enabled. Enable physical counter traps for nVHE whenever
    the offset is nonzero (we already trap physical timer registers in
    nVHE).
    
    FEAT_ECV provides a guest physical counter-timer offset register
    (CNTPOFF_EL2), but ECV-enabled hardware is nonexistent at the time of
    writing so support for it was elided for the sake of the author :)
    
    Signed-off-by: Oliver Upton <oupton@google.com>

--- arch/arm64/kvm/hyp/nvhe/timer-sr.c
+++ arch/arm64/kvm/hyp/nvhe/timer-sr.c
@@ -9,16 +9,13 @@
 #include <linux/kvm_host.h>
 
 #include <asm/kvm_hyp.h>
+#include <hyp/timer-sr.h>
 
 void __kvm_timer_set_cntvoff(u64 cntvoff)
 {
 	write_sysreg(cntvoff, cntvoff_el2);
 }
 
-/*
- * Should only be called on non-VHE systems.
- * VHE systems use EL2 timers and configure EL1 timers in kvm_timer_init_vhe().
- */
 void __timer_disable_traps(struct kvm_vcpu *vcpu)
 {
 	u64 val;
@@ -29,20 +26,24 @@ void __timer_disable_traps(struct kvm_vcpu *vcpu)
 	write_sysreg(val, cnthctl_el2);
 }
 
-/*
- * Should only be called on non-VHE systems.
- * VHE systems use EL2 timers and configure EL1 timers in kvm_timer_init_vhe().
- */
 void __timer_enable_traps(struct kvm_vcpu *vcpu)
 {
 	u64 val;
 
 	/*
 	 * Disallow physical timer access for the guest
-	 * Physical counter access is allowed
 	 */
 	val = read_sysreg(cnthctl_el2);
 	val &= ~CNTHCTL_EL1PCEN;
-	val |= CNTHCTL_EL1PCTEN;
+
+	/*
+	 * Disallow physical counter access for the guest if offsetting is
+	 * requested.
+	 */
+	if (__timer_physical_emulation_required(vcpu))
+		val &= ~CNTHCTL_EL1PCTEN;
+	else
+		val |= CNTHCTL_EL1PCTEN;
+
 	write_sysreg(val, cnthctl_el2);
 }
