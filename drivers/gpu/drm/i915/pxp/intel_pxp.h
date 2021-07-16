5021b656c0a0358f956ee4480e0799ec1e824fb7
Huang, Sean Z <sean.z.huang@intel.com>
2021-07-15 21:10:28 -0700

    drm/i915/pxp: Implement PXP irq handler
    The HW will generate a teardown interrupt when session termination is
    required, which requires i915 to submit a terminating batch. Once the HW
    is done with the termination it will generate another interrupt, at
    which point it is safe to re-create the session.
    
    Since the termination and re-creation flow is something we want to
    trigger from the driver as well, use a common work function that can be
    called both from the irq handler and from the driver set-up flows, which
    has the addded benefit of allowing us to skip any extra locks because
    the work itself serializes the operations.
    
    v2: use struct completion instead of bool (Chris)
    v3: drop locks, clean up functions and improve comments (Chris),
        move to common work function.
    v4: improve comments, simplify wait logic (Rodrigo)
    v5: unconditionally set interrupts, rename state_attacked var (Rodrigo)
    
    Signed-off-by: Huang, Sean Z <sean.z.huang@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com> #v4

--- drivers/gpu/drm/i915/pxp/intel_pxp.h
+++ drivers/gpu/drm/i915/pxp/intel_pxp.h
@@ -30,6 +30,9 @@ void intel_pxp_fini(struct intel_pxp *pxp);
 
 void intel_pxp_init_hw(struct intel_pxp *pxp);
 void intel_pxp_fini_hw(struct intel_pxp *pxp);
+
+void intel_pxp_mark_termination_in_progress(struct intel_pxp *pxp);
+int intel_pxp_wait_for_arb_start(struct intel_pxp *pxp);
 #else
 static inline void intel_pxp_init(struct intel_pxp *pxp)
 {
@@ -37,6 +40,11 @@ static inline void intel_pxp_init(struct intel_pxp *pxp)
 
 static inline void intel_pxp_fini(struct intel_pxp *pxp)
 {
+}
+
+static inline int intel_pxp_wait_for_arb_start(struct intel_pxp *pxp)
+{
+	return 0;
 }
 #endif
 
