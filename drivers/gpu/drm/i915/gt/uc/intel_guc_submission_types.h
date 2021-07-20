af239a7c2de45e1f7ed0c1620edf561830eae5af
Matthew Brost <matthew.brost@intel.com>
2021-07-20 13:57:26 -0700

    drm/i915/guc: Introduce guc_submit_engine object
    Move fields related to controlling the GuC submission state machine to a
    unique object (guc_submit_engine) rather than the global GuC state
    (intel_guc). This encapsulation allows multiple instances of submission
    objects to operate in parallel and a single instance can block if needed
    while another can make forward progress. This is analogous to how the
    execlist mode works assigning a schedule object per physical engine but
    rather in GuC mode we assign a schedule object based on the blocking
    dependencies.
    
    The guc_submit_engine object also encapsulates the i915_sched_engine
    object as well.
    
    Lots of find-replace.
    
    Currently only 1 guc_submit_engine instantiated, future patches will
    instantiate more.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Cc: John Harrison <John.C.Harrison@Intel.com>

---
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_submission_types.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2014-2019 Intel Corporation
+ */
+
+#ifndef _INTEL_GUC_SUBMISSION_TYPES_H_
+#define _INTEL_GUC_SUBMISSION_TYPES_H_
+
+#include "gt/intel_engine_types.h"
+#include "gt/intel_context_types.h"
+#include "i915_scheduler_types.h"
+
+struct intel_guc;
+struct i915_request;
+
+/* GuC Virtual Engine */
+struct guc_virtual_engine {
+	struct intel_engine_cs base;
+	struct intel_context context;
+};
+
+/*
+ * Object which encapsulates the globally operated on i915_sched_engine +
+ * the GuC submission state machine described in intel_guc_submission.c.
+ */
+struct guc_submit_engine {
+	struct i915_sched_engine sched_engine;
+	struct work_struct retire_worker;
+	struct i915_request *stalled_rq;
+	struct intel_context *stalled_context;
+	unsigned long flags;
+	int total_num_rq_with_no_guc_id;
+	atomic_t num_guc_ids_not_ready;
+	int id;
+
+	/*
+	 * Submisson stall reason. See intel_guc_submission.c for detailed
+	 * description.
+	 */
+	enum {
+		STALL_NONE,
+		STALL_GUC_ID_WORKQUEUE,
+		STALL_GUC_ID_TASKLET,
+		STALL_SCHED_DISABLE,
+		STALL_REGISTER_CONTEXT,
+		STALL_DEREGISTER_CONTEXT,
+		STALL_MOVE_LRC_TAIL,
+		STALL_ADD_REQUEST,
+	} submission_stall_reason;
+};
+
+#endif
