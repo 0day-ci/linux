3f95c10307e8f1ea255691b88f1fc853d6042e72
Mike Kravetz <mike.kravetz@oracle.com>
2021-07-21 16:05:10 -0700

    hugetlb: prepare destroy and prep routines for vmemmap optimized pages
    When optimally demoting huge pages with vmemmap optimizations, the
    routines which destroy and prep hugetlb pages need to be modified.
    Currently, these routines expect all vmemmap pages to be present as
    they will write to all page structs for tail pages.  To optimallly
    handle demotion of huge pages not all vmemmap pages will be present.
    Only those pages required for the demoted pages will be present.
    Therefore, the destroy and prep routines must only write to struct pages
    for which vmammap pages are present.
    
    Modify destroy_compound_gigantic_page_for_demote and
    prep_compound_gigantic_page_for_demote to take vmemmap optimized pages
    into account.  Use the hugetlb specific flag HPageVmemmapOptimized to
    determine of this special processing is needed.
    
    These modifications will be used in subsequent patches where vmemmap
    optimizations for demote are fully enabled.
    
    Also modify the routine free_huge_page_vmemmap to immediately return if
    the passed page is already optimized.  With demotion, prep_new_huge_page
    can be called for vmemmap optimized pages.
    
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>

--- mm/hugetlb_vmemmap.h
+++ mm/hugetlb_vmemmap.h
@@ -10,6 +10,16 @@
 #define _LINUX_HUGETLB_VMEMMAP_H
 #include <linux/hugetlb.h>
 
+/*
+ * There are a lot of struct page structures associated with each HugeTLB page.
+ * For tail pages, the value of compound_head is the same. So we can reuse first
+ * page of tail page structures. We map the virtual addresses of the remaining
+ * pages of tail page structures to the first tail page struct, and then free
+ * these page frames. Therefore, we need to reserve two pages as vmemmap areas.
+ */
+#define RESERVE_VMEMMAP_NR		2U
+#define RESERVE_VMEMMAP_SIZE		(RESERVE_VMEMMAP_NR << PAGE_SHIFT)
+
 #ifdef CONFIG_HUGETLB_PAGE_FREE_VMEMMAP
 int alloc_huge_page_vmemmap(struct hstate *h, struct page *head);
 void free_huge_page_vmemmap(struct hstate *h, struct page *head);
