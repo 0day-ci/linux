a1dc873e0b10b7998080498d3e4abc6724f04675
Vinay Belgaumkar <vinay.belgaumkar@intel.com>
2021-07-26 12:07:47 -0700

    drm/i915/guc/slpc: Initial definitions for SLPC
    Add macros to check for SLPC support. This feature is currently supported
    for Gen12+ and enabled whenever GuC submission is enabled/selected.
    
    Include templates for SLPC init/fini and enable.
    
    v2: Move SLPC helper functions to intel_guc_slpc.c/.h. Define basic
    template for SLPC structure in intel_guc_slpc_types.h. Fix copyright (Michal W)
    
    v3: Review comments (Michal W)
    
    Signed-off-by: Vinay Belgaumkar <vinay.belgaumkar@intel.com>
    Signed-off-by: Sundaresan Sujaritha <sujaritha.sundaresan@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    
    drm/i915/guc/slpc: Lay out slpc init/enable/fini
    
    Declare init/fini and enable function templates.
    
    v2: Rebase
    
    Signed-off-by: Vinay Belgaumkar <vinay.belgaumkar@intel.com>
    Signed-off-by: Sundaresan Sujaritha <sujaritha.sundaresan@intel.com>

---
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_slpc_types.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2021 Intel Corporation
+ */
+
+#ifndef _INTEL_GUC_SLPC_TYPES_H_
+#define _INTEL_GUC_SLPC_TYPES_H_
+
+struct intel_guc_slpc {
+};
+
+#endif
