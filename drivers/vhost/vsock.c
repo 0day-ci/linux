1897d42ce769068ed6e37e6802f4e78b8786f4d7
Arseny Krasnov <arseny.krasnov@kaspersky.com>
2021-07-26 19:33:38 +0300

    vhost/vsock: support MSG_EOR bit processing
    It works in the same way as 'end-of-message' bit: if packet has
    'EOM' bit, also check for 'EOR' bit.
    
    Signed-off-by: Arseny Krasnov <arseny.krasnov@kaspersky.com>

--- drivers/vhost/vsock.c
+++ drivers/vhost/vsock.c
@@ -115,6 +115,7 @@ vhost_transport_do_send_pkt(struct vhost_vsock *vsock,
 		size_t iov_len, payload_len;
 		int head;
 		bool restore_msg_eom_flag = false;
+		bool restore_msg_eor_flag = false;
 
 		spin_lock_bh(&vsock->send_pkt_list_lock);
 		if (list_empty(&vsock->send_pkt_list)) {
@@ -188,6 +189,11 @@ vhost_transport_do_send_pkt(struct vhost_vsock *vsock,
 			if (le32_to_cpu(pkt->hdr.flags) & VIRTIO_VSOCK_SEQ_EOM) {
 				pkt->hdr.flags &= ~cpu_to_le32(VIRTIO_VSOCK_SEQ_EOM);
 				restore_msg_eom_flag = true;
+
+				if (le32_to_cpu(pkt->hdr.flags & VIRTIO_VSOCK_SEQ_EOR)) {
+					pkt->hdr.flags &= ~cpu_to_le32(VIRTIO_VSOCK_SEQ_EOR);
+					restore_msg_eor_flag = true;
+				}
 			}
 		}
 
@@ -224,8 +230,12 @@ vhost_transport_do_send_pkt(struct vhost_vsock *vsock,
 		 * to send it with the next available buffer.
 		 */
 		if (pkt->off < pkt->len) {
-			if (restore_msg_eom_flag)
+			if (restore_msg_eom_flag) {
 				pkt->hdr.flags |= cpu_to_le32(VIRTIO_VSOCK_SEQ_EOM);
+
+				if (restore_msg_eor_flag)
+					pkt->hdr.flags |= cpu_to_le32(VIRTIO_VSOCK_SEQ_EOR);
+			}
 
 			/* We are queueing the same virtio_vsock_pkt to handle
 			 * the remaining bytes, and we want to deliver it
