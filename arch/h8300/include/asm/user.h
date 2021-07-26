eff69221f99888bea02b7be2dedd9efe515b08a7
Jason Wang <wangborong@cdjrlc.com>
2021-07-26 20:14:22 +0800

    h8300: change 'long int' with 'long'
    Prefer 'long' over 'long int' as the int is unnecessary.
    
    Signed-off-by: Jason Wang <wangborong@cdjrlc.com>

--- arch/h8300/include/asm/user.h
+++ arch/h8300/include/asm/user.h
@@ -52,15 +52,15 @@ struct user {
 	struct user_regs_struct regs;	/* Where the registers are actually stored */
 /* ptrace does not yet supply these.  Someday.... */
 /* The rest of this junk is to help gdb figure out what goes where */
-	unsigned long int u_tsize;	/* Text segment size (pages). */
-	unsigned long int u_dsize;	/* Data segment size (pages). */
-	unsigned long int u_ssize;	/* Stack segment size (pages). */
+	unsigned long u_tsize;	/* Text segment size (pages). */
+	unsigned long u_dsize;	/* Data segment size (pages). */
+	unsigned long u_ssize;	/* Stack segment size (pages). */
 	unsigned long start_code;     /* Starting virtual address of text. */
 	unsigned long start_stack;	/* Starting virtual address of stack area.
 					   This is actually the bottom of the stack,
 					   the top of the stack is always found in the
 					   esp register.  */
-	long int signal;		/* Signal that caused the core dump. */
+	long signal;		/* Signal that caused the core dump. */
 	int reserved;			/* No longer used */
 	unsigned long u_ar0;		/* Used by gdb to help find the values for */
 	/* the registers. */
