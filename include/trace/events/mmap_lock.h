cc5f878f0a66fe7dd5c5bfe91e42a570707d21a2
Gang Li <ligang.bdlg@bytedance.com>
2021-07-29 17:28:53 +0800

    mm: mmap_lock: add ip to mmap_lock tracepoints
    The mmap_lock is acquired on most (all?) mmap / munmap / page fault
    operations, so a multi-threaded process which does a lot of these
    can experience significant contention. Sometimes we want to know
    where the lock is hold. And it's hard to locate without collecting ip.
    
    Here's an example: TP_printk("ip=%pS",ip)
    Log looks like this: "ip=do_user_addr_fault+0x274/0x640"
    
    We can find out who cause the contention amd make some improvements
    for it.
    
    Signed-off-by: Gang Li <ligang.bdlg@bytedance.com>

--- include/trace/events/mmap_lock.h
+++ include/trace/events/mmap_lock.h
@@ -15,35 +15,39 @@ extern void trace_mmap_lock_unreg(void);
 
 DECLARE_EVENT_CLASS(mmap_lock,
 
-	TP_PROTO(struct mm_struct *mm, const char *memcg_path, bool write),
+	TP_PROTO(struct mm_struct *mm, const char *memcg_path, bool write,
+		unsigned long ip),
 
-	TP_ARGS(mm, memcg_path, write),
+	TP_ARGS(mm, memcg_path, write, ip),
 
 	TP_STRUCT__entry(
 		__field(struct mm_struct *, mm)
 		__string(memcg_path, memcg_path)
 		__field(bool, write)
+		__field(void *, ip)
 	),
 
 	TP_fast_assign(
 		__entry->mm = mm;
 		__assign_str(memcg_path, memcg_path);
 		__entry->write = write;
+		__entry->ip = (void *)ip;
 	),
 
 	TP_printk(
-		"mm=%p memcg_path=%s write=%s",
+		"mm=%p memcg_path=%s write=%s ip=%pS",
 		__entry->mm,
 		__get_str(memcg_path),
-		__entry->write ? "true" : "false"
-   )
+		__entry->write ? "true" : "false",
+		__entry->ip
+	)
 );
 
 #define DEFINE_MMAP_LOCK_EVENT(name)                                    \
 	DEFINE_EVENT_FN(mmap_lock, name,                                \
 		TP_PROTO(struct mm_struct *mm, const char *memcg_path,  \
-			bool write),                                    \
-		TP_ARGS(mm, memcg_path, write),                         \
+			bool write, unsigned long ip),                  \
+		TP_ARGS(mm, memcg_path, write, ip),                     \
 		trace_mmap_lock_reg, trace_mmap_lock_unreg)
 
 DEFINE_MMAP_LOCK_EVENT(mmap_lock_start_locking);
@@ -52,14 +56,15 @@ DEFINE_MMAP_LOCK_EVENT(mmap_lock_released);
 TRACE_EVENT_FN(mmap_lock_acquire_returned,
 
 	TP_PROTO(struct mm_struct *mm, const char *memcg_path, bool write,
-		bool success),
+		unsigned long ip, bool success),
 
-	TP_ARGS(mm, memcg_path, write, success),
+	TP_ARGS(mm, memcg_path, write, ip, success),
 
 	TP_STRUCT__entry(
 		__field(struct mm_struct *, mm)
 		__string(memcg_path, memcg_path)
 		__field(bool, write)
+		__field(void *, ip)
 		__field(bool, success)
 	),
 
@@ -67,14 +72,16 @@ TRACE_EVENT_FN(mmap_lock_acquire_returned,
 		__entry->mm = mm;
 		__assign_str(memcg_path, memcg_path);
 		__entry->write = write;
+		__entry->ip = (void *)ip;
 		__entry->success = success;
 	),
 
 	TP_printk(
-		"mm=%p memcg_path=%s write=%s success=%s",
+		"mm=%p memcg_path=%s write=%s ip=%pS success=%s",
 		__entry->mm,
 		__get_str(memcg_path),
 		__entry->write ? "true" : "false",
+		__entry->ip,
 		__entry->success ? "true" : "false"
 	),
 
