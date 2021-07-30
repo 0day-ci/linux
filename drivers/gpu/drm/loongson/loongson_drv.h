50cdcf5c8436340c26b5af0289ec6040d941ab98
lichenyang <lichenyang@loongson.cn>
2021-07-30 17:41:48 +0800

    drm/loongson: Add interrupt driver for LS7A
    Add LS7A DC vsync interrupt enable and close function, and
    register irq_handler function interface.
    Add vbrank event processing flow.
    
    v4:
    - Replace drm_irq_install with devm_request_irq.
    - Delete the irq_ hooks in drm_driver.
    
    v3:
    - Improve code readability.
    - Use the to_pci_dev function to get pci_dev.
    
    v2:
    - Added error handling in the loongson_drm_load function.
    
    Signed-off-by: lichenyang <lichenyang@loongson.cn>

--- drivers/gpu/drm/loongson/loongson_drv.h
+++ drivers/gpu/drm/loongson/loongson_drv.h
@@ -15,9 +15,11 @@
 #include <drm/drm_gem.h>
 #include <drm/drm_gem_framebuffer_helper.h>
 #include <drm/drm_gem_vram_helper.h>
+#include <drm/drm_irq.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_plane.h>
 #include <drm/drm_plane_helper.h>
+#include <drm/drm_vblank.h>
 
 #include "loongson_i2c.h"
 
@@ -50,6 +52,7 @@
 #define FB_HSYNC_REG (0x1420)
 #define FB_VDISPLAY_REG (0x1480)
 #define FB_VSYNC_REG (0x14a0)
+#define FB_INT_REG (0x1570)
 
 #define CFG_FMT GENMASK(2, 0)
 #define CFG_FBSWITCH BIT(7)
@@ -61,6 +64,10 @@
 #define FB_PANCFG_DEF 0x80001311
 #define FB_HSYNC_PULSE (1 << 30)
 #define FB_VSYNC_PULSE (1 << 30)
+#define FB_VSYNC1_ENABLE (1 << 16)
+#define FB_VSYNC0_ENABLE (1 << 18)
+#define FB_VSYNC1_INT (1 << 0)
+#define FB_VSYNC0_INT (1 << 2)
 
 /* PIX PLL */
 #define LOOPC_MIN 24
@@ -148,6 +155,11 @@ struct loongson_plane *loongson_plane_init(struct drm_device *dev, int index);
 
 /* i2c */
 int loongson_dc_gpio_init(struct loongson_device *ldev);
+
+/* irq */
+int loongson_irq_init(struct loongson_device *ldev);
+int loongson_crtc_enable_vblank(struct drm_crtc *crtc);
+void loongson_crtc_disable_vblank(struct drm_crtc *crtc);
 
 /* device */
 u32 loongson_gpu_offset(struct drm_plane_state *state,
