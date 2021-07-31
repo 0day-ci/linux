adbfbf05e3231e295f6374316df1f23a6508e09b
DENG Qingfang <dqfext@gmail.com>
2021-08-01 03:10:20 +0800

    net: dsa: mt7530: use independent VLAN learning on VLAN-unaware bridges
    Consider the following bridge configuration, where bond0 is not
    offloaded:
    
             +-- br0 --+
            / /   |     \
           / /    |      \
          /  |    |     bond0
         /   |    |     /   \
       swp0 swp1 swp2 swp3 swp4
         .        .       .
         .        .       .
         A        B       C
    
    Ideally, when the switch receives a packet from swp3 or swp4, it should
    forward the packet to the CPU, according to the port matrix and unknown
    unicast flood settings.
    
    But packet loss will happen if the destination address is at one of the
    offloaded ports (swp0~2). For example, when client C sends a packet to
    A, the FDB lookup will indicate that it should be forwarded to swp0, but
    the port matrix of swp3 and swp4 is configured to only allow the CPU to
    be its destination, so it is dropped.
    
    However, this issue does not happen if the bridge is VLAN-aware. That is
    because VLAN-aware bridges use independent VLAN learning, i.e. use VID
    for FDB lookup, on offloaded ports. As swp3 and swp4 are not offloaded,
    shared VLAN learning with default filter ID of 0 is used instead. So the
    lookup for A with filter ID 0 never hits and the packet can be forwarded
    to the CPU.
    
    In the current code, only two combinations were used to toggle user
    ports' VLAN awareness: one is PCR.PORT_VLAN set to port matrix mode with
    PVC.VLAN_ATTR set to transparent port, the other is PCR.PORT_VLAN set to
    security mode with PVC.VLAN_ATTR set to user port.
    
    It turns out that only PVC.VLAN_ATTR contributes to VLAN awareness, and
    port matrix mode just skips the VLAN table lookup. The reference manual
    is somehow misleading when describing PORT_VLAN modes. It states that
    PORT_MEM (VLAN port member) is used for destination if the VLAN table
    lookup hits, but actually **PORT_MEM & PORT_MATRIX** (bitwise AND of
    VLAN port member and port matrix) is used instead, which means we can
    have two or more separate VLAN-aware bridges with the same PVID and
    traffic won't leak between them.
    
    Therefore, to solve this, enable independent VLAN learning with PVID 0
    on VLAN-unaware bridges, by setting their PCR.PORT_VLAN to fallback
    mode, while leaving standalone ports in port matrix mode. The CPU port
    is always set to fallback mode to serve those bridges.
    
    During testing, it is found that FDB lookup with filter ID of 0 will
    also hit entries with VID 0 even with independent VLAN learning. To
    avoid that, install all VLANs with filter ID of 1.
    
    Signed-off-by: DENG Qingfang <dqfext@gmail.com>

--- drivers/net/dsa/mt7530.h
+++ drivers/net/dsa/mt7530.h
@@ -148,6 +148,8 @@ enum mt7530_vlan_cmd {
 #define  VTAG_EN			BIT(28)
 /* VLAN Member Control */
 #define  PORT_MEM(x)			(((x) & 0xff) << 16)
+/* Filter ID */
+#define  FID(x)				(((x) & 0x7) << 1)
 /* VLAN Entry Valid */
 #define  VLAN_VALID			BIT(0)
 #define  PORT_MEM_SHFT			16
@@ -247,7 +249,7 @@ enum mt7530_vlan_port_attr {
 #define MT7530_PPBV1_P(x)		(0x2014 + ((x) * 0x100))
 #define  G0_PORT_VID(x)			(((x) & 0xfff) << 0)
 #define  G0_PORT_VID_MASK		G0_PORT_VID(0xfff)
-#define  G0_PORT_VID_DEF		G0_PORT_VID(1)
+#define  G0_PORT_VID_DEF		G0_PORT_VID(0)
 
 /* Register for port MAC control register */
 #define MT7530_PMCR_P(x)		(0x3000 + ((x) * 0x100))
