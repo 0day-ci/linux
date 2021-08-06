f7ed98d4ce0751aea0137a4612e01e391d1f3682
Brett Creeley <brett.creeley@intel.com>
2021-08-06 13:10:49 -0700

    ice: Use ether_addr_copy() instead of memcpy
    To save the netdev->dev_addr the driver is using memcpy with the length
    of a MAC address. ether_addr_copy() is meant specifically for this, so
    use it.
    
    Signed-off-by: Brett Creeley <brett.creeley@intel.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -5283,7 +5283,7 @@ err_update_filters:
 	}
 
 	/* change the netdev's MAC address */
-	memcpy(netdev->dev_addr, mac, netdev->addr_len);
+	ether_addr_copy(netdev->dev_addr, mac);
 	netif_addr_unlock_bh(netdev);
 	netdev_dbg(vsi->netdev, "updated MAC address to %pM\n",
 		   netdev->dev_addr);
