f6324480d4b27a24f4df68d663ec3b56aeca60c0
Chuck Lever <chuck.lever@oracle.com>
2021-08-09 14:09:24 -0400

    SUNRPC: Add a /sys/kernel/debug/fail_sunrpc/ directory
    This directory will contain a set of administrative controls for
    enabling error injection for kernel RPC consumers.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

---
+++ net/sunrpc/fail.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2021, Oracle. All rights reserved.
+ */
+
+#ifndef _NET_SUNRPC_FAIL_H_
+#define _NET_SUNRPC_FAIL_H_
+
+#include <linux/fault-inject.h>
+
+struct fail_sunrpc_attr {
+	struct fault_attr	attr;
+};
+
+extern struct fail_sunrpc_attr fail_sunrpc;
+
+#endif /* _NET_SUNRPC_FAIL_H_ */
