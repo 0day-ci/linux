39c374f1e8265e61b208cfe691d5d23205325782
Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
2021-08-13 21:33:15 +0200

    KVM: x86: Don't call kvm_mmu_change_mmu_pages() if the count hasn't changed
    There is no point in calling kvm_mmu_change_mmu_pages() for memslot
    operations that don't change the total page count, so do it just for
    KVM_MR_CREATE and KVM_MR_DELETE.
    
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -11588,20 +11588,22 @@ void kvm_arch_commit_memory_region(struct kvm *kvm,
 				const struct kvm_memory_slot *new,
 				enum kvm_mr_change change)
 {
-	if (change == KVM_MR_CREATE)
-		kvm->arch.n_memslots_pages += new->npages;
-	else if (change == KVM_MR_DELETE) {
-		WARN_ON(kvm->arch.n_memslots_pages < old->npages);
-		kvm->arch.n_memslots_pages -= old->npages;
-	}
+	if (change == KVM_MR_CREATE || change == KVM_MR_DELETE) {
+		if (change == KVM_MR_CREATE)
+			kvm->arch.n_memslots_pages += new->npages;
+		else {
+			WARN_ON(kvm->arch.n_memslots_pages < old->npages);
+			kvm->arch.n_memslots_pages -= old->npages;
+		}
 
-	if (!kvm->arch.n_requested_mmu_pages) {
-		unsigned long nr_mmu_pages;
+		if (!kvm->arch.n_requested_mmu_pages) {
+			unsigned long nr_mmu_pages;
 
-		nr_mmu_pages = kvm->arch.n_memslots_pages *
-			       KVM_PERMILLE_MMU_PAGES / 1000;
-		nr_mmu_pages = max(nr_mmu_pages, KVM_MIN_ALLOC_MMU_PAGES);
-		kvm_mmu_change_mmu_pages(kvm, nr_mmu_pages);
+			nr_mmu_pages = kvm->arch.n_memslots_pages *
+				KVM_PERMILLE_MMU_PAGES / 1000;
+			nr_mmu_pages = max(nr_mmu_pages, KVM_MIN_ALLOC_MMU_PAGES);
+			kvm_mmu_change_mmu_pages(kvm, nr_mmu_pages);
+		}
 	}
 
 	kvm_mmu_slot_apply_flags(kvm, old, new, change);
