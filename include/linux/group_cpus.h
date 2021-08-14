5cd330f089b08964c6aa50ca137a9aff27ffedd5
Ming Lei <ming.lei@redhat.com>
2021-08-14 20:35:31 +0800

    lib/group_cpus: allow to group cpus in case of !CONFIG_SMP
    Allows group_cpus_evenly() to be called in case of !CONFIG_SMP by simply
    assigning all CPUs into the 1st group.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- include/linux/group_cpus.h
+++ include/linux/group_cpus.h
@@ -9,6 +9,20 @@
 #include <linux/kernel.h>
 #include <linux/cpu.h>
 
+#ifdef CONFIG_SMP
 struct cpumask *group_cpus_evenly(unsigned int numgrps);
+#else
+static inline struct cpumask *group_cpus_evenly(unsigned int numgrps)
+{
+	struct cpumask *masks = kcalloc(numgrps, sizeof(*masks), GFP_KERNEL);
+
+	if (!masks)
+		return NULL;
+
+	/* assign all CPUs(cpu 0) to the 1st group only */
+	cpumask_copy(&masks[0], cpu_possible_mask);
+	return masks;
+}
+#endif
 
 #endif
