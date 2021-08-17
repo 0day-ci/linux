333f3ad958338bf7f16d15621f9543befe7aed89
Jeffle Xu <jefflexu@linux.alibaba.com>
2021-08-17 10:22:14 +0800

    fuse: Make DAX mount option a tri-state
    We add 'always', 'never', and 'inode' (default). '-o dax' continues to
    operate the same which is equivalent to 'always'.
    
    By the time this patch is applied, 'inode' mode is actually equal to
    'always' mode, before the per-file DAX flag is introduced in the
    following patch.
    
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>

--- fs/fuse/dax.c
+++ fs/fuse/dax.c
@@ -1284,10 +1284,13 @@ out_err:
 	return ret;
 }
 
-int fuse_dax_conn_alloc(struct fuse_conn *fc, struct dax_device *dax_dev)
+int fuse_dax_conn_alloc(struct fuse_conn *fc, enum fuse_dax_mode dax_mode,
+			struct dax_device *dax_dev)
 {
 	struct fuse_conn_dax *fcd;
 	int err;
+
+	fc->dax_mode = dax_mode;
 
 	if (!dax_dev)
 		return 0;
@@ -1335,8 +1338,10 @@ static const struct address_space_operations fuse_dax_file_aops  = {
 static bool fuse_should_enable_dax(struct inode *inode)
 {
 	struct fuse_conn *fc = get_fuse_conn(inode);
+	unsigned int dax_mode = fc->dax_mode;
 
-	if (!fc->dax)
+	/* If 'dax=always/inode', fc->dax couldn't be NULL */
+	if (dax_mode == FUSE_DAX_NEVER)
 		return false;
 
 	return true;
