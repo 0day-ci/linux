b6cf69fd75ac041840f6a706856c5db4690a04e2
Wojciech Drewek <wojciech.drewek@intel.com>
2021-08-19 16:29:57 -0700

    ice: add port representor ethtool ops and stats
    Introduce the following ethtool operations for VF's representor:
    	-get_drvinfo
    	-get_strings
    	-get_ethtool_stats
    	-get_sset_count
    	-get_link
    
    In all cases, existing operations were used with minor
    changes which allow us to detect if ethtool op was called for
    representor. Only VF VSI stats will be available for representor.
    
    Implement ndo_get_stats64 for port representor. This will update
    VF VSI stats and read them.
    
    Signed-off-by: Wojciech Drewek <wojciech.drewek@intel.com>

--- drivers/net/ethernet/intel/ice/ice_virtchnl_pf.h
+++ drivers/net/ethernet/intel/ice/ice_virtchnl_pf.h
@@ -176,6 +176,10 @@ int ice_set_vf_trust(struct net_device *netdev, int vf_id, bool trusted);
 
 int ice_set_vf_link_state(struct net_device *netdev, int vf_id, int link_state);
 
+int ice_check_vf_ready_for_cfg(struct ice_vf *vf);
+
+bool ice_is_vf_disabled(struct ice_vf *vf);
+
 int ice_set_vf_spoofchk(struct net_device *netdev, int vf_id, bool ena);
 
 int ice_calc_vf_reg_idx(struct ice_vf *vf, struct ice_q_vector *q_vector);
@@ -203,6 +207,15 @@ static inline void ice_vc_notify_link_state(struct ice_pf *pf) { }
 static inline void ice_vc_notify_reset(struct ice_pf *pf) { }
 static inline void ice_vc_notify_vf_link_state(struct ice_vf *vf) { }
 static inline void ice_vc_change_ops_to_repr(struct ice_vc_vf_ops *ops) { }
+static inline int ice_check_vf_ready_for_cfg(struct ice_vf *vf)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline bool ice_is_vf_disabled(struct ice_vf *vf)
+{
+	return true;
+}
 static inline void ice_vc_set_dflt_vf_ops(struct ice_vc_vf_ops *ops) { }
 static inline void ice_set_vf_state_qs_dis(struct ice_vf *vf) { }
 static inline
