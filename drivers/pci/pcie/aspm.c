e5431775a88ae79992ba22a1ea05014dac9ffa6b
Kai-Heng Feng <kai.heng.feng@canonical.com>
2021-08-19 13:45:41 +0800

    PCI/ASPM: Introduce a new helper to report ASPM support status
    Introduce a new helper, pcie_aspm_supported(), to report ASPM support
    status.
    
    The user will be introduced by next patch.
    
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>

--- drivers/pci/pcie/aspm.c
+++ drivers/pci/pcie/aspm.c
@@ -1201,6 +1201,17 @@ bool pcie_aspm_enabled(struct pci_dev *pdev)
 }
 EXPORT_SYMBOL_GPL(pcie_aspm_enabled);
 
+bool pcie_aspm_supported(struct pci_dev *pdev)
+{
+	struct pcie_link_state *link = pcie_aspm_get_link(pdev);
+
+	if (!link)
+		return false;
+
+	return link->aspm_support;
+}
+EXPORT_SYMBOL_GPL(pcie_aspm_supported);
+
 static ssize_t aspm_attr_show_common(struct device *dev,
 				     struct device_attribute *attr,
 				     char *buf, u8 state)
