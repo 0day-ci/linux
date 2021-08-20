13b97a54fa368d9a46bc090eb5a3aca23838546a
Niklas Lantau <niklaslantau@gmail.com>
2021-08-21 00:39:54 +0200

    Usb: storage: usb: fixed coding style issues
    Fixed coding style issues that generated 1 error and 1 warning.
    
    Signed-off-by: Niklas Lantau <niklaslantau@gmail.com>

--- drivers/usb/storage/usb.c
+++ drivers/usb/storage/usb.c
@@ -295,7 +295,7 @@ void fill_inquiry_response(struct us_data *us, unsigned char *data,
 }
 EXPORT_SYMBOL_GPL(fill_inquiry_response);
 
-static int usb_stor_control_thread(void * __us)
+static int usb_stor_control_thread(void *__us)
 {
 	struct us_data *us = (struct us_data *)__us;
 	struct Scsi_Host *host = us_to_host(us);
@@ -926,9 +926,8 @@ static unsigned int usb_stor_sg_tablesize(struct usb_interface *intf)
 {
 	struct usb_device *usb_dev = interface_to_usbdev(intf);
 
-	if (usb_dev->bus->sg_tablesize) {
+	if (usb_dev->bus->sg_tablesize)
 		return usb_dev->bus->sg_tablesize;
-	}
 	return SG_ALL;
 }
 
