ee5ea6c40ee6bff43bb7c32f88994980710633d8
Alexandru Elisei <alexandru.elisei@arm.com>
2021-08-25 17:17:43 +0100

    KVM: arm64: Unlock memslots after stage 2 tables are freed
    Unpin the backing pages mapped at stage 2 after the stage 2 translation
    tables are destroyed.
    
    Signed-off-by: Alexandru Elisei <alexandru.elisei@arm.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -1907,6 +1907,7 @@ void kvm_arch_memslots_updated(struct kvm *kvm, u64 gen)
 void kvm_arch_flush_shadow_all(struct kvm *kvm)
 {
 	kvm_free_stage2_pgd(&kvm->arch.mmu);
+	unlock_all_memslots(kvm);
 }
 
 void kvm_arch_flush_shadow_memslot(struct kvm *kvm,
