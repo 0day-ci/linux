c7616f6bb8c68ad3d16a9d89c4e59e9cb17798b7
Sugar Zhang <sugar.zhang@rock-chips.com>
2021-08-26 12:01:51 +0800

    ASoC: rockchip: i2s: Fix concurrency between tx/rx
    This patch adds lock to fix comcurrency between tx/rx
    to fix 'rockchip-i2s ff070000.i2s; fail to clear'
    
    Considering the situation;
    
           tx stream              rx stream
               |                      |
               |                   disable
             enable                   |
               |                    reset
    
    After this patch:
    
             lock
               |
           tx stream
               |
             enable
               |
            unlock
           --------               ---------
                                    lock
                                      |
                                  rx stream
                                      |
                                   disable
                                      |
                                    reset
                                      |
                                   unlock
    
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>

--- sound/soc/rockchip/rockchip_i2s.c
+++ sound/soc/rockchip/rockchip_i2s.c
@@ -15,6 +15,7 @@
 #include <linux/clk.h>
 #include <linux/pm_runtime.h>
 #include <linux/regmap.h>
+#include <linux/spinlock.h>
 #include <sound/pcm_params.h>
 #include <sound/dmaengine_pcm.h>
 
@@ -51,6 +52,9 @@ struct rk_i2s_dev {
 	const struct rk_i2s_pins *pins;
 	unsigned int bclk_ratio;
 };
+
+/* tx/rx ctrl lock */
+static DEFINE_SPINLOCK(lock);
 
 static int i2s_runtime_suspend(struct device *dev)
 {
@@ -93,6 +97,7 @@ static void rockchip_snd_txctrl(struct rk_i2s_dev *i2s, int on)
 	unsigned int val = 0;
 	int retry = 10;
 
+	spin_lock(&lock);
 	if (on) {
 		regmap_update_bits(i2s->regmap, I2S_DMACR,
 				   I2S_DMACR_TDE_ENABLE, I2S_DMACR_TDE_ENABLE);
@@ -133,6 +138,7 @@ static void rockchip_snd_txctrl(struct rk_i2s_dev *i2s, int on)
 			}
 		}
 	}
+	spin_unlock(&lock);
 }
 
 static void rockchip_snd_rxctrl(struct rk_i2s_dev *i2s, int on)
@@ -140,6 +146,7 @@ static void rockchip_snd_rxctrl(struct rk_i2s_dev *i2s, int on)
 	unsigned int val = 0;
 	int retry = 10;
 
+	spin_lock(&lock);
 	if (on) {
 		regmap_update_bits(i2s->regmap, I2S_DMACR,
 				   I2S_DMACR_RDE_ENABLE, I2S_DMACR_RDE_ENABLE);
@@ -180,6 +187,7 @@ static void rockchip_snd_rxctrl(struct rk_i2s_dev *i2s, int on)
 			}
 		}
 	}
+	spin_unlock(&lock);
 }
 
 static int rockchip_i2s_set_fmt(struct snd_soc_dai *cpu_dai,
