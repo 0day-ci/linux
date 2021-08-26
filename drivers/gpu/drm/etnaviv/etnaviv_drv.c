75568d74145b3a587ebebde48e514b1192502f0d
Michael Walle <michael@walle.cc>
2021-08-26 14:10:05 +0200

    drm/etnaviv: fix dma configuration of the virtual device
    The DMA configuration of the virtual device is inherited from the first
    actual etnaviv device. Unfortunately, this doesn't work with an IOMMU:
    
    [    5.191008] Failed to set up IOMMU for device (null); retaining platform DMA ops
    
    This is because there is no associated iommu_group with the device. The
    group is set in iommu_group_add_device() which is eventually called by
    device_add() via the platform bus:
      device_add()
        blocking_notifier_call_chain()
          iommu_bus_notifier()
            iommu_probe_device()
              __iommu_probe_device()
                iommu_group_get_for_dev()
                  iommu_group_add_device()
    
    Move of_dma_configure() into the probe function, which is called after
    device_add(). Normally, the platform code will already call it itself
    if .of_node is set. Unfortunately, this isn't the case here.
    
    Also move the dma mask assignemnts to probe() to keep all DMA related
    settings together.
    
    Signed-off-by: Michael Walle <michael@walle.cc>

--- drivers/gpu/drm/etnaviv/etnaviv_drv.c
+++ drivers/gpu/drm/etnaviv/etnaviv_drv.c
@@ -599,6 +599,7 @@ static int compare_str(struct device *dev, void *data)
 static int etnaviv_pdev_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
+	struct device_node *first_node = NULL;
 	struct component_match *match = NULL;
 
 	if (!dev->platform_data) {
@@ -607,6 +608,9 @@ static int etnaviv_pdev_probe(struct platform_device *pdev)
 		for_each_compatible_node(core_node, NULL, "vivante,gc") {
 			if (!of_device_is_available(core_node))
 				continue;
+
+			if (!first_node)
+				first_node = core_node;
 
 			drm_of_component_match_add(&pdev->dev, &match,
 						   compare_of, core_node);
@@ -618,6 +622,17 @@ static int etnaviv_pdev_probe(struct platform_device *pdev)
 		for (i = 0; names[i]; i++)
 			component_match_add(dev, &match, compare_str, names[i]);
 	}
+
+	pdev->dev.coherent_dma_mask = DMA_BIT_MASK(40);
+	pdev->dev.dma_mask = &pdev->dev.coherent_dma_mask;
+
+	/*
+	 * Apply the same DMA configuration to the virtual etnaviv
+	 * device as the GPU we found. This assumes that all Vivante
+	 * GPUs in the system share the same DMA constraints.
+	 */
+	if (first_node)
+		of_dma_configure(&pdev->dev, first_node, true);
 
 	return component_master_add_with_match(dev, &etnaviv_master_ops, match);
 }
@@ -669,15 +684,6 @@ static int __init etnaviv_init(void)
 			of_node_put(np);
 			goto unregister_platform_driver;
 		}
-		pdev->dev.coherent_dma_mask = DMA_BIT_MASK(40);
-		pdev->dev.dma_mask = &pdev->dev.coherent_dma_mask;
-
-		/*
-		 * Apply the same DMA configuration to the virtual etnaviv
-		 * device as the GPU we found. This assumes that all Vivante
-		 * GPUs in the system share the same DMA constraints.
-		 */
-		of_dma_configure(&pdev->dev, np, true);
 
 		ret = platform_device_add(pdev);
 		if (ret) {
