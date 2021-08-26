8f1c2aa70cb56ee5b6227bbe4cad105ace1f597f
Ferry Toth <ftoth@exalondelft.nl>
2021-08-26 20:57:38 +0200

    Revert "usb: gadget: f_uac2: add adaptive sync support for capture"
    This reverts commit 40c73b30546e759bedcec607fedc2d4be954508f.
    
    The commit is part of a series with commit
    24f779dac8f3efb9629adc0e486914d93dc45517 causing a BUG on dwc3
    hardware, at least on Intel Merrifield platform when configured
    through configfs:
    BUG: kernel NULL pointer dereference, address: 0000000000000008
    ...
    RIP: 0010:dwc3_gadget_del_and_unmap_request+0x19/0xe0
    ...
    Call Trace:
     dwc3_remove_requests.constprop.0+0x12f/0x170
     __dwc3_gadget_ep_disable+0x7a/0x160
     dwc3_gadget_ep_disable+0x3d/0xd0
     usb_ep_disable+0x1c/0x70
     u_audio_stop_capture+0x79/0x120 [u_audio]
     afunc_set_alt+0x73/0x80 [usb_f_uac2]
     composite_setup+0x224/0x1b90 [libcomposite]
    
    Pavel's suggestion to add
    `echo "adaptive" > functions/uac2.usb0/c_sync` to the configfs script
    resolves the issue.
    Thinh suggests "the crash is probably because of f_uac2 prematurely
    freeing feedback request before its completion. usb_ep_dequeue() is
    asynchronous. dwc2() may treat it as a synchronous call so you didn't
    get a crash."
    
    Revert as this is a regression and the kernel shouldn't crash depending
    on configuration parameters.
    
    Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

--- drivers/usb/gadget/function/u_uac2.h
+++ drivers/usb/gadget/function/u_uac2.h
@@ -21,7 +21,6 @@
 #define UAC2_DEF_CCHMASK 0x3
 #define UAC2_DEF_CSRATE 64000
 #define UAC2_DEF_CSSIZE 2
-#define UAC2_DEF_CSYNC		USB_ENDPOINT_SYNC_ASYNC
 #define UAC2_DEF_REQ_NUM 2
 
 struct f_uac2_opts {
@@ -32,7 +31,6 @@ struct f_uac2_opts {
 	int				c_chmask;
 	int				c_srate;
 	int				c_ssize;
-	int				c_sync;
 	int				req_number;
 	bool				bound;
 
