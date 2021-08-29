b30f907b05db2a06447fad362fed00c7c35c5a7d
Colin Ian King <colin.king@canonical.com>
2021-08-29 19:05:03 +0100

    rtlwifi: rtl8192de: Fix uninitialized variable place
    In the case where chnl <= 14 variable place is not initialized and
    the function returns an uninitialized value. This fixes an earlier
    cleanup where I introduced this bug. My bad.
    
    Addresses-Coverity: ("Uninitialized scalar variable")
    Fixes: 369956ae5720 ("rtlwifi: rtl8192de: Remove redundant variable initializations")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- drivers/net/wireless/realtek/rtlwifi/rtl8192de/phy.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8192de/phy.c
@@ -896,7 +896,7 @@ static void _rtl92d_ccxpower_index_check(struct ieee80211_hw *hw,
 
 static u8 _rtl92c_phy_get_rightchnlplace(u8 chnl)
 {
-	u8 place;
+	u8 place = chnl;
 
 	if (chnl > 14) {
 		for (place = 14; place < sizeof(channel5g); place++) {
