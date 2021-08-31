be14f8bf323bc6ca952e83e1df56282cf56c3daa
Cai Huoqing <caihuoqing@baidu.com>
2021-08-31 15:44:49 +0800

    drm/aspeed: Make use of the helper function devm_platform_ioremap_resource()
    Use the devm_platform_ioremap_resource() helper instead of
    calling platform_get_resource() and devm_ioremap_resource()
    separately
    
    Signed-off-by: Cai Huoqing <caihuoqing@baidu.com>

--- drivers/gpu/drm/aspeed/aspeed_gfx_drv.c
+++ drivers/gpu/drm/aspeed/aspeed_gfx_drv.c
@@ -133,11 +133,9 @@ static int aspeed_gfx_load(struct drm_device *drm)
 	struct device_node *np = pdev->dev.of_node;
 	const struct aspeed_gfx_config *config;
 	const struct of_device_id *match;
-	struct resource *res;
 	int ret;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	priv->base = devm_ioremap_resource(drm->dev, res);
+	priv->base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(priv->base))
 		return PTR_ERR(priv->base);
 
