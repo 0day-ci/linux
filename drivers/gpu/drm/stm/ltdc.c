4a10b70e4a2d1da42db4ddf49839b2852c6feb73
Raphael GALLAIS-POU - foss <raphael.gallais-pou@foss.st.com>
2021-09-02 15:30:57 +0000

    drm/stm: ltdc: attach immutable zpos property to planes
    Defines plane ordering by hard-coding an immutable Z position from the
    first plane, used as primary layer, to the next ones as overlay in order
    of instantiation.
    
    This zpos is only an information as it is not possible to modify it,
    blending operations are still applied from the top to the bottom layer.
    
    This patch helps to remove a warning message from the Android
    Hardware Composer.
    
    Signed-off-by: Raphael Gallais-Pou <raphael.gallais-pou@foss.st.com>

--- drivers/gpu/drm/stm/ltdc.c
+++ drivers/gpu/drm/stm/ltdc.c
@@ -1025,6 +1025,8 @@ static int ltdc_crtc_init(struct drm_device *ddev, struct drm_crtc *crtc)
 		return -EINVAL;
 	}
 
+	drm_plane_create_zpos_immutable_property(primary, 0);
+
 	ret = drm_crtc_init_with_planes(ddev, crtc, primary, NULL,
 					&ltdc_crtc_funcs, NULL);
 	if (ret) {
@@ -1047,6 +1049,7 @@ static int ltdc_crtc_init(struct drm_device *ddev, struct drm_crtc *crtc)
 			DRM_ERROR("Can not create overlay plane %d\n", i);
 			goto cleanup;
 		}
+		drm_plane_create_zpos_immutable_property(overlay, i);
 	}
 
 	return 0;
