1a41f7a861878903616e9fca56c35ca9b106181c
Kan Liang <kan.liang@linux.intel.com>
2021-09-02 14:59:54 +0800

    perf pmu: Add PMU alias support
    A perf uncore PMU may have two PMU names, a real name and an alias. The
    alias is exported at /sys/bus/event_source/devices/uncore_*/alias.
    The perf tool should support the alias as well.
    
    Add alias_name in the struct perf_pmu to store the alias. For the PMU
    which doesn't have an alias. It's NULL.
    
    Introduce two X86 specific functions to retrieve the real name and the
    alias separately.
    
    Only go through the sysfs to retrieve the mapping between the real name
    and the alias once. The result is cached in a list, uncore_pmu_list.
    
    Nothing changed for the other ARCHs.
    
    With the patch, the perf tool can monitor the PMU with either the real
    name or the alias.
    
    Use the real name,
     $ perf stat -e uncore_cha_2/event=1/ -x,
       4044879584,,uncore_cha_2/event=1/,2528059205,100.00,,
    
    Use the alias,
     $ perf stat -e uncore_type_0_2/event=1/ -x,
       3659675336,,uncore_type_0_2/event=1/,2287306455,100.00,,
    
    Co-developed-by: Jin Yao <yao.jin@linux.intel.com>
    Co-developed-by: Arnaldo Carvalho de Melo <acme@kernel.org>
    Signed-off-by: Jin Yao <yao.jin@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/arch/x86/util/pmu.c
+++ tools/perf/arch/x86/util/pmu.c
@@ -1,12 +1,30 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <string.h>
-
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <fcntl.h>
 #include <linux/stddef.h>
 #include <linux/perf_event.h>
+#include <linux/zalloc.h>
+#include <api/fs/fs.h>
+#include <errno.h>
 
 #include "../../../util/intel-pt.h"
 #include "../../../util/intel-bts.h"
 #include "../../../util/pmu.h"
+#include "../../../util/fncache.h"
+
+#define TEMPLATE_ALIAS	"%s/bus/event_source/devices/%s/alias"
+
+struct perf_pmu_alias_name {
+	char *name;
+	char *alias;
+	struct list_head list;
+};
+
+static LIST_HEAD(pmu_alias_name_list);
+static bool cached_list;
 
 struct perf_event_attr *perf_pmu__get_default_config(struct perf_pmu *pmu __maybe_unused)
 {
@@ -17,4 +35,139 @@ struct perf_event_attr *perf_pmu__get_default_config(struct perf_pmu *pmu __mayb
 		pmu->selectable = true;
 #endif
 	return NULL;
+}
+
+static void pmu_alias__delete(struct perf_pmu_alias_name *pmu)
+{
+	if (!pmu)
+		return;
+
+	zfree(&pmu->name);
+	zfree(&pmu->alias);
+	free(pmu);
+}
+
+static struct perf_pmu_alias_name *pmu_alias__new(char *name, char *alias)
+{
+	struct perf_pmu_alias_name *pmu = zalloc(sizeof(*pmu));
+
+	if (pmu) {
+		pmu->name = strdup(name);
+		if (!pmu->name)
+			goto out_delete;
+
+		pmu->alias = strdup(alias);
+		if (!pmu->alias)
+			goto out_delete;
+	}
+	return pmu;
+
+out_delete:
+	pmu_alias__delete(pmu);
+	return NULL;
+}
+
+static int setup_pmu_alias_list(void)
+{
+	char path[PATH_MAX];
+	DIR *dir;
+	struct dirent *dent;
+	const char *sysfs = sysfs__mountpoint();
+	struct perf_pmu_alias_name *pmu;
+	char buf[MAX_PMU_NAME_LEN];
+	FILE *file;
+	int ret = -ENOMEM;
+
+	if (!sysfs)
+		return -1;
+
+	snprintf(path, PATH_MAX,
+		 "%s" EVENT_SOURCE_DEVICE_PATH, sysfs);
+
+	dir = opendir(path);
+	if (!dir)
+		return -errno;
+
+	while ((dent = readdir(dir))) {
+		if (!strcmp(dent->d_name, ".") ||
+		    !strcmp(dent->d_name, ".."))
+			continue;
+
+		snprintf(path, PATH_MAX,
+			 TEMPLATE_ALIAS, sysfs, dent->d_name);
+
+		if (!file_available(path))
+			continue;
+
+		file = fopen(path, "r");
+		if (!file)
+			continue;
+
+		if (!fgets(buf, sizeof(buf), file)) {
+			fclose(file);
+			continue;
+		}
+
+		fclose(file);
+
+		/* Remove the last '\n' */
+		buf[strlen(buf) - 1] = 0;
+
+		pmu = pmu_alias__new(dent->d_name, buf);
+		if (!pmu)
+			goto close_dir;
+
+		list_add_tail(&pmu->list, &pmu_alias_name_list);
+	}
+
+	ret = 0;
+
+close_dir:
+	closedir(dir);
+	return ret;
+}
+
+static char *__pmu_find_real_name(const char *name)
+{
+	struct perf_pmu_alias_name *pmu;
+
+	list_for_each_entry(pmu, &pmu_alias_name_list, list) {
+		if (!strcmp(name, pmu->alias))
+			return pmu->name;
+	}
+
+	return (char *)name;
+}
+
+char *pmu_find_real_name(const char *name)
+{
+	if (cached_list)
+		return __pmu_find_real_name(name);
+
+	setup_pmu_alias_list();
+	cached_list = true;
+
+	return __pmu_find_real_name(name);
+}
+
+static char *__pmu_find_alias_name(const char *name)
+{
+	struct perf_pmu_alias_name *pmu;
+
+	list_for_each_entry(pmu, &pmu_alias_name_list, list) {
+		if (!strcmp(name, pmu->name))
+			return pmu->alias;
+	}
+	return NULL;
+}
+
+char *pmu_find_alias_name(const char *name)
+{
+	if (cached_list)
+		return __pmu_find_alias_name(name);
+
+	setup_pmu_alias_list();
+	cached_list = true;
+
+	return __pmu_find_alias_name(name);
 }
