6e7cc9db9aa9e68a640480fd0260e65dd2df15b1
Miquel Raynal <miquel.raynal@bootlin.com>
2021-09-02 23:14:30 +0200

    iio: adc: max1027: Ensure a default cnvst trigger configuration
    We don't expect the (hardware) cnvst trigger to be enabled at boot time,
    this is a user choice made in sysfs and there is a dedicated callback to
    enable/disable this trigger. Hence, we can just ensure it is disabled in
    the probe at initialization time and then assume that whenever a
    ->read_raw() call happens, the trigger has been disabled and conversions
    will start on register write.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/iio/adc/max1027.c
+++ drivers/iio/adc/max1027.c
@@ -289,10 +289,6 @@ static int max1027_read_single_value(struct iio_dev *indio_dev,
 		return -EBUSY;
 	}
 
-	ret = max1027_enable_trigger(indio_dev, false);
-	if (ret)
-		return ret;
-
 	/* Configure conversion register with the requested chan */
 	st->reg = MAX1027_CONV_REG | MAX1027_CHAN(chan->channel) |
 		  MAX1027_NOSCAN;
@@ -541,6 +537,11 @@ static int max1027_probe(struct spi_device *spi)
 		dev_err(&indio_dev->dev, "Failed to configure averaging register\n");
 		goto free_buffer;
 	}
+
+	/* Assume conversion on register write for now */
+	ret = max1027_enable_trigger(indio_dev, false);
+	if (ret)
+		return ret;
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 
