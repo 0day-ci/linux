f0696adbe28cba83ea49923729b101f70e2d0f43
Alexandre Bailon <abailon@baylibre.com>
2021-09-06 21:04:53 +0200

    dt-bindings: Add bindings for the virtual thermal sensor
    This adds the device tree bidings for the virtual thermal sensor.
    The virtual sensor could be used to a temperature computed from
    many thermal sensors.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

---
+++ include/dt-bindings/thermal/virtual-sensor.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * This header provides constants for virtual thermal sensor bindings.
+ *
+ * Copyright (C) 2021 BayLibre
+ */
+
+#ifndef _DT_BINDINGS_THERMAL_VIRTUAL_SENSOR_H
+#define _DT_BINDINGS_THERMAL_VIRTUAL_SENSOR_H
+
+#define VIRTUAL_SENSOR_MIN 0
+#define VIRTUAL_SENSOR_MAX 1
+#define VIRTUAL_SENSOR_AVG 2
+
+#endif /* _DT_BINDINGS_THERMAL_VIRTUAL_SENSOR_H */
