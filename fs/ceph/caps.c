9812f3d0b81a1320ae24d994b450b285ab14eaed
Dan Carpenter <dan.carpenter@oracle.com>
2021-09-06 12:43:01 +0300

    ceph: fix off by one bugs in unsafe_request_wait()
    The "> max" tests should be ">= max" to prevent an out of bounds access
    on the next lines.
    
    Fixes: e1a4541ec0b9 ("ceph: flush the mdlog before waiting on unsafe reqs")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -2260,7 +2260,7 @@ retry:
 			list_for_each_entry(req, &ci->i_unsafe_dirops,
 					    r_unsafe_dir_item) {
 				s = req->r_session;
-				if (unlikely(s->s_mds > max)) {
+				if (unlikely(s->s_mds >= max)) {
 					spin_unlock(&ci->i_unsafe_lock);
 					goto retry;
 				}
@@ -2274,7 +2274,7 @@ retry:
 			list_for_each_entry(req, &ci->i_unsafe_iops,
 					    r_unsafe_target_item) {
 				s = req->r_session;
-				if (unlikely(s->s_mds > max)) {
+				if (unlikely(s->s_mds >= max)) {
 					spin_unlock(&ci->i_unsafe_lock);
 					goto retry;
 				}
