050f264a3627694b86a8b4d99bb2781e27e41777
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2021-09-08 18:02:52 +0800

    dt-bindings: nvmem: add cell-type to nvmem cells
    Some of the nvmem providers encode data for certain type of nvmem cell,
    example mac-address is stored in ascii or with delimiter or in reverse order.
    
    This is much specific to vendor, so having a cell-type would allow nvmem
    provider drivers to post-process this before using it.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Joakim Zhang <qiangqing.zhang@nxp.com>

---
+++ include/dt-bindings/nvmem/nvmem.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_NVMMEM_H
+#define __DT_NVMMEM_H
+
+#define NVMEM_CELL_TYPE_UNKNOWN		0
+#define NVMEM_CELL_TYPE_MAC_ADDRESS	1
+
+#endif /* __DT_NVMMEM_H */
