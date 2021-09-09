da0d8930d870766cffc5548a54609e0fcc7dac43
Hou Tao <houtao1@huawei.com>
2021-09-09 17:00:54 +0800

    block: check the existence of tags[hctx_idx] in blk_mq_clear_rq_mapping()
    According to commit 4412efecf7fd ("Revert "blk-mq: remove code for
    dealing with remapping queue""), for some devices queue hctx may not
    being mapped, and tagset->tags[hctx_idx] will be released and be NULL.
    
    If an IO scheduler is used on these devices, blk_mq_clear_rq_mapping()
    will be called for all hctxs in blk_mq_sched_free_requests() during
    scheduler switch, and these will be oops. So checking the existence of
    tags[hctx_idx] before going on in blk_mq_clear_rq_mapping().
    
    Signed-off-by: Hou Tao <houtao1@huawei.com>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2309,6 +2309,10 @@ static void blk_mq_clear_rq_mapping(struct blk_mq_tag_set *set,
 	struct page *page;
 	unsigned long flags;
 
+	/* If the hctx is unmapped, drv_tags may be NULL */
+	if (!drv_tags)
+		return;
+
 	list_for_each_entry(page, &tags->page_list, lru) {
 		unsigned long start = (unsigned long)page_address(page);
 		unsigned long end = start + order_to_size(page->private);
