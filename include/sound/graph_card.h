b5ce49ac5dc76b68460729e0ec976c7f7ad870cf
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-09-10 10:22:15 +0900

    ASoC: add Rich Graph Card driver
    We already have audio-graph-card which is Of-graph base of general
    sound card driver.
    
    It is supporting DPCM connection, but was forcibly expanded.
    Thus, it is very difficult to add new features on it, for example
    Multi CPU/Codec support, Codec2Codec support, etc.
    
    This patch adds more flexible new Rich Graph Card driver for it.
    audio-graph-card and rich-graph-card are similar, but don't have
    full compatibility.
    
    Rich Graph Card supports very generic connection, but some users
    want to have its own settings, for example PLL settings, etc.
    For such case, it has customizing support.
    In users own driver, it can use Rich Graph Card parsing by using
    rich_graph_parse_of(), and doing its own customizing.
    
    Because Rich Graph Card is still under experimental stage,
    it will indicate such warning when probing, and the DT syntax
    might be changed.
    
    Link: https://lore.kernel.org/r/87k0xszlep.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/graph_card.h
+++ include/sound/graph_card.h
@@ -9,6 +9,21 @@
 
 #include <sound/simple_card_utils.h>
 
+typedef int (*GRAPH_CUSTOM)(struct asoc_simple_priv *priv,
+			    struct device_node *lnk,
+			    struct link_info *li);
+
+struct graph_custom_hooks {
+	int (*hook_pre)(struct asoc_simple_priv *priv);
+	int (*hook_post)(struct asoc_simple_priv *priv);
+	GRAPH_CUSTOM custom_normal;
+};
+
 int audio_graph_parse_of(struct asoc_simple_priv *priv, struct device *dev);
+int rich_graph_parse_of(struct asoc_simple_priv *priv, struct device *dev,
+			struct graph_custom_hooks *hooks);
+
+int rich_graph_link_normal(struct asoc_simple_priv *priv,
+			   struct device_node *lnk, struct link_info *li);
 
 #endif /* __GRAPH_CARD_H */
