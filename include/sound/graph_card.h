de945a7007ad77e5853a6a7dcd2299ff61bf4999
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-09-10 10:22:27 +0900

    ASoC: rich-graph-card: add DPCM support
    This patch adds DPCM support to rich-graph-card.
    It uses "dpcm" node (= C), needs to have routing (= A),
    need to indicate both FE/BE at links (= B).
    dpcm ports@0 is for FE (= D), port@1 is for BE (= D).
    remote-endpoint can use both Single/Multi connection.
    
    			DSP
    		  ************
    	PCM0 <--> * fe0  be0 * <--> DAI0: Codec Headset
    	PCM1 <--> * fe1  be1 * <--> DAI1: Codec Speakers
    	PCM2 <--> * fe2  be2 * <--> DAI2: MODEM
    	PCM3 <--> * fe3  be3 * <--> DAI3: BT
    		  *	 be4 * <--> DAI4: DMIC
    		  *	 be5 * <--> DAI5: FM
    		  ************
    
    	sound {
    		compatible = "rich-graph-card";
    
    		// indicate routing
    (A)		routing = "xxx Playback", "xxx Playback",
    			  "xxx Playback", "xxx Playback",
    			  "xxx Playback", "xxx Playback";
    
    		// indicate all Front-End, Back-End in DPCM case
    (B)		links = <&fe0, &fe1, ...
    			 &be0, &be1, ...
    
    (C)		dpcm {
    			// Front-End
    (D)			ports@0 {
    				fe0: port@0 { fe0_ep: endpoint { remote-endpoint = <&pcm0_ep>; }; };
    				fe1: port@1 { fe1_ep: endpoint { remote-endpoint = <&pcm1_ep>; }; };
    				...
    			};
    			// Back-End
    (E)			ports@1 {
    				be0: port@0 { be0_ep: endpoint { remote-endpoint = <&dai0_ep>; }; };
    				be1: port@1 { be1_ep: endpoint { remote-endpoint = <&dai1_ep>; }; };
    				...
    			};
    		};
    	};
    
    	CPU {
    		ports {
    			bitclock-master;
    			frame-master;
    			port@0 { pcm0_ep: endpoint { remote-endpoint = <&fe0_ep>; }; };
    			port@1 { pcm1_ep: endpoint { remote-endpoint = <&fe1_ep>; }; };
    			...
    		};
    	};
    
    	Codec {
    		ports {
    			port@0 { dai0_ep: endpoint { remote-endpoint = <&be0_ep>; }; };
    			port@1 { dai1_ep: endpoint { remote-endpoint = <&be1_ep>; }; };
    			...
    		};
    	};
    
    Link: https://lore.kernel.org/r/87k0xszlep.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- include/sound/graph_card.h
+++ include/sound/graph_card.h
@@ -17,6 +17,7 @@ struct graph_custom_hooks {
 	int (*hook_pre)(struct asoc_simple_priv *priv);
 	int (*hook_post)(struct asoc_simple_priv *priv);
 	GRAPH_CUSTOM custom_normal;
+	GRAPH_CUSTOM custom_dpcm;
 };
 
 int audio_graph_parse_of(struct asoc_simple_priv *priv, struct device *dev);
@@ -25,5 +26,7 @@ int rich_graph_parse_of(struct asoc_simple_priv *priv, struct device *dev,
 
 int rich_graph_link_normal(struct asoc_simple_priv *priv,
 			   struct device_node *lnk, struct link_info *li);
+int rich_graph_link_dpcm(struct asoc_simple_priv *priv,
+			 struct device_node *lnk, struct link_info *li);
 
 #endif /* __GRAPH_CARD_H */
