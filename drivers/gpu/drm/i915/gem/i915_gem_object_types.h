49e483c91fbcea546218f23c6a13b1987acd817c
Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
2021-09-10 08:36:20 -0700

    drm/i915/pxp: interfaces for using protected objects
    This api allow user mode to create protected buffers and to mark
    contexts as making use of such objects. Only when using contexts
    marked in such a way is the execution guaranteed to work as expected.
    
    Contexts can only be marked as using protected content at creation time
    (i.e. the parameter is immutable) and they must be both bannable and not
    recoverable. Given that the protected session gets invalidated on
    suspend, contexts created this way hold a runtime pm wakeref until
    they're either destroyed or invalidated.
    
    All protected objects and contexts will be considered invalid when the
    PXP session is destroyed and all new submissions using them will be
    rejected. All intel contexts within the invalidated gem contexts will be
    marked banned. Userspace can detect that an invalidation has occurred via
    the RESET_STATS ioctl, where we report it the same way as a ban due to a
    hang.
    
    v5: squash patches, rebase on proto_ctx, update kerneldoc
    
    v6: rebase on obj create_ext changes
    
    v7: Use session counter to check if an object it valid, hold wakeref in
        context, don't add a new flag to RESET_STATS (Daniel)
    
    v8: don't increase guilty count for contexts banned during pxp
        invalidation (Rodrigo)
    
    v9: better comments, avoid wakeref put race between pxp_inval and
        context_close, add usage examples (Rodrigo)
    
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Signed-off-by: Bommu Krishnaiah <krishnaiah.bommu@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: Jason Ekstrand <jason@jlekstrand.net>
    Cc: Daniel Vetter <daniel.vetter@intel.com>

--- drivers/gpu/drm/i915/gem/i915_gem_object_types.h
+++ drivers/gpu/drm/i915/gem/i915_gem_object_types.h
@@ -298,6 +298,7 @@ struct drm_i915_gem_object {
 			     I915_BO_ALLOC_USER)
 #define I915_BO_READONLY         BIT(4)
 #define I915_TILING_QUIRK_BIT    5 /* unknown swizzling; do not release! */
+#define I915_BO_PROTECTED        BIT(6)
 
 	/**
 	 * @mem_flags - Mutable placement-related flags
@@ -536,6 +537,13 @@ struct drm_i915_gem_object {
 		struct i915_gem_object_page_iter get_io_page;
 		bool created:1;
 	} ttm;
+
+	/*
+	 * Record which PXP key instance this object was created against (if
+	 * any), so we can use it to determine if the encryption is valid by
+	 * comparing against the current key instance.
+	 */
+	u32 pxp_key_instance;
 
 	/** Record of address bit 17 of each page at last unbind. */
 	unsigned long *bit_17;
--- drivers/gpu/drm/i915/pxp/intel_pxp.h
+++ drivers/gpu/drm/i915/pxp/intel_pxp.h
@@ -9,6 +9,8 @@
 #include "gt/intel_gt_types.h"
 #include "intel_pxp_types.h"
 
+struct drm_i915_gem_object;
+
 static inline struct intel_gt *pxp_to_gt(const struct intel_pxp *pxp)
 {
 	return container_of(pxp, struct intel_gt, pxp);
@@ -33,6 +35,10 @@ void intel_pxp_fini_hw(struct intel_pxp *pxp);
 
 void intel_pxp_mark_termination_in_progress(struct intel_pxp *pxp);
 int intel_pxp_wait_for_arb_start(struct intel_pxp *pxp);
+
+int intel_pxp_key_check(struct intel_pxp *pxp, struct drm_i915_gem_object *obj);
+
+void intel_pxp_invalidate(struct intel_pxp *pxp);
 #else
 static inline void intel_pxp_init(struct intel_pxp *pxp)
 {
@@ -43,6 +49,12 @@ static inline void intel_pxp_fini(struct intel_pxp *pxp)
 }
 
 static inline int intel_pxp_wait_for_arb_start(struct intel_pxp *pxp)
+{
+	return -ENODEV;
+}
+
+static inline int intel_pxp_key_check(struct intel_pxp *pxp,
+				      struct drm_i915_gem_object *obj)
 {
 	return -ENODEV;
 }
