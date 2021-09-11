e30f39558c3e9f330c8049bcebc59154ee54ba7b
Simon Ser <contact@emersion.fr>
2021-09-11 10:24:40 +0000

    drm/panel-orientation-quirks: add Valve Steam Deck
    Valve's Steam Deck has a 800x1280 LCD screen.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Cc: Jared Baldridge <jrb@expunge.us>
    Cc: Emil Velikov <emil.l.velikov@gmail.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Hans de Goede <hdegoede@redhat.com>

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -231,6 +231,13 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "LTH17"),
 		},
 		.driver_data = (void *)&lcd800x1280_rightside_up,
+	}, {	/* Valve Steam Deck */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Valve"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "Jupiter"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_VERSION, "1"),
+		},
+		.driver_data = (void *)&lcd800x1280_rightside_up,
 	},
 	{}
 };
