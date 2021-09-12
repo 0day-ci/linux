bb8b9f4b0d393e899516242735fe4b6eaa595bbc
Mianhan Liu <liumh1@shanghaitech.edu.cn>
2021-09-12 21:36:40 +0800

    include/linux/mm.h: move nr_free_buffer_pages from swap.h to mm.h
    nr_free_buffer_pages could be exposed through mm.h instead of swap.h. The
    advantage of this change is that it can reduce the obsolete includes. For
    example, net/ipv4/tcp.c wouldn't need swap.h any more since it has already
    included mm.h. Similarly, after checking all the other files, it comes that
    tcp.c, udp.c meter.c ,... follow the same rule, so these files can have swap.h
    removed too.
    
    Moreover, after preprocessing all the files that use nr_free_buffer_pages, it
    turns out that those files have already included mm.h.Thus, we can move
    nr_free_buffer_pages from swap.h to mm.h safely. This change will not affect
    the compilation of other files.
    
    Signed-off-by: Mianhan Liu <liumh1@shanghaitech.edu.cn>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -869,7 +869,7 @@ void put_pages_list(struct list_head *pages);
 
 void split_page(struct page *page, unsigned int order);
 void folio_copy(struct folio *dst, struct folio *src);
-
+unsigned long nr_free_buffer_pages(void);
 /*
  * Compound pages have a destructor function.  Provide a
  * prototype for that function and accessor functions.
