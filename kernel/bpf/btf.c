833763d1e1c04f8cbd71f1fd46d7a1f45326d350
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2021-09-14 18:07:44 +0530

    bpf: Enable TCP congestion control kfunc from modules
    This commit moves BTF ID lookup into the newly added registration
    helper, in a way that the bbr, cubic, and dctcp implementation set up
    their sets in the bpf_tcp_ca kfunc_btf_set list, while the ones not
    dependent on modules are looked up from the wrapper function.
    
    This lifts the restriction for them to be compiled as built in objects,
    and can be loaded as modules if required. Also modify Makefile.modfinal
    to resolve_btfids in TCP congestion control modules if the config option
    is set, using the base BTF support added in the previous commit.
    
    See following commits for background on use of:
    
     CONFIG_X86 ifdef:
     569c484f9995 (bpf: Limit static tcp-cc functions in the .BTF_ids list to x86)
    
     CONFIG_DYNAMIC_FTRACE ifdef:
     7aae231ac93b (bpf: tcp: Limit calling some tcp cc functions to CONFIG_DYNAMIC_FTRACE)
    
    [ resolve_btfids uses --no-fail because some crypto kernel modules
      under arch/x86/crypto generated from ASM do not have the .BTF sections ]
    
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -6266,3 +6266,6 @@ EXPORT_SYMBOL_GPL(unregister_kfunc_btf_id_set);
 		mutex_unlock(&list_name.mutex);                                \
 		return false;                                                  \
 	}
+
+DEFINE_KFUNC_BTF_ID_LIST(bpf_tcp_ca_kfunc_list);
+DEFINE_CHECK_KFUNC_CALLBACK(bpf_tcp_ca, bpf_tcp_ca_kfunc_list);
