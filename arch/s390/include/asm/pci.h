49466878880094c87d1ba7b4d905900c9be6d82f
Niklas Schnelle <schnelle@linux.ibm.com>
2021-09-16 11:33:34 +0200

    s390/pci: implement reset_slot for hotplug slot
    This is done by adding a zpci_hot_reset_device() call which does a low
    level reset of the PCI function without changing its higher level
    function state. This way it can be used while the zPCI function is bound
    to a driver and with DMA tables being controlled either through the
    IOMMU or DMA APIs which is prohibited when using zpci_disable_device()
    as that drop existing DMA translations.
    
    As this reset, unlike a normal FLR, also calls zpci_clear_irq() we need
    to implement arch_restore_msi_irqs() and make sure we re-enable IRQs for
    the PCI function if they were previously disabled.
    
    Signed-off-by: Niklas Schnelle <schnelle@linux.ibm.com>

--- arch/s390/include/asm/pci.h
+++ arch/s390/include/asm/pci.h
@@ -208,6 +208,7 @@ int zpci_disable_device(struct zpci_dev *);
 int zpci_scan_configured_device(struct zpci_dev *zdev, u32 fh);
 int zpci_deconfigure_device(struct zpci_dev *zdev);
 
+int zpci_hot_reset_device(struct zpci_dev *zdev);
 int zpci_register_ioat(struct zpci_dev *, u8, u64, u64, u64);
 int zpci_unregister_ioat(struct zpci_dev *, u8);
 void zpci_remove_reserved_devices(void);
