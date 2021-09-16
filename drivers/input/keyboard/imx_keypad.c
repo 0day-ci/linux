1b97a7d797f4dfaaf912f9b9093f3ab3765116ca
Cai Huoqing <caihuoqing@baidu.com>
2021-09-16 23:32:48 +0800

    Input: imx_keypad - Make use of the helper function dev_err_probe()
    When possible use dev_err_probe help to properly deal with the
    PROBE_DEFER error, the benefit is that DEFER issue will be logged
    in the devices_deferred debugfs file.
    Using dev_err_probe() can reduce code size, and the error value
    gets printed.
    
    Signed-off-by: Cai Huoqing <caihuoqing@baidu.com>

--- drivers/input/keyboard/imx_keypad.c
+++ drivers/input/keyboard/imx_keypad.c
@@ -448,10 +448,9 @@ static int imx_keypad_probe(struct platform_device *pdev)
 		return PTR_ERR(keypad->mmio_base);
 
 	keypad->clk = devm_clk_get(&pdev->dev, NULL);
-	if (IS_ERR(keypad->clk)) {
-		dev_err(&pdev->dev, "failed to get keypad clock\n");
-		return PTR_ERR(keypad->clk);
-	}
+	if (IS_ERR(keypad->clk))
+		return dev_err_probe(&pdev->dev, PTR_ERR(keypad->clk),
+				     "failed to get keypad clock\n");
 
 	/* Init the Input device */
 	input_dev->name = pdev->name;
