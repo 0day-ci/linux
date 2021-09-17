9d8a869af459dcc27ecc2da831318ebaf12f3c66
NeilBrown <neilb@suse.de>
2021-09-17 12:56:57 +1000

    XFS: remove congestion_wait() loop from kmem_alloc()
    Documentation commment in gfp.h discourages indefinite retry loops on
    ENOMEM and says of __GFP_NOFAIL that it
    
        is definitely preferable to use the flag rather than opencode
        endless loop around allocator.
    
    So remove the loop, instead specifying __GFP_NOFAIL if KM_MAYFAIL was
    not given.
    
    As we no longer have the opportunity to report a warning after some
    failures, clear __GFP_NOWARN so that the default warning (rate-limited
    to 1 ever 10 seconds) will be reported instead.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/xfs/kmem.c
+++ fs/xfs/kmem.c
@@ -11,23 +11,16 @@
 void *
 kmem_alloc(size_t size, xfs_km_flags_t flags)
 {
-	int	retries = 0;
 	gfp_t	lflags = kmem_flags_convert(flags);
-	void	*ptr;
 
 	trace_kmem_alloc(size, flags, _RET_IP_);
 
-	do {
-		ptr = kmalloc(size, lflags);
-		if (ptr || (flags & KM_MAYFAIL))
-			return ptr;
-		if (!(++retries % 100))
-			xfs_err(NULL,
-	"%s(%u) possible memory allocation deadlock size %u in %s (mode:0x%x)",
-				current->comm, current->pid,
-				(unsigned int)size, __func__, lflags);
-		congestion_wait(BLK_RW_ASYNC, HZ/50);
-	} while (1);
+	if (!(flags & KM_MAYFAIL)) {
+		lflags |= __GFP_NOFAIL;
+		lflags &= ~__GFP_NOWARN;
+	}
+
+	return kmalloc(size, lflags);
 }
 
 
