9e7b83ff9c4224629ab3fcb6b1e873723988c434
Gaelan Steele <gbs@canishe.com>
2021-09-20 10:56:49 +0100

    fs: move dirent.h into uapi
    The structures defined in dirent.h are part of Linux's uAPI, but
    it was previously necessary for user code to duplicate the struct
    definitions themselves. Let's make them public.
    
    Signed-off-by: Gaelan Steele <gbs@canishe.com>

--- include/uapi/linux/dirent.h
+++ include/uapi/linux/dirent.h
@@ -1,6 +1,6 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _LINUX_DIRENT_H
-#define _LINUX_DIRENT_H
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI_LINUX_DIRENT_H
+#define _UAPI_LINUX_DIRENT_H
 
 struct linux_dirent {
 	unsigned long	d_ino;
