6c831e8db2ab8035880d45cd0fe43f774a3326e4
Chaitanya Kulkarni <kch@nvidia.com>
2021-09-21 02:21:19 -0700

    loop: use sysfs_emit() in the sysfs partscan show
    Output defects can exist in sysfs content using sprintf and snprintf.
    
    sprintf does not know the PAGE_SIZE maximum of the temporary buffer
    used for outputting sysfs content and it's possible to overrun the
    PAGE_SIZE buffer length.
    
    Use a generic sysfs_emit function that knows that the size of the
    temporary buffer and ensures that no overrun is done for partscan
    attribute.
    
    Signed-off-by: Chaitanya Kulkarni <kch@nvidia.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -875,7 +875,7 @@ static ssize_t loop_attr_partscan_show(struct loop_device *lo, char *buf)
 {
 	int partscan = (lo->lo_flags & LO_FLAGS_PARTSCAN);
 
-	return sprintf(buf, "%s\n", partscan ? "1" : "0");
+	return sysfs_emit(buf, "%s\n", partscan ? "1" : "0");
 }
 
 static ssize_t loop_attr_dio_show(struct loop_device *lo, char *buf)
