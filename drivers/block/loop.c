73e1b8f4cd46d3088b9d9a5f0fc94364b8b2c606
Chaitanya Kulkarni <kch@nvidia.com>
2021-09-21 02:21:20 -0700

    loop: use sysfs_emit() in the sysfs dio show
    Output defects can exist in sysfs content using sprintf and snprintf.
    
    sprintf does not know the PAGE_SIZE maximum of the temporary buffer
    used for outputting sysfs content and it's possible to overrun the
    PAGE_SIZE buffer length.
    
    Use a generic sysfs_emit function that knows that the size of the
    temporary buffer and ensures that no overrun is done for dio
    attribute.
    
    Signed-off-by: Chaitanya Kulkarni <kch@nvidia.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -882,7 +882,7 @@ static ssize_t loop_attr_dio_show(struct loop_device *lo, char *buf)
 {
 	int dio = (lo->lo_flags & LO_FLAGS_DIRECT_IO);
 
-	return sprintf(buf, "%s\n", dio ? "1" : "0");
+	return sysfs_emit(buf, "%s\n", dio ? "1" : "0");
 }
 
 LOOP_ATTR_RO(backing_file);
