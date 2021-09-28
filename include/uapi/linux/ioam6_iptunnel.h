24fd78690879f60a78835a821159b2718ad8c13c
Justin Iurman <justin.iurman@uliege.be>
2021-09-28 21:03:27 +0200

    ipv6: ioam: Add support for the ip6ip6 encapsulation
    This patch adds support for the ip6ip6 encapsulation by providing three encap
    modes: inline, encap and auto.
    
    Signed-off-by: Justin Iurman <justin.iurman@uliege.be>

--- include/uapi/linux/ioam6_iptunnel.h
+++ include/uapi/linux/ioam6_iptunnel.h
@@ -9,9 +9,26 @@
 #ifndef _UAPI_LINUX_IOAM6_IPTUNNEL_H
 #define _UAPI_LINUX_IOAM6_IPTUNNEL_H
 
+#include <linux/in6.h>
+#include <linux/ioam6.h>
+#include <linux/types.h>
+
+enum {
+	IOAM6_IPTUNNEL_MODE_UNSPEC,
+	IOAM6_IPTUNNEL_MODE_INLINE,	/* direct insertion only */
+	IOAM6_IPTUNNEL_MODE_ENCAP,	/* encap (ip6ip6) only */
+	IOAM6_IPTUNNEL_MODE_AUTO,	/* inline or encap based on situation */
+};
+
+struct ioam6_iptunnel_trace {
+	__u8 mode;
+	struct in6_addr tundst;	/* unused for inline mode */
+	struct ioam6_trace_hdr trace;
+};
+
 enum {
 	IOAM6_IPTUNNEL_UNSPEC,
-	IOAM6_IPTUNNEL_TRACE,		/* struct ioam6_trace_hdr */
+	IOAM6_IPTUNNEL_TRACE,
 	__IOAM6_IPTUNNEL_MAX,
 };
 
