3762373b3337c264ebd5dc08a3072bbc8d1e49a7
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2021-10-09 12:38:51 +0800

    scsi scsi_transport_iscsi.c: fix misuse of %llu in scsi_transport_iscsi.c
    Pointers should be printed with %p or %px rather than
    cast to (unsigned long long) and printed with %llu.
    Change %llu to %p to print the pointer into sysfs.
    
    Signed-off-by: Guo Zhi <qtxuning1999@sjtu.edu.cn>

--- drivers/scsi/scsi_transport_iscsi.c
+++ drivers/scsi/scsi_transport_iscsi.c
@@ -129,8 +129,7 @@ show_transport_handle(struct device *dev, struct device_attribute *attr,
 
 	if (!capable(CAP_SYS_ADMIN))
 		return -EACCES;
-	return sysfs_emit(buf, "%llu\n",
-		  (unsigned long long)iscsi_handle(priv->iscsi_transport));
+	return sysfs_emit(buf, "%p\n", priv->iscsi_transport);
 }
 static DEVICE_ATTR(handle, S_IRUGO, show_transport_handle, NULL);
 
