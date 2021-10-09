4ca15c11970e36c4f523337e3d28cd51a3d99ace
Dongdong Liu <liudongdong3@huawei.com>
2021-10-09 18:49:34 +0800

    PCI/sysfs: Add a 10-Bit Tag sysfs file PCIe Endpoint devices
    PCIe spec 5.0 r1.0 section 2.2.6.2 says:
    
      If an Endpoint supports sending Requests to other Endpoints (as
      opposed to host memory), the Endpoint must not send 10-Bit Tag
      Requests to another given Endpoint unless an implementation-specific
      mechanism determines that the Endpoint supports 10-Bit Tag Completer
      capability.
    
    Add a 10bit_tag sysfs file, write 0 to disable 10-Bit Tag Requester
    when the driver does not bind the device. The typical use case is for
    p2pdma when the peer device does not support 10-Bit Tag Completer.
    Write 1 to enable 10-Bit Tag Requester when RC supports 10-Bit Tag
    Completer capability. The typical use case is for host memory targeted
    by DMA Requests. The 10bit_tag file content indicate current status of
    10-Bit Tag Requester Enable.
    
    Signed-off-by: Dongdong Liu <liudongdong3@huawei.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -2042,6 +2042,20 @@ static void pci_configure_mps(struct pci_dev *dev)
 		 p_mps, mps, mpss);
 }
 
+bool pcie_rp_10bit_tag_cmp_supported(struct pci_dev *dev)
+{
+	struct pci_dev *root;
+
+	root = pcie_find_root_port(dev);
+	if (!root)
+		return false;
+
+	if (!(root->devcap2 & PCI_EXP_DEVCAP2_10BIT_TAG_COMP))
+		return false;
+
+	return true;
+}
+
 int pci_configure_extended_tags(struct pci_dev *dev, void *ign)
 {
 	struct pci_host_bridge *host;
