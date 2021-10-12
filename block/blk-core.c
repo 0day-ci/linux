39a278a3981459122f56f4f496c3bb0c0b4c825b
Pankaj Raghav <pankydev8@gmail.com>
2021-10-12 09:42:43 +0200

    block: Remove the redundant empty list check in blk_flush_plug_list
    The callee: blk_mq_flush_plug_list already has an empty list check for
    plug->mq_list. Hence, removed the check for empty list from the caller.
    
    Signed-off-by: Pankaj Raghav <p.raghav@samsung.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -1724,8 +1724,8 @@ void blk_flush_plug_list(struct blk_plug *plug, bool from_schedule)
 {
 	flush_plug_callbacks(plug, from_schedule);
 
-	if (!list_empty(&plug->mq_list))
-		blk_mq_flush_plug_list(plug, from_schedule);
+	blk_mq_flush_plug_list(plug, from_schedule);
+
 	if (unlikely(!from_schedule && plug->cached_rq))
 		blk_mq_free_plug_rqs(plug);
 }
