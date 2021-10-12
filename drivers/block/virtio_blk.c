a449f9f87c135f3e14556feaa376b5cc8755d5bb
Jason Wang <jasowang@redhat.com>
2021-10-12 14:52:26 +0800

    virtio-blk: don't let virtio core to validate used length
    We never tries to use used length, so the patch prevents the virtio
    core from validating used length.
    
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- drivers/block/virtio_blk.c
+++ drivers/block/virtio_blk.c
@@ -1007,6 +1007,7 @@ static struct virtio_driver virtio_blk = {
 	.feature_table_size		= ARRAY_SIZE(features),
 	.feature_table_legacy		= features_legacy,
 	.feature_table_size_legacy	= ARRAY_SIZE(features_legacy),
+	.validate_used			= true,
 	.driver.name			= KBUILD_MODNAME,
 	.driver.owner			= THIS_MODULE,
 	.id_table			= id_table,
