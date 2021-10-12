eab046bc632d697b2a7642722c1945ba3f3721fc
Jason Wang <jasowang@redhat.com>
2021-10-12 14:52:17 +0800

    virtio: add doc for validate() method
    This patch adds doc for validate() method.
    
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- include/linux/virtio.h
+++ include/linux/virtio.h
@@ -152,6 +152,7 @@ size_t virtio_max_dma_size(struct virtio_device *vdev);
  * @feature_table_size: number of entries in the feature table array.
  * @feature_table_legacy: same as feature_table but when working in legacy mode.
  * @feature_table_size_legacy: number of entries in feature table legacy array.
+ * @validate: optional function to do early checks
  * @probe: the function to call when a device is found.  Returns 0 or -errno.
  * @scan: optional function to call after successful probe; intended
  *    for virtio-scsi to invoke a scan.
