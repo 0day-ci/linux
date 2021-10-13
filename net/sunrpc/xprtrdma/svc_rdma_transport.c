2f547148f73f9f45442ae047bd5b2adc01504ca4
Chuck Lever <chuck.lever@oracle.com>
2021-10-13 10:47:09 -0400

    svcrdma: Remove dprintk call site in svc_rdma_parse_connect_private()
    Deprecation. The effect of the RPCRDMA_CMP_F_SND_W_INV_OK setting is
    obvious from subsequent server activity.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- net/sunrpc/xprtrdma/svc_rdma_transport.c
+++ net/sunrpc/xprtrdma/svc_rdma_transport.c
@@ -164,16 +164,9 @@ svc_rdma_parse_connect_private(struct svcxprt_rdma *newxprt,
 
 	if (pmsg &&
 	    pmsg->cp_magic == rpcrdma_cmp_magic &&
-	    pmsg->cp_version == RPCRDMA_CMP_VERSION) {
+	    pmsg->cp_version == RPCRDMA_CMP_VERSION)
 		newxprt->sc_snd_w_inv = pmsg->cp_flags &
 					RPCRDMA_CMP_F_SND_W_INV_OK;
-
-		dprintk("svcrdma: client send_size %u, recv_size %u "
-			"remote inv %ssupported\n",
-			rpcrdma_decode_buffer_size(pmsg->cp_send_size),
-			rpcrdma_decode_buffer_size(pmsg->cp_recv_size),
-			newxprt->sc_snd_w_inv ? "" : "un");
-	}
 }
 
 /*
