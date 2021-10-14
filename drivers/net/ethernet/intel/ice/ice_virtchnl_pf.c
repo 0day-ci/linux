d0be073aa722349480379347a88d478d0bef2912
Dan Carpenter <dan.carpenter@oracle.com>
2021-10-14 13:18:18 -0700

    ice: fix an error code in ice_ena_vfs()
    Return the error code if ice_eswitch_configure() fails.  Don't return
    success.
    
    Fixes: 1c54c839935b ("ice: enable/disable switchdev when managing VFs")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>

--- drivers/net/ethernet/intel/ice/ice_virtchnl_pf.c
+++ drivers/net/ethernet/intel/ice/ice_virtchnl_pf.c
@@ -1903,7 +1903,8 @@ static int ice_ena_vfs(struct ice_pf *pf, u16 num_vfs)
 
 	clear_bit(ICE_VF_DIS, pf->state);
 
-	if (ice_eswitch_configure(pf))
+	ret = ice_eswitch_configure(pf);
+	if (ret)
 		goto err_unroll_sriov;
 
 	/* rearm global interrupts */
