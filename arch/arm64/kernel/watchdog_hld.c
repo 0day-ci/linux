4c319c41e49036be58068395965bd33a5de02396
Sumit Garg <sumit.garg@linaro.org>
2021-10-14 10:41:55 +0800

    arm64: Enable perf events based hard lockup detector
    With the recent feature added to enable perf events to use pseudo NMIs
    as interrupts on platforms which support GICv3 or later, its now been
    possible to enable hard lockup detector (or NMI watchdog) on arm64
    platforms. So enable corresponding support.
    
    One thing to note here is that normally lockup detector is initialized
    just after the early initcalls but PMU on arm64 comes up much later as
    device_initcall(). To cope with that, overriding watchdog_nmi_probe() to
    let the watchdog framework know PMU not ready, and inform the framework
    to re-initialize lockup detection once PMU has been initialized.
    
    [1]: http://lore.kernel.org/linux-arm-kernel/1610712101-14929-1-git-send-email-sumit.garg@linaro.org
    
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>
    (Pingfan: adapt it to watchdog_hld async model based on [1])
    Co-developed-by: Pingfan Liu <kernelfans@gmail.com>
    Signed-off-by: Pingfan Liu <kernelfans@gmail.com>
    Cc: Sumit Garg <sumit.garg@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Masahiro Yamada <masahiroy@kernel.org>
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Wang Qing <wangqing@vivo.com>
    Cc: "Peter Zijlstra (Intel)" <peterz@infradead.org>
    Cc: Santosh Sivaraj <santosh@fossix.org>
    Cc: linux-kernel@vger.kernel.org
    To: linux-arm-kernel@lists.infradead.org

---
+++ arch/arm64/kernel/watchdog_hld.c
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/nmi.h>
+#include <linux/cpufreq.h>
+#include <linux/perf/arm_pmu.h>
+
+/*
+ * Safe maximum CPU frequency in case a particular platform doesn't implement
+ * cpufreq driver. Although, architecture doesn't put any restrictions on
+ * maximum frequency but 5 GHz seems to be safe maximum given the available
+ * Arm CPUs in the market which are clocked much less than 5 GHz. On the other
+ * hand, we can't make it much higher as it would lead to a large hard-lockup
+ * detection timeout on parts which are running slower (eg. 1GHz on
+ * Developerbox) and doesn't possess a cpufreq driver.
+ */
+#define SAFE_MAX_CPU_FREQ	5000000000UL // 5 GHz
+u64 hw_nmi_get_sample_period(int watchdog_thresh)
+{
+	unsigned int cpu = smp_processor_id();
+	unsigned long max_cpu_freq;
+
+	max_cpu_freq = cpufreq_get_hw_max_freq(cpu) * 1000UL;
+	if (!max_cpu_freq)
+		max_cpu_freq = SAFE_MAX_CPU_FREQ;
+
+	return (u64)max_cpu_freq * watchdog_thresh;
+}
+
+int __init watchdog_nmi_probe(void)
+{
+	if (detector_delay_init_state != DELAY_INIT_READY)
+		return -EBUSY;
+	else if (!arm_pmu_irq_is_nmi())
+		return -ENODEV;
+
+	return hardlockup_detector_perf_init();
+}
