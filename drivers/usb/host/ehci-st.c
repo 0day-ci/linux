eb3e874344819a4c6a32585ff51f99051b332b2a
Sergey Shtylyov <s.shtylyov@omp.ru>
2021-10-18 21:39:15 +0300

    usb: host: ehci-st: deny IRQ0
    If platform_get_irq() returns IRQ0 (considered invalid according to Linus)
    the driver blithely passes it to usb_add_hcd() that treats IRQ0 as no IRQ
    at all. Deny IRQ0 right away, returning -EINVAL from the probe() method...
    
    Fixes: e47c5a0906f9 ("usb: host: ehci-st: Add EHCI support for ST STB devices")
    Signed-off-by: Sergey Shtylyov <s.shtylyov@omp.ru>

--- drivers/usb/host/ehci-st.c
+++ drivers/usb/host/ehci-st.c
@@ -160,6 +160,8 @@ static int st_ehci_platform_probe(struct platform_device *dev)
 	irq = platform_get_irq(dev, 0);
 	if (irq < 0)
 		return irq;
+	if (!irq)
+		return -EINVAL;
 	res_mem = platform_get_resource(dev, IORESOURCE_MEM, 0);
 	if (!res_mem) {
 		dev_err(&dev->dev, "no memory resource provided");
