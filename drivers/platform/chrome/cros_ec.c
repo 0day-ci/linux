d2c1956b7b20b3b031619b800d1a3fffe98562fa
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2021-10-19 22:38:50 +0200

    platform/chrome: cros_ec: Make cros_ec_unregister() return void
    Up to now cros_ec_unregister() returns zero unconditionally. Make it
    return void instead which makes it easier to see in the callers that
    there is no error to handle.
    
    Also the return value of i2c, platform and spi remove callbacks is
    ignored anyway.
    
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- drivers/platform/chrome/cros_ec.c
+++ drivers/platform/chrome/cros_ec.c
@@ -302,7 +302,7 @@ EXPORT_SYMBOL(cros_ec_register);
  *
  * Return: 0 on success or negative error code.
  */
-int cros_ec_unregister(struct cros_ec_device *ec_dev)
+void cros_ec_unregister(struct cros_ec_device *ec_dev)
 {
 	if (ec_dev->pd)
 		platform_device_unregister(ec_dev->pd);
