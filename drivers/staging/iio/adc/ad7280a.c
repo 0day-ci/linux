fbca0eca4ba9d3ecbe658f25110dc8e9e10d3bc0
sesankm <sesank.mallikarjuna@gmail.com>
2021-10-20 01:10:27 -0500

    Staging: iio: adc: ad7280a: fixed coding style
    fixed issue with coding style
    
    Signed-off-by: sesankm <26676400+sesankm@users.noreply.github.com>

--- drivers/staging/iio/adc/ad7280a.c
+++ drivers/staging/iio/adc/ad7280a.c
@@ -829,14 +829,14 @@ out:
  * The function argument is stringified and doesn't need a fix
  */
 static IIO_DEVICE_ATTR_NAMED(in_thresh_low_value,
-			     in_voltage-voltage_thresh_low_value,
+			     in_voltage - voltage_thresh_low_value,
 			     0644,
 			     ad7280_read_channel_config,
 			     ad7280_write_channel_config,
 			     AD7280A_CELL_UNDERVOLTAGE);
 
 static IIO_DEVICE_ATTR_NAMED(in_thresh_high_value,
-			     in_voltage-voltage_thresh_high_value,
+			     in_voltage - voltage_thresh_high_value,
 			     0644,
 			     ad7280_read_channel_config,
 			     ad7280_write_channel_config,
