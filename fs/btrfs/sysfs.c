641d77504697746ea54d4fea27b40a24852e0c36
Anand Jain <anand.jain@oracle.com>
2021-10-21 23:31:17 +0800

    btrfs: sysfs add devinfo/fsid to retrieve fsid from the device
    In the case of the seed device, the fsid can be different from the mounted
    sprout fsid.  The userland has to read the device superblock to know the
    fsid but, that idea fails if the device is missing. So add a sysfs
    interface devinfo/<devid>/fsid to show the fsid of the device.
    
    For example:
     $ cd /sys/fs/btrfs/b10b02a5-f9de-4276-b9e8-2bfd09a578a8
    
     $ cat devinfo/1/fsid
     c44d771f-639d-4df3-99ec-5bc7ad2af93b
     $ cat  devinfo/3/fsid
     b10b02a5-f9de-4276-b9e8-2bfd09a578a8
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/sysfs.c
+++ fs/btrfs/sysfs.c
@@ -1539,6 +1539,16 @@ static ssize_t btrfs_devinfo_writeable_show(struct kobject *kobj,
 }
 BTRFS_ATTR(devid, writeable, btrfs_devinfo_writeable_show);
 
+static ssize_t btrfs_devinfo_fsid_show(struct kobject *kobj,
+				       struct kobj_attribute *a, char *buf)
+{
+	struct btrfs_device *device = container_of(kobj, struct btrfs_device,
+						   devid_kobj);
+
+	return sysfs_emit(buf, "%pU\n", device->fs_devices->fsid);
+}
+BTRFS_ATTR(devid, fsid, btrfs_devinfo_fsid_show);
+
 static ssize_t btrfs_devinfo_error_stats_show(struct kobject *kobj,
 		struct kobj_attribute *a, char *buf)
 {
@@ -1579,6 +1589,7 @@ static struct attribute *devid_attrs[] = {
 	BTRFS_ATTR_PTR(devid, replace_target),
 	BTRFS_ATTR_PTR(devid, scrub_speed_max),
 	BTRFS_ATTR_PTR(devid, writeable),
+	BTRFS_ATTR_PTR(devid, fsid),
 	NULL
 };
 ATTRIBUTE_GROUPS(devid);
