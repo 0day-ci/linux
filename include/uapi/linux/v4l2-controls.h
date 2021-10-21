014607ac2112aed988e4b5c715585fbc9f143d80
Marek Vasut <marex@denx.de>
2021-10-21 18:03:08 +0200

    media: i2c: isl7998x: Add driver for Intersil ISL7998x
    Add driver for the Intersil ISL7998x Analog to MIPI CSI-2/BT656 decoder.
    This chip supports 1/2/4 analog video inputs and converts them into
    1/2/4 VCs in MIPI CSI2 stream.
    
    This driver currently supports ISL79987 and both 720x480 and 720x576
    resolutions, however as per specification, all inputs must use the
    same resolution and standard. The only supported pixel format is now
    YUYV/YUV422. The chip should support RGB565 on the CSI2 as well, but
    this is currently unsupported.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Sakari Ailus <sakari.ailus@linux.intel.com>
    Cc: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    Cc: Rob Herring <robh+dt@kernel.org>
    To: linux-media@vger.kernel.org
    Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -212,6 +212,12 @@ enum v4l2_colorfx {
  */
 #define V4L2_CID_USER_CCS_BASE			(V4L2_CID_USER_BASE + 0x10f0)
 
+/*
+ * The base for the isl7998x driver controls.
+ * We reserve 16 controls for this driver.
+ */
+#define V4L2_CID_USER_ISL7998X_BASE		(V4L2_CID_USER_BASE + 0x1180)
+
 /* MPEG-class control IDs */
 /* The MPEG controls are applicable to all codec controls
  * and the 'MPEG' part of the define is historical */
