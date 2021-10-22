a8e58cd6fd8b4fb2f2b5d7192a471674fcba9947
Cai Huoqing <caihuoqing@baidu.com>
2021-10-22 10:57:08 +0800

    trace/osnoise: Make use of the helper function kthread_run_on_cpu()
    Replace kthread_create_on_cpu/wake_up_process()
    with kthread_run_on_cpu() to simplify the code.
    
    Signed-off-by: Cai Huoqing <caihuoqing@baidu.com>

--- kernel/trace/trace_osnoise.c
+++ kernel/trace/trace_osnoise.c
@@ -1525,7 +1525,7 @@ static int start_kthread(unsigned int cpu)
 #else
 	snprintf(comm, 24, "osnoise/%d", cpu);
 #endif
-	kthread = kthread_create_on_cpu(main, NULL, cpu, comm);
+	kthread = kthread_run_on_cpu(main, NULL, cpu, comm);
 
 	if (IS_ERR(kthread)) {
 		pr_err(BANNER "could not start sampling thread\n");
@@ -1534,7 +1534,6 @@ static int start_kthread(unsigned int cpu)
 	}
 
 	per_cpu(per_cpu_osnoise_var, cpu).kthread = kthread;
-	wake_up_process(kthread);
 
 	return 0;
 }
