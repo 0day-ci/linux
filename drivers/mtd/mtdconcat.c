f8b2d57aaf5467863e079b99c2befac716145289
Sean Nyekjaer <sean@geanix.com>
2021-10-25 11:27:50 +0200

    mtd: mtdconcat: add suspend lock handling
    Use MTD hooks to control suspend/resume of MTD devices.
    concat_{suspend,resume} will be called from mtd_{suspend,resume},
    which already have taken the suspend/resume lock.
    It's safe to proceed with calling MTD device hooks directly from here.
    
    Fixes: 013e6292aaf5 ("mtd: rawnand: Simplify the locking")
    Suggested-by: Boris Brezillon <boris.brezillon@collabora.com>
    Signed-off-by: Sean Nyekjaer <sean@geanix.com>

--- drivers/mtd/mtdconcat.c
+++ drivers/mtd/mtdconcat.c
@@ -566,9 +566,15 @@ static int concat_suspend(struct mtd_info *mtd)
 
 	for (i = 0; i < concat->num_subdev; i++) {
 		struct mtd_info *subdev = concat->subdev[i];
-		if ((rc = mtd_suspend(subdev)) < 0)
+		/*
+		 * Call MTD hook directly from here,
+		 * mtd_suspend() have the suspend/resume lock.
+		 */
+		rc = subdev->_suspend ? subdev->_suspend(subdev) : 0;
+		if (rc < 0)
 			return rc;
 	}
+
 	return rc;
 }
 
@@ -579,7 +585,12 @@ static void concat_resume(struct mtd_info *mtd)
 
 	for (i = 0; i < concat->num_subdev; i++) {
 		struct mtd_info *subdev = concat->subdev[i];
-		mtd_resume(subdev);
+		/*
+		 * Call MTD hook directly from here,
+		 * mtd_resume() have the suspend/resume lock.
+		 */
+		if (subdev->_resume)
+			subdev->_resume(subdev);
 	}
 }
 
