d75d8f99dddfbd661e54621a9593d762413ff211
Vincent Mailhol <mailhol.vincent@wanadoo.fr>
2021-10-26 02:22:45 +0900

    can: dev: add sanity check in can_set_static_ctrlmode()
    Previous patch removed can_priv::ctrlmode_static to replace it with
    can_get_static_ctrlmode().
    
    A condition sine qua non for this to work is that the controller
    static modes should never be set in can_priv::ctrlmode_supported
    (c.f. the comment on can_priv::ctrlmode_supported which states that it
    is for "options that can be *modified* by netlink"). Also, this
    condition is already correctly fulfilled by all existing drivers
    which rely on the ctrlmode_static feature.
    
    Nonetheless, we added an extra safeguard in can_set_static_ctrlmode()
    to return an error value and to warn the developer who would be
    adventurous enough to set to static a given feature that is already
    set to supported.
    
    The drivers which rely on the static controller mode are then updated
    to check the return value of can_set_static_ctrlmode().
    
    Link: https://lore.kernel.org/all/20211025172247.1774451-3-mailhol.vincent@wanadoo.fr
    Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -1463,7 +1463,7 @@ static bool m_can_niso_supported(struct m_can_classdev *cdev)
 static int m_can_dev_setup(struct m_can_classdev *cdev)
 {
 	struct net_device *dev = cdev->net;
-	int m_can_version;
+	int m_can_version, err;
 
 	m_can_version = m_can_check_core_release(cdev);
 	/* return if unsupported version */
@@ -1493,13 +1493,17 @@ static int m_can_dev_setup(struct m_can_classdev *cdev)
 	switch (cdev->version) {
 	case 30:
 		/* CAN_CTRLMODE_FD_NON_ISO is fixed with M_CAN IP v3.0.x */
-		can_set_static_ctrlmode(dev, CAN_CTRLMODE_FD_NON_ISO);
+		err = can_set_static_ctrlmode(dev, CAN_CTRLMODE_FD_NON_ISO);
+		if (err)
+			return err;
 		cdev->can.bittiming_const = &m_can_bittiming_const_30X;
 		cdev->can.data_bittiming_const = &m_can_data_bittiming_const_30X;
 		break;
 	case 31:
 		/* CAN_CTRLMODE_FD_NON_ISO is fixed with M_CAN IP v3.1.x */
-		can_set_static_ctrlmode(dev, CAN_CTRLMODE_FD_NON_ISO);
+		err = can_set_static_ctrlmode(dev, CAN_CTRLMODE_FD_NON_ISO);
+		if (err)
+			return err;
 		cdev->can.bittiming_const = &m_can_bittiming_const_31X;
 		cdev->can.data_bittiming_const = &m_can_data_bittiming_const_31X;
 		break;
