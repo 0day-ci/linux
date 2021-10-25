090f0885f5318891bc3f99de19e839959b1418c4
Jiasheng Jiang <jiasheng@iscas.ac.cn>
2021-10-25 07:31:18 +0000

    seq_file: Fix potential addition overflow
    After seq_hlist_start_percpu(), the value of &iter->li_cpu might be
    MAX_UINT.
    In that case, there will be addition overflow in the cpumask_next().
    Therefore, it might be better to add the check before.
    
    Fixes: 0bc7738 ("seq_file: add seq_list_*_percpu helpers")
    Signed-off-by: Jiasheng Jiang <jiasheng@iscas.ac.cn>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -1117,6 +1117,9 @@ seq_hlist_next_percpu(void *v, struct hlist_head __percpu *head,
 	if (node->next)
 		return node->next;
 
+	if (*cpu >= nr_cpu_ids)
+		return NULL;
+
 	for (*cpu = cpumask_next(*cpu, cpu_possible_mask); *cpu < nr_cpu_ids;
 	     *cpu = cpumask_next(*cpu, cpu_possible_mask)) {
 		struct hlist_head *bucket = per_cpu_ptr(head, *cpu);
