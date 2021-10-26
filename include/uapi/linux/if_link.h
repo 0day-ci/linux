763554c403b1ae91104d9f48f2fb699e444cbf87
Maciej Machnikowski <maciej.machnikowski@intel.com>
2021-10-26 19:31:43 +0200

    rtnetlink: Add new RTM_GETEECSTATE message to get SyncE status
    This patch series introduces basic interface for reading the Ethernet
    Equipment Clock (EEC) state on a SyncE capable device. This state gives
    information about the state of EEC. This interface is required to
    implement Synchronization Status Messaging on upper layers.
    
    Initial implementation returns SyncE EEC state and flags attributes.
    The only flag currently implemented is the EEC_SRC_PORT. When it's set
    the EEC is synchronized to the recovered clock recovered from the
    current port.
    
    SyncE EEC state read needs to be implemented as a ndo_get_eec_state
    function.
    
    Signed-off-by: Maciej Machnikowski <maciej.machnikowski@intel.com>

--- include/uapi/linux/if_link.h
+++ include/uapi/linux/if_link.h
@@ -1273,4 +1273,31 @@ enum {
 
 #define IFLA_MCTP_MAX (__IFLA_MCTP_MAX - 1)
 
+/* SyncE section */
+
+enum if_eec_state {
+	IF_EEC_STATE_INVALID = 0,
+	IF_EEC_STATE_FREERUN,
+	IF_EEC_STATE_LOCKED,
+	IF_EEC_STATE_LOCKED_HO_ACQ,
+	IF_EEC_STATE_HOLDOVER,
+};
+
+#define EEC_SRC_PORT		(1 << 0) /* recovered clock from the port is
+					  * currently the source for the EEC
+					  */
+
+struct if_eec_state_msg {
+	__u32 ifindex;
+};
+
+enum {
+	IFLA_EEC_UNSPEC,
+	IFLA_EEC_STATE,
+	IFLA_EEC_SRC_IDX,
+	__IFLA_EEC_MAX,
+};
+
+#define IFLA_EEC_MAX (__IFLA_EEC_MAX - 1)
+
 #endif /* _UAPI_LINUX_IF_LINK_H */
