cee86877024eba5190c7dd331b22a4907b3d691c
Sandeep Maheswaram <quic_c_sanm@quicinc.com>
2021-10-26 23:49:31 +0530

    usb: host: xhci-plat: Add device property to set XHCI_SKIP_PHY_INIT quirk
    Add device property usb-skip-phy-init to check and set XHCI_SKIP_PHY_INIT
    quirk.
    
    Signed-off-by: Sandeep Maheswaram <quic_c_sanm@quicinc.com>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -323,6 +323,9 @@ static int xhci_plat_probe(struct platform_device *pdev)
 		if (device_property_read_bool(tmpdev, "quirk-broken-port-ped"))
 			xhci->quirks |= XHCI_BROKEN_PORT_PED;
 
+		if (device_property_read_bool(tmpdev, "usb-skip-phy-init"))
+			xhci->quirks |= XHCI_SKIP_PHY_INIT;
+
 		device_property_read_u32(tmpdev, "imod-interval-ns",
 					 &xhci->imod_interval);
 	}
