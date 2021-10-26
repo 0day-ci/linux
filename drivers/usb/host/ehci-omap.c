99339edc4591ee2104acf45913d0dcb2a75bf1bf
Sergey Shtylyov <s.shtylyov@omp.ru>
2021-10-26 20:39:25 +0300

    usb: host: ehci-omap: deny IRQ0
    If platform_get_irq() returns IRQ0 (considered invalid according to Linus)
    the driver blithely passes it to usb_add_hcd() that treats IRQ0 as no IRQ
    at all. Deny IRQ0 right away, returning -EINVAL from the probe() method...
    
    Fixes: b33f37064b74 ("USB: host: ehci: introduce omap ehci-hcd driver")
    Signed-off-by: Sergey Shtylyov <s.shtylyov@omp.ru>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>

--- drivers/usb/host/ehci-omap.c
+++ drivers/usb/host/ehci-omap.c
@@ -117,6 +117,8 @@ static int ehci_hcd_omap_probe(struct platform_device *pdev)
 	irq = platform_get_irq(pdev, 0);
 	if (irq < 0)
 		return irq;
+	if (!irq)
+		return -ENIVAL;
 
 	res =  platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	regs = devm_ioremap_resource(dev, res);
