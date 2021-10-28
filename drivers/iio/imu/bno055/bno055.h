734efd9783b775998fe2a1acee56eb79c664c16d
Andrea Merello <andrea.merello@gmail.com>
2021-10-28 12:18:37 +0200

    iio: imu: add Bosch Sensortec BNO055 core driver
    This patch adds a core driver for the BNO055 IMU from Bosch. This IMU
    can be connected via both serial and I2C busses; separate patches will
    add support for them.
    
    The driver supports "AMG" (Accelerometer, Magnetometer, Gyroscope) mode,
    that provides raw data from the said internal sensors, and a couple of
    "fusion" modes (i.e. the IMU also do calculations in order to provide
    euler angles, quaternions, linear acceleration and gravity measurements).
    
    In fusion modes the AMG data is still available (with some calibration
    refinements done by the IMU), but certain settings such as low pass
    filters cut-off frequency and sensors ranges are fixed, while in AMG mode
    they can be customized; this is why AMG mode can still be interesting.
    
    Signed-off-by: Andrea Merello <andrea.merello@iit.it>

---
+++ drivers/iio/imu/bno055/bno055.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef __BNO055_H__
+#define __BNO055_H__
+
+#include <linux/regmap.h>
+
+struct device;
+int bno055_probe(struct device *dev, struct regmap *regmap,
+		 int xfer_burst_break_thr);
+extern const struct regmap_config bno055_regmap_config;
+
+#endif
