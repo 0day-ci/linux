823676337c7296d27d38957169f1cc4d87b0ab1f
Andrea Merello <andrea.merello@gmail.com>
2021-10-28 12:18:35 +0200

    iio: add modifers for pitch, yaw, roll
    This patch adds modifiers for reporting rotations as euler angles (i.e.
    yaw, pitch and roll).
    
    Signed-off-by: Andrea Merello <andrea.merello@iit.it>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -136,7 +136,10 @@ static const char * const iio_modifier_names[] = {
 	[IIO_MOD_O2] = "o2",
 	[IIO_MOD_ACCEL_LINEAR_X] = "linear_x",
 	[IIO_MOD_ACCEL_LINEAR_Y] = "linear_y",
-	[IIO_MOD_ACCEL_LINEAR_Z] = "linear_z"
+	[IIO_MOD_ACCEL_LINEAR_Z] = "linear_z",
+	[IIO_MOD_PITCH] = "pitch",
+	[IIO_MOD_YAW] = "yaw",
+	[IIO_MOD_ROLL] = "roll"
 };
 
 /* relies on pairs of these shared then separate */
