442831987f34576a8739aaa6e992ae779e0dcb4b
Ming Lei <ming.lei@redhat.com>
2021-11-01 19:25:47 +0800

    livepatch: free klp_patch object without holding klp_mutex
    kobject_del() is called from kobject_put(), and after the klp_patch
    kobject is deleted, any show()/store() are done.
    
    Once the klp_patch object is removed from list and prepared for
    releasing, no need to hold the global mutex of klp_mutex, so
    move the freeing outside of klp_mutex.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- kernel/livepatch/core.h
+++ kernel/livepatch/core.h
@@ -13,8 +13,7 @@ extern struct list_head klp_patches;
 #define klp_for_each_patch(patch)	\
 	list_for_each_entry(patch, &klp_patches, list)
 
-void klp_free_patch_async(struct klp_patch *patch);
-void klp_free_replaced_patches_async(struct klp_patch *new_patch);
+void klp_free_patches_async(struct list_head *to_free);
 void klp_unpatch_replaced_patches(struct klp_patch *new_patch);
 void klp_discard_nops(struct klp_patch *new_patch);
 
