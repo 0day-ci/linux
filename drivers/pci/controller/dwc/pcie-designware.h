933fb89ec34e641c98594b1e7b84fbe02227f292
Richard Zhu <hongxing.zhu@nxp.com>
2021-11-01 14:17:56 +0800

    PCI: dwc: Add dw_pcie_host_ops.host_exit() callback
    When link is never came up in the link training after host_init.
    The clocks and power supplies usage counter balance should be handled
    properly on some DWC platforms (for example, i.MX PCIe).
    
    Add a new host_exit() callback into dw_pcie_host_ops, then it could be
    invoked to handle the unbalance issue in the error handling after
    host_init() function when link is down.
    
    Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>

--- drivers/pci/controller/dwc/pcie-designware.h
+++ drivers/pci/controller/dwc/pcie-designware.h
@@ -174,6 +174,7 @@ enum dw_pcie_device_mode {
 
 struct dw_pcie_host_ops {
 	int (*host_init)(struct pcie_port *pp);
+	void (*host_exit)(struct pcie_port *pp);
 	int (*msi_host_init)(struct pcie_port *pp);
 };
 
