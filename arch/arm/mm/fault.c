2bce1fbe26c0463f6ed4fcd7b6f26ecbe2a22438
Kefeng Wang <wangkefeng.wang@huawei.com>
2021-11-03 21:38:45 +0800

    ARM: Support KFENCE for ARM
    Add architecture specific implementation details for KFENCE and enable
    KFENCE on ARM. In particular, this implements the required interface in
     <asm/kfence.h>.
    
    KFENCE requires that attributes for pages from its memory pool can
    individually be set. Therefore, force the kfence pool to be mapped
    at page granularity.
    
    Testing this patch using the testcases in kfence_test.c and all passed
    with or without ARM_LPAE.
    
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -17,6 +17,7 @@
 #include <linux/sched/debug.h>
 #include <linux/highmem.h>
 #include <linux/perf_event.h>
+#include <linux/kfence.h>
 
 #include <asm/system_misc.h>
 #include <asm/system_info.h>
@@ -99,6 +100,11 @@ void show_pte(const char *lvl, struct mm_struct *mm, unsigned long addr)
 { }
 #endif					/* CONFIG_MMU */
 
+static inline bool is_write_fault(unsigned int fsr)
+{
+	return (fsr & FSR_WRITE) && !(fsr & FSR_CM);
+}
+
 static void die_kernel_fault(const char *msg, struct mm_struct *mm,
 			     unsigned long addr, unsigned int fsr,
 			     struct pt_regs *regs)
@@ -131,10 +137,14 @@ __do_kernel_fault(struct mm_struct *mm, unsigned long addr, unsigned int fsr,
 	/*
 	 * No handler, we'll have to terminate things with extreme prejudice.
 	 */
-	if (addr < PAGE_SIZE)
+	if (addr < PAGE_SIZE) {
 		msg = "NULL pointer dereference";
-	else
+	} else {
+		if (kfence_handle_page_fault(addr, is_write_fault(fsr), regs))
+			return;
+
 		msg = "paging request";
+	}
 
 	die_kernel_fault(msg, mm, addr, fsr, regs);
 }
@@ -205,11 +215,6 @@ static inline bool is_permission_fault(unsigned int fsr)
 		return true;
 #endif
 	return false;
-}
-
-static inline bool is_write_fault(unsigned int fsr)
-{
-	return (fsr & FSR_WRITE) && !(fsr & FSR_CM);
 }
 
 static vm_fault_t __kprobes
