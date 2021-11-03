04f0ed915eb5297537d75de5025b90049f7220c6
Sakari Ailus <sakari.ailus@linux.intel.com>
2021-11-03 15:34:05 +0200

    ACPI: Get acpi_device's parent from the parent field
    Printk modifier %pfw is used to print the full path of the device name.
    This is obtained device by device until a device no longer has a parent.
    
    On ACPI getting the parent fwnode is done by calling acpi_get_parent()
    which tries to down() a semaphore. But local IRQs are now disabled in
    vprintk_store() before the mutex is acquired. This is obviously a problem.
    
    Luckily struct acpi_device has a parent field already. Use that field to
    get the parent instead of relying on acpi_get_parent().
    
    Fixes: 002eb6ad0751 ("printk: track/limit recursion")
    Cc: stable@vger.kernel.org # v5.15 and up
    Depends-on: ("ACPI: Make acpi_fwnode_handle safer")
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/acpi/property.c
+++ drivers/acpi/property.c
@@ -1089,16 +1089,12 @@ struct fwnode_handle *acpi_node_get_parent(const struct fwnode_handle *fwnode)
 	if (is_acpi_data_node(fwnode)) {
 		/* All data nodes have parent pointer so just return that */
 		return to_acpi_data_node(fwnode)->parent;
-	} else if (is_acpi_device_node(fwnode)) {
-		acpi_handle handle, parent_handle;
+	}
 
-		handle = to_acpi_device_node(fwnode)->handle;
-		if (ACPI_SUCCESS(acpi_get_parent(handle, &parent_handle))) {
-			struct acpi_device *adev;
+	if (is_acpi_device_node(fwnode)) {
+		struct acpi_device *device = to_acpi_device_node(fwnode);
 
-			if (!acpi_bus_get_device(parent_handle, &adev))
-				return acpi_fwnode_handle(adev);
-		}
+		return acpi_fwnode_handle(device->parent);
 	}
 
 	return NULL;
