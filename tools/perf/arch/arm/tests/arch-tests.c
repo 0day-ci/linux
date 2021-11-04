3f1a17911ddbc878af98f265cdf06f24d2f0c5e1
Ian Rogers <irogers@google.com>
2021-11-03 23:41:49 -0700

    perf test: Make each test/suite its own struct.
    By switching to an array of pointers to tests (later to be suites)
    the definition of the tests can be moved to the file containing the
    tests.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/arch/arm/tests/arch-tests.c
+++ tools/perf/arch/arm/tests/arch-tests.c
@@ -3,18 +3,12 @@
 #include "tests/tests.h"
 #include "arch-tests.h"
 
-struct test arch_tests[] = {
+DEFINE_SUITE("Vectors page", vectors_page);
+
+struct test *arch_tests[] = {
 #ifdef HAVE_DWARF_UNWIND_SUPPORT
-	{
-		.desc = "DWARF unwind",
-		.func = test__dwarf_unwind,
-	},
+	&dwarf_unwind,
 #endif
-	{
-		.desc = "Vectors page",
-		.func = test__vectors_page,
-	},
-	{
-		.func = NULL,
-	},
+	&vectors_pages,
+	NULL,
 };
