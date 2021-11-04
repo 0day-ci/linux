ca3213ddffb73da26e79f9ed0bff011881f2fc6a
Song Liu <songliubraving@fb.com>
2021-11-04 00:00:16 -0700

    selftests/bpf: add tests for bpf_find_vma
    Add tests for bpf_find_vma in perf_event program and kprobe program. The
    perf_event program is triggered from NMI context, so the second call of
    bpf_find_vma() will return -EBUSY (irq_work busy). The kprobe program,
    on the other hand, does not have this constraint.
    
    Also add test for illegal writes to task or vma from the callback
    function. The verifier should reject both cases.
    
    Signed-off-by: Song Liu <songliubraving@fb.com>

---
+++ tools/testing/selftests/bpf/progs/find_vma_fail2.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2021 Facebook */
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+char _license[] SEC("license") = "GPL";
+
+struct callback_ctx {
+	int dummy;
+};
+
+static __u64
+write_task(struct task_struct *task, struct vm_area_struct *vma,
+	   struct callback_ctx *data)
+{
+	/* writing to task, which is illegal */
+	task->mm = NULL;
+
+	return 0;
+}
+
+SEC("kprobe/__x64_sys_getpgid")
+int handle_getpid(void)
+{
+	struct task_struct *task = bpf_get_current_task_btf();
+	struct callback_ctx data = {0};
+
+	bpf_find_vma(task, 0, write_task, &data, 0);
+	return 0;
+}
