9a2fb678f74157bef2f918d8691b87ed057f906e
Barry Song <song.bao.hua@hisilicon.com>
2021-11-05 18:51:36 +0800

    sched&net: avoid over-pulling tasks due to network interrupts
    In LPC2021, both Libo Chen and Tim Chen have reported the overpull
    of network interrupts[1]. For example, while running one database,
    ethernet is located in numa0, numa1 might be almost idle due to
    interrupts are pulling tasks to numa0 because of wake_up affine.
    I have seen the same problem. One way to solve this problem is
    moving to a normal wakeup in network rather than using a sync
    wakeup which will be more aggressively pulling tasks in scheduler
    core.
    
    On kunpeng920 with 4numa, ethernet is located at numa0, storage
    disk is located at numa2. While using sysbench to connect this
    mysql machine, I am seeing numa1 is idle though numa0,2 and 3
    are quite busy.
    
    The benchmark command:
    
     sysbench --db-driver=mysql --mysql-user=sbtest_user \
     --mysql_password=password --mysql-db=sbtest \
     --mysql-host=192.168.101.3 --mysql-port=3306 \
     --point-selects=10 --simple-ranges=1 \
     --sum-ranges=1 --order-ranges=1 --distinct-ranges=1 \
     --index-updates=1 --non-index-updates=1 \
     --delete-inserts=1 --range-size=100 \
     --time=600 --events=0 --report-interval=60 \
     --tables=64 --table-size=2000000 --threads=128 \
      /usr/share/sysbench/oltp_read_only.lua run
    
    The benchmark result is as below:
                     tps        qps
    w/o patch     31748.22     507971.56
    w/  patch     35075.20     561203.13
                  +10.5%
    
    With the patch I am seeing NUMA1 becomes busy as well so I am
    getting 10%+ performance improvement.
    
    I am not saying this patch is exactly the right approach, But I'd
    like to use this RFC to connect the people of net and scheduler,
    and start the discussion in this wider range.
    
    Testing was done based on the latest linus tree commit d4439a1189.
    with the .config[2]
    
    [1] https://linuxplumbersconf.org/event/11/contributions/1044/attachments/801/1508/lpc21_wakeup_pulling_libochen.pdf
    [2] http://www.linuxep.com/patches/config
    
    Cc: Libo Chen <libo.chen@oracle.com>
    Cc: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Barry Song <song.bao.hua@hisilicon.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -3133,7 +3133,7 @@ void sock_def_readable(struct sock *sk)
 	rcu_read_lock();
 	wq = rcu_dereference(sk->sk_wq);
 	if (skwq_has_sleeper(wq))
-		wake_up_interruptible_sync_poll(&wq->wait, EPOLLIN | EPOLLPRI |
+		wake_up_interruptible_poll(&wq->wait, EPOLLIN | EPOLLPRI |
 						EPOLLRDNORM | EPOLLRDBAND);
 	sk_wake_async(sk, SOCK_WAKE_WAITD, POLL_IN);
 	rcu_read_unlock();
@@ -3151,7 +3151,7 @@ static void sock_def_write_space(struct sock *sk)
 	if ((refcount_read(&sk->sk_wmem_alloc) << 1) <= READ_ONCE(sk->sk_sndbuf)) {
 		wq = rcu_dereference(sk->sk_wq);
 		if (skwq_has_sleeper(wq))
-			wake_up_interruptible_sync_poll(&wq->wait, EPOLLOUT |
+			wake_up_interruptible_poll(&wq->wait, EPOLLOUT |
 						EPOLLWRNORM | EPOLLWRBAND);
 
 		/* Should agree with poll, otherwise some programs break */
