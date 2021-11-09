233b31a0e118bfbcbcc48a465358f1fdf8748288
Martin Kaistra <martin.kaistra@linutronix.de>
2021-11-09 10:50:09 +0100

    net: dsa: b53: Expose PTP timestamping ioctls to userspace
    Allow userspace to use the PTP support. Currently only L2 is supported.
    
    Signed-off-by: Martin Kaistra <martin.kaistra@linutronix.de>
    Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>

--- drivers/net/dsa/b53/b53_ptp.h
+++ drivers/net/dsa/b53/b53_ptp.h
@@ -17,6 +17,8 @@ int b53_ptp_init(struct b53_device *dev);
 void b53_ptp_exit(struct b53_device *dev);
 int b53_get_ts_info(struct dsa_switch *ds, int port,
 		    struct ethtool_ts_info *info);
+int b53_port_hwtstamp_set(struct dsa_switch *ds, int port, struct ifreq *ifr);
+int b53_port_hwtstamp_get(struct dsa_switch *ds, int port, struct ifreq *ifr);
 bool b53_port_rxtstamp(struct dsa_switch *ds, int port, struct sk_buff *skb,
 		       unsigned int type);
 void b53_port_txtstamp(struct dsa_switch *ds, int port, struct sk_buff *skb);
@@ -34,6 +36,18 @@ static inline void b53_ptp_exit(struct b53_device *dev)
 
 static inline int b53_get_ts_info(struct dsa_switch *ds, int port,
 				  struct ethtool_ts_info *info)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int b53_port_hwtstamp_set(struct dsa_switch *ds, int port,
+					struct ifreq *ifr)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int b53_port_hwtstamp_get(struct dsa_switch *ds, int port,
+					struct ifreq *ifr)
 {
 	return -EOPNOTSUPP;
 }
