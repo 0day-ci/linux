f09a836262e8f33a74e1985b36ee791b87e1ed6d
Jackie Liu <liuyun01@kylinos.cn>
2021-11-10 15:09:48 +0800

    drm/msm/hdmi: fix build without CONFIG_COMMON_CLK
    HDMI 8996 PHY/PLL support need common clk. avoid like:
    
    [...]
    x86_64-linux-gnu-ld: drivers/gpu/drm/msm/hdmi/hdmi_phy.o:(.rodata+0x3e0): undefined reference to `msm_hdmi_phy_8996_cfg'
    
    Fixes: e17afdceb4f2 ("drm/msm/hdmi: HDMI 8996 PHY/PLL support")
    Reported-by: kernelbot <kernel-bot@kylinos.cn>
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>

--- drivers/gpu/drm/msm/hdmi/hdmi_phy.c
+++ drivers/gpu/drm/msm/hdmi/hdmi_phy.c
@@ -192,8 +192,10 @@ static const struct of_device_id msm_hdmi_phy_dt_match[] = {
 	  .data = &msm_hdmi_phy_8x74_cfg },
 	{ .compatible = "qcom,hdmi-phy-8084",
 	  .data = &msm_hdmi_phy_8x74_cfg },
+#ifdef CONFIG_COMMON_CLK
 	{ .compatible = "qcom,hdmi-phy-8996",
 	  .data = &msm_hdmi_phy_8996_cfg },
+#endif
 	{}
 };
 
