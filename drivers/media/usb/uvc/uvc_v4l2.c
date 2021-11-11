ee9b2cfae665b1399eb7845a19b4d0ef3d4a27fb
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2021-11-11 09:06:11 +0100

    media: uvcvideo: Fix a memory leak in an error handling path of 'uvc_ioctl_ctrl_map()'
    If 'map->name' can't be allocated, 'map' must be released before returning.
    
    Fixes: 70fa906d6fce ("media: uvcvideo: Use control names from framework")
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>

--- drivers/media/usb/uvc/uvc_v4l2.c
+++ drivers/media/usb/uvc/uvc_v4l2.c
@@ -44,8 +44,10 @@ static int uvc_ioctl_ctrl_map(struct uvc_video_chain *chain,
 	if (v4l2_ctrl_get_name(map->id) == NULL) {
 		map->name = kmemdup(xmap->name, sizeof(xmap->name),
 				    GFP_KERNEL);
-		if (!map->name)
+		if (!map->name) {
+			kfree(map);
 			return -ENOMEM;
+		}
 	}
 	memcpy(map->entity, xmap->entity, sizeof(map->entity));
 	map->selector = xmap->selector;
