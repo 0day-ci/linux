91548968d0f2f01512bdebeb016b73d0e569023d
Yang Yang <yang.yang29@zte.com.cn>
2021-11-12 08:38:14 +0000

    delayacct: Support swapin delay accounting for swapping without blkio
    Currently delayacct accounts swapin delay only for swapping that cause
    blkio. If we use zram for swapping, tools/accounting/getdelays can't
    get any SWAP delay.
    
    It's useful to get zram swapin delay information, for example to adjust
    compress algorithm or /proc/sys/vm/swappiness.
    
    Reference to PSI, it accounts any kind of swapping by doing it's work
    in swap_readpage(), no matter whether swapping cause blkio. Let
    delayacct do the similar work.
    
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: Yang Yang <yang.yang29@zte.com.cn>

--- mm/memory.c
+++ mm/memory.c
@@ -3511,7 +3511,6 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 	if (unlikely(!si))
 		goto out;
 
-	delayacct_set_flag(current, DELAYACCT_PF_SWAPIN);
 	page = lookup_swap_cache(entry, vma, vmf->address);
 	swapcache = page;
 
@@ -3559,7 +3558,6 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 					vmf->address, &vmf->ptl);
 			if (likely(pte_same(*vmf->pte, vmf->orig_pte)))
 				ret = VM_FAULT_OOM;
-			delayacct_clear_flag(current, DELAYACCT_PF_SWAPIN);
 			goto unlock;
 		}
 
@@ -3573,13 +3571,11 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 		 * owner processes (which may be unknown at hwpoison time)
 		 */
 		ret = VM_FAULT_HWPOISON;
-		delayacct_clear_flag(current, DELAYACCT_PF_SWAPIN);
 		goto out_release;
 	}
 
 	locked = lock_page_or_retry(page, vma->vm_mm, vmf->flags);
 
-	delayacct_clear_flag(current, DELAYACCT_PF_SWAPIN);
 	if (!locked) {
 		ret |= VM_FAULT_RETRY;
 		goto out_release;
