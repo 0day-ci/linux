eb746af2d47c0338a33eb49577e60ee927776c66
Like Xu <likexu@tencent.com>
2021-11-12 17:51:33 +0800

    KVM: x86/pmu: Make top-down.slots event unavailable in supported leaf
    When we choose to disable the fourth fixed counter TOPDOWN.SLOTS,
    we need to also reduce the length of the 0AH.EBX bit vector, which
    enumerates architecture performance monitoring events, and set
    0AH.EBX.[bit 7] to 1 if the new value of EAX[31:24] is still > 7.
    
    Fixes: 2e8cd7a3b8287 ("kvm: x86: limit the maximum number of vPMU fixed counters to 3")
    Signed-off-by: Like Xu <likexu@tencent.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -746,6 +746,20 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		eax.split.mask_length = cap.events_mask_len;
 
 		edx.split.num_counters_fixed = min(cap.num_counters_fixed, MAX_FIXED_COUNTERS);
+
+		/*
+		 * The 8th architectural event (top-down slots) will be supported
+		 * if the 4th fixed counter exists && EAX[31:24] > 7 && EBX[7] = 0.
+		 *
+		 * For now, KVM needs to make this event unavailable.
+		 */
+		if (edx.split.num_counters_fixed < 4) {
+			if (eax.split.mask_length > 7)
+				eax.split.mask_length--;
+			if (eax.split.mask_length > 7)
+				cap.events_mask |= BIT_ULL(7);
+		}
+
 		edx.split.bit_width_fixed = cap.bit_width_fixed;
 		if (cap.version)
 			edx.split.anythread_deprecated = 1;
