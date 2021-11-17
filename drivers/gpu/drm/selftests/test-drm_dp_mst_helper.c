8924a51fe637ae2373fc337bea357c98a0ef6891
Bernard Zhao <bernard@vivo.com>
2021-11-16 17:48:28 -0800

    drm/selftest: fix potential memleak in error branch
    This patch try to fix the potential memleak in error branch.
    
    Signed-off-by: Bernard Zhao <bernard@vivo.com>

--- drivers/gpu/drm/selftests/test-drm_dp_mst_helper.c
+++ drivers/gpu/drm/selftests/test-drm_dp_mst_helper.c
@@ -131,8 +131,10 @@ sideband_msg_req_encode_decode(struct drm_dp_sideband_msg_req_body *in)
 		return false;
 
 	txmsg = kzalloc(sizeof(*txmsg), GFP_KERNEL);
-	if (!txmsg)
-		return false;
+	if (!txmsg) {
+		result = false;
+		goto out;
+	}
 
 	drm_dp_encode_sideband_req(in, txmsg);
 	ret = drm_dp_decode_sideband_req(txmsg, out);
