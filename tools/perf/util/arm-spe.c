d7fe423775e43098d8fcf0a1cbf4c49963e994e3
James Clark <james.clark@arm.com>
2021-11-17 14:28:32 +0000

    perf arm-spe: report all SPE records as "all" events
    Currently perf-report and perf-inject are dropping a large number of SPE
    records because they don't contain any of the existing events, but the
    contextual information of the records is still useful to keep.
    
    The synthesized event "all" is generated for every SPE record that is
    processed, regardless of whether the record contains interesting events
    or not. The event can be filtered with the flag "--itrace=o".
    
    Signed-off-by: James Clark <james.clark@arm.com>
    Signed-off-by: German Gomez <german.gomez@arm.com>

--- tools/perf/util/arm-spe.c
+++ tools/perf/util/arm-spe.c
@@ -57,6 +57,7 @@ struct arm_spe {
 	u8				sample_branch;
 	u8				sample_remote_access;
 	u8				sample_memory;
+	u8				sample_other;
 
 	u64				l1d_miss_id;
 	u64				l1d_access_id;
@@ -67,6 +68,7 @@ struct arm_spe {
 	u64				branch_miss_id;
 	u64				remote_access_id;
 	u64				memory_id;
+	u64				all_id;
 
 	u64				kernel_start;
 
@@ -299,6 +301,23 @@ static int arm_spe__synth_branch_sample(struct arm_spe_queue *speq,
 	return arm_spe_deliver_synth_event(spe, speq, event, &sample);
 }
 
+static int arm_spe__synth_other_sample(struct arm_spe_queue *speq,
+				       u64 spe_events_id)
+{
+	struct arm_spe *spe = speq->spe;
+	struct arm_spe_record *record = &speq->decoder->record;
+	union perf_event *event = speq->event_buf;
+	struct perf_sample sample = { .ip = 0, };
+
+	arm_spe_prep_sample(spe, speq, event, &sample);
+
+	sample.id = spe_events_id;
+	sample.stream_id = spe_events_id;
+	sample.addr = record->to_ip;
+
+	return arm_spe_deliver_synth_event(spe, speq, event, &sample);
+}
+
 #define SPE_MEM_TYPE	(ARM_SPE_L1D_ACCESS | ARM_SPE_L1D_MISS | \
 			 ARM_SPE_LLC_ACCESS | ARM_SPE_LLC_MISS | \
 			 ARM_SPE_REMOTE_ACCESS)
@@ -424,6 +443,12 @@ static int arm_spe_sample(struct arm_spe_queue *speq)
 
 	if (spe->sample_memory && arm_spe__is_memory_event(record->type)) {
 		err = arm_spe__synth_mem_sample(speq, spe->memory_id, data_src);
+		if (err)
+			return err;
+	}
+
+	if (spe->sample_other) {
+		err = arm_spe__synth_other_sample(speq, spe->all_id);
 		if (err)
 			return err;
 	}
@@ -1026,6 +1051,17 @@ arm_spe_synth_events(struct arm_spe *spe, struct perf_session *session)
 			return err;
 		spe->memory_id = id;
 		arm_spe_set_event_name(evlist, id, "memory");
+		id += 1;
+	}
+
+	if (spe->synth_opts.other_events) {
+		spe->sample_other = true;
+
+		err = arm_spe_synth_event(session, &attr, id);
+		if (err)
+			return err;
+		spe->all_id = id;
+		arm_spe_set_event_name(evlist, id, "all");
 	}
 
 	return 0;
