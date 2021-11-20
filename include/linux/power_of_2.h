f8b838097f1c9bd0523687d45463c5b4bab7ffca
Alejandro Colomar <alx.manpages@gmail.com>
2021-11-20 14:01:04 +0100

    linux/power_of_2.h: Implement [__]BUILD_BUG_ON_NOT_POWER_OF_2() in terms of __IS_POWER_OF_2[_OR_0]()
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- include/linux/power_of_2.h
+++ include/linux/power_of_2.h
@@ -10,10 +10,8 @@
 #define __IS_POWER_OF_2(n)       (__IS_POWER_OF_2_OR_0(n) && ((n) != 0))
 
 /* Force a compilation error if a constant expression is not a power of 2 */
-#define __BUILD_BUG_ON_NOT_POWER_OF_2(n)	\
-	BUILD_BUG_ON(((n) & ((n) - 1)) != 0)
-#define BUILD_BUG_ON_NOT_POWER_OF_2(n)			\
-	BUILD_BUG_ON((n) == 0 || (((n) & ((n) - 1)) != 0))
+#define __BUILD_BUG_ON_NOT_POWER_OF_2(n)  BUILD_BUG_ON(!__IS_POWER_OF_2_OR_0(n))
+#define BUILD_BUG_ON_NOT_POWER_OF_2(n)    BUILD_BUG_ON(!__IS_POWER_OF_2(n))
 
 
 #endif	/* _LINUX_POWER_OF_2_H */
