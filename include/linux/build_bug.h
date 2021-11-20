87392d4675d32d0fc84c0f787ddcec3370be7b63
Alejandro Colomar <alx.manpages@gmail.com>
2021-11-20 14:00:56 +0100

    Move BUILD_BUG_ON_ZERO to <linux/must_be.h>
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- include/linux/build_bug.h
+++ include/linux/build_bug.h
@@ -2,19 +2,10 @@
 #ifndef _LINUX_BUILD_BUG_H
 #define _LINUX_BUILD_BUG_H
 
-#include <linux/compiler.h>
 
-#ifdef __CHECKER__
-#define BUILD_BUG_ON_ZERO(e) (0)
-#else /* __CHECKER__ */
-/*
- * Force a compilation error if condition is true, but also produce a
- * result (of value 0 and type int), so the expression can be used
- * e.g. in a structure initializer (or where-ever else comma expressions
- * aren't permitted).
- */
-#define BUILD_BUG_ON_ZERO(e) ((int)(sizeof(struct { int:(-!!(e)); })))
-#endif /* __CHECKER__ */
+#include <linux/compiler.h>
+#include <linux/must_be.h>
+
 
 /* Force a compilation error if a constant expression is not a power of 2 */
 #define __BUILD_BUG_ON_NOT_POWER_OF_2(n)	\
--- include/linux/must_be.h
+++ include/linux/must_be.h
@@ -3,6 +3,18 @@
 #define _LINUX_MUST_BE_H
 
 
+#ifdef __CHECKER__
+#define BUILD_BUG_ON_ZERO(e) (0)
+#else  /* __CHECKER__ */
+/*
+ * Force a compilation error if condition is true, but also produce a
+ * result (of value 0 and type int), so the expression can be used
+ * e.g. in a structure initializer (or where-ever else comma expressions
+ * aren't permitted).
+ */
+#define BUILD_BUG_ON_ZERO(e)  ((int)(sizeof(struct { int:(-!!(e)); })))
+#endif  /* __CHECKER__ */
+
 #define __must_be(e)  BUILD_BUG_ON_ZERO(!(e))
 
 
