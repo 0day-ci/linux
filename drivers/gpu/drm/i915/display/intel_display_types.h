f863b031fae230f1688d53cf08bb10634e5b336d
Jani Nikula <jani.nikula@intel.com>
2021-11-22 15:51:03 +0200

    drm/i915/display: remove intel_wait_for_vblank()
    There are only three call sites remaining for
    intel_wait_for_vblank(). Remove the function, and open code it to avoid
    new users from showing up.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -2017,14 +2017,6 @@ intel_crtc_needs_modeset(const struct intel_crtc_state *crtc_state)
 }
 
 static inline void
-intel_wait_for_vblank(struct drm_i915_private *dev_priv, enum pipe pipe)
-{
-	struct intel_crtc *crtc = intel_get_crtc_for_pipe(dev_priv, pipe);
-
-	drm_crtc_wait_one_vblank(&crtc->base);
-}
-
-static inline void
 intel_wait_for_vblank_if_active(struct drm_i915_private *dev_priv, enum pipe pipe)
 {
 	struct intel_crtc *crtc = intel_get_crtc_for_pipe(dev_priv, pipe);
