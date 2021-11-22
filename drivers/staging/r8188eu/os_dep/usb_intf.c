e9985eaccfd8c958f9b526152efc511eb518350b
Vihas Mak <makvihas@gmail.com>
2021-11-23 01:23:50 +0530

    staging: r8188eu: remove unnecessary NULL check
    remove unnecessary NULL check surrounding rtw_free_netdev(), as the check
    is already performed inside rtw_free_netdev() in
    drivers/staging/r8188eu/os_dep/osdep_service.c.
    
    Signed-off-by: Vihas Mak <makvihas@gmail.com>

--- drivers/staging/r8188eu/os_dep/usb_intf.c
+++ drivers/staging/r8188eu/os_dep/usb_intf.c
@@ -466,8 +466,7 @@ static void rtw_usb_if1_deinit(struct adapter *if1)
 		if1->hw_init_completed);
 	rtw_handle_dualmac(if1, 0);
 	rtw_free_drv_sw(if1);
-	if (pnetdev)
-		rtw_free_netdev(pnetdev);
+	rtw_free_netdev(pnetdev);
 }
 
 static int rtw_drv_init(struct usb_interface *pusb_intf, const struct usb_device_id *pdid)
