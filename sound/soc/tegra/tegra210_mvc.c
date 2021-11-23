a3126e7e75ab0875951ee009b275a5a3f1270c26
Sameer Pujar <spujar@nvidia.com>
2021-11-23 19:37:34 +0530

    ASoC: tegra: Balance runtime PM count
    After successful application of volume/mute settings via mixer control
    put calls, the control returns without balancing the runtime PM count.
    This makes device to be always runtime active. Fix this by allowing
    control to reach pm_runtime_put() call.
    
    Fixes: e539891f9687 ("ASoC: tegra: Add Tegra210 based MVC driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Sameer Pujar <spujar@nvidia.com>

--- sound/soc/tegra/tegra210_mvc.c
+++ sound/soc/tegra/tegra210_mvc.c
@@ -156,7 +156,7 @@ static int tegra210_mvc_put_mute(struct snd_kcontrol *kcontrol,
 	if (err < 0)
 		goto end;
 
-	return 1;
+	err = 1;
 
 end:
 	pm_runtime_put(cmpnt->dev);
@@ -222,7 +222,7 @@ static int tegra210_mvc_put_vol(struct snd_kcontrol *kcontrol,
 			   TEGRA210_MVC_VOLUME_SWITCH_MASK,
 			   TEGRA210_MVC_VOLUME_SWITCH_TRIGGER);
 
-	return 1;
+	err = 1;
 
 end:
 	pm_runtime_put(cmpnt->dev);
