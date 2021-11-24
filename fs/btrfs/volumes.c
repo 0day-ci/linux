2032ad0af1afa1f9a581c1154513df0c206cd9e4
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2021-11-24 01:30:29 -0800

    btrfs: zoned: sink zone check into btrfs_repair_one_zone
    Sink zone check into btrfs_repair_one_zone() so we don't need to do it in
    all callers.
    
    Also as btrfs_repair_one_zone() doesn't return a sensible error, just
    make it a void function.
    
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -8335,23 +8335,26 @@ out:
 	return ret;
 }
 
-int btrfs_repair_one_zone(struct btrfs_fs_info *fs_info, u64 logical)
+void btrfs_repair_one_zone(struct btrfs_fs_info *fs_info, u64 logical)
 {
 	struct btrfs_block_group *cache;
 
+	if (!btrfs_is_zoned(fs_info))
+		return;
+
 	/* Do not attempt to repair in degraded state */
 	if (btrfs_test_opt(fs_info, DEGRADED))
-		return 0;
+		return;
 
 	cache = btrfs_lookup_block_group(fs_info, logical);
 	if (!cache)
-		return 0;
+		return;
 
 	spin_lock(&cache->lock);
 	if (cache->relocating_repair) {
 		spin_unlock(&cache->lock);
 		btrfs_put_block_group(cache);
-		return 0;
+		return;
 	}
 	cache->relocating_repair = 1;
 	spin_unlock(&cache->lock);
@@ -8359,5 +8362,5 @@ int btrfs_repair_one_zone(struct btrfs_fs_info *fs_info, u64 logical)
 	kthread_run(relocating_repair_kthread, cache,
 		    "btrfs-relocating-repair");
 
-	return 0;
+	return;
 }
