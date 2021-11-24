15ea50c77bd80f86d36c9c801fd534d3e621f7f5
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2021-11-24 01:30:44 -0800

    btrfs: move btrfs_scrub_continue() definition to scrub.h
    Now that we have a scrub.h move btrfs_scrub_continue()'s definition there.
    
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>

--- fs/btrfs/scrub.h
+++ fs/btrfs/scrub.h
@@ -54,4 +54,5 @@ int btrfs_scrub_dev(struct btrfs_fs_info *fs_info, u64 devid, u64 start,
 		    u64 end, struct btrfs_scrub_progress *progress,
 		    int readonly, int is_dev_replace);
 void btrfs_scrub_pause(struct btrfs_fs_info *fs_info);
+void btrfs_scrub_continue(struct btrfs_fs_info *fs_info);
 #endif /* BTRFS_SCRUB_H */
