2c4e744c33b4787443b0b47b6e90330912567b20
Rongwei Wang <rongwei.wang@linux.alibaba.com>
2021-11-26 01:05:59 +0800

    arm64: trans_pgd: unify the use of pmd_populate_kernel
    In commit 5de59884ac0e ("arm64: trans_pgd: pass NULL instead
    of init_mm to *_populate functions"), simply replace init_mm
    with NULL for pmd_populate_kernel. But in commit 59511cfd08f3
    ("arm64: mm: use XN table mapping attributes for user/kernel
    mappings"), adding the check of mm context in
    'pmd_populate_kernel'. And 'pmd_populate_kernel' always
    called with init_mm under arm64, nice to unify its use.
    
    This patch make a simple change, just restoring init_mm for
    'pmd_populate_kernel' in 'copy_pte'.
    
    Suggested-by: Abaci <abaci@linux.alibaba.com>
    Signed-off-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>

--- arch/arm64/mm/trans_pgd.c
+++ arch/arm64/mm/trans_pgd.c
@@ -67,7 +67,7 @@ static int copy_pte(struct trans_pgd_info *info, pmd_t *dst_pmdp,
 	dst_ptep = trans_alloc(info);
 	if (!dst_ptep)
 		return -ENOMEM;
-	pmd_populate_kernel(NULL, dst_pmdp, dst_ptep);
+	pmd_populate_kernel(&init_mm, dst_pmdp, dst_ptep);
 	dst_ptep = pte_offset_kernel(dst_pmdp, start);
 
 	src_ptep = pte_offset_kernel(src_pmdp, start);
