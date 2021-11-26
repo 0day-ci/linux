3d370392efcb79136efe320a547c51c4de5caf02
Daniel Lezcano <daniel.lezcano@linaro.org>
2021-11-26 19:14:56 +0100

    powercap/drivers/dtpm: Add DT initialization support
    The DTPM framework is available but without a way to configure it.
    
    These changes add DT support to initialize the powercap hierarchy via
    the powerzones description.
    
    It acts in two steps. First it reads the powerzone dependencies and
    build the DTPM hierarchy. Second, it search for all devices which
    belongs to a powerzone and attach them to the hierarchy.
    
    This approach makes the initialization self-encapsulated for the DTPM
    components.
    
    In order to ensure a nice self-encapsulation, the DTPM table
    descriptors contains a couple of initialization functions, one to
    setup the DTPM backend and one to initialize it up. With this
    approach, the DTPM framework has a very few functions to export.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- include/linux/dtpm.h
+++ include/linux/dtpm.h
@@ -32,23 +32,28 @@ struct dtpm_ops {
 	void (*release)(struct dtpm *);
 };
 
+struct device_node;
+
 typedef int (*dtpm_init_t)(void);
+typedef int (*dtpm_setup_t)(struct dtpm *, struct device_node *);
 
 struct dtpm_descr {
 	dtpm_init_t init;
+	dtpm_setup_t setup;
 };
 
 /* Init section thermal table */
 extern struct dtpm_descr __dtpm_table[];
 extern struct dtpm_descr __dtpm_table_end[];
 
-#define DTPM_TABLE_ENTRY(name, __init)				\
+#define DTPM_TABLE_ENTRY(name, __init, __setup)			\
 	static struct dtpm_descr __dtpm_table_entry_##name	\
 	__used __section("__dtpm_table") = {			\
 		.init = __init,					\
+		.setup = __setup,				\
 	}
 
-#define DTPM_DECLARE(name, init)	DTPM_TABLE_ENTRY(name, init)
+#define DTPM_DECLARE(name, init, setup)	DTPM_TABLE_ENTRY(name, init, setup)
 
 #define for_each_dtpm_table(__dtpm)	\
 	for (__dtpm = __dtpm_table;	\
@@ -58,6 +63,16 @@ extern struct dtpm_descr __dtpm_table_end[];
 static inline struct dtpm *to_dtpm(struct powercap_zone *zone)
 {
 	return container_of(zone, struct dtpm, zone);
+}
+
+static inline void dtpm_set_data(struct dtpm *dtpm, void *data)
+{
+	powercap_set_zone_data(&dtpm->zone, data);
+}
+
+static inline void *dtpm_get_data(struct dtpm *dtpm)
+{
+	return powercap_get_zone_data(&dtpm->zone);
 }
 
 int dtpm_update_power(struct dtpm *dtpm);
