37b7e7f8602cea9512f4cfec9d479e4bc322a516
Alexandre Bailon <abailon@baylibre.com>
2021-11-26 13:52:43 +0100

    dt-bindings: Add DT bindings for the DT-based virtual sensor driver
    This adds the DT bindings for the DT-based virtual sensor driver.
    This driver provides a way, using DT, to aggregate the temperature
    of multiple thermal zones and get some useful data from it.
    
    Signed-off-by: Alexandre Bailon <abailon@baylibre.com>

---
+++ include/dt-bindings/thermal/virtual-sensor.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * This header provides constants for virtual thermal sensor bindings.
+ *
+ * Copyright (C) 2021 BayLibre
+ */
+
+#ifndef _DT_BINDINGS_THERMAL_VIRTUAL_SENSOR_H
+#define _DT_BINDINGS_THERMAL_VIRTUAL_SENSOR_H
+
+#define VIRTUAL_THERMAL_SENSOR_MIN_VAL 0
+#define VIRTUAL_THERMAL_SENSOR_MAX_VAL 1
+#define VIRTUAL_THERMAL_SENSOR_AVG_VAL 2
+
+#endif /* _DT_BINDINGS_THERMAL_VIRTUAL_SENSOR_H */
