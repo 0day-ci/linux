58eae5600236c006336d2352b85f205df79e582b
Heiko Carstens <hca@linux.ibm.com>
2021-11-28 22:22:20 +0100

    topology/sysfs: export cluster attributes only if an architectures has support
    The cluster_id and cluster_cpus topology sysfs attributes have been
    added with c5e22feffdd7 ("topology: Represent clusters of CPUs within
    a die").
    
    They are currently only used for x86, arm64, and riscv (via generic
    arch topology), however they are still present with bogus default
    values for all other architectures. Instead of enforcing such new
    sysfs attributes to all architectures, make them only optional visible
    if an architecture opts in by defining both the topology_cluster_id
    and topology_cluster_cpumask attributes.
    
    This is similar to what was done when the book and drawer topology
    levels were introduced: avoid useless and therefore confusing sysfs
    attributes for architectures which cannot make use of them.
    
    This should not break any existing applications, since this is a
    new interface introduced with the v5.16 merge window.
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- include/linux/topology.h
+++ include/linux/topology.h
@@ -189,6 +189,7 @@ static inline int cpu_to_mem(int cpu)
 #endif
 #ifndef topology_cluster_id
 #define topology_cluster_id(cpu)		((void)(cpu), -1)
+#define topology_cluster_no_sysfs		1
 #endif
 #ifndef topology_core_id
 #define topology_core_id(cpu)			((void)(cpu), 0)
@@ -201,6 +202,7 @@ static inline int cpu_to_mem(int cpu)
 #endif
 #ifndef topology_cluster_cpumask
 #define topology_cluster_cpumask(cpu)		cpumask_of(cpu)
+#define topology_cluster_no_sysfs		1
 #endif
 #ifndef topology_die_cpumask
 #define topology_die_cpumask(cpu)		cpumask_of(cpu)
