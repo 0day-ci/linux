aaa97695826b5985759923c8c1526256817bdf45
Yinbo Zhu <zhuyinbo@loongson.cn>
2021-11-30 16:21:56 +0800

    modpost: file2alias: make mdio alias configure match mdio uevent
    The do_mdio_entry was responsible for generating a phy alias configure
    that according to the phy driver's mdio_device_id, before apply this
    patch, which alias configure is like "alias mdio:000000010100000100001
    1011101????", it doesn't match the phy_id of mdio_uevent, because of
    the phy_id was a hexadecimal digit and the mido uevent is consisit of
    phy_id with the char 'p', the uevent string is different from alias.
    Add this patch that mdio alias configure will can match mdio uevent.
    
    Signed-off-by: Yinbo Zhu <zhuyinbo@loongson.cn>

--- scripts/mod/file2alias.c
+++ scripts/mod/file2alias.c
@@ -1038,24 +1038,9 @@ static int do_platform_entry(const char *filename,
 static int do_mdio_entry(const char *filename,
 			 void *symval, char *alias)
 {
-	int i;
 	DEF_FIELD(symval, mdio_device_id, phy_id);
-	DEF_FIELD(symval, mdio_device_id, phy_id_mask);
-
 	alias += sprintf(alias, MDIO_MODULE_PREFIX);
-
-	for (i = 0; i < 32; i++) {
-		if (!((phy_id_mask >> (31-i)) & 1))
-			*(alias++) = '?';
-		else if ((phy_id >> (31-i)) & 1)
-			*(alias++) = '1';
-		else
-			*(alias++) = '0';
-	}
-
-	/* Terminate the string */
-	*alias = 0;
-
+	ADD(alias, "p", phy_id != MDIO_ANY_ID, phy_id);
 	return 1;
 }
 
