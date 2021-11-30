25c6660acc33e97145b2c38aa2e8c110780ad15a
Andrey Konovalov <andreyknvl@google.com>
2021-11-30 23:05:17 +0100

    kasan, page_alloc: move SetPageSkipKASanPoison in post_alloc_hook
    Pull the SetPageSkipKASanPoison() call in post_alloc_hook() out of the
    big if clause for better code readability. This also allows for more
    simplifications in the following patches.
    
    Also turn the kasan_has_integrated_init() check into the proper
    CONFIG_KASAN_HW_TAGS one. These checks evaluate to the same value,
    but logically skipping kasan poisoning has nothing to do with
    integrated init.
    
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2421,9 +2421,6 @@ inline void post_alloc_hook(struct page *page, unsigned int order,
 		init = false;
 	}
 	if (kasan_has_integrated_init()) {
-		if (gfp_flags & __GFP_SKIP_KASAN_POISON)
-			SetPageSkipKASanPoison(page);
-
 		if (!init_tags)
 			kasan_unpoison_pages(page, order, init);
 	} else {
@@ -2432,6 +2429,10 @@ inline void post_alloc_hook(struct page *page, unsigned int order,
 		if (init)
 			kernel_init_free_pages(page, 1 << order);
 	}
+	/* Propagate __GFP_SKIP_KASAN_POISON to page flags. */
+	if (IS_ENABLED(CONFIG_KASAN_HW_TAGS) &&
+	    (gfp_flags & __GFP_SKIP_KASAN_POISON))
+		SetPageSkipKASanPoison(page);
 
 	set_page_owner(page, order, gfp_flags);
 }
