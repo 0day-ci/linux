31b9d8385ccfced187a2804ff9572b2636fbf51e
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2021-11-30 15:10:48 +0100

    drm/msm: Initialize MDSS irq domain at probe time
    Since commit 8f59ee9a570c ("drm/msm/dsi: Adjust probe order"), the
    DSI host gets initialized earlier, but this caused unability to probe
    the entire stack of components because they all depend on interrupts
    coming from the main `mdss` node (mdp5, or dpu1).
    
    To fix this issue, anticipate registering the irq domain from mdp5/dpu1
    at msm_mdev_probe() time, as to make sure that the interrupt controller
    is available before dsi and/or other components try to initialize,
    finally satisfying the dependency.
    
    Moreover, to balance this operation while avoiding to always check if
    the irq domain is registered everytime we call bind() on msm_pdev, add
    a new *remove function pointer to msm_mdss_funcs, used to remove the
    irq domain only at msm_pdev_remove() time.
    
    Fixes: 8f59ee9a570c ("drm/msm/dsi: Adjust probe order")
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1375,6 +1375,20 @@ static int msm_pdev_probe(struct platform_device *pdev)
 	if (!priv)
 		return -ENOMEM;
 
+	switch (get_mdp_ver(pdev)) {
+	case KMS_MDP5:
+		ret = mdp5_mdss_early_init(&pdev->dev, priv);
+		break;
+	case KMS_DPU:
+		ret = dpu_mdss_early_init(&pdev->dev, priv);
+		break;
+	default:
+		ret = 0;
+		break;
+	}
+	if (ret)
+		return ret;
+
 	platform_set_drvdata(pdev, priv);
 
 	if (get_mdp_ver(pdev)) {
@@ -1407,6 +1421,12 @@ fail:
 
 static int msm_pdev_remove(struct platform_device *pdev)
 {
+	struct msm_drm_private *priv = platform_get_drvdata(pdev);
+
+	if (priv->mdss && priv->mdss->funcs)
+		priv->mdss->funcs->remove(priv->mdss);
+
+	priv->mdss = NULL;
 	component_master_del(&pdev->dev, &msm_drm_ops);
 	of_platform_depopulate(&pdev->dev);
 
@@ -1418,7 +1438,7 @@ static void msm_pdev_shutdown(struct platform_device *pdev)
 	struct msm_drm_private *priv = platform_get_drvdata(pdev);
 	struct drm_device *drm = priv ? priv->dev : NULL;
 
-	if (!priv || !priv->kms)
+	if (!priv || !priv->kms || !drm->mode_config.funcs)
 		return;
 
 	drm_atomic_helper_shutdown(drm);
