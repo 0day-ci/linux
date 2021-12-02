77c4e806b45de9c4bfd8e922d9d39f2d946d6e0a
Alistair Francis <alistair.francis@wdc.com>
2021-12-02 21:16:59 +1000

    selftests: futex: Use futex_waitv helper function
    Use the publically exposed __kernel_futex_syscall_waitv() helper
    function for the futex_waitv tests.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- tools/testing/selftests/futex/include/futex2test.h
+++ tools/testing/selftests/futex/include/futex2test.h
@@ -5,6 +5,7 @@
  * Copyright 2021 Collabora Ltd.
  */
 #include <stdint.h>
+#include <linux/futex_syscall.h>
 
 #define u64_to_ptr(x) ((void *)(uintptr_t)(x))
 
@@ -18,5 +19,5 @@
 static inline int futex_waitv(volatile struct futex_waitv *waiters, unsigned long nr_waiters,
 			      unsigned long flags, struct timespec *timo, clockid_t clockid)
 {
-	return syscall(__NR_futex_waitv, waiters, nr_waiters, flags, timo, clockid);
+	return __kernel_futex_syscall_waitv(waiters, nr_waiters, flags, timo, clockid);
 }
