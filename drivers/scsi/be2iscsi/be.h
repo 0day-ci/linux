46ae6b75188a7c555a80135814182b82a0fad7c8
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2021-12-02 21:05:56 +0100

    scsi: be2iscsi: Replace irq_poll with threaded IRQ handler.
    The driver is using irq_poll() (a NAPI like generic interface) for
    completing individual I/O requests.
    This could be replaced with threaded interrupts. The threaded interrupt
    runs as a RT thread with priority 50-FIFO. It should run as the first
    task after the interrupt unless a task with higher priority is active.
    It can be interrupt by another hardware interrupt or a softirq.
    
    This has been compile tested only. I'm interested if it causes any
    regressions, improves the situation or neither.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- drivers/scsi/be2iscsi/be.h
+++ drivers/scsi/be2iscsi/be.h
@@ -12,7 +12,7 @@
 
 #include <linux/pci.h>
 #include <linux/if_vlan.h>
-#include <linux/irq_poll.h>
+
 #define FW_VER_LEN	32
 #define MCC_Q_LEN	128
 #define MCC_CQ_LEN	256
@@ -90,7 +90,6 @@ struct be_eq_obj {
 	struct beiscsi_hba *phba;
 	struct be_queue_info *cq;
 	struct work_struct mcc_work; /* Work Item */
-	struct irq_poll	iopoll;
 };
 
 struct be_mcc_obj {
