e92eba39eb6fa02e7bdceded42576f9b6c4cf820
Josef Bacik <josef@toxicpanda.com>
2021-12-03 17:18:11 -0500

    btrfs: only call inode_sub_bytes in truncate paths that care
    We currently have a bunch of awkward checks to make sure we only update
    the inode i_bytes if we're truncating the real inode.  Instead keep
    track of the number of bytes we need to sub in the
    btrfs_truncate_control, and then do the appropriate adjustment in the
    truncate paths that care.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -8616,6 +8616,7 @@ static int btrfs_truncate(struct inode *inode, bool skip_writeback)
 		ret = btrfs_truncate_inode_items(trans, root, BTRFS_I(inode),
 						 &control);
 
+		inode_sub_bytes(inode, control.sub_bytes);
 		btrfs_inode_safe_disk_i_size_write(BTRFS_I(inode),
 						   control.last_size);
 
