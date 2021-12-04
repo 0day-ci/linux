1ca827cddb6cdf25d1691dbbcbfd439506adedb3
Alberto Merciai <alb3rt0.m3rciai@gmail.com>
2021-12-04 01:34:11 +0100

    staging: vt6655: refactor byMaxChannel to max_channel
    Replace camelcase hungarian notated variable "byMaxChannel"
    into linux kernel coding style equivalent variable "max_channel".
    
    Signed-off-by: Alberto Merciai <alb3rt0.m3rciai@gmail.com>

--- drivers/staging/vt6655/device.h
+++ drivers/staging/vt6655/device.h
@@ -201,7 +201,7 @@ struct vnt_private {
 	unsigned char byTopCCKBasicRate;
 
 	unsigned char byMinChannel;
-	unsigned char byMaxChannel;
+	unsigned char max_channel;
 
 	unsigned char preamble_type;
 	unsigned char byShortPreamble;
--- drivers/staging/vt6655/device_main.c
+++ drivers/staging/vt6655/device_main.c
@@ -229,7 +229,7 @@ static void device_init_registers(struct vnt_private *priv)
 
 	/* Get Channel range */
 	priv->byMinChannel = 1;
-	priv->byMaxChannel = CB_MAX_CHANNEL;
+	priv->max_channel = CB_MAX_CHANNEL;
 
 	/* Get Antena */
 	byValue = SROMbyReadEmbedded(priv->port_offset, EEP_OFS_ANTENNA);
