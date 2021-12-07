b80bcedbce43267089289f8490b7da76aaa86f77
Matthew Rosato <mjrosato@linux.ibm.com>
2021-12-07 15:57:24 -0500

    KVM: s390: pci: add basic kvm_zdev structure
    This structure will be used to carry kvm passthrough information related to
    zPCI devices.
    
    Signed-off-by: Matthew Rosato <mjrosato@linux.ibm.com>

---
+++ arch/s390/kvm/pci.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * s390 kvm PCI passthrough support
+ *
+ * Copyright IBM Corp. 2021
+ *
+ *    Author(s): Matthew Rosato <mjrosato@linux.ibm.com>
+ */
+
+#include <linux/kvm_host.h>
+#include <linux/pci.h>
+#include <asm/kvm_pci.h>
+
+int kvm_s390_pci_dev_open(struct zpci_dev *zdev)
+{
+	struct kvm_zdev *kzdev;
+
+	if (zdev == NULL)
+		return -ENODEV;
+
+	kzdev = kzalloc(sizeof(struct kvm_zdev), GFP_KERNEL);
+	if (!kzdev)
+		return -ENOMEM;
+
+	kzdev->zdev = zdev;
+	zdev->kzdev = kzdev;
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(kvm_s390_pci_dev_open);
+
+void kvm_s390_pci_dev_release(struct zpci_dev *zdev)
+{
+	struct kvm_zdev *kzdev;
+
+	if (!zdev || !zdev->kzdev)
+		return;
+
+	kzdev = zdev->kzdev;
+	WARN_ON(kzdev->zdev != zdev);
+	zdev->kzdev = 0;
+	kfree(kzdev);
+
+}
+EXPORT_SYMBOL_GPL(kvm_s390_pci_dev_release);
+
+int kvm_s390_pci_attach_kvm(struct zpci_dev *zdev, struct kvm *kvm)
+{
+	struct kvm_zdev *kzdev = zdev->kzdev;
+
+	if (!kzdev)
+		return -ENODEV;
+
+	kzdev->kvm = kvm;
+	return 0;
+}
+EXPORT_SYMBOL_GPL(kvm_s390_pci_attach_kvm);
