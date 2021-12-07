1ea5d416b2ea3aa9ca5c0729926805cdc3df5646
Matthew Rosato <mjrosato@linux.ibm.com>
2021-12-07 15:57:29 -0500

    KVM: s390: expose guest Adapter Event Notification Interpretation facility
    This facility will be used to enable forwarding of PCI interrupts from
    firmware directly to guests.
    
    Signed-off-by: Matthew Rosato <mjrosato@linux.ibm.com>

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -2759,6 +2759,10 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 		set_kvm_facility(kvm->arch.model.fac_mask, 70);
 		set_kvm_facility(kvm->arch.model.fac_list, 70);
 	}
+	if (sclp.has_aeni && test_facility(71)) {
+		set_kvm_facility(kvm->arch.model.fac_mask, 71);
+		set_kvm_facility(kvm->arch.model.fac_list, 71);
+	}
 
 	if (css_general_characteristics.aiv && test_facility(65))
 		set_kvm_facility(kvm->arch.model.fac_mask, 65);
