f7eddd9a1d365ec2eb4f2c9ae8aa9aea342623a2
Zong Li <zong.li@sifive.com>
2021-12-08 18:06:18 +0800

    clk: sifive: Fix W=1 kernel build warning
    This commit reverts commit 487dc7bb6a0c ("clk: sifive: fu540-prci:
    Declare static const variable 'prci_clk_fu540' where it's used").
    For fixing W=1 kernel build warning(s) about ‘prci_clk_fu540’ defined
    but not used [-Wunused-const-variable=], the problem is that the C file
    of fu540 and fu740 doesn't use these variables, but they includes the
    header files. We could refine the code by moving the definition of these
    variable into fu540 and fu740 implementation respectively, instead of
    common core code, then we could still separate the SoCs-dependent data
    in their own implementation.
    
    Signed-off-by: Zong Li <zong.li@sifive.com>

--- drivers/clk/sifive/fu740-prci.h
+++ drivers/clk/sifive/fu740-prci.h
@@ -7,15 +7,6 @@
 #ifndef __SIFIVE_CLK_FU740_PRCI_H
 #define __SIFIVE_CLK_FU740_PRCI_H
 
-#include "sifive-prci.h"
-
-#define NUM_CLOCK_FU740	9
-
-extern struct __prci_clock __prci_init_clocks_fu740[NUM_CLOCK_FU740];
-
-static const struct prci_clk_desc prci_clk_fu740 = {
-	.clks = __prci_init_clocks_fu740,
-	.num_clks = ARRAY_SIZE(__prci_init_clocks_fu740),
-};
+extern struct prci_clk_desc prci_clk_fu740;
 
 #endif /* __SIFIVE_CLK_FU740_PRCI_H */
