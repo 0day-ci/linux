53a851dd4a7f08ef316f76c5b89586e57e587baf
Chunfeng Yun <chunfeng.yun@mediatek.com>
2021-12-09 15:22:18 +0800

    usb: xhci: skip re-check pending port event if hibernated
    When xHCI controller hibernated, the root hub lost power, if controller
    support Port Power Control (PPC), PP is not set at xhci_resume() and
    set by hub_reset_resume() later, so no need check pending port event.
    If PPC is not supported, device is disconneced, seems do not send out
    U3 LFPS wake signal, no need re-check again and drop 120ms delay to
    save resume time.
    
    Reported-by: Yun-Chien Yu <yun-chien.yu@mediatek.com>
    Signed-off-by: Chunfeng Yun <chunfeng.yun@mediatek.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -1235,7 +1235,7 @@ int xhci_resume(struct xhci_hcd *xhci, bool hibernated)
 		 * the first wake signalling failed, give it that chance.
 		 */
 		pending_portevent = xhci_pending_portevent(xhci);
-		if (!pending_portevent) {
+		if (!pending_portevent && !hibernated) {
 			msleep(120);
 			pending_portevent = xhci_pending_portevent(xhci);
 		}
