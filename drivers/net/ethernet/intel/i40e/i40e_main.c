e45036534610c65ae40051dac9ce2dcc384ac6cb
Mateusz Palczewski <mateusz.palczewski@intel.com>
2021-12-13 13:13:01 +0100

    i40e: Disable hw-tc-offload feature on driver load.
    After loading driver hw-tc-offload is enabled by default. Change the
    behaviour of driver to disable hw-tc-offload by default.
    
    Signed-off-by: Norbert Zulinski <norbertx.zulinski@intel.com>
    Signed-off-by: Mateusz Palczewski <mateusz.palczewski@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -13422,6 +13422,8 @@ static int i40e_config_netdev(struct i40e_vsi *vsi)
 	netdev->features |= hw_features | NETIF_F_HW_VLAN_CTAG_FILTER;
 	netdev->hw_enc_features |= NETIF_F_TSO_MANGLEID;
 
+	netdev->features &= ~NETIF_F_HW_TC;
+
 	if (vsi->type == I40E_VSI_MAIN) {
 		SET_NETDEV_DEV(netdev, &pf->pdev->dev);
 		ether_addr_copy(mac_addr, hw->mac.perm_addr);
