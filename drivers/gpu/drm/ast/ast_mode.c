e29cb84c46d0db9c8085afdb2cc57c73eb530223
Jiasheng Jiang <jiasheng@iscas.ac.cn>
2021-12-13 13:39:12 +0800

    drm/ast: potential dereference of null pointer
    he return value of kzalloc() needs to be checked.
    To avoid use of null pointer '&ast_state->base' in case of the
    failure of alloc.
    
    Fixes: f0adbc382b8b ("drm/ast: Allocate initial CRTC state of the correct size")
    Signed-off-by: Jiasheng Jiang <jiasheng@iscas.ac.cn>

--- drivers/gpu/drm/ast/ast_mode.c
+++ drivers/gpu/drm/ast/ast_mode.c
@@ -1121,7 +1121,8 @@ static void ast_crtc_reset(struct drm_crtc *crtc)
 	if (crtc->state)
 		crtc->funcs->atomic_destroy_state(crtc, crtc->state);
 
-	__drm_atomic_helper_crtc_reset(crtc, &ast_state->base);
+	if (ast_state)
+		__drm_atomic_helper_crtc_reset(crtc, &ast_state->base);
 }
 
 static struct drm_crtc_state *
