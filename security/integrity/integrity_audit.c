86f065a12dce209e73d69e389816303e18f7fd37
Xiaoke Wang <xkernel.wang@foxmail.com>
2021-12-14 11:34:59 +0800

    integrity: check the return value of audit_log_start()
    audit_log_start() returns audit_buffer pointer on success or NULL on
    error, so it is better to check the return value of it to prevent
    potential memory access error.
    
    Signed-off-by: Xiaoke Wang <xkernel.wang@foxmail.com>

--- security/integrity/integrity_audit.c
+++ security/integrity/integrity_audit.c
@@ -45,23 +45,25 @@ void integrity_audit_message(int audit_msgno, struct inode *inode,
 		return;
 
 	ab = audit_log_start(audit_context(), GFP_KERNEL, audit_msgno);
-	audit_log_format(ab, "pid=%d uid=%u auid=%u ses=%u",
-			 task_pid_nr(current),
-			 from_kuid(&init_user_ns, current_uid()),
-			 from_kuid(&init_user_ns, audit_get_loginuid(current)),
-			 audit_get_sessionid(current));
-	audit_log_task_context(ab);
-	audit_log_format(ab, " op=%s cause=%s comm=", op, cause);
-	audit_log_untrustedstring(ab, get_task_comm(name, current));
-	if (fname) {
-		audit_log_format(ab, " name=");
-		audit_log_untrustedstring(ab, fname);
+	if (ab) {
+		audit_log_format(ab, "pid=%d uid=%u auid=%u ses=%u",
+				task_pid_nr(current),
+				from_kuid(&init_user_ns, current_uid()),
+				from_kuid(&init_user_ns, audit_get_loginuid(current)),
+				audit_get_sessionid(current));
+		audit_log_task_context(ab);
+		audit_log_format(ab, " op=%s cause=%s comm=", op, cause);
+		audit_log_untrustedstring(ab, get_task_comm(name, current));
+		if (fname) {
+			audit_log_format(ab, " name=");
+			audit_log_untrustedstring(ab, fname);
+		}
+		if (inode) {
+			audit_log_format(ab, " dev=");
+			audit_log_untrustedstring(ab, inode->i_sb->s_id);
+			audit_log_format(ab, " ino=%lu", inode->i_ino);
+		}
+		audit_log_format(ab, " res=%d errno=%d", !result, errno);
+		audit_log_end(ab);
 	}
-	if (inode) {
-		audit_log_format(ab, " dev=");
-		audit_log_untrustedstring(ab, inode->i_sb->s_id);
-		audit_log_format(ab, " ino=%lu", inode->i_ino);
-	}
-	audit_log_format(ab, " res=%d errno=%d", !result, errno);
-	audit_log_end(ab);
 }
