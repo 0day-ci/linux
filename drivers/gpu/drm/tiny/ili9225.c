49aa6cdc40c4eccf8b61ee642edb434bb2e85902
Javier Martinez Canillas <javierm@redhat.com>
2021-12-15 01:59:57 +0100

    drm/ili9225: Add support for the nomodeset kernel parameter
    According to disable Documentation/admin-guide/kernel-parameters.txt, this
    parameter can be used to disable kernel modesetting.
    
    DRM drivers will not perform display-mode changes or accelerated rendering
    and only the systewm system framebuffer will be available if it was set-up.
    
    But only a few DRM drivers currently check for nomodeset, make this driver
    to also support the command line parameter.
    
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- drivers/gpu/drm/tiny/ili9225.c
+++ drivers/gpu/drm/tiny/ili9225.c
@@ -369,6 +369,9 @@ static int ili9225_probe(struct spi_device *spi)
 	u32 rotation = 0;
 	int ret;
 
+	if (drm_firmware_drivers_only())
+		return -ENODEV;
+
 	dbidev = devm_drm_dev_alloc(dev, &ili9225_driver,
 				    struct mipi_dbi_dev, drm);
 	if (IS_ERR(dbidev))
