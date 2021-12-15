75dafd01be156c27878f38dd54d8845e8f2edd65
Alexandre Truong <alexandre.truong@arm.com>
2021-12-15 15:11:36 +0000

    perf tools: enable dwarf_callchain_users on arm64
    On arm64, enable dwarf_callchain_users which will be needed
    to do a dwarf unwind in order to get the caller of the leaf frame.
    
    Signed-off-by: Alexandre Truong <alexandre.truong@arm.com>
    Signed-off-by: German Gomez <german.gomez@arm.com>

--- tools/perf/util/callchain.c
+++ tools/perf/util/callchain.c
@@ -1600,7 +1600,7 @@ void callchain_cursor_reset(struct callchain_cursor *cursor)
 		map__zput(node->ms.map);
 }
 
-void callchain_param_setup(u64 sample_type)
+void callchain_param_setup(u64 sample_type, const char *arch)
 {
 	if (symbol_conf.use_callchain || symbol_conf.cumulate_callchain) {
 		if ((sample_type & PERF_SAMPLE_REGS_USER) &&
@@ -1612,6 +1612,13 @@ void callchain_param_setup(u64 sample_type)
 		else
 			callchain_param.record_mode = CALLCHAIN_FP;
 	}
+
+	/*
+	 * It's possible to determine the caller of leaf frames with omitted
+	 * frame pointers on aarch64 using libunwind, so enable it.
+	 */
+	if (callchain_param.record_mode == CALLCHAIN_FP && !strcmp(arch, "arm64"))
+		dwarf_callchain_users = true;
 }
 
 static bool chain_match(struct callchain_list *base_chain,
