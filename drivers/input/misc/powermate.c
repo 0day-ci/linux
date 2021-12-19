7812e5c8474534620107851268f6a68482971f67
Dongliang Mu <mudongliangabcd@gmail.com>
2021-12-19 14:02:15 +0800

    driver: powermate: kill urb in the disconnect function
    In powermate_disconnect, powermate_pulse_led will invoke
    powermate_sync_state and submit one urb with pm as its context.
    If powermate disconnect before the execution of complete handler,
    the pm will become a dangling pointer and lead to UAF.
    
    Fix this by calling usb_kill_urb(pm->config) in the disconnect function.
    Note that, the error handling error does not need to take care of this.
    
    Reported-by: syzbot+9780d2b05ac158d32284@syzkaller.appspotmail.com
    Fixes: ba0acb5ee318901 ("Input: move USB miscellaneous devices under drivers/input/misc")
    Signed-off-by: Dongliang Mu <mudongliangabcd@gmail.com>

--- drivers/input/misc/powermate.c
+++ drivers/input/misc/powermate.c
@@ -424,6 +424,7 @@ static void powermate_disconnect(struct usb_interface *intf)
 	if (pm) {
 		pm->requires_update = 0;
 		usb_kill_urb(pm->irq);
+		usb_kill_urb(pm->config);
 		input_unregister_device(pm->input);
 		usb_free_urb(pm->irq);
 		usb_free_urb(pm->config);
