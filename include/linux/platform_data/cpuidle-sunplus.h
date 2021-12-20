88d439d706dba378c4b684f5a39ceea6d782b3f5
Edwin Chiu <edwinchiu0505tw@gmail.com>
2021-12-20 13:37:32 +0800

    cpuidle:sunplus:create cpuidle driver for sunplus sp7021
    Create cpuidle driver for sunplus sp7021 chip
    
    Signed-off-by: Edwin Chiu <edwinchiu0505tw@gmail.com>

---
+++ include/linux/platform_data/cpuidle-sunplus.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __CPUIDLE_SP7021_H
+#define __CPUIDLE_SP7021_H
+
+extern int cpu_v7_do_idle(void);
+
+#endif
