b725ab82c7bc2ab9d490b6f8dafb06b109afddfd
Andrey Konovalov <andreyknvl@google.com>
2021-12-20 23:02:00 +0100

    kasan, page_alloc: allow skipping unpoisoning for HW_TAGS
    Add a new GFP flag __GFP_SKIP_KASAN_UNPOISON that allows skipping KASAN
    poisoning for page_alloc allocations. The flag is only effective with
    HW_TAGS KASAN.
    
    This flag will be used by vmalloc code for page_alloc allocations
    backing vmalloc() mappings in a following patch. The reason to skip
    KASAN poisoning for these pages in page_alloc is because vmalloc code
    will be poisoning them instead.
    
    Also reword the comment for __GFP_SKIP_KASAN_POISON.
    
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>

--- include/trace/events/mmflags.h
+++ include/trace/events/mmflags.h
@@ -52,7 +52,9 @@
 
 #ifdef CONFIG_KASAN_HW_TAGS
 #define __def_gfpflag_names_kasan					      \
-	, {(unsigned long)__GFP_SKIP_KASAN_POISON, "__GFP_SKIP_KASAN_POISON"}
+	, {(unsigned long)__GFP_SKIP_KASAN_POISON, "__GFP_SKIP_KASAN_POISON"} \
+	, {(unsigned long)__GFP_SKIP_KASAN_UNPOISON,			      \
+						"__GFP_SKIP_KASAN_UNPOISON"}
 #else
 #define __def_gfpflag_names_kasan
 #endif
