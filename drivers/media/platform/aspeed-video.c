575b6d3044394d96d4c26e56371db4c704221bb4
Jammy Huang <jammy_huang@aspeedtech.com>
2021-12-22 16:21:38 +0800

    media: aspeed: Correct values for detected timing
    Correct timing's fp/sync/bp value based on the information below.
    It should be noticed that the calculation formula should be changed
    per sync polarity.
    
    The sequence of signal: sync - backporch - video data - frontporch
    
    The following registers start counting from sync's rising edge:
    1. VR090: frame edge's left and right
    2. VR094: frame edge's top and bottom
    3. VR09C: counting from sync's rising edge to falling edge
    
    [Vertical timing]
                +--+     +-------------------+     +--+
                |  |     |    v i d e o      |     |  |
             +--+  +-----+                   +-----+  +---+
    
           vsync+--+
       frame_top+--------+
    frame_bottom+----------------------------+
    
                      +-------------------+
                      |    v i d e o      |
          +--+  +-----+                   +-----+  +---+
             |  |                               |  |
             +--+                               +--+
           vsync+-------------------------------+
       frame_top+-----+
    frame_bottom+-------------------------+
    
    [Horizontal timing]
                +--+     +-------------------+     +--+
                |  |     |    v i d e o      |     |  |
             +--+  +-----+                   +-----+  +---+
    
           hsync+--+
      frame_left+--------+
     frame_right+----------------------------+
    
                      +-------------------+
                      |    v i d e o      |
          +--+  +-----+                   +-----+  +---+
             |  |                               |  |
             +--+                               +--+
           hsync+-------------------------------+
      frame_left+-----+
     frame_right+-------------------------+
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>

--- drivers/media/platform/aspeed-video.c
+++ drivers/media/platform/aspeed-video.c
@@ -757,7 +757,7 @@ static void aspeed_video_get_resolution(struct aspeed_video *video)
 	u32 src_lr_edge;
 	u32 src_tb_edge;
 	u32 sync;
-	u32 htotal;
+	u32 htotal, vtotal, vsync, hsync;
 	struct v4l2_bt_timings *det = &video->detected_timings;
 
 	det->width = MIN_WIDTH;
@@ -807,21 +807,35 @@ static void aspeed_video_get_resolution(struct aspeed_video *video)
 		mds = aspeed_video_read(video, VE_MODE_DETECT_STATUS);
 		sync = aspeed_video_read(video, VE_SYNC_STATUS);
 		htotal = aspeed_video_read(video, VE_H_TOTAL_PIXELS);
+		vtotal = FIELD_GET(VE_MODE_DETECT_V_LINES, mds);
+		vsync = FIELD_GET(VE_SYNC_STATUS_VSYNC, sync);
+		hsync = FIELD_GET(VE_SYNC_STATUS_HSYNC, sync);
 
 		video->frame_bottom = FIELD_GET(VE_SRC_TB_EDGE_DET_BOT, src_tb_edge);
 		video->frame_top = FIELD_GET(VE_SRC_TB_EDGE_DET_TOP, src_tb_edge);
-		det->vfrontporch = video->frame_top;
-		det->vbackporch = FIELD_GET(VE_MODE_DETECT_V_LINES, mds) -
-			video->frame_bottom;
-		det->vsync = FIELD_GET(VE_SYNC_STATUS_VSYNC, sync);
+		if (det->polarities & V4L2_DV_VSYNC_POS_POL) {
+			det->vbackporch = video->frame_top - vsync;
+			det->vfrontporch = vtotal - video->frame_bottom;
+			det->vsync = vsync;
+		} else {
+			det->vbackporch = video->frame_top;
+			det->vfrontporch = vsync - video->frame_bottom;
+			det->vsync = vtotal - vsync;
+		}
 		if (video->frame_top > video->frame_bottom)
 			continue;
 
 		video->frame_right = FIELD_GET(VE_SRC_LR_EDGE_DET_RT, src_lr_edge);
 		video->frame_left = FIELD_GET(VE_SRC_LR_EDGE_DET_LEFT, src_lr_edge);
-		det->hfrontporch = video->frame_left;
-		det->hbackporch = htotal - video->frame_right;
-		det->hsync = FIELD_GET(VE_SYNC_STATUS_HSYNC, sync);
+		if (det->polarities & V4L2_DV_HSYNC_POS_POL) {
+			det->hbackporch = video->frame_left - hsync;
+			det->hfrontporch = htotal - video->frame_right;
+			det->hsync = hsync;
+		} else {
+			det->hbackporch = video->frame_left;
+			det->hfrontporch = hsync - video->frame_right;
+			det->hsync = htotal - hsync;
+		}
 		if (video->frame_left > video->frame_right)
 			continue;
 
