e6a8d53953d1487a7d9529a4a4bc591465714f3f
Florian Fainelli <f.fainelli@gmail.com>
2021-12-22 16:22:18 -0800

    mtd: rawnand: brcmnand: Assign soc as early as possible
    In order to key off the brcmnand_probe() code in subsequent changes
    depending upon ctlr->soc, assign that variable as early as possible,
    instead of much later when we have checked that it is non-NULL.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

--- drivers/mtd/nand/raw/brcmnand/brcmnand.c
+++ drivers/mtd/nand/raw/brcmnand/brcmnand.c
@@ -3008,6 +3008,7 @@ int brcmnand_probe(struct platform_device *pdev, struct brcmnand_soc *soc)
 
 	dev_set_drvdata(dev, ctrl);
 	ctrl->dev = dev;
+	ctrl->soc = soc;
 
 	init_completion(&ctrl->done);
 	init_completion(&ctrl->dma_done);
@@ -3148,8 +3149,6 @@ int brcmnand_probe(struct platform_device *pdev, struct brcmnand_soc *soc)
 	 * interesting ways
 	 */
 	if (soc) {
-		ctrl->soc = soc;
-
 		ret = devm_request_irq(dev, ctrl->irq, brcmnand_irq, 0,
 				       DRV_NAME, ctrl);
 
