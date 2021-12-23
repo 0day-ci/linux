877d952c261ef90dea1bdecf0d1fc9a5c14c551c
Heiner Kallweit <hkallweit1@gmail.com>
2021-12-23 08:21:41 +0100

    PM: runtime: Annotate pm_runtime_resume_and_get() as __must_check
    In the error case this function returns w/o the usage counter being
    incremented.
    
    Not checking the return code will likely result in a usage counter
    imbalance in the error case therefore.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/pm_runtime.h
+++ include/linux/pm_runtime.h
@@ -387,9 +387,8 @@ static inline int pm_runtime_get(struct device *dev)
  * The possible return values of this function are the same as for
  * pm_runtime_resume() and the runtime PM usage counter of @dev remains
  * incremented in all cases, even if it returns an error code.
- * Consider using pm_runtime_resume_and_get() instead of it, especially
- * if its return value is checked by the caller, as this is likely to result
- * in cleaner code.
+ * Consider using pm_runtime_resume_and_get() instead of it if its return
+ * value is checked by the caller, as this is likely to result in cleaner code.
  */
 static inline int pm_runtime_get_sync(struct device *dev)
 {
@@ -404,7 +403,7 @@ static inline int pm_runtime_get_sync(struct device *dev)
  * PM usage counter. Return 0 if the runtime PM usage counter of @dev has been
  * incremented or a negative error code otherwise.
  */
-static inline int pm_runtime_resume_and_get(struct device *dev)
+static inline __must_check int pm_runtime_resume_and_get(struct device *dev)
 {
 	int ret;
 
