b2e194a6e5c8067665a9111b56da817e55ce32ec
Menglong Dong <imagedong@tencent.com>
2021-12-29 22:32:05 +0800

    net: skb: use kfree_skb_with_reason() in tcp_v4_rcv()
    Replace kfree_skb() with kfree_skb_with_reason() in tcp_v4_rcv().
    Following drop reason are added:
    
    SKB_DROP_REASON_NO_SOCK
    SKB_DROP_REASON_BAD_PACKET
    SKB_DROP_REASON_TCP_CSUM
    
    After this patch, 'kfree_skb' event will print message like this:
    
    $           TASK-PID     CPU#  |||||  TIMESTAMP  FUNCTION
    $              | |         |   |||||     |         |
              <idle>-0       [000] ..s1.    36.113438: kfree_skb: skbaddr=(____ptrval____) protocol=2048 location=(____ptrval____) reason: NO_SOCK
    
    The reason of skb drop is printed too.
    
    Signed-off-by: Menglong Dong <imagedong@tencent.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1971,8 +1971,10 @@ int tcp_v4_rcv(struct sk_buff *skb)
 	const struct tcphdr *th;
 	bool refcounted;
 	struct sock *sk;
+	int drop_reason;
 	int ret;
 
+	drop_reason = 0;
 	if (skb->pkt_type != PACKET_HOST)
 		goto discard_it;
 
@@ -1984,8 +1986,10 @@ int tcp_v4_rcv(struct sk_buff *skb)
 
 	th = (const struct tcphdr *)skb->data;
 
-	if (unlikely(th->doff < sizeof(struct tcphdr) / 4))
+	if (unlikely(th->doff < sizeof(struct tcphdr) / 4)) {
+		drop_reason = SKB_DROP_REASON_BAD_PACKET;
 		goto bad_packet;
+	}
 	if (!pskb_may_pull(skb, th->doff * 4))
 		goto discard_it;
 
@@ -2124,6 +2128,7 @@ put_and_return:
 	return ret;
 
 no_tcp_socket:
+	drop_reason = SKB_DROP_REASON_NO_SOCK;
 	if (!xfrm4_policy_check(NULL, XFRM_POLICY_IN, skb))
 		goto discard_it;
 
@@ -2131,6 +2136,7 @@ no_tcp_socket:
 
 	if (tcp_checksum_complete(skb)) {
 csum_error:
+		drop_reason = SKB_DROP_REASON_TCP_CSUM;
 		trace_tcp_bad_csum(skb);
 		__TCP_INC_STATS(net, TCP_MIB_CSUMERRORS);
 bad_packet:
@@ -2141,7 +2147,7 @@ bad_packet:
 
 discard_it:
 	/* Discard frame. */
-	kfree_skb(skb);
+	kfree_skb_with_reason(skb, drop_reason);
 	return 0;
 
 discard_and_relse:
