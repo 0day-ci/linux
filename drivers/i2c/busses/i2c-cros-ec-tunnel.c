1dd6320c926eaec451c0f468cb74b16060386d86
Gwendal Grignou <gwendal@chromium.org>
2022-01-05 14:06:47 -0800

    i2c: cros-ec-tunnel: Add PL_CHROMEOS dependency
    Use 'make nsdeps' to add missing dependencies.
    
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>

--- drivers/i2c/busses/i2c-cros-ec-tunnel.c
+++ drivers/i2c/busses/i2c-cros-ec-tunnel.c
@@ -317,5 +317,6 @@ static struct platform_driver ec_i2c_tunnel_driver = {
 module_platform_driver(ec_i2c_tunnel_driver);
 
 MODULE_LICENSE("GPL");
+MODULE_IMPORT_NS(PL_CHROMEOS);
 MODULE_DESCRIPTION("EC I2C tunnel driver");
 MODULE_ALIAS("platform:cros-ec-i2c-tunnel");
