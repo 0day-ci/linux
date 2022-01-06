4d6f7b97de43efb831ee7dbf67913ef8bc4edd38
Fabiano Rosas <farosas@linux.ibm.com>
2022-01-06 17:03:03 -0300

    KVM: PPC: mmio: Return to guest after emulation failure
    If MMIO emulation fails we don't want to crash the whole guest by
    returning to userspace.
    
    The original commit bbf45ba57eae ("KVM: ppc: PowerPC 440 KVM
    implementation") added a todo:
    
      /* XXX Deliver Program interrupt to guest. */
    
    and later the commit d69614a295ae ("KVM: PPC: Separate loadstore
    emulation from priv emulation") added the Program interrupt injection
    but in another file, so I'm assuming it was missed that this block
    needed to be altered.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>

--- arch/powerpc/kvm/powerpc.c
+++ arch/powerpc/kvm/powerpc.c
@@ -309,7 +309,7 @@ int kvmppc_emulate_mmio(struct kvm_vcpu *vcpu)
 		kvmppc_get_last_inst(vcpu, INST_GENERIC, &last_inst);
 		kvmppc_core_queue_program(vcpu, 0);
 		pr_info("%s: emulation failed (%08x)\n", __func__, last_inst);
-		r = RESUME_HOST;
+		r = RESUME_GUEST;
 		break;
 	}
 	default:
