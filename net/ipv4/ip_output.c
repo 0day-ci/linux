88dee3adcf6168f1bf9a6b590fd9a836f11183c2
caixf <ooppublic@163.com>
2022-01-07 16:05:59 +0800

    net: fix fragments have the disallowed options
    When in function ip_do_fragment() enter fsat path,
    if skb have opthons, all fragments will have the same options.
    
    Just guarantee the second fragment not have the disallowed options.
    
    Signed-off-by: caixf <ooppublic@163.com>

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -825,15 +825,16 @@ int ip_do_fragment(struct net *net, struct sock *sk, struct sk_buff *skb,
 		/* Everything is OK. Generate! */
 		ip_fraglist_init(skb, iph, hlen, &iter);
 
-		if (iter.frag)
-			ip_options_fragment(iter.frag);
-
 		for (;;) {
 			/* Prepare header of the next frame,
 			 * before previous one went down. */
 			if (iter.frag) {
+				bool first_frag = (iter.offset == 0);
+
 				IPCB(iter.frag)->flags = IPCB(skb)->flags;
 				ip_fraglist_prepare(skb, &iter);
+				if (first_frag)
+					ip_options_fragment(iter.frag);
 			}
 
 			skb->tstamp = tstamp;
