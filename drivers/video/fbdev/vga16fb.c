abd2e1e49490a3418f85bfef91f4e85f77d68fd1
Javier Martinez Canillas <javierm@redhat.com>
2022-01-07 12:07:22 +0100

    video: vga16fb: Fix logic that checks for the display standard
    The vga16fb framebuffer driver supports both Enhanced Graphics Adapter
    (EGA) and Video Graphics Array (VGA) 16 color graphic cards.
    
    But the logic to check whether the EGA or VGA standard are used is not
    correct. It just checks if screen_info.orig_video_isVGA is set, but it
    should check if is set to VIDEO_TYPE_VGAC instead.
    
    This means that it assumes to be VGA even if is set to VIDEO_TYPE_EGAC.
    
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>

--- drivers/video/fbdev/vga16fb.c
+++ drivers/video/fbdev/vga16fb.c
@@ -1332,7 +1332,7 @@ static int vga16fb_probe(struct platform_device *dev)
 	printk(KERN_INFO "vga16fb: mapped to 0x%p\n", info->screen_base);
 	par = info->par;
 
-	par->isVGA = screen_info.orig_video_isVGA;
+	par->isVGA = screen_info.orig_video_isVGA == VIDEO_TYPE_VGAC;
 	par->palette_blanked = 0;
 	par->vesa_blanked = 0;
 
