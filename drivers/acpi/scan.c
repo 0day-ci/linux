c800fa9c2fafde094626773bb817e0b4d52b927f
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2022-01-11 17:50:22 +0100

    ACPI: scan: Change acpi_scan_init() return value type to void
    The only caller of acpi_scan_init(), acpi_init(), doesn't check its
    return value, so turn it into a void function.
    
    This avoids complaints from the Smatch static checker that the
    function should return a negative error code when it fails, which
    is not really a problem in this particular case.
    
    No intentional functional impact.
    
    Link: https://lore.kernel.org/linux-acpi/20220106082317.GA9123@kili/
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -2513,9 +2513,8 @@ static void __init acpi_get_spcr_uart_addr(void)
 
 static bool acpi_scan_initialized;
 
-int __init acpi_scan_init(void)
+void __init acpi_scan_init(void)
 {
-	int result;
 	acpi_status status;
 	struct acpi_table_stao *stao_ptr;
 
@@ -2565,8 +2564,7 @@ int __init acpi_scan_init(void)
 	/*
 	 * Enumerate devices in the ACPI namespace.
 	 */
-	result = acpi_bus_scan(ACPI_ROOT_OBJECT);
-	if (result)
+	if (acpi_bus_scan(ACPI_ROOT_OBJECT))
 		goto out;
 
 	acpi_root = acpi_fetch_acpi_dev(ACPI_ROOT_OBJECT);
@@ -2575,8 +2573,7 @@ int __init acpi_scan_init(void)
 
 	/* Fixed feature devices do not exist on HW-reduced platform */
 	if (!acpi_gbl_reduced_hardware) {
-		result = acpi_bus_scan_fixed();
-		if (result) {
+		if (acpi_bus_scan_fixed()) {
 			acpi_detach_data(acpi_root->handle,
 					 acpi_scan_drop_device);
 			acpi_device_del(acpi_root);
@@ -2591,7 +2588,6 @@ int __init acpi_scan_init(void)
 
  out:
 	mutex_unlock(&acpi_scan_lock);
-	return result;
 }
 
 static struct acpi_probe_entry *ape;
