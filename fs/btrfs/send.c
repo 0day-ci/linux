5f9d5408af621e2ca1517d7b41b312c7cfc31adb
Minghao Chi <chi.minghao@zte.com.cn>
2022-01-11 01:57:16 +0000

    fs/btrfs: remove redundant ret variable
    Return value from fs_path_add_path() directly instead
    of taking this in another redundant variable.
    
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: Minghao Chi <chi.minghao@zte.com.cn>
    Signed-off-by: CGEL ZTE <cgel.zte@gmail.com>

--- fs/btrfs/send.c
+++ fs/btrfs/send.c
@@ -528,14 +528,10 @@ out:
 
 static int fs_path_copy(struct fs_path *p, struct fs_path *from)
 {
-	int ret;
-
 	p->reversed = from->reversed;
 	fs_path_reset(p);
 
-	ret = fs_path_add_path(p, from);
-
-	return ret;
+	return fs_path_add_path(p, from);
 }
 
 
