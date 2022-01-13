c183cb11e7ea2a8b7ef88b7b0bba43d2f4416493
Kuogee Hsieh <quic_khsieh@quicinc.com>
2022-01-13 15:53:37 -0800

    drm/msm/dp: populate connector of struct dp_panel
    DP CTS test case 4.2.2.6 has valid edid with bad checksum on purpose
    and expect DP source return correct checksum. During drm edid read,
    correct edid checksum is calculated and stored at
    connector::real_edid_checksum.
    
    The problem is struct dp_panel::connector never be assigned, instead the
    connector is stored in struct msm_dp::connector. When we run compliance
    testing test case 4.2.2.6 dp_panel_handle_sink_request() won't have a valid
    edid set in struct dp_panel::edid so we'll try to use the connectors
    real_edid_checksum and hit a NULL pointer dereference error because the
    connector pointer is never assigned.
    
    Changes in V2:
    -- populate panel connector at msm_dp_modeset_init() instead of at dp_panel_read_sink_caps()
    
    Changes in V3:
    -- remove unhelpful kernel crash trace commit text
    -- remove renaming dp_display parameter to dp
    
    Changes in V4:
    -- add more details to commit text
    
    Changes in v10:
    --  group into one series
    
    Changes in v11:
    -- drop drm/msm/dp: dp_link_parse_sink_count() return immediately if aux read
    
    Fixes: 7948fe12d47 ("drm/msm/dp: return correct edid checksum after corrupted edid checksum read")
    Signee-off-by: Kuogee Hsieh <quic_khsieh@quicinc.com>
    
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -1483,6 +1483,7 @@ int msm_dp_modeset_init(struct msm_dp *dp_display, struct drm_device *dev,
 			struct drm_encoder *encoder)
 {
 	struct msm_drm_private *priv;
+	struct dp_display_private *dp_priv;
 	int ret;
 
 	if (WARN_ON(!encoder) || WARN_ON(!dp_display) || WARN_ON(!dev))
@@ -1490,6 +1491,8 @@ int msm_dp_modeset_init(struct msm_dp *dp_display, struct drm_device *dev,
 
 	priv = dev->dev_private;
 	dp_display->drm_dev = dev;
+
+	dp_priv = container_of(dp_display, struct dp_display_private, dp_display);
 
 	ret = dp_display_request_irq(dp_display);
 	if (ret) {
@@ -1507,6 +1510,8 @@ int msm_dp_modeset_init(struct msm_dp *dp_display, struct drm_device *dev,
 		dp_display->connector = NULL;
 		return ret;
 	}
+
+	dp_priv->panel->connector = dp_display->connector;
 
 	priv->connectors[priv->num_connectors++] = dp_display->connector;
 
