5b315d3f338999eebdca46e5e02e1465fe2c51da
Liang Yang <liang.yang@amlogic.com>
2022-01-13 19:57:44 +0800

    clk: meson: add DT documentation for emmc clock controller
    Document the MMC sub clock controller driver, the potential consumer
    of this driver is MMC or NAND. Also add four clock bindings IDs which
    provided by this driver.
    
    Signed-off-by: Liang Yang <liang.yang@amlogic.com>

---
+++ include/dt-bindings/clock/amlogic,mmc-clkc.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#ifndef __MMC_CLKC_H
+#define __MMC_CLKC_H
+
+#define CLKID_MMC_DIV		0
+#define CLKID_MMC_PHASE_CORE	1
+#define CLKID_MMC_PHASE_TX	2
+#define CLKID_MMC_PHASE_RX	3
+
+#endif
