57262f8c591e2e3879ebbab9b18263ececd9c8e8
Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
2022-01-14 18:41:36 +0800

    btrfs: zoned: Remove redundant initialization of to_add
    to_add is being initialized to len but this is never read as
    to_add is overwritten later on. Remove the redundant
    initialization.
    
    Cleans up the following clang-analyzer warning:
    
    fs/btrfs/extent-tree.c:2769:8: warning: Value stored to 'to_add' during
    its initialization is never read [clang-analyzer-deadcode.DeadStores].
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Signed-off-by: Jiapeng Chong <jiapeng.chong@linux.alibaba.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -2766,7 +2766,7 @@ static int unpin_extent_range(struct btrfs_fs_info *fs_info,
 		spin_unlock(&cache->lock);
 		if (!readonly && return_free_space &&
 		    global_rsv->space_info == space_info) {
-			u64 to_add = len;
+			u64 to_add;
 
 			spin_lock(&global_rsv->lock);
 			if (!global_rsv->full) {
