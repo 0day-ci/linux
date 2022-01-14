0299c343a5d6828a7444d10812bf2bd89340d3ab
Marek Vasut <marex@denx.de>
2022-01-14 04:48:33 +0100

    drm: bridge: icn6211: Use DSI burst mode without EoT and with LP command mode
    The DSI burst mode is more energy efficient than the DSI sync pulse mode,
    make use of the burst mode since the chip supports it as well. Disable the
    generation of EoT packet, the chip ignores it, so no point in emitting it.
    Enable transmission of data in LP mode, otherwise register read via DSI
    does not work with this chip.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Jagan Teki <jagan@amarulasolutions.com>
    Cc: Robert Foss <robert.foss@linaro.org>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    To: dri-devel@lists.freedesktop.org

--- drivers/gpu/drm/bridge/chipone-icn6211.c
+++ drivers/gpu/drm/bridge/chipone-icn6211.c
@@ -529,7 +529,8 @@ static int chipone_probe(struct mipi_dsi_device *dsi)
 
 	dsi->lanes = icn->dsi_lanes;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO_SYNC_PULSE;
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
+			  MIPI_DSI_MODE_LPM | MIPI_DSI_MODE_NO_EOT_PACKET;
 
 	ret = mipi_dsi_attach(dsi);
 	if (ret < 0) {
