f880a69bb1098edc464e69c061b45e3d34ce456d
Xin Hao <xhao@linux.alibaba.com>
2022-01-21 00:04:28 +0800

    mm/damon/dbgfs: Modify Damon dbfs interface dependency in Kconfig
    If you want to support "DAMON_DBGFS" in config file, it only depends on
    any one of "DAMON_VADDR" and "DAMON_PADDR", and sometimes we just want to
    use damon virtual address function, but it is unreasonable to include
    "DAMON_PADDR" in config file which cause the damon/paddr.c be compiled.
    
    So there just do little change to fix it.
    
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -501,12 +501,28 @@ int damon_stop(struct damon_ctx **ctxs, int nr_ctxs);
 #ifdef CONFIG_DAMON_VADDR
 bool damon_va_target_valid(void *t);
 void damon_va_set_primitives(struct damon_ctx *ctx);
+#else
+static inline void damon_va_set_primitives(struct damon_ctx *ctx) { }
+static inline bool damon_va_target_valid(void *t)
+{
+	return false;
+}
 #endif	/* CONFIG_DAMON_VADDR */
 
 #ifdef CONFIG_DAMON_PADDR
 bool damon_pa_check(char *buf, size_t count);
 bool damon_pa_target_valid(void *t);
 void damon_pa_set_primitives(struct damon_ctx *ctx);
+#else
+static inline bool damon_pa_check(char *buf, size_t count)
+{
+	return false;
+}
+static inline void damon_pa_set_primitives(struct damon_ctx *ctx) { }
+static inline bool damon_pa_target_valid(void *t)
+{
+	return false;
+}
 #endif	/* CONFIG_DAMON_PADDR */
 
 #endif	/* _DAMON_H */
