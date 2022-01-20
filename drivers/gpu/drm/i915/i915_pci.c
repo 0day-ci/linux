cf1a6660ac07b3b3618b35dccab57042d592ea2c
Ramalingam C <ramalingam.c@intel.com>
2022-01-20 21:51:02 +0530

    drm/i915: Add needs_compact_pt flag
    Add a new platform flag, needs_compact_pt, to mark the requirement of
    compact pt layout support for the ppGTT when using 64K GTT pages.
    
    With this flag has_64k_pages will only indicate requirement of 64K
    GTT page sizes or larger for device local memory access.
    
    Suggested-by: Matthew Auld <matthew.auld@intel.com>
    Signed-off-by: Ramalingam C <ramalingam.c@intel.com>
    cc: Robert Beckett <bob.beckett@collabora.com>

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -1028,6 +1028,7 @@ static const struct intel_device_info xehpsdv_info = {
 	PLATFORM(INTEL_XEHPSDV),
 	.display = { },
 	.has_64k_pages = 1,
+	.needs_compact_pt = 1,
 	.platform_engine_mask =
 		BIT(RCS0) | BIT(BCS0) |
 		BIT(VECS0) | BIT(VECS1) | BIT(VECS2) | BIT(VECS3) |
@@ -1046,6 +1047,7 @@ static const struct intel_device_info dg2_info = {
 	.media.rel = 55,
 	PLATFORM(INTEL_DG2),
 	.has_64k_pages = 1,
+	.needs_compact_pt = 1,
 	.platform_engine_mask =
 		BIT(RCS0) | BIT(BCS0) |
 		BIT(VECS0) | BIT(VECS1) |
