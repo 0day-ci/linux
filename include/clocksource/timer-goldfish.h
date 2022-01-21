538312745182ae5d9866c21d916c7f433f0d16f1
Laurent Vivier <laurent@vivier.eu>
2022-01-21 21:07:37 +0100

    clocksource/drivers: Add a goldfish-timer clocksource
    Add a clocksource based on the goldfish-rtc device.
    
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

---
+++ include/clocksource/timer-goldfish.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * goldfish-timer clocksource
+ */
+
+#ifndef _CLOCKSOURCE_GOLDFISH_TIMER_H
+#define _CLOCKSOURCE_GOLDFISH_TIMER_H
+
+extern void goldfish_timer_init(int irq, void __iomem *base);
+
+#endif /* _CLOCKSOURCE_GOLDFISH_TIMER_H */
