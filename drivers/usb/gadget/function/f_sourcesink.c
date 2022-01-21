5c4b6ae693aa905954b7e8cc09a88130a858545e
Pavankumar Kondeti <quic_pkondeti@quicinc.com>
2022-01-21 17:01:24 +0530

    usb: gadget: f_sourcesink: Fix isoc transfer for USB_SPEED_SUPER_PLUS
    Currently when gadget enumerates in super speed plus, the isoc
    endpoint request buffer size is not calculated correctly. Fix
    this by checking the gadget speed against USB_SPEED_SUPER_PLUS
    and update the request buffer size.
    
    Signed-off-by: Pavankumar Kondeti <quic_pkondeti@quicinc.com>

--- drivers/usb/gadget/function/f_sourcesink.c
+++ drivers/usb/gadget/function/f_sourcesink.c
@@ -584,6 +584,8 @@ static int source_sink_start_ep(struct f_sourcesink *ss, bool is_in,
 
 	if (is_iso) {
 		switch (speed) {
+		case USB_SPEED_SUPER_PLUS:
+			fallthrough;
 		case USB_SPEED_SUPER:
 			size = ss->isoc_maxpacket *
 					(ss->isoc_mult + 1) *
