ffc4e5032b8fceab84181f9d147f701c06185a7f
Akhil R <akhilrajeev@nvidia.com>
2022-01-22 17:02:46 +0530

    i2c: smbus: Use device_*() functions instead of of_*()
    Change of_*() functions to device_*() for firmware agnostic usage.
    This allows to have the smbus_alert interrupt without any changes
    in the controller drivers using the ACPI table.
    
    Signed-off-by: Akhil R <akhilrajeev@nvidia.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>

--- drivers/i2c/i2c-smbus.c
+++ drivers/i2c/i2c-smbus.c
@@ -128,7 +128,8 @@ static int smbalert_probe(struct i2c_client *ara,
 	if (setup) {
 		irq = setup->irq;
 	} else {
-		irq = of_irq_get_byname(adapter->dev.of_node, "smbus_alert");
+		irq = fwnode_irq_get_byname(dev_fwnode(adapter->dev.parent),
+					    "smbus_alert");
 		if (irq <= 0)
 			return irq;
 	}
