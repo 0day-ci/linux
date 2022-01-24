3e5f14125bbabee585d4727c45ca4abaca4717ef
NeilBrown <neilb@suse.de>
2022-01-24 14:48:32 +1100

    MM: move responsibility for setting SWP_FS_OPS to ->swap_activate
    If a filesystem wishes to handle all swap IO itself (via ->direct_IO),
    rather than just providing devices addresses for submit_bio(),
    SWP_FS_OPS must be set.
    Currently the protocol for setting this it to have ->swap_activate
    return zero.  In that case SWP_FS_OPS is set, and add_swap_extent()
    is called for the entire file.
    
    This is a little clumsy as different return values for ->swap_activate
    have quite different meanings, and it makes it hard to search for which
    filesystems require SWP_FS_OPS to be set.
    
    So remove the special meaning of a zero return, and require the
    filesystem to set SWP_FS_OPS if it so desires, and to always call
    add_swap_extent() as required.
    
    Currently only NFS and CIFS return zero for add_swap_extent().
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/nfs/file.c
+++ fs/nfs/file.c
@@ -488,6 +488,7 @@ static int nfs_swap_activate(struct swap_info_struct *sis, struct file *file,
 {
 	unsigned long blocks;
 	long long isize;
+	int ret;
 	struct rpc_clnt *clnt = NFS_CLIENT(file->f_mapping->host);
 	struct inode *inode = file->f_mapping->host;
 
@@ -500,9 +501,17 @@ static int nfs_swap_activate(struct swap_info_struct *sis, struct file *file,
 		return -EINVAL;
 	}
 
+	ret = rpc_clnt_swap_activate(clnt);
+	if (ret)
+		return ret;
+	ret = add_swap_extent(sis, 0, sis->max, 0);
+	if (ret < 0) {
+		rpc_clnt_swap_deactivate(clnt);
+		return ret;
+	}
 	*span = sis->pages;
-
-	return rpc_clnt_swap_activate(clnt);
+	sis->flags |= SWP_FS_OPS;
+	return ret;
 }
 
 static void nfs_swap_deactivate(struct file *file)
