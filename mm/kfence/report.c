eeff9719cbff761fc89257d0850c67e3b81e1526
Tiezhu Yang <yangtiezhu@loongson.cn>
2022-01-28 19:42:25 +0800

    kfence: unset panic_on_warn before calling panic()
    As done in the full WARN() handler, panic_on_warn needs to be cleared
    before calling panic() to avoid recursive panics.
    
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>

--- mm/kfence/report.c
+++ mm/kfence/report.c
@@ -267,8 +267,16 @@ void kfence_report_error(unsigned long address, bool is_write, struct pt_regs *r
 
 	lockdep_on();
 
-	if (panic_on_warn)
+	if (panic_on_warn) {
+		/*
+		 * This thread may hit another WARN() in the panic path.
+		 * Resetting this prevents additional WARN() from panicking the
+		 * system on this thread.  Other threads are blocked by the
+		 * panic_mutex in panic().
+		 */
+		panic_on_warn = 0;
 		panic("panic_on_warn set ...\n");
+	}
 
 	/* We encountered a memory safety error, taint the kernel! */
 	add_taint(TAINT_BAD_PAGE, LOCKDEP_STILL_OK);
