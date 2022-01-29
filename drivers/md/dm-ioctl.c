494fed5461aa05e0efaf098b57a2a47dc19ba226
Jordy Zomer <jordy@pwning.systems>
2022-01-29 15:37:22 +0100

    dm ioct: prevent potential specter v1 gadget
    It appears like cmd could be a Spectre v1 gadget as it's supplied by a
    user and used as an array index. Prevent the contents
    of kernel memory from being leaked to userspace via speculative
    execution by using array_index_nospec.
    
    Signed-off-by: Jordy Zomer <jordy@pwning.systems>

--- drivers/md/dm-ioctl.c
+++ drivers/md/dm-ioctl.c
@@ -1788,6 +1788,7 @@ static ioctl_fn lookup_ioctl(unsigned int cmd, int *ioctl_flags)
 	if (unlikely(cmd >= ARRAY_SIZE(_ioctls)))
 		return NULL;
 
+	cmd = array_index_nospec(cmd, ARRAY_SIZE(_ioctls));
 	*ioctl_flags = _ioctls[cmd].flags;
 	return _ioctls[cmd].fn;
 }
