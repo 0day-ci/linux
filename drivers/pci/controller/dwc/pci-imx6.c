67ff7a0983a6a856dc2b91305724e293bf2d3102
Jason Liu <jason.hui.liu@nxp.com>
2022-01-31 08:52:35 +0100

    PCI: imx6: Handle the abort from user-space
    The driver install one hook to handle the external abort, but issue
    is that if the abort introduced from user space code, the following
    code unsigned long instr = *(unsigned long *)pc; which will created
    another data-abort(page domain fault) if CONFIG_CPU_SW_DOMAIN_PAN.
    
    The patch does not intent to use copy_from_user and then do the hack
    due to the security consideration. In fact, we can just return and
    report the external abort to user-space.
    
    Link: https://lore.kernel.org/all/20220128082920.591115-1-francesco.dolcini@toradex.com
    Signed-off-by: Jason Liu <jason.hui.liu@nxp.com>
    Reviewed-by: Richard Zhu <hongxing.zhu@nxp.com>
    Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>
    Acked-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -22,6 +22,7 @@
 #include <linux/of_address.h>
 #include <linux/pci.h>
 #include <linux/platform_device.h>
+#include <linux/ptrace.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
 #include <linux/resource.h>
@@ -297,8 +298,15 @@ static int imx6q_pcie_abort_handler(unsigned long addr,
 		unsigned int fsr, struct pt_regs *regs)
 {
 	unsigned long pc = instruction_pointer(regs);
-	unsigned long instr = *(unsigned long *)pc;
-	int reg = (instr >> 12) & 15;
+	unsigned long instr;
+	int reg;
+
+	/* if the abort from user-space, just return and report it */
+	if (user_mode(regs))
+		return 1;
+
+	instr = *(unsigned long *)pc;
+	reg = (instr >> 12) & 15;
 
 	/*
 	 * If the instruction being executed was a read,
