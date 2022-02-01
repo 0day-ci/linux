7a97431df0adffe38be6051e2fa3fbe9fb10815b
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2022-02-01 20:07:08 +0100

    drivers: net: Replace acpi_bus_get_device()
    Replace acpi_bus_get_device() that is going to be dropped with
    acpi_fetch_acpi_dev().
    
    No intentional functional impact.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/net/ethernet/cavium/thunder/thunder_bgx.c
+++ drivers/net/ethernet/cavium/thunder/thunder_bgx.c
@@ -1407,9 +1407,9 @@ static acpi_status bgx_acpi_register_phy(acpi_handle handle,
 {
 	struct bgx *bgx = context;
 	struct device *dev = &bgx->pdev->dev;
-	struct acpi_device *adev;
+	struct acpi_device *adev = acpi_fetch_acpi_dev(handle);
 
-	if (acpi_bus_get_device(handle, &adev))
+	if (!adev)
 		goto out;
 
 	acpi_get_mac_address(dev, adev, bgx->lmac[bgx->acpi_lmac_idx].mac);
