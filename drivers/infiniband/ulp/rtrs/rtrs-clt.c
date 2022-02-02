e0bd45ea06c9975d59b87d803ba3ae7dc6c46b97
Md Haris Iqbal <haris.iqbal@ionos.com>
2022-02-02 16:08:55 +0100

    RDMA/rtrs-clt: Move free_permit from free_clt to rtrs_clt_close
    Error path of rtrs_clt_open calls free_clt, where free_permit is called.
    This is wrong since error path of rtrs_clt_open does not need to call
    free_permit.
    
    Also, moving free_permits call to rtrs_clt_close, makes it more aligned
    with the call to alloc_permit in rtrs_clt_open.
    
    Signed-off-by: Md Haris Iqbal <haris.iqbal@ionos.com>
    Reviewed-by: Jack Wang <jinpu.wang@ionos.com>

--- drivers/infiniband/ulp/rtrs/rtrs-clt.c
+++ drivers/infiniband/ulp/rtrs/rtrs-clt.c
@@ -2774,7 +2774,6 @@ err_put:
 
 static void free_clt(struct rtrs_clt_sess *clt)
 {
-	free_permits(clt);
 	free_percpu(clt->pcpu_path);
 
 	/*
@@ -2896,6 +2895,7 @@ void rtrs_clt_close(struct rtrs_clt_sess *clt)
 		rtrs_clt_destroy_path_files(clt_path, NULL);
 		kobject_put(&clt_path->kobj);
 	}
+	free_permits(clt);
 	free_clt(clt);
 }
 EXPORT_SYMBOL(rtrs_clt_close);
