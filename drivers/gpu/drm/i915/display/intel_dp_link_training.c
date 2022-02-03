8f92902be30e12228b61053345b446a400ef9913
Jani Nikula <jani.nikula@intel.com>
2022-02-03 11:03:57 +0200

    HACK: drm/i915/dp: give more time for CDS
    Try to avoid the timeout during debugging.
    
    Cc: Uma Shankar <uma.shankar@intel.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp_link_training.c
+++ drivers/gpu/drm/i915/display/intel_dp_link_training.c
@@ -1288,6 +1288,9 @@ intel_dp_128b132b_lane_cds(struct intel_dp *intel_dp,
 	/* Time budget for the LANEx_CDS_DONE Sequence */
 	deadline = jiffies + msecs_to_jiffies((lttpr_count + 1) * 20);
 
+	/* FIXME: Give some slack for CDS. */
+	deadline += msecs_to_jiffies(500);
+
 	for (;;) {
 		usleep_range(2000, 3000);
 
