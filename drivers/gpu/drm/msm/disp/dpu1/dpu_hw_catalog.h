58cca890c9e3eb97d9b120b59bbc2ad2e7166b83
Loic Poulain <loic.poulain@linaro.org>
2022-02-08 17:42:31 +0100

    drm/msm: add support for QCM2290 MDSS
    Add compatibility for QCM2290 display subsystem, including
    required entries in DPU hw catalog.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.h
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.h
@@ -41,6 +41,7 @@
 #define DPU_HW_VER_501	DPU_HW_VER(5, 0, 1) /* sm8150 v2.0 */
 #define DPU_HW_VER_600	DPU_HW_VER(6, 0, 0) /* sm8250 */
 #define DPU_HW_VER_620	DPU_HW_VER(6, 2, 0) /* sc7180 v1.0 */
+#define DPU_HW_VER_650	DPU_HW_VER(6, 5, 0) /* qcm2290|sm4125 */
 #define DPU_HW_VER_720	DPU_HW_VER(7, 2, 0) /* sc7280 */
 
 #define IS_MSM8996_TARGET(rev) IS_DPU_MAJOR_MINOR_SAME((rev), DPU_HW_VER_170)
