1572a8fac5d3154bf5bc2f1fd86cd08b90257dce
Mario Limonciello <mario.limonciello@amd.com>
2022-02-11 13:32:48 -0600

    drm/radeon: drop the use of `pci_is_thunderbolt_attached`
    Currently `pci_is_thunderbolt_attached` is used to indicate a device
    is connected externally.
    
    The PCI core now marks such devices as removable and downstream drivers
    can use this instead.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/gpu/drm/radeon/radeon_device.c
+++ drivers/gpu/drm/radeon/radeon_device.c
@@ -1439,7 +1439,7 @@ int radeon_device_init(struct radeon_device *rdev,
 
 	if (rdev->flags & RADEON_IS_PX)
 		runtime = true;
-	if (!pci_is_thunderbolt_attached(rdev->pdev))
+	if (!dev_is_removable(&rdev->pdev->dev))
 		vga_switcheroo_register_client(rdev->pdev,
 					       &radeon_switcheroo_ops, runtime);
 	if (runtime)
@@ -1527,7 +1527,7 @@ void radeon_device_fini(struct radeon_device *rdev)
 	/* evict vram memory */
 	radeon_bo_evict_vram(rdev);
 	radeon_fini(rdev);
-	if (!pci_is_thunderbolt_attached(rdev->pdev))
+	if (!dev_is_removable(&rdev->pdev->dev))
 		vga_switcheroo_unregister_client(rdev->pdev);
 	if (rdev->flags & RADEON_IS_PX)
 		vga_switcheroo_fini_domain_pm_ops(rdev->dev);
