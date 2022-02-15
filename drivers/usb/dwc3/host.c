1208d046e0088c8f102397b8bdf8ac0eca6647a4
Sandeep Maheswaram <quic_c_sanm@quicinc.com>
2022-02-15 23:54:14 +0530

    usb: dwc: host: add xhci_plat_priv quirk XHCI_SKIP_PHY_INIT
    dwc3 manages PHY by own DRD driver, so skip the management by
    HCD core.
    During runtime suspend phy was not getting suspend because
    runtime_usage value is 2.
    
    Signed-off-by: Sandeep Maheswaram <quic_c_sanm@quicinc.com>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -13,6 +13,14 @@
 #include <linux/platform_device.h>
 
 #include "core.h"
+#include <linux/usb/hcd.h>
+#include <linux/usb/xhci-plat.h>
+#include <linux/usb/xhci-quirks.h>
+
+
+static const struct xhci_plat_priv xhci_plat_dwc3_xhci = {
+	.quirks = XHCI_SKIP_PHY_INIT,
+};
 
 static void dwc3_host_fill_xhci_irq_res(struct dwc3 *dwc,
 					int irq, char *name)
@@ -120,6 +128,13 @@ int dwc3_host_init(struct dwc3 *dwc)
 			dev_err(dwc->dev, "failed to add properties to xHCI\n");
 			goto err;
 		}
+	}
+
+	ret = platform_device_add_data(xhci, &xhci_plat_dwc3_xhci,
+			sizeof(struct xhci_plat_priv));
+	if (ret) {
+		dev_err(dwc->dev, "failed to add data to xHCI\n");
+		goto err;
 	}
 
 	ret = platform_device_add(xhci);
