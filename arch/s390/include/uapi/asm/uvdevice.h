fc8d42bd9a0a9bd87be0f7f3918f2f59130ae101
Steffen Eiden <seiden@linux.ibm.com>
2022-02-17 06:37:16 -0500

    drivers/s390/char: Add Ultravisor attestation to uvdevice
    This patch enables userspace to call the Retrieve Attestation Measurement
    Ultravisor Call using IOCTLs on the uvdevice.
    
    The uvdevice will do some sanity checks first.
    Then, copy the request data to kernel space, build the uvcb,
    perform the UV call, and copy the result back to userspace.
    
    Userspace is now able to call the Retrieve Attestation Measurement
    Ultravisor Call through the uvdevice.
    
    Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>

--- arch/s390/include/uapi/asm/uvdevice.h
+++ arch/s390/include/uapi/asm/uvdevice.h
@@ -17,11 +17,30 @@ struct uvio_ioctl_cb {
 	__u8  reserved14[0x40 - 0x14];	/* must be zero */
 };
 
+#define UVIO_ATT_USER_DATA_LEN		0x100
+#define UVIO_ATT_UID_LEN		0x10
+struct uvio_attest {
+	__u64 arcb_addr;				/* 0x0000 */
+	__u64 meas_addr;				/* 0x0008 */
+	__u64 add_data_addr;				/* 0x0010 */
+	__u8  user_data[UVIO_ATT_USER_DATA_LEN];	/* 0x0018 */
+	__u8  config_uid[UVIO_ATT_UID_LEN];		/* 0x0118 */
+	__u32 arcb_len;					/* 0x0128 */
+	__u32 meas_len;					/* 0x012c */
+	__u32 add_data_len;				/* 0x0130 */
+	__u16 user_data_len;				/* 0x0134 */
+	__u16 reserved136;				/* 0x0136 */
+};
+
 #define UVIO_QUI_MAX_LEN		0x8000
+#define UVIO_ATT_ARCB_MAX_LEN		0x100000
+#define UVIO_ATT_MEASUREMENT_MAX_LEN	0x8000
+#define UVIO_ATT_ADDITIONAL_MAX_LEN	0x8000
 
 #define UVIO_DEVICE_NAME "uv"
 #define UVIO_TYPE_UVC 'u'
 
 #define UVIO_IOCTL_QUI _IOWR(UVIO_TYPE_UVC, 0x01, struct uvio_ioctl_cb)
+#define UVIO_IOCTL_ATT _IOWR(UVIO_TYPE_UVC, 0x02, struct uvio_ioctl_cb)
 
 #endif  /* __S390X_ASM_UVDEVICE_H */
