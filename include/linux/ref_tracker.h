c431b38f2a2636a4d641e387c01793522bd74291
Andrzej Hajda <andrzej.hajda@intel.com>
2022-02-22 00:16:44 +0100

    lib/ref_tracker: add unlocked leak print helper
    To have reliable detection of leaks, caller must be able to check under the same
    lock both: tracked counter and the leaks. dir.lock is natural candidate for such
    lock and unlocked print helper can be called with this lock taken.
    As a bonus we can reuse this helper in ref_tracker_dir_exit.
    
    Signed-off-by: Andrzej Hajda <andrzej.hajda@intel.com>
    Reviewed-by: Chris Wilson <chris.p.wilson@intel.com>

--- include/linux/ref_tracker.h
+++ include/linux/ref_tracker.h
@@ -32,6 +32,9 @@ static inline void ref_tracker_dir_init(struct ref_tracker_dir *dir,
 
 void ref_tracker_dir_exit(struct ref_tracker_dir *dir);
 
+void __ref_tracker_dir_print(struct ref_tracker_dir *dir,
+			   unsigned int display_limit);
+
 void ref_tracker_dir_print(struct ref_tracker_dir *dir,
 			   unsigned int display_limit);
 
@@ -49,6 +52,11 @@ static inline void ref_tracker_dir_init(struct ref_tracker_dir *dir,
 }
 
 static inline void ref_tracker_dir_exit(struct ref_tracker_dir *dir)
+{
+}
+
+static inline void __ref_tracker_dir_print(struct ref_tracker_dir *dir,
+					   unsigned int display_limit)
 {
 }
 
