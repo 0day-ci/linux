15ef7e8942cc14311219ae4ecf806488b81c83a9
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2022-02-22 14:04:15 +0000

    drm/i915: Explicitly track DRM clients
    Tracking DRM clients more explicitly will allow later patches to
    accumulate past and current GPU usage in a centralised place and also
    consolidate access to owning task pid/name.
    
    Unique client id is also assigned for the purpose of distinguishing/
    consolidating between multiple file descriptors owned by the same process.
    
    v2:
     Chris Wilson:
     * Enclose new members into dedicated structs.
     * Protect against failed sysfs registration.
    
    v3:
     * sysfs_attr_init.
    
    v4:
     * Fix for internal clients.
    
    v5:
     * Use cyclic ida for client id. (Chris)
     * Do not leak pid reference. (Chris)
     * Tidy code with some locals.
    
    v6:
     * Use xa_alloc_cyclic to simplify locking. (Chris)
     * No need to unregister individial sysfs files. (Chris)
     * Rebase on top of fpriv kref.
     * Track client closed status and reflect in sysfs.
    
    v7:
     * Make drm_client more standalone concept.
    
    v8:
     * Simplify sysfs show. (Chris)
     * Always track name and pid.
    
    v9:
     * Fix cyclic id assignment.
    
    v10:
     * No need for a mutex around xa_alloc_cyclic.
     * Refactor sysfs into own function.
     * Unregister sysfs before freeing pid and name.
     * Move clients setup into own function.
    
    v11:
     * Call clients init directly from driver init. (Chris)
    
    v12:
     * Do not fail client add on id wrap. (Maciej)
    
    v13 (Lucas): Rebase.
    
    v14:
     * Dropped sysfs bits.
    
    v15:
     * Dropped tracking of pid/ and name.
     * Dropped RCU freeing of the client object.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk> # v11
    Reviewed-by: Aravind Iddamsetty <aravind.iddamsetty@intel.com> # v11
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>

--- drivers/gpu/drm/i915/i915_driver.c
+++ drivers/gpu/drm/i915/i915_driver.c
@@ -76,6 +76,7 @@
 #include "i915_file_private.h"
 #include "i915_debugfs.h"
 #include "i915_driver.h"
+#include "i915_drm_client.h"
 #include "i915_drv.h"
 #include "i915_getparam.h"
 #include "i915_ioc32.h"
@@ -355,6 +356,8 @@ static int i915_driver_early_probe(struct drm_i915_private *dev_priv)
 
 	__intel_gt_init_early(to_gt(dev_priv), dev_priv);
 
+	i915_drm_clients_init(&dev_priv->clients, dev_priv);
+
 	i915_gem_init_early(dev_priv);
 
 	/* This must be called before any calls to HAS_PCH_* */
@@ -374,6 +377,7 @@ static int i915_driver_early_probe(struct drm_i915_private *dev_priv)
 
 err_gem:
 	i915_gem_cleanup_early(dev_priv);
+	i915_drm_clients_fini(&dev_priv->clients);
 	intel_gt_driver_late_release(to_gt(dev_priv));
 	intel_region_ttm_device_fini(dev_priv);
 err_ttm:
@@ -393,6 +397,7 @@ static void i915_driver_late_release(struct drm_i915_private *dev_priv)
 	intel_irq_fini(dev_priv);
 	intel_power_domains_cleanup(dev_priv);
 	i915_gem_cleanup_early(dev_priv);
+	i915_drm_clients_fini(&dev_priv->clients);
 	intel_gt_driver_late_release(to_gt(dev_priv));
 	intel_region_ttm_device_fini(dev_priv);
 	vlv_suspend_cleanup(dev_priv);
@@ -1023,6 +1028,7 @@ static void i915_driver_postclose(struct drm_device *dev, struct drm_file *file)
 	struct drm_i915_file_private *file_priv = file->driver_priv;
 
 	i915_gem_context_close(file);
+	i915_drm_client_put(file_priv->client);
 
 	kfree_rcu(file_priv, rcu);
 
