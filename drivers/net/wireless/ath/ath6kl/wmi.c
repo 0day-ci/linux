e69962cdd2dbb7b729e0231c0a2063e3c87361d8
Gustavo A. R. Silva <gustavoars@kernel.org>
2022-02-22 20:39:11 -0600

    ath6kl: wmi: Replace one-element array with flexible-array member in struct wmi_aplist_event
    Replace one-element array with flexible-array member in struct
    wmi_aplist_event.
    
    It's also worth noting that due to the flexible array transformation,
    the size of struct wmi_aplist_event changed (now the size is 8-byte
    smaller), and in order to preserve the logic of before the transformation,
    the following change is needed:
    
            -       if (len < sizeof(struct wmi_aplist_event))
            +       if (len <= sizeof(struct wmi_aplist_event))
    
    sizeof(struct wmi_aplist_event) before the flex-array transformation:
    
    struct wmi_aplist_event {
    	u8                         ap_list_ver;          /*     0     1 */
    	u8                         num_ap;               /*     1     1 */
    	union wmi_ap_info          ap_list[1];           /*     2     8 */
    
    	/* size: 10, cachelines: 1, members: 3 */
    	/* last cacheline: 10 bytes */
    };
    
    sizeof(struct wmi_aplist_event) after the flex-array transformation:
    
    struct wmi_aplist_event {
    	u8                         ap_list_ver;          /*     0     1 */
    	u8                         num_ap;               /*     1     1 */
    	union wmi_ap_info          ap_list[];            /*     2     0 */
    
    	/* size: 2, cachelines: 1, members: 3 */
    	/* last cacheline: 2 bytes */
    };
    
    Also, make use of the struct_size() helper and remove unneeded variable
    ap_info_entry_size.
    
    This issue was found with the help of Coccinelle and audited and fixed,
    manually.
    
    Link: https://www.kernel.org/doc/html/v5.16/process/deprecated.html#zero-length-and-one-element-arrays
    Link: https://github.com/KSPP/linux/issues/79
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/net/wireless/ath/ath6kl/wmi.c
+++ drivers/net/wireless/ath/ath6kl/wmi.c
@@ -1750,23 +1750,20 @@ static int ath6kl_wmi_snr_threshold_event_rx(struct wmi *wmi, u8 *datap,
 
 static int ath6kl_wmi_aplist_event_rx(struct wmi *wmi, u8 *datap, int len)
 {
-	u16 ap_info_entry_size;
 	struct wmi_aplist_event *ev = (struct wmi_aplist_event *) datap;
 	struct wmi_ap_info_v1 *ap_info_v1;
 	u8 index;
 
-	if (len < sizeof(struct wmi_aplist_event) ||
+	if (len <= sizeof(struct wmi_aplist_event) ||
 	    ev->ap_list_ver != APLIST_VER1)
 		return -EINVAL;
 
-	ap_info_entry_size = sizeof(struct wmi_ap_info_v1);
 	ap_info_v1 = (struct wmi_ap_info_v1 *) ev->ap_list;
 
 	ath6kl_dbg(ATH6KL_DBG_WMI,
 		   "number of APs in aplist event: %d\n", ev->num_ap);
 
-	if (len < (int) (sizeof(struct wmi_aplist_event) +
-			 (ev->num_ap - 1) * ap_info_entry_size))
+	if (len < struct_size(ev, ap_list, ev->num_ap))
 		return -EINVAL;
 
 	/* AP list version 1 contents */
