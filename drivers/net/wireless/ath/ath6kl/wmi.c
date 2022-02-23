a3579cc1d895034df0807090cde09b2bdbb0dbfd
Gustavo A. R. Silva <gustavoars@kernel.org>
2022-02-22 20:38:50 -0600

    ath6kl: wmi: Replace one-element array with flexible-array member in struct wmi_channel_list_reply
    Replace one-element array with flexible-array member in struct
    wmi_channel_list_reply.
    
    It's also worth noting that due to the flexible array transformation,
    the size of struct wmi_channel_list_reply changed, see below.
    
    Before flex-array transformation:
    
    struct wmi_channel_list_reply {
    	u8                         reserved;             /*     0     1 */
    	u8                         num_ch;               /*     1     1 */
    	__le16                     ch_list[1];           /*     2     2 */
    
    	/* size: 4, cachelines: 1, members: 3 */
    	/* last cacheline: 4 bytes */
    };
    
    After flex-array transformation:
    
    struct wmi_channel_list_reply {
    	u8                         reserved;             /*     0     1 */
    	u8                         num_ch;               /*     1     1 */
    	__le16                     ch_list[];            /*     2     0 */
    
    	/* size: 2, cachelines: 1, members: 3 */
    	/* last cacheline: 2 bytes */
    };
    
    So, the following change preserves the logic that if _len_ is at least
    4 bytes in size, this is the existence of at least one channel in
    ch_list[] is being considered, then the execution jumps to call
    ath6kl_wakeup_event(wmi->parent_dev);, otherwise _len_ is 2 bytes or
    less and the code returns -EINVAL:
    
    	-       if (len < sizeof(struct wmi_channel_list_reply))
    	+       if (len <= sizeof(struct wmi_channel_list_reply))
    
    This issue was found with the help of Coccinelle and audited and fixed,
    manually.
    
    Link: https://www.kernel.org/doc/html/v5.16/process/deprecated.html#zero-length-and-one-element-arrays
    Link: https://github.com/KSPP/linux/issues/79
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/net/wireless/ath/ath6kl/wmi.c
+++ drivers/net/wireless/ath/ath6kl/wmi.c
@@ -1240,7 +1240,7 @@ static int ath6kl_wmi_ratemask_reply_rx(struct wmi *wmi, u8 *datap, int len)
 
 static int ath6kl_wmi_ch_list_reply_rx(struct wmi *wmi, u8 *datap, int len)
 {
-	if (len < sizeof(struct wmi_channel_list_reply))
+	if (len <= sizeof(struct wmi_channel_list_reply))
 		return -EINVAL;
 
 	ath6kl_wakeup_event(wmi->parent_dev);
