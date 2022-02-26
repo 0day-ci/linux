32816f5586fec411ef7e4d90982eafe8acd8e837
Oliver Upton <oupton@google.com>
2022-02-26 00:21:24 +0000

    KVM: x86: Introduce KVM_CAP_DISABLE_QUIRKS2
    KVM_CAP_DISABLE_QUIRKS is irrevocably broken. The capability does not
    advertise the set of quirks which may be disabled to userspace, so it is
    impossible to predict the behavior of KVM. Worse yet,
    KVM_CAP_DISABLE_QUIRKS will tolerate any value for cap->args[0], meaning
    it fails to reject attempts to set invalid quirk bits.
    
    The only valid workaround for the quirky quirks API is to add a new CAP.
    Actually advertise the set of quirks that can be disabled to userspace
    so it can predict KVM's behavior. Reject values for cap->args[0] that
    contain invalid bits.
    
    Finally, add documentation for the new capability and describe the
    existing quirks.
    
    Signed-off-by: Oliver Upton <oupton@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4331,6 +4331,9 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 			r = sizeof(struct kvm_xsave);
 		break;
 	}
+	case KVM_CAP_DISABLE_QUIRKS2:
+		r = KVM_X86_VALID_QUIRKS;
+		break;
 	default:
 		break;
 	}
@@ -5877,6 +5880,11 @@ int kvm_vm_ioctl_enable_cap(struct kvm *kvm,
 		return -EINVAL;
 
 	switch (cap->cap) {
+	case KVM_CAP_DISABLE_QUIRKS2:
+		r = -EINVAL;
+		if (cap->args[0] & ~KVM_X86_VALID_QUIRKS)
+			break;
+		fallthrough;
 	case KVM_CAP_DISABLE_QUIRKS:
 		kvm->arch.disabled_quirks = cap->args[0];
 		r = 0;
