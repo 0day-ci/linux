feceace8c28a520181a36583553b97cadbe78154
Adrian Hunter <adrian.hunter@intel.com>
2022-02-28 13:36:51 +0200

    scsi: Add quiet_suspend flag for SCSI devices to suppress some PM messages
    Kernel messages produced during runtime PM can cause a never-ending
    cycle because user space utilities (e.g. journald or rsyslog) write the
    messages back to storage, causing runtime resume, more messages, and so
    on.
    
    Messages that tell of things that are expected to happen, are arguably
    unnecessary, so add a flag to suppress them. This flag is used by the UFS
    driver in a subsequent patch.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -206,6 +206,12 @@ struct scsi_device {
 	unsigned rpm_autosuspend:1;	/* Enable runtime autosuspend at device
 					 * creation time */
 	unsigned ignore_media_change:1; /* Ignore MEDIA CHANGE on resume */
+	/*
+	 * quiet_suspend to suppress messages due to runtime PM to avoid
+	 * never-ending cycles of messages written back to storage by user space
+	 * causing runtime resume, causing more messages and so on.
+	 */
+	unsigned quiet_suspend:1;	/* Do not print runtime PM related messages */
 
 	unsigned int queue_stopped;	/* request queue is quiesced */
 	bool offline_already;		/* Device offline message logged */
