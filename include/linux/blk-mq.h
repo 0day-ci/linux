176e39bc0acb20f8fd869d170b429b7253b089c4
Ming Lei <ming.lei@redhat.com>
2022-02-28 17:04:30 +0800

    blk-mq: manage hctx map via xarray
    Firstly code becomes more clean by switching to xarray from plain array.
    
    Secondly use-after-free on q->queue_hw_ctx can be fixed because
    queue_for_each_hw_ctx() may be run when updating nr_hw_queues is
    in-progress. With this patch, q->hctx_table is defined as xarray, and
    this structure will share same lifetime with request queue, so
    queue_for_each_hw_ctx() can use q->hctx_table to lookup hctx reliably.
    
    Reported-by: Yu Kuai <yukuai3@huawei.com>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- include/linux/blk-mq.h
+++ include/linux/blk-mq.h
@@ -919,12 +919,12 @@ static inline void *blk_mq_rq_to_pdu(struct request *rq)
 static inline struct blk_mq_hw_ctx *blk_mq_get_hctx(struct request_queue *q,
 		unsigned int hctx_idx)
 {
-	return q->queue_hw_ctx[hctx_idx];
+	return xa_load(&q->hctx_table, hctx_idx);
 }
 
 #define queue_for_each_hw_ctx(q, hctx, i)				\
 	for ((i) = 0; (i) < (q)->nr_hw_queues &&			\
-	     ({ hctx = blk_mq_get_hctx((q), (i)); 1; }); (i)++)
+	     (hctx = blk_mq_get_hctx((q), (i))); (i)++)
 
 #define hctx_for_each_ctx(hctx, ctx, i)					\
 	for ((i) = 0; (i) < (hctx)->nr_ctx &&				\
