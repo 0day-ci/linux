cd3bdea7842b58e70467e9390757de0cc52628b1
Thomas Gleixner <tglx@linutronix.de>
2022-03-01 19:30:32 +0100

    sched: Make preempt_enable_no_resched() behave like preempt_enable() on PREEMPT_RT
    Use the distinction that has been made earlier between
    preempt_enable_no_resched() and sched_preempt_enable_no_resched().
    sched_preempt_enable_no_resched() is used within the scheduler where an
    immediate schedule() follows. preempt_enable_no_resched() is used
    outside of the scheduler and an immediate schedule() invocation may not
    follow for other reasons.
    
    The lack of an immediate schedule() invocation matters on PREEMPT_RT
    where it will introduce latencies while the scheduler expects a context
    switch.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/preempt.h
+++ include/linux/preempt.h
@@ -210,7 +210,11 @@ do { \
 	preempt_count_dec(); \
 } while (0)
 
-#define preempt_enable_no_resched() sched_preempt_enable_no_resched()
+#ifdef CONFIG_PREEMPT_RT
+# define preempt_enable_no_resched() preempt_enable()
+#else
+# define preempt_enable_no_resched() sched_preempt_enable_no_resched()
+#endif
 
 #define preemptible()	(preempt_count() == 0 && !irqs_disabled())
 
