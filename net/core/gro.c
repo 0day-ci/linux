cbd6643a923fa65d56ec559ccb7b495ab2eca15c
Xiaomeng Tong <xiam0nd.tong@gmail.com>
2022-03-01 15:58:38 +0800

    net/core: remove iterator use outside the loop
    Demonstrations for:
     - list_for_each_entry_safe_reverse_inside
    
    Signed-off-by: Xiaomeng Tong <xiam0nd.tong@gmail.com>

--- net/core/gro.c
+++ net/core/gro.c
@@ -286,9 +286,8 @@ static void __napi_gro_flush_chain(struct napi_struct *napi, u32 index,
 				   bool flush_old)
 {
 	struct list_head *head = &napi->gro_hash[index].list;
-	struct sk_buff *skb, *p;
 
-	list_for_each_entry_safe_reverse(skb, p, head, list) {
+	list_for_each_entry_safe_reverse_inside(skb, p, struct sk_buff, head, list) {
 		if (flush_old && NAPI_GRO_CB(skb)->age == jiffies)
 			return;
 		skb_list_del_init(skb);
