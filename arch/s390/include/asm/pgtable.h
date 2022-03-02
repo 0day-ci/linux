f6f0ab5252e1b168a36416c1ad92194490d1761b
Claudio Imbrenda <imbrenda@linux.ibm.com>
2022-03-02 19:11:37 +0100

    s390/mm: KVM: pv: when tearing down, try to destroy protected pages
    When ptep_get_and_clear_full is called for a mm teardown, we will now
    attempt to destroy the secure pages. This will be faster than export.
    
    In case it was not a teardown, or if for some reason the destroy page
    UVC failed, we try with an export page, like before.
    
    Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Acked-by: Janosch Frank <frankja@linux.ibm.com>

--- arch/s390/include/asm/pgtable.h
+++ arch/s390/include/asm/pgtable.h
@@ -1118,9 +1118,21 @@ static inline pte_t ptep_get_and_clear_full(struct mm_struct *mm,
 	} else {
 		res = ptep_xchg_lazy(mm, addr, ptep, __pte(_PAGE_INVALID));
 	}
-	/* At this point the reference through the mapping is still present */
-	if (mm_is_protected(mm) && pte_present(res))
-		uv_convert_owned_from_secure(pte_val(res) & PAGE_MASK);
+	/* Nothing to do */
+	if (!mm_is_protected(mm) || !pte_present(res))
+		return res;
+	/*
+	 * At this point the reference through the mapping is still present.
+	 * The notifier should have destroyed all protected vCPUs at this
+	 * point, so the destroy should be successful.
+	 */
+	if (full && !uv_destroy_owned_page(pte_val(res) & PAGE_MASK))
+		return res;
+	/*
+	 * But if something went wrong and the pages could not be destroyed,
+	 * the slower export is used as fallback instead.
+	 */
+	uv_convert_owned_from_secure(pte_val(res) & PAGE_MASK);
 	return res;
 }
 
