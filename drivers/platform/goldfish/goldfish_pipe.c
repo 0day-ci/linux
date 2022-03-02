927c55096910ff453023d8489799db70ebac9528
sujiaxun <sujiaxun@uniontech.com>
2022-03-02 14:49:28 +0800

    goldfish_pipe: Use platform_get_irq() to get the interrupt
    platform_get_resource(pdev, IORESOURCE_IRQ, ..) relies on static
    allocation of IRQ resources in DT core code, this causes an issue
    when using hierarchical interrupt domains using "interrupts" property
    in the node as this bypassed the hierarchical setup and messed up the
    irq chaining.
    
    In preparation for removal of static setup of IRQ resource from DT core
    code use platform_get_irq().
    
    Signed-off-by: sujiaxun <sujiaxun@uniontech.com>

--- drivers/platform/goldfish/goldfish_pipe.c
+++ drivers/platform/goldfish/goldfish_pipe.c
@@ -896,11 +896,9 @@ static int goldfish_pipe_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
-	r = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
-	if (!r)
+	dev->irq = platform_get_irq(pdev, 0);
+	if (dev->irq < 0)
 		return -EINVAL;
-
-	dev->irq = r->start;
 
 	/*
 	 * Exchange the versions with the host device
