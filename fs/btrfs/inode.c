f594a9bfaf2e54f2f9c93f02d63e890167c3a02b
Qu Wenruo <wqu@suse.com>
2022-03-14 17:07:30 +0800

    btrfs: remove the stripe boundary calcluation for encoded IO
    In btrfs_encoded_read_regular_fill_pages(), we have a loop to handle the
    bio split due to stripe boundary.
    
    Since btrfs_map_bio() can handle it for us now, there is no need to
    manually do the split anymore.
    
    Just remove the related btrfs_get_io_geometry() call inside
    btrfs_encoded_read_regular_fill_pages().
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -10331,7 +10331,6 @@ static int btrfs_encoded_read_regular_fill_pages(struct btrfs_inode *inode,
 						 u64 disk_io_size,
 						 struct page **pages)
 {
-	struct btrfs_fs_info *fs_info = inode->root->fs_info;
 	struct btrfs_encoded_read_private priv = {
 		.inode = inode,
 		.file_offset = file_offset,
@@ -10340,7 +10339,6 @@ static int btrfs_encoded_read_regular_fill_pages(struct btrfs_inode *inode,
 	};
 	unsigned long i = 0;
 	u64 cur = 0;
-	int ret;
 
 	init_waitqueue_head(&priv.wait);
 	/*
@@ -10348,25 +10346,9 @@ static int btrfs_encoded_read_regular_fill_pages(struct btrfs_inode *inode,
 	 * necessary.
 	 */
 	while (cur < disk_io_size) {
-		struct extent_map *em;
-		struct btrfs_io_geometry geom;
 		struct bio *bio = NULL;
-		u64 remaining;
+		u64 remaining = disk_io_size - cur;
 
-		em = btrfs_get_chunk_map(fs_info, disk_bytenr + cur,
-					 disk_io_size - cur);
-		if (IS_ERR(em)) {
-			ret = PTR_ERR(em);
-		} else {
-			ret = btrfs_get_io_geometry(fs_info, em, BTRFS_MAP_READ,
-						    disk_bytenr + cur, &geom);
-			free_extent_map(em);
-		}
-		if (ret) {
-			WRITE_ONCE(priv.status, errno_to_blk_status(ret));
-			break;
-		}
-		remaining = min(geom.len, disk_io_size - cur);
 		while (bio || remaining) {
 			size_t bytes = min_t(u64, remaining, PAGE_SIZE);
 
