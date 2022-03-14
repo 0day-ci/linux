ac4da4b938975ce118f196ef5ac30eced64f3cb0
Qu Wenruo <wqu@suse.com>
2022-03-14 17:07:23 +0800

    btrfs: make metadata write endio functions to be split bio compatible
    Two modifications are needed:
    
    - Convert to __bio_for_each_segment()
      bio_for_each_segment_all() should not be called on cloned bio, as it
      will iterate range which no longer belongs to the split bio.
    
    - Avoid bio_first_page_all() for end_bio_subpage_eb_writepage()
      bio_first_page_all() will use the original bvec, thus on cloned bio it
      will trigger a WARN_ON().
    
      Introduce a helper to grab page and fs_info from bios.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -4483,6 +4483,21 @@ static struct extent_buffer *find_extent_buffer_nolock(
 }
 
 /*
+ * Since the bio can be cloned, we can no longer use bio_first_*_all()
+ * calls to grab a page.
+ * Thus here we introduce such helper to grab page and fs_info correctly.
+ */
+static struct btrfs_fs_info *bio_to_fs_info(struct bio *bio)
+{
+	struct bio_vec bvec;
+
+	bvec = bio_iter_iovec(bio, btrfs_bio(bio)->iter);
+
+	ASSERT(bvec.bv_page->mapping);
+	return btrfs_sb(bvec.bv_page->mapping->host->i_sb);
+}
+
+/*
  * The endio function for subpage extent buffer write.
  *
  * Unlike end_bio_extent_buffer_writepage(), we only call end_page_writeback()
@@ -4491,20 +4506,20 @@ static struct extent_buffer *find_extent_buffer_nolock(
 static void end_bio_subpage_eb_writepage(struct bio *bio)
 {
 	struct btrfs_fs_info *fs_info;
-	struct bio_vec *bvec;
-	struct bvec_iter_all iter_all;
+	struct bvec_iter iter;
+	struct bio_vec bvec;
 
-	fs_info = btrfs_sb(bio_first_page_all(bio)->mapping->host->i_sb);
+	fs_info = bio_to_fs_info(bio);
 	ASSERT(fs_info->sectorsize < PAGE_SIZE);
 
-	ASSERT(!bio_flagged(bio, BIO_CLONED));
-	bio_for_each_segment_all(bvec, bio, iter_all) {
-		struct page *page = bvec->bv_page;
-		u64 bvec_start = page_offset(page) + bvec->bv_offset;
-		u64 bvec_end = bvec_start + bvec->bv_len - 1;
+	ASSERT(btrfs_bio(bio)->iter.bi_size);
+	__bio_for_each_segment(bvec, bio, iter, btrfs_bio(bio)->iter) {
+		struct page *page = bvec.bv_page;
+		u64 bvec_start = page_offset(page) + bvec.bv_offset;
+		u64 bvec_end = bvec_start + bvec.bv_len - 1;
 		u64 cur_bytenr = bvec_start;
 
-		ASSERT(IS_ALIGNED(bvec->bv_len, fs_info->nodesize));
+		ASSERT(IS_ALIGNED(bvec.bv_len, fs_info->nodesize));
 
 		/* Iterate through all extent buffers in the range */
 		while (cur_bytenr <= bvec_end) {
@@ -4547,14 +4562,14 @@ static void end_bio_subpage_eb_writepage(struct bio *bio)
 
 static void end_bio_extent_buffer_writepage(struct bio *bio)
 {
-	struct bio_vec *bvec;
 	struct extent_buffer *eb;
+	struct bvec_iter iter;
+	struct bio_vec bvec;
 	int done;
-	struct bvec_iter_all iter_all;
 
-	ASSERT(!bio_flagged(bio, BIO_CLONED));
-	bio_for_each_segment_all(bvec, bio, iter_all) {
-		struct page *page = bvec->bv_page;
+	ASSERT(btrfs_bio(bio)->iter.bi_size);
+	__bio_for_each_segment(bvec, bio, iter, btrfs_bio(bio)->iter) {
+		struct page *page = bvec.bv_page;
 
 		eb = (struct extent_buffer *)page->private;
 		BUG_ON(!eb);
